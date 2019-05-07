99299cc3daaff4e6177f7bd449a1d8d5e7db6012
Raymond Luong <raymond_luong@sil.org>
2019-05-07 03:48:33 -0400

    SF-220 Finish invite a friend
    * Added sf_reviewer role and rights
    * Added ProjectAdminRole to ProjectsRpcController
    * Added tests to check if project share is enabled

---
+++ src/SIL.XForge/Models/ShareOptions.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SIL.XForge.Models
+{
+    public class ShareOptions
+    {
+        public const string Email = "email";
+        public const string Facebook = "facebook";
+
+    }
+}
--- test/SIL.XForge.Tests/Controllers/TestProjectsRpcController.cs
+++ test/SIL.XForge.Tests/Controllers/TestProjectsRpcController.cs
@@ -1,4 +1,6 @@
+using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
+using MongoDB.Bson;
 using SIL.XForge.Configuration;
 using SIL.XForge.DataAccess;
 using SIL.XForge.Models;
@@ -6,13 +8,36 @@ using SIL.XForge.Services;
 
 namespace SIL.XForge.Controllers
 {
-    public class TestProjectsRpcController : ProjectsRpcController<TestProjectEntity>
+    public class TestProjectsRpcController : ProjectsRpcController<TestProjectEntity, TestProjectUserEntity>
     {
         public TestProjectsRpcController(IUserAccessor userAccessor, IHttpRequestAccessor httpRequestAccessor,
             IRepository<TestProjectEntity> projects, IRepository<UserEntity> users, IEmailService emailService,
             IOptions<SiteOptions> siteOptions)
             : base(userAccessor, httpRequestAccessor, projects, users, emailService, siteOptions)
         {
+        }
+
+        protected override TestProjectUserEntity CreateProjectUser(string userId)
+        {
+            return new TestProjectUserEntity
+            {
+                Id = ObjectId.GenerateNewId().ToString(),
+                UserRef = userId,
+                ProjectRef = ResourceId,
+                Role = TestProjectRoles.Reviewer
+            };
+        }
+
+        protected override string ProjectAdminRole => TestProjectRoles.Administrator;
+
+        protected override async Task<bool> IsProjectSharingOptionEnabled(string option)
+        {
+            TestProjectEntity project = await Projects.Query().FirstOrDefaultAsync(p => p.Id == ResourceId);
+            if (!project.CheckingConfig.share.Enabled)
+                return false;
+            if (option == ShareOptions.Email)
+                return project.CheckingConfig.share.ViaEmail;
+            return false;
         }
     }
 }
