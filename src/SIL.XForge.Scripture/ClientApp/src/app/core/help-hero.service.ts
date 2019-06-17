aabd0a39638f2a799104147b9c928a2ab06a4e26
feliks-montez <feliks.montez@gmail.com>
2019-06-17 16:42:31 +0700

    alphabetize imports

--- src/SIL.XForge.Scripture/ClientApp/src/app/core/help-hero.service.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/core/help-hero.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HelpHero, HEvent, HEventInfo, HEventKind } from 'help-hero';
 import initHelpHero from 'helphero';
-import { HelpHero, HEventKind, HEvent, HEventInfo } from 'help-hero';
 
 @Injectable({
   providedIn: 'root'
