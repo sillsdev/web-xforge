597b319c9844dca88f8bbc26d8c930e4ab6c71f3
Damien Daspit <damien_daspit@sil.org>
2019-06-20 18:01:01 +0700

    Add staging configuration to angular.json (#78)

---
+++ src/SIL.XForge.Scripture/ClientApp/src/environments/environment.staging.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  pwaTest: false,
+  issueEmail: 'issues@qa.beta.scriptureforge.org',
+  siteName: 'Scripture Forge',
+  realtimePort: undefined as number,
+  realtimeUrl: '/realtime-api/',
+  authDomain: 'dev-sillsdev.auth0.com',
+  authClientId: '4eHLjo40mAEGFU6zUxdYjnpnC1K1Ydnj',
+  audience: 'https://scriptureforge.org/',
+  scope: 'sf_data',
+  prefix: 'sf'
+};
