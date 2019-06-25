df0e953a5d584a3a949b0b4906e24bb70c6b5f1b
nigel-wells <nigel-wells@hotmail.com>
2019-06-25 16:36:16 +1200

    Ability to record audio with an answer (SF-156)

---
+++ src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/checking-audio-recorder/checking-audio-recorder.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+// import * as RecordRTC from "recordrtc";
+import RecordRTC from 'recordrtc';
+
+@Component({
+  selector: 'app-checking-audio-recorder',
+  templateUrl: './checking-audio-recorder.component.html',
+  styleUrls: ['./checking-audio-recorder.component.scss']
+})
+export class CheckingAudioRecorderComponent {
+  audioUrl: string = '';
+  private stream: MediaStream;
+  private recordRTC: any;
+
+  get hasAudioAttachment(): boolean {
+    return this.audioUrl !== '';
+  }
+
+  get isRecording(): boolean {
+    return this.recordRTC && this.recordRTC.state === 'recording';
+  }
+
+  errorCallback() {
+    // handle error here
+  }
+
+  processAudio(audioVideoWebMURL: string) {
+    this.audioUrl = audioVideoWebMURL;
+    this.recordRTC.getDataURL(() => {});
+  }
+
+  resetRecording(): void {
+    this.audioUrl = '';
+  }
+
+  startRecording() {
+    const mediaConstraints: MediaStreamConstraints = {
+      audio: true
+    };
+    navigator.mediaDevices
+      .getUserMedia(mediaConstraints)
+      .then(this.successCallback.bind(this), this.errorCallback.bind(this));
+  }
+
+  stopRecording() {
+    this.recordRTC.stopRecording(this.processAudio.bind(this));
+    this.stream.getAudioTracks().forEach(track => track.stop());
+  }
+
+  successCallback(stream: MediaStream) {
+    const options = {
+      type: 'audio/webm'
+    };
+    this.stream = stream;
+    this.recordRTC = RecordRTC(stream, options);
+    this.recordRTC.startRecording();
+  }
+}
