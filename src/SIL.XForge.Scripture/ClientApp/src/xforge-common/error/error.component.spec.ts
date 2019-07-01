25f2d09c774ee957ac31ee12b1175ab9cd56a2f0
Ira Hopkinson <irahopkinson@users.noreply.github.com>
2019-07-01 09:57:30 +0700

    SF-64 Fix PT login auth access cancel (#90)
    * when logging in with Paratext, if the authorize app page is cancelled it now returns to the login
    * also fixed error page still showing the loading bar

--- src/SIL.XForge.Scripture/ClientApp/src/xforge-common/error/error.component.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/error/error.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
 import { instance, mock, when } from 'ts-mockito';
+import { NoticeService } from 'xforge-common/notice.service';
 import { ErrorComponent } from './error.component';
 
 describe('ErrorComponent', () => {
@@ -26,13 +27,17 @@ class TestEnvironment {
   component: ErrorComponent;
 
   mockedActivatedRoute: ActivatedRoute = mock(ActivatedRoute);
+  mockedNoticeService: NoticeService = mock(NoticeService);
 
   constructor() {
     when(this.mockedActivatedRoute.queryParams).thenReturn(
       of({ stack: 'Some made up component', errorCode: 'Error: 404: Not found' })
     );
     TestBed.configureTestingModule({
-      providers: [{ provide: ActivatedRoute, useFactory: () => instance(this.mockedActivatedRoute) }],
+      providers: [
+        { provide: ActivatedRoute, useFactory: () => instance(this.mockedActivatedRoute) },
+        { provide: NoticeService, useFactory: () => instance(this.mockedNoticeService) }
+      ],
       declarations: [ErrorComponent]
     });
     this.fixture = TestBed.createComponent(ErrorComponent);
