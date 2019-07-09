a6f12390e52c9dbf18e8ec99e6e0655d7d0a0281
Feliks Montez <feliks.montez@gmail.com>
2019-07-09 14:47:04 +0700

    Add semi-typings

--- src/SIL.XForge.Scripture/ClientApp/src/app/core/help-hero.service.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/core/help-hero.service.ts
@@ -3,10 +3,25 @@ import initHelpHero from 'helphero';
 
 @Injectable({ providedIn: 'root' })
 export class HelpHeroService {
-  helpHeroClient = initHelpHero('XXXXX');
-  on = this.helpHeroClient.on;
-  setIdentity = this.helpHeroClient.identify;
-  anonymous = this.helpHeroClient.anonymous;
-  setProperty = this.helpHeroClient.update;
-  startTour = this.helpHeroClient.startTour;
+  readonly helpHeroClient = initHelpHero('9yZMlWWMsDS');
+
+  on(id: string, callback: () => void) {
+    this.helpHeroClient.on(id as any, callback);
+  }
+
+  setIdentity(id: string): void {
+    this.helpHeroClient.identify(id);
+  }
+
+  anonymous() {
+    this.helpHeroClient.anonymous();
+  }
+
+  setProperty(jObj: any): void {
+    this.helpHeroClient.update(jObj);
+  }
+
+  startTour(tourId: string, options?: any) {
+    this.helpHeroClient.startTour(tourId, options);
+  }
 }
