5094761a9e464aaf8eb0fb0e90afd716451e885a
Raymond Luong <raymond_luong@sil.org>
2019-07-22 18:35:43 -0400

    Use Xabe library

--- src/SIL.XForge/Services/ProjectService.cs
+++ src/SIL.XForge/Services/ProjectService.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
@@ -9,6 +8,7 @@ using AutoMapper;
 using JsonApiDotNetCore.Internal.Query;
 using JsonApiDotNetCore.Services;
 using Microsoft.Extensions.Options;
+using Xabe.FFmpeg;
 using SIL.XForge.Configuration;
 using SIL.XForge.DataAccess;
 using SIL.XForge.Models;
@@ -128,13 +128,13 @@ namespace SIL.XForge.Services
             }
             using (var fileStream = new FileStream(path, FileMode.Create))
                 await inputStream.CopyToAsync(fileStream);
-            string mp3Path = ConvertToMp3(path);
+            string mp3Path = await ConvertToMp3Async(path);
             var uri = new Uri(_siteOptions.Value.Origin,
                 $"/assets/audio/{mp3Path}");
             return uri;
         }
 
-        private string ConvertToMp3(string filePath)
+        private async Task<string> ConvertToMp3Async(string filePath)
         {
             if (Path.GetExtension(filePath) == ".mp3")
             {
@@ -145,19 +145,7 @@ namespace SIL.XForge.Services
             {
                 File.Delete(mp3FilePath);
             }
-            var process = new Process()
-            {
-                StartInfo = new ProcessStartInfo
-                {
-                    FileName = "/bin/bash",
-                    Arguments = $"-c \"/usr/local/bin/ffmpeg -i {filePath} {mp3FilePath}\"",
-                    UseShellExecute = false,
-                    CreateNoWindow = true
-                }
-            };
-
-            process.Start();
-            process.WaitForExit();
+            var result = await Conversion.Convert(filePath, mp3FilePath).Start();
             File.Delete(filePath);
             return mp3FilePath;
         }
