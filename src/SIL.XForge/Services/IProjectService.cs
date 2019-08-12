9ec782c6ee3cc1ede5d2f573ccc7e6971e96a23f
MarkS <marksvc@gmail.com>
2019-08-12 10:34:25 -0600

    SF-359 Show invitees in user list. Handle canceling invitation
    Add endpoints 'invitedUsers', 'uninviteUser'.
    Show invitees in collaborators user list.
    Hook up Cancel invite button.

--- src/SIL.XForge/Services/IProjectService.cs
+++ src/SIL.XForge/Services/IProjectService.cs
@@ -11,8 +11,10 @@ namespace SIL.XForge.Services
         Task UpdateRoleAsync(string curUserId, string systemRole, string projectId, string projectRole);
         Task<bool> InviteAsync(string curUserId, string projectId, string email);
         Task<bool> IsAlreadyInvitedAsync(string curUserId, string projectId, string email);
+        Task UninviteUserAsync(string curUserId, string projectId, string email);
         Task CheckLinkSharingAsync(string curUserId, string projectId, string shareKey = null);
         Task<bool> IsAuthorizedAsync(string curUserId, string projectId);
+        Task<string[]> InvitedUsersAsync(string curUserId, string projectId, string curUserSystemRole = null);
         Task<Uri> SaveAudioAsync(string curUserId, string projectId, string dataId, string extension,
             Stream inputStream);
         Task DeleteAudioAsync(string curUserId, string projectId, string ownerId, string dataId);
