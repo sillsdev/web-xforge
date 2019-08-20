ebb2fa93e9340a7dc69e04d9b10dd0d25f709c62
Raymond Luong <raymond_luong@sil.org>
2019-08-20 12:39:40 -0400

    SF-432 Warn on editing a question with answers

---
+++ src/SIL.XForge.Scripture/ClientApp/src/app/checking/question-answered-dialog/question-answered-dialog.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-question-answered-dialog',
+  templateUrl: './question-answered-dialog.component.html'
+})
+export class QuestionAnsweredDialogComponent {}
