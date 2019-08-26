fd2bfaadbf2fd7441360ffe94c57fd6c554c3115
Damien Daspit <damien_daspit@sil.org>
2019-08-26 10:56:29 +0700

    SF-405: Security risk uploading audio file (#191)

--- src/SIL.XForge/Utils/StringUtils.cs
+++ src/SIL.XForge/Utils/StringUtils.cs
@@ -1,3 +1,4 @@
+using MongoDB.Bson;
 using Newtonsoft.Json.Serialization;
 
 namespace SIL.XForge.Utils
@@ -9,6 +10,11 @@ namespace SIL.XForge.Utils
         public static string ToCamelCase(this string str)
         {
             return CamelCaseNamingStrategy.GetPropertyName(str, false);
+        }
+
+        public static bool ValidateId(string id)
+        {
+            return ObjectId.TryParse(id, out _);
         }
     }
 }
