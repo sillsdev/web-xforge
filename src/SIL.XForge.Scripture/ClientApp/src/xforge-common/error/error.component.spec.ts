1bafb6f1eef8b7c0df65c085b4c2484ef35bede6
Nathaniel Paulus <nateowami@gmail.com>
2019-09-12 15:58:09 -0400

    Only offer to show more in error handler when stack exists

--- src/SIL.XForge.Scripture/ClientApp/src/xforge-common/error/error.component.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/error/error.component.spec.ts
@@ -3,11 +3,14 @@ import { OverlayContainer } from '@angular-mdc/web';
 import { Component, NgModule } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UICommonModule } from 'xforge-common/ui-common.module';
-import { ErrorComponent } from './error.component';
+import { ErrorAlert, ErrorComponent } from './error.component';
 
 describe('ErrorComponent', () => {
   it('should display error dialog', () => {
-    const env = new TestEnvironment();
+    const env = new TestEnvironment({
+      message: 'The error message',
+      stack: 'The error stack'
+    });
 
     expect(env.errorMessage.textContent).toBe('The error message');
     expect(env.showDetails.textContent).toBe('Show details');
@@ -24,6 +27,17 @@ describe('ErrorComponent', () => {
 
     env.closeButton.click();
   });
+
+  it('should only offer to show more when a stack trace is available', () => {
+    const env = new TestEnvironment({
+      message: 'Testing without stack'
+    });
+
+    expect(env.errorMessage.textContent).toBe('Testing without stack');
+    expect(env.showDetails.style.display).toBe('none');
+    expect(env.stackTrace.style.display).toBe('none');
+    env.closeButton.click();
+  });
 });
 
 @NgModule({
@@ -37,17 +51,12 @@ class TestEnvironment {
   fixture: ComponentFixture<DialogOpenerComponent>;
   element: HTMLElement;
 
-  constructor() {
+  constructor(dialogData: ErrorAlert) {
     TestBed.configureTestingModule({
       declarations: [DialogOpenerComponent],
       imports: [DialogTestModule]
     });
-    TestBed.get(MdcDialog).open(ErrorComponent, {
-      data: {
-        message: 'The error message',
-        stack: 'The error stack'
-      }
-    });
+    TestBed.get(MdcDialog).open(ErrorComponent, { data: dialogData });
     this.fixture = TestBed.createComponent(DialogOpenerComponent);
     this.element = TestBed.get(OverlayContainer).getContainerElement();
   }
