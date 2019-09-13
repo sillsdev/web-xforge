142bd540bd9956af3d8f38a039452e9fa4d15e56
Damien Daspit <damien_daspit@sil.org>
2019-09-13 17:27:29 +0700

    Sort PT projects on backend

--- src/SIL.XForge.Scripture/ClientApp/src/app/settings/settings.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/settings/settings.component.ts
@@ -252,7 +252,7 @@ export class SettingsComponent extends DataLoadingComponent implements OnInit {
     if (curSource != null) {
       const sourceProject = sourceProjects.find(p => p.paratextId === curSource.paratextId);
       if (sourceProject == null) {
-        sourceProjects.push({
+        sourceProjects.unshift({
           paratextId: curSource.paratextId,
           name: curSource.name,
           inputSystem: curSource.inputSystem,
@@ -261,7 +261,6 @@ export class SettingsComponent extends DataLoadingComponent implements OnInit {
         });
       }
     }
-    sourceProjects.sort((a, b) => a.name.localeCompare(b.name));
     this.sourceProjects = sourceProjects;
   }
 }
