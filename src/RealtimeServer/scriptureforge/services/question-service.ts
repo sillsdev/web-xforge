d38fefb125c0380a938409405c02aa6a640e14ed
Damien Daspit <damien_daspit@sil.org>
2019-09-20 16:46:58 +0700

    SF-515: Remove id from "read" arrays in SFProjectUserConfig when object deleted

--- src/RealtimeServer/scriptureforge/services/question-service.ts
+++ src/RealtimeServer/scriptureforge/services/question-service.ts
@@ -1,7 +1,15 @@
+import ShareDB = require('sharedb');
+import { Doc } from 'sharedb/lib/client';
+import { createProxy, getPath } from 'ts-object-path';
+import { OwnedData } from '../../common/models/owned-data';
 import { ANY_INDEX } from '../../common/path-template';
 import { ProjectDomainConfig } from '../../common/services/project-data-service';
+import { createFetchQuery, docSubmitOp } from '../../common/utils';
+import { Answer } from '../models/answer';
+import { Comment } from '../models/comment';
 import { Question, QUESTIONS_COLLECTION } from '../models/question';
 import { SFProjectDomain } from '../models/sf-project-rights';
+import { SF_PROJECT_USER_CONFIGS_COLLECTION, SFProjectUserConfig } from '../models/sf-project-user-config';
 import { QUESTION_MIGRATIONS } from './question-migrations';
 import { SFProjectDataService } from './sf-project-data-service';
 
@@ -10,6 +18,7 @@ import { SFProjectDataService } from './sf-project-data-service';
  */
 export class QuestionService extends SFProjectDataService<Question> {
   readonly collection = QUESTIONS_COLLECTION;
+  readonly listenForUpdates = true;
 
   constructor() {
     super(QUESTION_MIGRATIONS);
@@ -49,5 +58,80 @@ export class QuestionService extends SFProjectDataService<Question> {
         pathTemplate: this.createPathTemplate(q => q.answers[ANY_INDEX].likes[ANY_INDEX])
       }
     ];
+  }
+
+  protected onDelete(userId: string, docId: string, projectDomain: SFProjectDomain, entity: OwnedData): Promise<void> {
+    if (projectDomain === SFProjectDomain.Answers || projectDomain === SFProjectDomain.AnswerComments) {
+      this.removeEntityReadRefs(userId, docId, projectDomain, entity);
+    }
+    return Promise.resolve();
+  }
+
+  private async removeEntityReadRefs(
+    userId: string,
+    docId: string,
+    projectDomain: SFProjectDomain,
+    entity: OwnedData
+  ): Promise<void> {
+    const parts = docId.split(':');
+    const projectId = parts[0];
+    const conn = this.server!.backend.connect(undefined, { userId });
+    const query = await createFetchQuery(conn, SF_PROJECT_USER_CONFIGS_COLLECTION, { projectRef: projectId });
+    const promises: Promise<void>[] = [];
+    for (const doc of query.results) {
+      let ops: ShareDB.Op[] | undefined;
+      switch (projectDomain) {
+        case SFProjectDomain.Answers:
+          ops = this.removeAnswerReadRefs(doc, entity as Answer);
+          break;
+        case SFProjectDomain.AnswerComments:
+          ops = this.removeCommentReadRefs(doc, entity as Comment);
+          break;
+      }
+      if (ops != null && ops.length > 0) {
+        promises.push(docSubmitOp(doc, ops));
+      }
+    }
+    await Promise.all(promises);
+  }
+
+  private removeAnswerReadRefs(doc: Doc, answer: Answer): ShareDB.Op[] {
+    const puc = createProxy<SFProjectUserConfig>();
+    const data = doc.data as SFProjectUserConfig;
+    const index = data.answerRefsRead.indexOf(answer.dataId);
+    const ops: ShareDB.Op[] = [];
+    if (index !== -1) {
+      const op: ShareDB.ListDeleteOp = {
+        p: getPath(puc.answerRefsRead[index]),
+        ld: answer.dataId
+      };
+      ops.push(op);
+    }
+
+    const commentIds = new Set<string>(answer.comments.map(c => c.dataId));
+    for (let i = data.commentRefsRead.length - 1; i >= 0; i--) {
+      if (commentIds.has(data.commentRefsRead[i])) {
+        const op: ShareDB.ListDeleteOp = {
+          p: getPath(puc.commentRefsRead[i]),
+          ld: data.commentRefsRead[i]
+        };
+        ops.push(op);
+      }
+    }
+    return ops;
+  }
+
+  private removeCommentReadRefs(doc: Doc, comment: Comment): ShareDB.Op[] {
+    const puc = createProxy<SFProjectUserConfig>();
+    const data = doc.data as SFProjectUserConfig;
+    const index = data.commentRefsRead.indexOf(comment.dataId);
+    if (index !== -1) {
+      const op: ShareDB.ListDeleteOp = {
+        p: getPath(puc.commentRefsRead[index]),
+        ld: comment.dataId
+      };
+      return [op];
+    }
+    return [];
   }
 }
