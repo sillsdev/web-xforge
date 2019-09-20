7bbe69e0b62873d5890391fbe53dc0243bf08e2f
Damien Daspit <damien_daspit@sil.org>
2019-09-20 14:35:50 +0700

    Fix Angular environment files
    - the environment object cannot be generated dynamically

--- src/SIL.XForge.Scripture/ClientApp/src/environments/environment.pwa-test.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/environments/environment.pwa-test.ts
@@ -1,4 +1,19 @@
-import merge from 'lodash/merge';
-import { development } from './environment.defaults';
-
-export const environment = merge(development, { pwaTest: true });
+export const environment = {
+  releaseStage: 'dev',
+  pwaTest: true,
+  production: false,
+  issueEmail: 'scriptureforgeissues@sil.org',
+  siteName: 'Scripture Forge',
+  audience: 'https://scriptureforge.org/',
+  scope: 'sf_data',
+  siteId: 'sf',
+  assets: {
+    audio: '/assets/audio/'
+  },
+  helps: 'https://help.scriptureforge.org/#t=Overview/Getting_Started.htm',
+  bugsnagApiKey: 'b72a46a8924a3cd161d4c5534287923c',
+  realtimePort: 5003,
+  realtimeUrl: '/',
+  authDomain: 'sil-appbuilder.auth0.com',
+  authClientId: 'aoAGb9Yx1H5WIsvCW6JJCteJhSa37ftH'
+};
