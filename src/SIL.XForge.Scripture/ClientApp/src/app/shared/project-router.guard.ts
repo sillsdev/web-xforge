sillsdev/web-xforge

commit e8fe19cf0e94a92603b5790a068a90f37e7ab755

SF-600 Hide translate and checking app when they're not available (#354)

src/SIL.XForge.Scripture/ClientApp/src/app/shared/project-router.guard.ts
@@ -1,0 +1,81 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { SFProjectRole } from 'realtime-server/lib/scriptureforge/models/sf-project-role';
+import { from, Observable, of } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { AuthGuard } from 'xforge-common/auth.guard';
+import { UserService } from 'xforge-common/user.service';
+import { SFProjectDoc } from '../core/models/sf-project-doc';
+import { canAccessTranslateApp } from '../core/models/sf-project-role-info';
+import { SFProjectService } from '../core/sf-project.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+abstract class RouterGuard implements CanActivate {
+  constructor(
+    public readonly authGuard: AuthGuard,
+    public readonly userService: UserService,
+    public readonly projectService: SFProjectService,
+    public readonly router: Router
+  ) {}
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    const projectId = 'projectId' in next.params ? next.params['projectId'] : '';
+    return this.authGuard.canActivate(next, state).pipe(switchMap(() => this.allowTransition(projectId)));
+  }
+
+  allowTransition(projectId: string): Observable<boolean> {
+    return this.authGuard.allowTransition().pipe(
+      switchMap(isLoggedIn => {
+        if (isLoggedIn) {
+          return from(this.projectService.get(projectId)).pipe(map(projectDoc => this.check(projectDoc)));
+        }
+        return of(false);
+      })
+    );
+  }
+
+  abstract check(project: SFProjectDoc): boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SFAdminAuthGuard extends RouterGuard {
+  check(projectDoc: SFProjectDoc): boolean {
+    return (
+      projectDoc.data != null &&
+      projectDoc.data.userRoles[this.userService.currentUserId] === SFProjectRole.ParatextAdministrator
+    );
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CheckingAuthGuard extends RouterGuard {
+  check(projectDoc: SFProjectDoc): boolean {
+    if (projectDoc.data != null && projectDoc.data.checkingConfig.checkingEnabled) {
+      return true;
+    }
+    this.router.navigate(['/projects', projectDoc.id], { replaceUrl: true });
+    return false;
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TranslateAuthGuard extends RouterGuard {
+  check(projectDoc: SFProjectDoc): boolean {
+    if (projectDoc.data != null) {
+      const role = projectDoc.data.userRoles[this.userService.currentUserId] as SFProjectRole;
+      if (canAccessTranslateApp(role)) {
+        return true;
+      }
+    }
+    this.router.navigate(['/projects', projectDoc.id], { replaceUrl: true });
+    return false;
+  }
+}

src/SIL.XForge.Scripture/ClientApp/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { SystemAdministrationComponent } from 'xforge-common/system-administrati
 import { ConnectProjectComponent } from './connect-project/connect-project.component';
 import { ProjectComponent } from './project/project.component';
 import { SettingsComponent } from './settings/settings.component';
-import { SFAdminAuthGuard } from './shared/sfadmin-auth.guard';
+import { SFAdminAuthGuard } from './shared/project-router.guard';
 import { StartComponent } from './start/start.component';
 import { SyncComponent } from './sync/sync.component';
 

src/SIL.XForge.Scripture/ClientApp/src/app/app.component.html
@@ -217,5 +217,10 @@
   </mdc-drawer-content>
 </mdc-drawer>
 <div #appContent class="app-content mdc-top-app-bar--short-fixed-adjust">
-  <div><router-outlet></router-outlet></div>
+  <div>
+    <router-outlet></router-outlet>
+    <p *ngIf="showCheckingDisabled" class="checking-unavailable">
+      Scripture checking is not available in this project.
+    </p>
+  </div>
 </div>

src/SIL.XForge.Scripture/ClientApp/src/app/app.component.scss
@@ -84,3 +84,7 @@
 a {
   text-decoration: none;
 }
+
+.checking-unavailable {
+  text-align: center;
+}

src/SIL.XForge.Scripture/ClientApp/src/app/app.component.spec.ts
@@ -32,7 +32,7 @@ import { SFProjectDoc } from './core/models/sf-project-doc';
 import { SF_REALTIME_DOC_TYPES } from './core/models/sf-realtime-doc-types';
 import { SFProjectService } from './core/sf-project.service';
 import { ProjectDeletedDialogComponent } from './project-deleted-dialog/project-deleted-dialog.component';
-import { SFAdminAuthGuard } from './shared/sfadmin-auth.guard';
+import { SFAdminAuthGuard } from './shared/project-router.guard';
 
 const mockedAuthService = mock(AuthService);
 const mockedUserService = mock(UserService);

src/SIL.XForge.Scripture/ClientApp/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { MdcDialog, MdcSelect, MdcTopAppBar } from '@angular-mdc/web';
 import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MediaChange, MediaObserver } from '@angular/flex-layout';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { Site } from 'realtime-server/lib/common/models/site';
 import { SystemRole } from 'realtime-server/lib/common/models/system-role';
 import { AuthType, getAuthType, User } from 'realtime-server/lib/common/models/user';
 import { SFProjectRole } from 'realtime-server/lib/scriptureforge/models/sf-project-role';
@@ -22,9 +21,10 @@ import { version } from '../../../version.json';
 import { environment } from '../environments/environment';
 import { HelpHeroService } from './core/help-hero.service';
 import { SFProjectDoc } from './core/models/sf-project-doc';
+import { canAccessTranslateApp } from './core/models/sf-project-role-info.js';
 import { SFProjectService } from './core/sf-project.service';
 import { ProjectDeletedDialogComponent } from './project-deleted-dialog/project-deleted-dialog.component';
-import { SFAdminAuthGuard } from './shared/sfadmin-auth.guard';
+import { SFAdminAuthGuard } from './shared/project-router.guard';
 declare function gtag(...args: any): void;
 
 export const CONNECT_PROJECT_OPTION = '*connect-project*';
@@ -89,6 +89,15 @@ export class AppComponent extends DataLoadingComponent implements OnInit, OnDest
     }
   }
 
+  get showCheckingDisabled(): boolean {
+    return (
+      this.selectedProjectDoc != null &&
+      this.selectedProjectDoc.data != null &&
+      !this.selectedProjectDoc.data.checkingConfig.checkingEnabled &&
+      !canAccessTranslateApp(this.selectedProjectRole)
+    );
+  }
+
   get issueMailTo(): string {
     return issuesEmailTemplate();
   }
@@ -147,7 +156,7 @@ export class AppComponent extends DataLoadingComponent implements OnInit, OnDest
   }
 
   get isTranslateEnabled(): boolean {
-    return this.selectedProjectRole != null && this.selectedProjectRole !== SFProjectRole.CommunityChecker;
+    return canAccessTranslateApp(this.selectedProjectRole);
   }
 
   get isCheckingEnabled(): boolean {

src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AuthGuard } from 'xforge-common/auth.guard';
+import { CheckingAuthGuard } from '../shared/project-router.guard';
 import { CheckingOverviewComponent } from './checking-overview/checking-overview.component';
 import { CheckingComponent } from './checking/checking.component';
 
 const routes: Routes = [
-  { path: 'projects/:projectId/checking/:bookId', component: CheckingComponent, canActivate: [AuthGuard] },
-  { path: 'projects/:projectId/checking', component: CheckingOverviewComponent, canActivate: [AuthGuard] }
+  { path: 'projects/:projectId/checking/:bookId', component: CheckingComponent, canActivate: [CheckingAuthGuard] },
+  { path: 'projects/:projectId/checking', component: CheckingOverviewComponent, canActivate: [CheckingAuthGuard] }
 ];
 
 @NgModule({

src/SIL.XForge.Scripture/ClientApp/src/app/core/models/sf-project-role-info.ts
@@ -9,3 +9,7 @@ export const SF_PROJECT_ROLES: ProjectRoleInfo[] = [
   { role: SFProjectRole.Reviewer, displayName: 'Reviewer' },
   { role: SFProjectRole.CommunityChecker, displayName: 'Community Checker' }
 ];
+
+export function canAccessTranslateApp(role?: SFProjectRole) {
+  return role != null && role !== SFProjectRole.CommunityChecker;
+}

commit 33c777056ffe1e4af06079f28442353aef80ac6f

Fix production build router guard dependency injection errors
These errors were introduced in e8fe19cf0e94a92603b5790a068a90f37e7ab755

src/SIL.XForge.Scripture/ClientApp/src/app/shared/project-router.guard.ts
@@ -9,16 +9,8 @@ import { SFProjectDoc } from '../core/models/sf-project-doc';
 import { canAccessTranslateApp } from '../core/models/sf-project-role-info';
 import { SFProjectService } from '../core/sf-project.service';
 
-@Injectable({
-  providedIn: 'root'
-})
 abstract class RouterGuard implements CanActivate {
-  constructor(
-    public readonly authGuard: AuthGuard,
-    public readonly userService: UserService,
-    public readonly projectService: SFProjectService,
-    public readonly router: Router
-  ) {}
+  constructor(protected authGuard: AuthGuard, protected projectService: SFProjectService) {}
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
     const projectId = 'projectId' in next.params ? next.params['projectId'] : '';
@@ -43,6 +35,10 @@ abstract class RouterGuard implements CanActivate {
   providedIn: 'root'
 })
 export class SFAdminAuthGuard extends RouterGuard {
+  constructor(authGuard: AuthGuard, projectService: SFProjectService, private userService: UserService) {
+    super(authGuard, projectService);
+  }
+
   check(projectDoc: SFProjectDoc): boolean {
     return (
       projectDoc.data != null &&
@@ -55,6 +51,10 @@ export class SFAdminAuthGuard extends RouterGuard {
   providedIn: 'root'
 })
 export class CheckingAuthGuard extends RouterGuard {
+  constructor(authGuard: AuthGuard, projectService: SFProjectService, private router: Router) {
+    super(authGuard, projectService);
+  }
+
   check(projectDoc: SFProjectDoc): boolean {
     if (projectDoc.data != null && projectDoc.data.checkingConfig.checkingEnabled) {
       return true;
@@ -68,6 +68,15 @@ export class CheckingAuthGuard extends RouterGuard {
   providedIn: 'root'
 })
 export class TranslateAuthGuard extends RouterGuard {
+  constructor(
+    authGuard: AuthGuard,
+    projectService: SFProjectService,
+    private userService: UserService,
+    private router: Router
+  ) {
+    super(authGuard, projectService);
+  }
+
   check(projectDoc: SFProjectDoc): boolean {
     if (projectDoc.data != null) {
       const role = projectDoc.data.userRoles[this.userService.currentUserId] as SFProjectRole;