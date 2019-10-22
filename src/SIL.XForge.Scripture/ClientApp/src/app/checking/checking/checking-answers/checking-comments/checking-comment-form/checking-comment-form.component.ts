330bdde6f3661435726bf394ced4f75c9423019b
Ira Hopkinson <ira_hopkinson@sil.org>
2019-10-22 17:16:01 +0700

    Fix remaining LGTM.com alerts
    * change duplicated `id` to `class` in HTML
    * remove unused items
    * remove useless assignment to local variable
    * refactor for parse state - remove `ref` use

--- src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/checking-answers/checking-comments/checking-comment-form/checking-comment-form.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/checking-answers/checking-comments/checking-comment-form/checking-comment-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { XFValidators } from 'xforge-common/xfvalidators';
 
