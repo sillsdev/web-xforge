941470474ea0b9dfc889ccbc6654ac19a76d98b1
Raymond Luong <raymond_luong@sil.org>
2019-12-20 18:13:46 -0500

    Internationalize project admin pages

--- src/SIL.XForge.Scripture/ClientApp/src/app/settings/delete-project-dialog/delete-project-dialog.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/settings/delete-project-dialog/delete-project-dialog.component.ts
@@ -1,6 +1,7 @@
 import { MDC_DIALOG_DATA } from '@angular-mdc/web/dialog';
 import { Component, Inject } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { I18nService } from 'xforge-common/i18n.service';
 
 @Component({
   templateUrl: 'delete-project-dialog.component.html'
@@ -8,7 +9,7 @@ import { FormControl } from '@angular/forms';
 export class DeleteProjectDialogComponent {
   projectNameEntry = new FormControl('');
 
-  constructor(@Inject(MDC_DIALOG_DATA) public data: { name: string }) {}
+  constructor(@Inject(MDC_DIALOG_DATA) public data: { name: string }, readonly i18n: I18nService) {}
 
   get deleteDisabled() {
     return !(
