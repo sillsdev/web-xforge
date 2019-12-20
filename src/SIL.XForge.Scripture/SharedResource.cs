b07e300e4311e0ac7446738f3cf1b54cbdb6dc94
Nathaniel Paulus <nateowami@gmail.com>
2019-12-20 16:43:35 -0500

    Add Indonesian to back end
    Front-end strings are already up to date

--- src/SIL.XForge.Scripture/SharedResource.cs
+++ src/SIL.XForge.Scripture/SharedResource.cs
@@ -73,6 +73,15 @@ namespace SIL.XForge.Scripture
                     Production = false
                 }
             },
+            { "id", new InterfaceLanguage
+                {
+                    LocalName = "Bahasa Indonesia",
+                    EnglishName = "Indonesian",
+                    CanonicalTag = "id",
+                    Tags = new string[]{ "id", "id-ID" },
+                    Production = false
+                }
+            },
             { "az", new InterfaceLanguage
                 {
                     LocalName = "Azərbaycanca",
