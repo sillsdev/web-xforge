3a2f33c4badc7325007ce250b831dffe5eeb3b91
Raymond Luong <raymond_luong@sil.org>
2020-01-10 17:24:58 -0500

    SF-749 Change text when selecting different chapter

--- src/SIL.XForge.Scripture/ClientApp/src/app/shared/chapter-nav/chapter-nav.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/chapter-nav/chapter-nav.component.ts
@@ -7,13 +7,34 @@ import { I18nService } from 'xforge-common/i18n.service';
   styleUrls: ['./chapter-nav.component.scss']
 })
 export class ChapterNavComponent {
-  @Input() bookNum?: number;
   @Input() chapters: number[] = [];
   @Input() chapter?: number;
 
   @Output() chapterChange = new EventEmitter<number>();
 
+  private _bookNum?: number;
+
   constructor(readonly i18n: I18nService) {}
+
+  @Input() set bookNum(value: number | undefined) {
+    if (this._bookNum == null) {
+      this._bookNum = value;
+      return;
+    }
+    this.chapter = undefined;
+    this._bookNum = value;
+    // Wait for bookNum to update so we get the updated book and chapter in the select control
+    setTimeout(() => {
+      this.chapter = 1;
+    });
+  }
+
+  get bookName(): string {
+    if (this._bookNum == null) {
+      return '';
+    }
+    return this.i18n.translateBook(this._bookNum);
+  }
 
   prevChapter(): void {
     const index = this.chapters.findIndex(c => c === this.chapter);
