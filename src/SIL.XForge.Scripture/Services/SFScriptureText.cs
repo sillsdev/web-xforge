6ad924fcf0fa0ca73d3553af6973d4593f8a1a82
MarkS <marksvc@gmail.com>
2020-01-21 13:20:40 -0700

    SF-701 Handle missing ops or doc in SFScriptureText
    * Treat missing doc ops as an empty ops.
    - This fixes SF-701 where a `KeyNotFoundException: Element 'ops' not
    found.` exception can occur when syncing, if the SF DB has chapter
    texts with no ops.
    * Crash earlier with a stated reason if doc is null.
    * Add SFScriptureText tests, comments

--- src/SIL.XForge.Scripture/Services/SFScriptureText.cs
+++ src/SIL.XForge.Scripture/Services/SFScriptureText.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,11 +8,21 @@ using SIL.Machine.Tokenization;
 
 namespace SIL.XForge.Scripture.Services
 {
+    /// <summary>Set of Scripture text segments.</summary>
     public class SFScriptureText : IText
     {
+        /// <remarks>Builds segments from texts and references. Will use ops in doc that have an insert and a segment attribute providing reference information.
+        /// For example,
+        /// { "insert": "In the beginning ...",
+        ///   "attributes": { "segment": "verse_1_1" } }
+        /// </remarks>
         public SFScriptureText(ITokenizer<string, int> wordTokenizer, string projectId, int book, int chapter,
             BsonDocument doc)
         {
+            if (doc == null)
+            {
+                throw new ArgumentNullException(nameof(doc));
+            }
             Id = $"{projectId}_{book}_{chapter}";
             Segments = GetSegments(wordTokenizer, doc).OrderBy(s => s.SegmentRef).ToArray();
         }
@@ -24,8 +35,12 @@ namespace SIL.XForge.Scripture.Services
         {
             string prevRef = null;
             var sb = new StringBuilder();
-            var ops = (BsonArray)doc["ops"];
-            foreach (BsonDocument op in ops.Cast<BsonDocument>())
+            doc.TryGetValue("ops", out BsonValue ops);
+            if (ops as BsonArray == null)
+            {
+                yield break;
+            }
+            foreach (BsonDocument op in ((BsonArray)ops).Cast<BsonDocument>())
             {
                 // skip embeds
                 if (!op.TryGetValue("insert", out BsonValue value) || value.BsonType != BsonType.String)
