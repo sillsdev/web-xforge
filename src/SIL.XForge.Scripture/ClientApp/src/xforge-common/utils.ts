91785dcfe6c74af98292e9e52af7ff79a1b8adb3
Nathaniel Paulus <nateowami@gmail.com>
2020-01-31 21:55:49 -0500

    SF-778 Internationalize the report issue email

--- src/SIL.XForge.Scripture/ClientApp/src/xforge-common/utils.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/utils.ts
@@ -47,49 +47,18 @@ export function supportedBrowser(): boolean {
 
 export function issuesEmailTemplate(errorId?: string): string {
   const bowser = Bowser.getParser(window.navigator.userAgent);
-  const template = `mailto:${environment.issueEmail}?subject=${encodeURIComponent(
-    environment.siteName + ' issue'
-  )}&body=`;
-  const body = `Thanks for reporting the issue!
-It would help us if you fill out some of the information below, but please submit even if you can't fill out much.
-If you are requesting a feature many of the fields may not be applicable.
-Be aware your bug report will be publicly available. Never submit passwords or other secrets.
+  const subject = translate('issue_email.subject', { siteName: environment.siteName });
+  const body = translate('issue_email.body', {
+    siteName: environment.siteName,
+    siteVersion: version,
+    browserName: bowser.getBrowserName(),
+    browserVersion: bowser.getBrowserVersion(),
+    operatingSystem: bowser.getOSName(),
+    operatingSystemVersion: bowser.getOSVersion() || translate('issue_email.unknown'),
+    errorId: errorId || translate('issue_email.not_applicable')
+  });
 
-Bug report
-A clear and concise description of what the bug is.
-
-Steps to reproduce
-For example:
-1. Go to ...
-2. Click on ...
-3. Scroll down to ...
-4. See error
-
-Actual behavior
-Please describe actual behavior of the issue you are observing.
-
-Expected behavior
-A clear and concise description of what you expected to happen.
-
-Screenshots
-If applicable, add screenshots to help explain your issue.
-
-Additional context
-Add any other context about the problem here.
-
-Possible solution
-Add any possible solutions to the problem here.
-
-Your environment:
-Software                 Version
------------------------------------------
-Scripture Forge - ${version}
-${bowser.getBrowserName()} - ${bowser.getBrowserVersion()}
-${bowser.getOSName()} - ${bowser.getOSVersion() || 'unknown'}
-
-Error id: ${errorId || 'not applicable'}`;
-  // Encode all reserved characters in the body because some chars cause problems (e.g. truncating the body)
-  return template + encodeURIComponent(body);
+  return `mailto:${environment.issueEmail}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
 }
 
 export function parseJSON(str: string): any | undefined {
