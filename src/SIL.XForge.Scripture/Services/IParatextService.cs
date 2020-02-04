73c3e1e82ac6069947344033c986da1de741bee5
Damien Daspit <damien_daspit@sil.org>
2020-02-04 17:19:31 +0700

    Refactor ParatextSyncRunner to work with PT send/receive

--- src/SIL.XForge.Scripture/Services/IParatextService.cs
+++ src/SIL.XForge.Scripture/Services/IParatextService.cs
@@ -8,15 +8,19 @@ namespace SIL.XForge.Scripture.Services
 {
     public interface IParatextService
     {
+        void Init();
+
         Task<IReadOnlyList<ParatextProject>> GetProjectsAsync(UserSecret userSecret);
         string GetParatextUsername(UserSecret userSecret);
         Task<Attempt<string>> TryGetProjectRoleAsync(UserSecret userSecret, string paratextId);
-        Task<IReadOnlyList<string>> GetBooksAsync(UserSecret userSecret, string projectId);
-        Task<string> GetBookTextAsync(UserSecret userSecret, string projectId, string bookId);
-        Task<string> UpdateBookTextAsync(UserSecret userSecret, string projectId, string bookId,
-            string revision, string usxText);
-        Task<string> GetNotesAsync(UserSecret userSecret, string projectId, string bookId);
-        Task<string> UpdateNotesAsync(UserSecret userSecret, string projectId, string notesText);
         Task<IReadOnlyDictionary<string, string>> GetProjectRolesAsync(UserSecret userSecret, string projectId);
+
+        IReadOnlyList<int> GetBooks(string projectId);
+        string GetBookText(string projectId, int bookNum);
+        void PutBookText(string projectId, int bookNum, string usx);
+        string GetNotes(string projectId, int bookNum);
+        void PutNotes(string projectId, string notesText);
+
+        void SendReceive(UserSecret userSecret, IEnumerable<string> projectIds);
     }
 }
