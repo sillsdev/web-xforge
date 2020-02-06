de50f38fc24bf0a5388b0478fee1b28e6c519cd8
Nathaniel Paulus <nateowami@gmail.com>
2020-02-05 22:53:56 -0500

    Internationalize range label and show invited users with blank role

--- src/SIL.XForge.Scripture/ClientApp/src/xforge-common/paginator/paginator.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/paginator/paginator.component.ts
@@ -20,6 +20,15 @@ export class Paginator extends MatPaginatorIntl implements OnDestroy {
     });
   }
 
+  // It needs to be a member property, not a member function, in order to compile
+  getRangeLabel = (page: number, pageSize: number, length: number) => {
+    // The logic for this function is copied from the docs and then modified for the sake of internationalization
+    length = Math.max(length, 0);
+    const startIndex = page * pageSize;
+    const endIndex = startIndex < length ? Math.min(startIndex + pageSize, length) : startIndex + pageSize;
+    return translate('paginator.range_label', { startIndex: startIndex + 1, endIndex, length });
+  };
+
   ngOnDestroy(): void {
     this.changeSubscription.unsubscribe();
   }
