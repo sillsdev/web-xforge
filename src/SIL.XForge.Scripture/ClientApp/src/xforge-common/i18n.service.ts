4319327a2aee73b7605fbd1900b49a6404f10109
Nathaniel Paulus <nateowami@gmail.com>
2020-02-05 22:53:56 -0500

    Internationalize range label and show invited users with blank role

--- src/SIL.XForge.Scripture/ClientApp/src/xforge-common/i18n.service.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/i18n.service.ts
@@ -157,9 +157,6 @@ export class I18nService {
   }
 
   translateRole(role: string) {
-    if (role === '') {
-      role = 'unspecified';
-    }
     return this.transloco.translate(`roles.${role}`);
   }
 
