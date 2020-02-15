0769c573e633c6c1bd12f1fd34aeaac0bbc3ab96
Nathaniel Paulus <nateowami@gmail.com>
2020-02-14 22:52:08 -0500

    Base static page Bugsnag config on config file

--- src/SIL.XForge.Scripture/Pages/Index.cshtml.cs
+++ src/SIL.XForge.Scripture/Pages/Index.cshtml.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using SIL.XForge.Configuration;
 using SIL.XForge.Services;
 
@@ -32,23 +35,35 @@ namespace SIL.XForge.Scripture.Pages
         private readonly IOptions<AuthOptions> _authOptions;
         private readonly IOptions<SiteOptions> _siteOptions;
         private readonly IEmailService _emailService;
+        private readonly IConfiguration _configuration;
 
         public IndexModel(IHostingEnvironment env, IOptions<AuthOptions> authOptions, IOptions<SiteOptions> siteOptions,
-            IEmailService emailService)
+            IEmailService emailService, IConfiguration configuration)
         {
             _env = env;
             _authOptions = authOptions;
             _siteOptions = siteOptions;
             _emailService = emailService;
+            _configuration = configuration;
         }
 
         public void OnGet()
         {
+            ViewData["ProductVersion"] = System.Diagnostics.FileVersionInfo.GetVersionInfo(@System.Reflection.Assembly.GetEntryAssembly().Location).ProductVersion;
+
+            var bugsnagConfig = new Dictionary<string, object>
+                {
+                    { "apiKey", _configuration.GetValue<string>("Bugsnag:ApiKey") },
+                    { "appVersion", ViewData["ProductVersion"] },
+                    { "notifyReleaseStages", _configuration.GetSection("Bugsnag:NotifyReleaseStages").Get<string[]>() },
+                    { "releaseStage", _configuration.GetValue<string>("Bugsnag:ReleaseStage") }
+                };
+            ViewData["BugsnagConfig"] = JsonConvert.SerializeObject(bugsnagConfig, Formatting.Indented);
+
             ViewData["Domain"] = _authOptions.Value.Domain;
             ViewData["ClientId"] = _authOptions.Value.FrontendClientId;
             ViewData["Audience"] = _authOptions.Value.Audience;
             ViewData["Scope"] = _authOptions.Value.Scope;
-            ViewData["ReleaseStage"] = _env.IsProduction() ? "live" : (_env.IsStaging() ? "qa" : "dev");
         }
 
         public async Task<IActionResult> OnPostAsync()
