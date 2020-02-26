d5c79351ad443b188dea1ff517aa829aaea36e16
Raymond Luong <raymond_luong@sil.org>
2020-02-26 18:34:11 -0500

    Steps toward getting project sources with an access token

--- src/SIL.XForge.Scripture/Services/SFInternetSharedRepositorySource.cs
+++ src/SIL.XForge.Scripture/Services/SFInternetSharedRepositorySource.cs
@@ -1,4 +1,6 @@
+using Paratext.Data;
 using Paratext.Data.Repository;
+using SIL.Reflection;
 using SIL.XForge.Models;
 
 namespace SIL.XForge.Scripture.Services
@@ -13,6 +15,18 @@ namespace SIL.XForge.Scripture.Services
         {
             // TODO: this won't work, because authentication is already set in the base constructor
             client.JwtToken = userSecret.ParatextTokens.AccessToken;
+
+            // RESTClient only uses the jwtToken if authentication is null;
+            ReflectionHelper.SetField(client, "authentication", null);
+        }
+    }
+
+    class JwtRESTClient : RESTClient
+    {
+        public JwtRESTClient(string baseUri, string applicationProductVersion, string jwtToken) : base(baseUri, applicationProductVersion)
+        {
+            this.JwtToken = jwtToken;
+            ReflectionHelper.SetField(this, "authentication", null);
         }
     }
 }
