2be84301099c07312dacce79a3fdd4c3d7dc9015
MarkS <marksvc@gmail.com>
2020-02-27 15:34:54 -0700

    Get SF to tell PT to clone and do S/R
    * Commenting out is-syncing check in sync.component.ts so it's easier
    to keep re-entering the C# code to test.
    * Adding DevEntryPoint to ParatextService and instructing
    ParatextSyncRunner RunAsync to go right to that, to make it easier to
    isolate the environment and functionality to only testing a specific
    scenario and behaviour.
    * Turned off Mutex but that probably wasn't necessary.
    * In ParatextService, in new DevEntryPoint, writing out steps to do a
    hg clone and PT S/R by mimicing the same ordering and steps that Tom
    did in the cli tool, with the aim of having a working structure that
    we can adapt to our usage
    * add some py and sty files that are needed by pt hg merging etc

---
+++ changedChapter.py
@@ -0,0 +1,83 @@
+# Mercurial extension to provide the 'hg changedChapter' command
+
+'''command to list all changesets which changed the specified chapter'''
+
+from mercurial import commands, registrar
+from mercurial.i18n import _
+
+from mercurial.node import short
+import re
+
+cmdtable = {}
+command = registrar.command(cmdtable)
+
+@command('changedChapter', 
+    [('c', 'chap', '', _('show revision changing the chapter'))] + commands.globalopts, 
+	_('hg changedChapter [-c CHAPTER] [FILE]'))
+def changedChapter(ui, repo, file_=None, **opts):
+    parts = opts.get('chap').split(".")
+    chapter = parts[0]
+    if len(parts[1].split("-")) == 2:
+        (v1, v2) = parts[1].split("-")
+    else:
+        # verse bridges present, just look for changed chapter text
+        v1 = ""
+        v2 = ""
+        
+    fl = repo.file(file_)    # get filelog 
+
+    lastVerseText = ""   # track verse text seen
+	
+    for rev in fl:
+        bookText = fl.revision(fl.node(rev)) 
+        chapterText = getChapter(chapter, bookText)    
+        if chapterText == "": continue
+        
+        if v1 != "":
+            verseText = getVerses(v1, v2, chapterText)
+        else:
+            verseText = chapterText
+        
+        if verseText == lastVerseText: continue
+        lastVerseText = verseText
+        
+        ui.write(str(int(fl.linkrev(rev))) + '\r\n')
+        ui.flush()
+
+# Extract the text of the specificed chapter from the text of the book
+
+def getChapter(chapter, text):
+    parts = re.split(r"(\\c\s+\S+)", text)
+    i = findIndex(parts, chapter)
+    if i == -1: return ""
+    
+    if chapter == "1":
+        return parts[i-1] + parts[i] + parts[i+1]
+    
+    return parts[i] + parts[i+1]
+
+def getVerses(v1, v2, text):
+    parts = re.split(r"(\\v\s+\S+)", text)
+    i = findIndex(parts, v1)
+    if i == -1: return text
+    j = findIndex(parts, v2)
+    if j == -1: return text
+    
+    result = ""
+    if i == 1:
+        result = parts[0]
+        
+    while i <= j:
+        result += parts[i] + parts[i+1]
+        i = i + 2
+        
+    return result
+    
+def findIndex(parts, match):
+    for i in range(1, len(parts), 2):
+        pieces = parts[i].split()
+        if pieces[1] == match:
+            return i
+        
+    return -1
+
