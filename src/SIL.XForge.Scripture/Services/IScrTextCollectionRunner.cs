e83602d45686b4f22a98b9f500a3286ff9aeb3a5
MarkS <marksvc@gmail.com>
2020-03-02 13:05:52 -0700

    SF-822 Implementing fetch book text
    * clean up. rearrange initialization

--- src/SIL.XForge.Scripture/Services/IScrTextCollectionRunner.cs
+++ src/SIL.XForge.Scripture/Services/IScrTextCollectionRunner.cs
@@ -4,8 +4,11 @@ using Paratext.Data;
 
 namespace SIL.XForge.Scripture.Services
 {
-    interface IScrTextCollectionRunner
+    public interface IScrTextCollectionRunner
     {
+        void Initialize(string settingsDir = null, bool allowMigration = false);
         ScrText FindById(string projectId, string shortName = null, bool allowInaccessible = false, bool allowUnsupported = false);
+        ScrText GetById(string projectId, string shortName = null);
+
     }
 }
