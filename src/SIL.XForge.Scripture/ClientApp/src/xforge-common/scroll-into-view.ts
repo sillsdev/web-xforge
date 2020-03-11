ea4b3bb2f963730342268d36aa78ec048ea1b0c6
Nathaniel Paulus <Nateowami@users.noreply.github.com>
2020-03-11 09:13:49 -0400

    SF-833 Auto scroll to answer and comment forms (#581)

---
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/scroll-into-view.ts
@@ -0,0 +1,45 @@
+import { AfterViewInit, Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appScrollIntoView]'
+})
+export class ScrollIntoViewDirective implements AfterViewInit {
+  constructor(private readonly elementRef: ElementRef<HTMLElement>) {}
+
+  ngAfterViewInit() {
+    setTimeout(() => this.scrollIntoViewIfNeeded(this.elementRef.nativeElement), 0);
+  }
+
+  private scrollIntoViewIfNeeded(element: HTMLElement) {
+    const scrollElement = this.nearestScrollingAncestor(element);
+    const padding = 5;
+    if (scrollElement) {
+      let insufficientScroll = element.getBoundingClientRect().bottom - scrollElement.getBoundingClientRect().bottom;
+      insufficientScroll += padding;
+      if (insufficientScroll > 0) {
+        scrollElement.scrollBy(0, insufficientScroll);
+      } else {
+        let excessiveScroll = scrollElement.getBoundingClientRect().top - element.getBoundingClientRect().top;
+        excessiveScroll += padding;
+        if (excessiveScroll > 0) {
+          scrollElement.scrollBy(0, -excessiveScroll);
+        }
+      }
+    }
+  }
+
+  private nearestScrollingAncestor(element: HTMLElement): HTMLElement | null {
+    const parent = element.parentElement;
+    if (parent == null) {
+      return null;
+    } else if (
+      parent != null &&
+      parent.scrollHeight > parent.clientHeight &&
+      ['scroll', 'auto'].includes(window.getComputedStyle(parent).overflowY as string)
+    ) {
+      return parent;
+    } else {
+      return this.nearestScrollingAncestor(parent);
+    }
+  }
+}
