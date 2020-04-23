069af30a905295544034ff1847a156d1cbc68ba9
Raymond Luong <raymond_luong@sil.org>
2020-04-23 13:36:12 -0600

    Clone target and source as subfolders in the main sync project
    * require the text type when accessing paratext project data
    * add test for failing pt send/receive
    * specify custom mercurial required in README

--- src/SIL.XForge.Scripture/Services/IParatextService.cs
+++ src/SIL.XForge.Scripture/Services/IParatextService.cs
@@ -16,13 +16,13 @@ namespace SIL.XForge.Scripture.Services
         Task<Attempt<string>> TryGetProjectRoleAsync(UserSecret userSecret, string paratextId);
         Task<IReadOnlyDictionary<string, string>> GetProjectRolesAsync(UserSecret userSecret, string projectId);
 
-        IReadOnlyList<int> GetBookList(UserSecret userSecret, string ptProjectId);
-        string GetBookText(UserSecret userSecret, string ptProjectId, int bookNum);
+        IReadOnlyList<int> GetBookList(UserSecret userSecret, string ptProjectId, TextType textType);
+        string GetBookText(UserSecret userSecret, string ptProjectId, int bookNum, TextType textType);
         void PutBookText(UserSecret userSecret, string ptProjectId, int bookNum, string usx);
         string GetNotes(UserSecret userSecret, string ptProjectId, int bookNum);
         void PutNotes(UserSecret userSecret, string ptProjectId, string notesText);
 
-        Task SendReceiveAsync(UserSecret userSecret, IEnumerable<string> ptProjectIds,
+        Task SendReceiveAsync(UserSecret userSecret, string ptTargetId, string ptSourceId,
             IProgress<ProgressState> progress = null);
     }
 }
