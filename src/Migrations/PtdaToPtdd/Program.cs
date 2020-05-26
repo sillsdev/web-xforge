475326a752694461e097c811baf1f862233882b4
MarkS <marksvc@gmail.com>
2020-05-26 16:17:03 -0600

    Add empty PtdaToPtdd migrator tool

---
+++ src/Migrations/PtdaToPtdd/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PtdaToPtdd
+{
+    /// <summary>Migrate SF project data from being used via
+    /// Paratext Data Access Web Api, to being used via
+    /// ParatextData.dll.</summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
