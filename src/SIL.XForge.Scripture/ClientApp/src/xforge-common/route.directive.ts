sillsdev/web-xforge

commit e7296f9460a169a6957853afc695d23a7f235c8e

SF-918 Open new tab upon ctrl click or middle mouse button (#690)

src/SIL.XForge.Scripture/ClientApp/src/app/app.component.html
@@ -98,10 +98,14 @@
                 <div>
                   <mdc-list-divider></mdc-list-divider>
                   <mdc-list>
-                    <mdc-list-item *ngIf="isSystemAdmin" routerLink="/system-administration" [disabled]="!isAppOnline">
+                    <mdc-list-item
+                      *ngIf="isSystemAdmin"
+                      appRouterLink="/system-administration"
+                      [disabled]="!isAppOnline"
+                    >
                       {{ t("system_administration") }}
                     </mdc-list-item>
-                    <mdc-list-item routerLink="/projects">{{ t("project_home") }}</mdc-list-item>
+                    <mdc-list-item appRouterLink="/projects">{{ t("project_home") }}</mdc-list-item>
                     <mdc-list-item *ngIf="canChangePassword" (click)="changePassword()" [disabled]="!isAppOnline">
                       {{ t("change_password") }}
                     </mdc-list-item>
@@ -166,7 +170,7 @@
         </mdc-list-item>
         <div *ngIf="isTranslateEnabled && (translateVisible || hasSingleAppEnabled)">
           <mdc-list-item
-            [routerLink]="['/projects', selectedProjectId, 'translate']"
+            [appRouterLink]="['/projects', selectedProjectId, 'translate']"
             [activated]="rlaTranslateOverview.isActive"
             routerLinkActive
             [routerLinkActiveOptions]="{ exact: true }"
@@ -178,7 +182,7 @@
           </mdc-list-item>
           <mdc-list-item
             *ngFor="let text of texts"
-            [routerLink]="['/projects', selectedProjectId, 'translate', getBookId(text)]"
+            [appRouterLink]="['/projects', selectedProjectId, 'translate', getBookId(text)]"
             [activated]="rla.isActive"
             routerLinkActive
             #rla="routerLinkActive"
@@ -202,7 +206,7 @@
         </mdc-list-item>
         <div *ngIf="isCheckingEnabled && (checkingVisible || hasSingleAppEnabled)">
           <mdc-list-item
-            [routerLink]="['/projects', selectedProjectId, 'checking']"
+            [appRouterLink]="['/projects', selectedProjectId, 'checking']"
             [activated]="rlaCheckingOverview.isActive"
             routerLinkActive
             [routerLinkActiveOptions]="{ exact: true }"
@@ -214,7 +218,7 @@
           </mdc-list-item>
           <mdc-list-item
             *ngIf="showAllQuestions"
-            [routerLink]="['/projects', selectedProjectId, 'checking', 'ALL']"
+            [appRouterLink]="['/projects', selectedProjectId, 'checking', 'ALL']"
             [activated]="rlaCheckingAllBooks.isActive"
             routerLinkActive
             [routerLinkActiveOptions]="{ exact: true }"
@@ -227,7 +231,7 @@
           <ng-container *ngFor="let text of texts">
             <mdc-list-item
               *ngIf="hasQuestions(text)"
-              [routerLink]="['/projects', selectedProjectId, 'checking', getBookId(text)]"
+              [appRouterLink]="['/projects', selectedProjectId, 'checking', getBookId(text)]"
               [activated]="rla.isActive"
               routerLinkActive
               #rla="routerLinkActive"
@@ -241,7 +245,7 @@
         <div *ngIf="(isProjectAdmin$ | async)">
           <mdc-list-divider *ngIf="isCheckingEnabled"></mdc-list-divider>
           <mdc-list-item
-            [routerLink]="['/projects', selectedProjectId, 'sync']"
+            [appRouterLink]="['/projects', selectedProjectId, 'sync']"
             [activated]="rlaSync.isActive"
             [class.list-item-disabled]="!isAppOnline"
             routerLinkActive
@@ -253,7 +257,7 @@
             {{ t("synchronize") }}
           </mdc-list-item>
           <mdc-list-item
-            [routerLink]="['/projects', selectedProjectId, 'settings']"
+            [appRouterLink]="['/projects', selectedProjectId, 'settings']"
             [activated]="rlaSettings.isActive"
             [class.list-item-disabled]="!isAppOnline"
             routerLinkActive
@@ -265,7 +269,7 @@
             {{ t("settings") }}
           </mdc-list-item>
           <mdc-list-item
-            [routerLink]="['/projects', selectedProjectId, 'users']"
+            [appRouterLink]="['/projects', selectedProjectId, 'users']"
             [activated]="rlaUsers.isActive"
             [class.list-item-disabled]="!isAppOnline"
             routerLinkActive

src/SIL.XForge.Scripture/ClientApp/src/xforge-common/route.directive.ts
@@ -1,0 +1,54 @@
+import { Directive, ElementRef, HostListener, Inject, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { WINDOW } from './browser-globals';
+
+@Directive({
+  selector: '[appRouterLink]'
+})
+export class RouterDirective {
+  private _route!: string | string[];
+
+  constructor(private element: ElementRef, private router: Router, @Inject(WINDOW) private window: Window) {}
+
+  @Input('appRouterLink')
+  set route(value: string | string[]) {
+    this._route = value;
+    const element = this.element!.nativeElement as HTMLElement;
+    if (this.isLink) {
+      element.setAttribute('href', this.url);
+    }
+  }
+
+  get route() {
+    return this._route;
+  }
+
+  @HostListener('click', ['$event'])
+  onClick(event: MouseEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    if (event.ctrlKey || event.metaKey) {
+      this.window.open(this.url, '_blank', 'noopener');
+    } else {
+      this.router.navigate(Array.isArray(this.route) ? this.route : [this.route]);
+    }
+  }
+
+  @HostListener('mouseup', ['$event'])
+  onMouseUp(event: MouseEvent) {
+    // if it's the middle mouse button (but not an a tag, because preventDefault doesn't stop it from opening a new tab)
+    if (event.which === 2 && !this.isLink) {
+      event.preventDefault();
+      event.stopPropagation();
+      this.window.open(this.url, '_blank', 'noopener');
+    }
+  }
+
+  private get url(): string {
+    return Array.isArray(this.route) ? this.route.join('/') : this.route;
+  }
+
+  private get isLink(): boolean {
+    return (this.element!.nativeElement as HTMLElement).tagName === 'A';
+  }
+}

src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking-overview/checking-overview.component.html
@@ -117,7 +117,7 @@
         <ng-container *ngFor="let text of texts">
           <mdc-list-item
             *ngIf="bookQuestionCount(text) > 0"
-            [routerLink]="['/projects', projectId, 'checking', getBookId(text)]"
+            [appRouterLink]="['/projects', projectId, 'checking', getBookId(text)]"
           >
             <div fxFlex="grow" fxLayout="row" fxLayoutAlign="start center">
               <span fxFlex>{{ getBookName(text) }}</span>

src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/checking.component.html
@@ -173,7 +173,7 @@
               class="next-question"
               icon="chevron_right"
             ></button>
-            <a [routerLink]="['/projects', projectDoc?.id, 'checking']" id="project-summary">
+            <a [appRouterLink]="['/projects', projectDoc?.id, 'checking']" id="project-summary">
               <app-donut-chart
                 [colors]="['#3a3a3a', '#fff', '#B8D332']"
                 [data]="[summary.unread, summary.read, summary.answered]"

src/SIL.XForge.Scripture/ClientApp/src/app/settings/settings.component.html
@@ -129,7 +129,7 @@
                 <div class="helper-text checkbox-helper-text" fxLayout="row">
                   <span>
                     <span [innerHTML]="shareDescription?.before"></span>
-                    <a [routerLink]="['/projects', projectId, 'users']">{{ shareDescription?.templateTagText }}</a>
+                    <a [appRouterLink]="['/projects', projectId, 'users']">{{ shareDescription?.templateTagText }}</a>
                     <span>{{ shareDescription?.after }}</span>
                   </span>
                 </div>
@@ -164,7 +164,7 @@
         <p>{{ t("delete_project_cannot_be_undone") }}</p>
         <p>
           <span [innerHTML]="synchronizeWarning?.before"></span>
-          <a [routerLink]="['/projects', projectId, 'sync']">{{ synchronizeWarning?.templateTagText }}</a>
+          <a [appRouterLink]="['/projects', projectId, 'sync']">{{ synchronizeWarning?.templateTagText }}</a>
           <span>{{ synchronizeWarning?.after }}</span>
         </p>
         <div fxLayout="row" fxLayoutAlign="end stretch">

src/SIL.XForge.Scripture/ClientApp/src/app/shared/page-not-found/page-not-found.component.html
@@ -1,4 +1,4 @@
 <ng-container *transloco="let t; read: 'app'">
   <h2><mdc-icon>error_outline</mdc-icon>{{ t("page_not_found") }}</h2>
-  <a routerLink="/projects">{{ t("project_home") }}</a>
+  <a appRouterLink="/projects">{{ t("project_home") }}</a>
 </ng-container>

src/SIL.XForge.Scripture/ClientApp/src/app/start/start.component.html
@@ -2,7 +2,7 @@
   <div fxLayout="column" fxLayoutAlign="center center" *ngIf="!isAppLoading">
     <p mdcBody1>{{ t("not_connected_to_any_projects") }}</p>
     <p>
-      <button mdc-button primary type="button" [routerLink]="['/connect-project']">
+      <button mdc-button primary type="button" [appRouterLink]="['/connect-project']">
         <mdc-icon>add</mdc-icon>{{ t("connect_project") }}
       </button>
     </p>

src/SIL.XForge.Scripture/ClientApp/src/app/translate/translate-overview/translate-overview.component.html
@@ -18,7 +18,7 @@
       <mdc-list *ngIf="texts != null" twoLine [interactive]="false">
         <mdc-list-item
           *ngFor="let textProgress of texts; trackBy: trackTextByBookNum"
-          [routerLink]="['./', getBookId(textProgress.text)]"
+          [appRouterLink]="['./', getBookId(textProgress.text)]"
         >
           <mdc-icon mdcListItemGraphic>book</mdc-icon>
           <mdc-list-item-text

commit ff7e895595eae8a629986ef2dd0669ecd818c7d3

SF-979 Fix highlighting of active item in navigation drawer
This was broken in e7296f9460a169a6957853afc695d23a7f235c8e

src/SIL.XForge.Scripture/ClientApp/src/app/app.component.html
@@ -171,10 +171,6 @@
         <div *ngIf="isTranslateEnabled && (translateVisible || hasSingleAppEnabled)">
           <mdc-list-item
             [appRouterLink]="['/projects', selectedProjectId, 'translate']"
-            [activated]="rlaTranslateOverview.isActive"
-            routerLinkActive
-            [routerLinkActiveOptions]="{ exact: true }"
-            #rlaTranslateOverview="routerLinkActive"
             (selectionChange)="itemSelected()"
           >
             <mdc-icon mdcListItemGraphic>apps</mdc-icon>
@@ -183,9 +179,6 @@
           <mdc-list-item
             *ngFor="let text of texts"
             [appRouterLink]="['/projects', selectedProjectId, 'translate', getBookId(text)]"
-            [activated]="rla.isActive"
-            routerLinkActive
-            #rla="routerLinkActive"
             (selectionChange)="itemSelected()"
           >
             <mdc-icon mdcListItemGraphic>book</mdc-icon>
@@ -207,10 +200,6 @@
         <div *ngIf="isCheckingEnabled && (checkingVisible || hasSingleAppEnabled)">
           <mdc-list-item
             [appRouterLink]="['/projects', selectedProjectId, 'checking']"
-            [activated]="rlaCheckingOverview.isActive"
-            routerLinkActive
-            [routerLinkActiveOptions]="{ exact: true }"
-            #rlaCheckingOverview="routerLinkActive"
             (selectionChange)="itemSelected()"
           >
             <mdc-icon mdcListItemGraphic>apps</mdc-icon>
@@ -219,10 +208,6 @@
           <mdc-list-item
             *ngIf="showAllQuestions"
             [appRouterLink]="['/projects', selectedProjectId, 'checking', 'ALL']"
-            [activated]="rlaCheckingAllBooks.isActive"
-            routerLinkActive
-            [routerLinkActiveOptions]="{ exact: true }"
-            #rlaCheckingAllBooks="routerLinkActive"
             (selectionChange)="itemSelected()"
           >
             <mdc-icon mdcListItemGraphic>bookmarks</mdc-icon>
@@ -232,9 +217,6 @@
             <mdc-list-item
               *ngIf="hasQuestions(text)"
               [appRouterLink]="['/projects', selectedProjectId, 'checking', getBookId(text)]"
-              [activated]="rla.isActive"
-              routerLinkActive
-              #rla="routerLinkActive"
               (selectionChange)="itemSelected()"
             >
               <mdc-icon mdcListItemGraphic>book</mdc-icon>
@@ -246,10 +228,7 @@
           <mdc-list-divider *ngIf="isCheckingEnabled"></mdc-list-divider>
           <mdc-list-item
             [appRouterLink]="['/projects', selectedProjectId, 'sync']"
-            [activated]="rlaSync.isActive"
             [class.list-item-disabled]="!isAppOnline"
-            routerLinkActive
-            #rlaSync="routerLinkActive"
             id="sync-item"
             (selectionChange)="itemSelected()"
           >
@@ -258,10 +237,7 @@
           </mdc-list-item>
           <mdc-list-item
             [appRouterLink]="['/projects', selectedProjectId, 'settings']"
-            [activated]="rlaSettings.isActive"
             [class.list-item-disabled]="!isAppOnline"
-            routerLinkActive
-            #rlaSettings="routerLinkActive"
             id="settings-item"
             (selectionChange)="itemSelected()"
           >
@@ -270,10 +246,7 @@
           </mdc-list-item>
           <mdc-list-item
             [appRouterLink]="['/projects', selectedProjectId, 'users']"
-            [activated]="rlaUsers.isActive"
             [class.list-item-disabled]="!isAppOnline"
-            routerLinkActive
-            #rlaUsers="routerLinkActive"
             id="users-item"
             (selectionChange)="itemSelected()"
           >

src/SIL.XForge.Scripture/ClientApp/src/xforge-common/route.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostListener, Inject, Input } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Inject, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { WINDOW } from './browser-globals';
 
@@ -21,6 +21,10 @@ export class RouterDirective {
 
   get route() {
     return this._route;
+  }
+
+  @HostBinding('class.mdc-list-item--activated') get active(): boolean {
+    return (this.element!.nativeElement as HTMLElement).tagName === 'MDC-LIST-ITEM' && this.router.url === this.url;
   }
 
   @HostListener('click', ['$event'])