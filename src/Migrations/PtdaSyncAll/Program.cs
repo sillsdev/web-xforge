c27f6954668b866c732502898b7bc16dc8ba69c8
MarkS <marksvc@gmail.com>
2020-06-24 16:06:59 -0600

    Migrator: Add PtdaSyncAll migrator tool: List projects, admins, if can access
    Initially only queries information.

---
+++ src/Migrations/PtdaSyncAll/Program.cs
@@ -0,0 +1,168 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using SIL.XForge.DataAccess;
+using SIL.XForge.Models;
+using SIL.XForge.Scripture.Models;
+using SIL.XForge.Realtime;
+using SIL.XForge.Scripture.Services;
+
+namespace PtdaSyncAll
+{
+    /// <summary>
+    /// Sync all SF projects between SF DB and Paratext Data Access Web API.
+    /// This is the first step in migrating to using ParatextData.dll for sync
+    /// and storing data.
+    /// Fails to run if SF server is running, with error
+    /// > System.Net.Http.HttpRequestException: An error occurred while sending the request.
+    /// >  ---> System.IO.IOException: The response ended prematurely.
+    /// </summary>
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            string sfAppDir = Environment.GetEnvironmentVariable("SF_APP_DIR") ?? "../../SIL.XForge.Scripture";
+
+            Directory.SetCurrentDirectory(sfAppDir);
+            IWebHostBuilder builder = SIL.XForge.Scripture.Program.CreateWebHostBuilder(args);
+            IWebHost webHost = builder.Build();
+            webHost.Start();
+            await PtdaSyncAll.Program.Inquiry(webHost);
+            Console.WriteLine("Migrator done.");
+        }
+
+        /// <summary>
+        /// Query information that will show whether we should be able to sync
+        /// all projects. In addition to reporting information on projects and
+        /// whether there is an admin that can sync the project, this method
+        /// shows that the admin can successfully perform queries to both the
+        /// PT Registry and the PT Data Access web APIs, via various
+        /// ParatextService method calls.
+        /// </summary>
+        public static async Task Inquiry(IWebHost webHost)
+        {
+            IRealtimeService realtimeService = webHost.Services.GetService<IRealtimeService>();
+            IParatextService paratextService = webHost.Services.GetService<IParatextService>();
+            IRepository<UserSecret> userSecretRepo = webHost.Services.GetService<IRepository<UserSecret>>();
+            IQueryable<SFProject> allSfProjects = realtimeService.QuerySnapshots<SFProject>();
+            char bullet1 = '>';
+            char bullet2 = '*';
+            char bullet3 = '-';
+
+            // Report on all SF projects.
+            foreach (SFProject sfProject in allSfProjects)
+            {
+                Console.WriteLine($"{bullet1} PT project {sfProject.ShortName}, "
+                    + $"PT project id {sfProject.ParatextId}, SF project id {sfProject.Id}.");
+                IEnumerable<string> projectSfAdminUserIds = sfProject.UserRoles
+                    .Where(ur => ur.Value == "pt_administrator").Select(ur => ur.Key);
+                if (projectSfAdminUserIds.Count() < 1)
+                {
+                    IEnumerable<string> projectSfUserIds = sfProject.UserRoles.Select(ur => ur.Key);
+                    string users = string.Join(", ", projectSfUserIds);
+                    if (projectSfUserIds.Count() < 1)
+                    {
+                        users = "None";
+                    }
+                    Console.WriteLine($"  {bullet2} Warning: no admin users. Non-admin users include: {users}");
+                }
+
+                // Report on all admins in a project
+                foreach (string sfUserId in projectSfAdminUserIds)
+                {
+                    UserSecret userSecret = userSecretRepo.Query().FirstOrDefault((UserSecret us) => us.Id == sfUserId);
+                    string ptUsername = null;
+                    string ptUserId = null;
+                    try
+                    {
+                        ptUsername = paratextService.GetParatextUsername(userSecret);
+                        ptUserId = GetParatextUserId(userSecret);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"  Failure. Skipping. Error was {e.Message}");
+                        continue;
+                    }
+                    Console.WriteLine($"  {bullet2} PT user '{ptUsername}', "
+                        + $"id {ptUserId}, using SF admin user id {sfUserId} on SF project.");
+
+                    string rt = $"{userSecret.ParatextTokens.RefreshToken.Substring(0, 5)}..";
+                    string at = $"{userSecret.ParatextTokens.AccessToken.Substring(0, 5)}..";
+                    bool atv = userSecret.ParatextTokens.ValidateLifetime();
+                    Console.WriteLine($"    {bullet3} Paratext RefreshToken: {rt}, "
+                        + $"AccessToken: {at}, AccessToken initially valid: {atv}.");
+
+                    // Demonstrate access to PT Registry, and report Registry's statement of role.
+                    Console.Write($"    {bullet3} PT Registry report on role on PT project: ");
+                    IReadOnlyDictionary<string, string> ptProjectRoles = null;
+                    try
+                    {
+                        ptProjectRoles = await paratextService.GetProjectRolesAsync(userSecret, sfProject.ParatextId);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"      Failure. Skipping. Error was {e.Message}");
+                        continue;
+                    }
+                    if (ptProjectRoles.TryGetValue(ptUserId, out string ptRole))
+                    {
+                        Console.WriteLine($"{ptRole}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Not found.");
+                    }
+                    // Demonstrate access to PT Data Access.
+                    IReadOnlyList<ParatextProject> userPtProjects = null;
+                    try
+                        await paratextService.GetProjectRolesAsync(userSecret, project.ParatextId);
+                    {
+                        userPtProjects = await paratextService.GetProjectsAsync(userSecret);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"    Failure. Skipping. Error was {e.Message}");
+                        continue;
+                    }
+
+                    Console.Write($"    {bullet3} PT Data Access and PT Registry "
+                        + "based report on projects the user can access, narrowed to this project: ");
+                    IEnumerable<string> ptProjectNamesList = userPtProjects
+                        .Where(ptProject => ptProject.ParatextId == sfProject.ParatextId)
+                        .Select(ptProject => ptProject.ShortName);
+                    string ptProjectNames = string.Join(',', ptProjectNamesList);
+                    if (ptProjectNamesList.Count() < 1)
+                    {
+                        ptProjectNames = "None.";
+                    }
+                    Console.WriteLine(ptProjectNames);
+                }
+            }
+        }
+
+        public static async Task MigrateAsync(IWebHost webHost)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// As claimed by tokens in userSecret. Looks like it corresponds to
+        /// `userId` in PT Registry project members query.
+        /// </summary>
+        private static string GetParatextUserId(UserSecret userSecret)
+        {
+            if (userSecret.ParatextTokens == null)
+                return null;
+            var accessToken = new JwtSecurityToken(userSecret.ParatextTokens.AccessToken);
+            Claim claim = accessToken.Claims.FirstOrDefault(c => c.Type == "sub");
+            return claim?.Value;
+        }
+    }
+}
