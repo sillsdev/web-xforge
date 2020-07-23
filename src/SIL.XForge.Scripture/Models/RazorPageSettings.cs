22e9bf829d4a3935f07fbba3586a4d5c4e9c13f8
nigel-wells <nigel-wells@hotmail.com>
2020-07-23 13:52:15 +1200

    SF-541 - Remaining homepage sections - Separated out the Learn More section on to its own page - Created a component to be used for the footer logic - Added CTA buttons for the home page and new Learn More page - Removed carousel

---
+++ src/SIL.XForge.Scripture/Models/RazorPageSettings.cs
@@ -0,0 +1,13 @@
+namespace SIL.XForge.Scripture.Models
+{
+    public class RazorPageSettings
+    {
+        public string ProductVersion { get; set; }
+        public string BugsnagConfig { get; set; }
+        public string Domain { get; set; }
+        public string ClientId { get; set; }
+        public string Audience { get; set; }
+        public string Scope { get; set; }
+        public string Origin { get; set; }
+    }
+}
