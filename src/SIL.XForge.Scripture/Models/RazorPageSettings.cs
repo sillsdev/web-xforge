6e71cffddc1f21e0a0061a65024bd645454f8942
Nigel Wells <nigel-wells@hotmail.com>
2020-07-30 03:49:48 +1200

    SF-541e - Remaining homepage sections (#743)
    - Separated out the Learn More section on to its own page
    - Created a component to be used for the footer logic
    - Added CTA buttons for the home page and new Learn More page
    - Removed carousel
    
    - Center the quote
    
    - Added new large button
    - Increased spacing at the top of inside pages
    - Adjusted language-picker font to swap for an alternative while it downloads to avoid FOIT
    
    - Moved auth0 connection code and checks back to the index page
    - Adjusted localizer names
    - Added class comments for `FooterViewComponent` and `RazorPageSettings`
    
    - Code review fixes
    
    - include origin in youtube player

---
+++ src/SIL.XForge.Scripture/Models/RazorPageSettings.cs
@@ -0,0 +1,9 @@
+namespace SIL.XForge.Scripture.Models
+{
+    /// <summary>Various settings to be used Razor pages</summary>
+    public class RazorPageSettings
+    {
+        public string ProductVersion { get; set; }
+        public string BugsnagConfig { get; set; }
+    }
+}
