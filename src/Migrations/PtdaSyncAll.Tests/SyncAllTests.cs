a8484898afb8ed5219cf826d1e13f66739d378ad
MarkS <marksvc@gmail.com>
2020-08-18 16:48:02 -0600

    Test that task throws are reported

--- src/Migrations/PtdaSyncAll.Tests/SyncAllTests.cs
+++ src/Migrations/PtdaSyncAll.Tests/SyncAllTests.cs
@@ -18,6 +18,9 @@ using SIL.XForge.Scripture.Services;
 
 namespace PtdaSyncAll
 {
+    /// <remarks>
+    /// These tests use timers, but are not part of a CI suite.
+    /// </remarks>
     [TestFixture]
     public class SyncAllTests
     {
@@ -29,7 +32,6 @@ namespace PtdaSyncAll
             await env.ParatextService.Received().GetProjectsAsync(Arg.Any<UserSecret>());
         }
 
-        // This test uses a timer, but isn't part of a CI test suite.
         [Test]
         public async Task Sync()
         {
@@ -39,7 +41,7 @@ namespace PtdaSyncAll
             {
                 syncTask = Task.Run(() =>
                 {
-                    Task.Delay(1000).Wait();
+                    Task.Delay(10000).Wait();
                 });
                 return syncTask;
             });
@@ -51,11 +53,42 @@ namespace PtdaSyncAll
             Assert.That(syncTask?.Status, Is.EqualTo(TaskStatus.RanToCompletion));
             await env.ParatextService.Received().GetProjectsAsync(Arg.Any<UserSecret>());
             await env.ParatextSyncRunner.Received().RunAsync("project01", "user01", Arg.Any<bool>());
+
+            env.ProgramLogger.Received().Log(Arg.Is<string>((string message) =>
+                message.Contains("All sync tasks finished with a claimed Task status of Completed Successfully")));
+            env.ProgramLogger.DidNotReceive().Log(Arg.Is<string>((string message) =>
+                message.Contains("There was a problem with one or more synchronization tasks")));
+        }
+
+        [Test]
+        public async Task Sync_ThrowReported()
+        {
+            var env = new TestEnvironment();
+            Task syncTask = null;
+            env.ParatextSyncRunner.RunAsync("project01", "user01", Arg.Any<bool>()).Returns((callInfo) =>
+            {
+                syncTask = Task.Run(() =>
+                {
+                    Task.Delay(10000).Wait();
+                    throw new Exception("Everything broke.");
+                });
+                return syncTask;
+            });
+            Assert.That(syncTask?.Status, Is.Not.EqualTo(TaskStatus.RanToCompletion));
+
+            await env.syncAll.SynchronizeAllProjectsAsync(env.WebHost, true);
+
+            // Exceptions thrown from the synchronization are reported.
+            Assert.That(syncTask?.Status, Is.EqualTo(TaskStatus.Faulted));
+            env.ProgramLogger.Received().Log(Arg.Is<string>((string message) =>
+                message.Contains("There was a problem with one or more synchronization tasks")));
+            env.ProgramLogger.Received().Log(Arg.Is<string>((string message) =>
+                message.Contains("One or more sync tasks did not complete successfully")));
         }
 
         private class TestEnvironment
         {
-            public SyncAll syncAll = new SyncAll(new ProgramLogger(123));
+            public SyncAll syncAll;
 
             public TestEnvironment()
             {
@@ -80,12 +113,16 @@ namespace PtdaSyncAll
 
                 ParatextSyncRunner = Substitute.For<IParatextSyncRunner>();
                 serviceProvider.GetService<IParatextSyncRunner>().Returns(ParatextSyncRunner);
+
+                ProgramLogger = Substitute.For<IProgramLogger>();
+                syncAll = new SyncAll(ProgramLogger);
             }
 
             public IWebHost WebHost { get; }
             public IParatextService ParatextService { get; }
             public SFMemoryRealtimeService RealtimeService { get; }
             public IParatextSyncRunner ParatextSyncRunner { get; }
+            public IProgramLogger ProgramLogger { get; }
 
             // The SetupSFData method was copied from ParatextSyncRunnerTests.cs and trimmed.
             public void SetupSFData(bool translationSuggestionsEnabled, bool checkingEnabled)
