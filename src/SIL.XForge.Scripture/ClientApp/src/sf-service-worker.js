bd9686fb6fc368fd061171034708b1b9d9abb0b9
nigel-wells <nigel-wells@hotmail.com>
2020-08-26 10:40:02 +1200

    Removed console logs from service worker

--- src/SIL.XForge.Scripture/ClientApp/src/sf-service-worker.js
+++ src/SIL.XForge.Scripture/ClientApp/src/sf-service-worker.js
@@ -7,9 +7,7 @@ self.addEventListener('install', async event => {
 
 self.addEventListener('fetch', event => {
   const path = event.request.url.substring(event.request.url.indexOf('/', event.request.url.indexOf('://') + 3) + 1);
-  console.log('fetch5', event.request.destination, path, event.request);
   if (event.request.mode === 'navigate' && path === '') {
-    console.log('fetch something', event);
     event.respondWith(
       (async () => {
         try {
