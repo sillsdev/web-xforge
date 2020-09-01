4eb608b60627af23599e4f94daa4adc1618fbcfe
Nathaniel Paulus <nateowami@gmail.com>
2020-08-31 22:46:03 -0400

    Fix a test in checking component due to broken selector

--- src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/checking.component.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/checking.component.spec.ts
@@ -1612,7 +1612,7 @@ class TestEnvironment {
   }
 
   get selectVersesButton(): DebugElement {
-    return this.fixture.debugElement.query(By.css('.answer-select-text button[primary]'));
+    return this.fixture.debugElement.query(By.css('.answer-select-text button[unelevated]'));
   }
 
   get showUnreadAnswersButton(): DebugElement {
