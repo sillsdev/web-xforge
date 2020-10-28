291b07f13944364a8a8622ea189784fa6dc230ab
Peter Chapman <peter@conglomo.co.nz>
2020-10-28 15:59:40 +1300

    Partial work on model changes

--- src/SIL.XForge.Scripture/Models/TextInfo.cs
+++ src/SIL.XForge.Scripture/Models/TextInfo.cs
@@ -19,9 +19,6 @@ namespace SIL.XForge.Scripture.Models
                 case TextType.Target:
                     textTypeDir = "target";
                     break;
-                case TextType.Source:
-                    textTypeDir = "source";
-                    break;
                 default:
                     throw new InvalidEnumArgumentException(nameof(textType), (int)textType, typeof(TextType));
             }
@@ -35,6 +32,5 @@ namespace SIL.XForge.Scripture.Models
         public bool HasSource { get; set; }
         public List<Chapter> Chapters { get; set; } = new List<Chapter>();
         public Dictionary<string, string> Permissions { get; set; } = new Dictionary<string, string>();
-        public Dictionary<string, string> SourcePermissions { get; set; } = new Dictionary<string, string>();
     }
 }
