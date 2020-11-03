2bdc09d8cb806b18872ad86aa30da88a909d0702
Peter Chapman <peter@conglomo.co.nz>
2020-11-04 12:46:12 +1300

    Finished most of the new split source/target code

--- src/SIL.XForge.Scripture/Services/ISFProjectService.cs
+++ src/SIL.XForge.Scripture/Services/ISFProjectService.cs
@@ -8,6 +8,7 @@ namespace SIL.XForge.Scripture.Services
     public interface ISFProjectService : IProjectService
     {
         Task<string> CreateProjectAsync(string curUserId, SFProjectCreateSettings settings);
+        Task<string> CreateResourceProjectAsync(string curUserId, string paratextId);
         Task DeleteProjectAsync(string curUserId, string projectId);
         Task UpdateSettingsAsync(string curUserId, string projectId, SFProjectSettings settings);
         Task AddTranslateMetricsAsync(string curUserId, string projectId, TranslateMetrics metrics);
