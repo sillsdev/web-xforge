4e67d955ea76b2c0bbf46a4304d5dec44e857b7e
Peter Chapman <peter@conglomo.co.nz>
2020-11-03 14:17:17 +1300

    Realtime server updates for resource permissions

--- src/RealtimeServer/common/services/user-migrations.ts
+++ src/RealtimeServer/common/services/user-migrations.ts
@@ -1,3 +1,26 @@
-import { MigrationConstructor } from '../migration';
+import { Doc, Op, RawOp } from 'sharedb/lib/client';
+import { submitMigrationOp } from '../../common/realtime-server';
+import { Migration, MigrationConstructor } from '../migration';
 
-export const USER_MIGRATIONS: MigrationConstructor[] = [];
+class UserMigration1 implements Migration {
+  static readonly VERSION = 1;
+
+  async migrateDoc(doc: Doc): Promise<void> {
+    const ops: Op[] = [];
+    for (const key in doc.data.sites) {
+      if (doc.data.sites[key].resources === undefined) {
+        const resources: string[] = [];
+        ops.push({ p: ['sites', key, 'resources'], oi: resources });
+      }
+    }
+    if (ops.length > 0) {
+      await submitMigrationOp(UserMigration1.VERSION, doc, ops);
+    }
+  }
+
+  migrateOp(_op: RawOp): void {
+    // do nothing
+  }
+}
+
+export const USER_MIGRATIONS: MigrationConstructor[] = [UserMigration1];
