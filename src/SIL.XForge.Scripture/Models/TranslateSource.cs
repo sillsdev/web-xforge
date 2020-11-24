48e7b65c266e559d584a3b5933c3882855625274
Peter Chapman <peter@conglomo.co.nz>
2020-11-24 16:37:25 +1300

    Source ParatextId requires TranslationSuggestions

--- src/SIL.XForge.Scripture/Models/TranslateSource.cs
+++ src/SIL.XForge.Scripture/Models/TranslateSource.cs
@@ -4,7 +4,26 @@ namespace SIL.XForge.Scripture.Models
 {
     public class TranslateSource
     {
+        /// <summary>
+        /// Gets or sets the paratext identifier.
+        /// </summary>
+        /// <value>
+        /// The paratext identifier.
+        /// </value>
+        /// <remarks>
+        /// Only use this if <see cref="TranslateConfig.TranslationSuggestionsEnabled" /> is set to <c>true.</c>
+        /// </remarks>
         public string ParatextId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the project reference.
+        /// </summary>
+        /// <value>
+        /// The project reference.
+        /// </value>
+        /// <remarks>
+        /// Only use this if <see cref="TranslateConfig.TranslationSuggestionsEnabled" /> is set to <c>true.</c>
+        /// </remarks>
         public string ProjectRef { get; set; }
         public string Name { get; set; }
         public string ShortName { get; set; }
