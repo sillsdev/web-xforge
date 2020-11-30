2f6529bb0a979ef7ac9f7f119803acdb480ec591
Peter Chapman <peter@conglomo.co.nz>
2020-12-01 07:49:25 +1300

    Split migration so source-target branch can merge

--- src/RealtimeServer/scriptureforge/services/sf-project-migrations.spec.ts
+++ src/RealtimeServer/scriptureforge/services/sf-project-migrations.spec.ts
@@ -54,6 +54,32 @@ describe('SFProjectMigrations', () => {
       expect(projectDoc.data.texts[0].permissions['user03']).toBe(TextInfoPermission.Read);
       expect(projectDoc.data.texts[0].permissions['user04']).toBe(TextInfoPermission.Read);
       expect(Object.keys(projectDoc.data.texts[0].permissions).length).toBe(4);
+      expect(projectDoc.data.texts[1].permissions['user01']).toBe(TextInfoPermission.Write);
+      expect(projectDoc.data.texts[1].permissions['user02']).toBe(TextInfoPermission.Write);
+      expect(projectDoc.data.texts[1].permissions['user03']).toBe(TextInfoPermission.Read);
+      expect(projectDoc.data.texts[1].permissions['user04']).toBe(TextInfoPermission.Read);
+      expect(Object.keys(projectDoc.data.texts[1].permissions).length).toBe(4);
+    });
+  });
+  describe('version 3', () => {
+    it('migrates docs', async () => {
+      const env = new TestEnvironment(1);
+      const conn = env.server.connect();
+      await createDoc(conn, SF_PROJECTS_COLLECTION, 'project01', {
+        texts: [
+          { bookNum: 40, chapters: [{ number: 1 }, { number: 2, isValid: true }], hasSource: true },
+          { bookNum: 41, chapters: [{ number: 1 }, { number: 2, isValid: true }], hasSource: false }
+        ],
+        userRoles: {
+          user01: SFProjectRole.ParatextAdministrator,
+          user02: SFProjectRole.ParatextTranslator,
+          user03: SFProjectRole.ParatextConsultant,
+          user04: SFProjectRole.ParatextObserver
+        }
+      });
+      await env.server.migrateIfNecessary();
+
+      const projectDoc = await fetchDoc(conn, SF_PROJECTS_COLLECTION, 'project01');
       expect(projectDoc.data.texts[0].chapters[0].permissions['user01']).toBe(TextInfoPermission.Write);
       expect(projectDoc.data.texts[0].chapters[0].permissions['user02']).toBe(TextInfoPermission.Write);
       expect(projectDoc.data.texts[0].chapters[0].permissions['user03']).toBe(TextInfoPermission.Read);
@@ -63,11 +89,6 @@ describe('SFProjectMigrations', () => {
       expect(projectDoc.data.texts[0].chapters[1].permissions['user02']).toBe(TextInfoPermission.Write);
       expect(projectDoc.data.texts[0].chapters[1].permissions['user03']).toBe(TextInfoPermission.Read);
       expect(projectDoc.data.texts[0].chapters[1].permissions['user04']).toBe(TextInfoPermission.Read);
-      expect(Object.keys(projectDoc.data.texts[0].chapters[1].permissions).length).toBe(4);
-      expect(projectDoc.data.texts[1].permissions['user01']).toBe(TextInfoPermission.Write);
-      expect(projectDoc.data.texts[1].permissions['user02']).toBe(TextInfoPermission.Write);
-      expect(projectDoc.data.texts[1].permissions['user03']).toBe(TextInfoPermission.Read);
-      expect(projectDoc.data.texts[1].permissions['user04']).toBe(TextInfoPermission.Read);
       expect(Object.keys(projectDoc.data.texts[1].permissions).length).toBe(4);
       expect(projectDoc.data.texts[1].chapters[0].permissions['user01']).toBe(TextInfoPermission.Write);
       expect(projectDoc.data.texts[1].chapters[0].permissions['user02']).toBe(TextInfoPermission.Write);
