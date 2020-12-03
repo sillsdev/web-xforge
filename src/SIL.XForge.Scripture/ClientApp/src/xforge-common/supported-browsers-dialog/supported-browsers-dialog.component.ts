98837422456c3abd8a872b3ac1656496fe59cee0
Raymond Luong <raymond_luong@sil.org>
2020-12-03 12:00:50 -0700

    SF-1148 Improve message when audio recording is not supported
    * iOS devices specifically mention using Safari

--- src/SIL.XForge.Scripture/ClientApp/src/xforge-common/supported-browsers-dialog/supported-browsers-dialog.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/supported-browsers-dialog/supported-browsers-dialog.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
-import { browserLinks } from 'xforge-common/utils';
+import { MDC_DIALOG_DATA } from '@angular-mdc/web';
+import { Component, Inject } from '@angular/core';
+import { browserLinks, isIosDevice } from 'xforge-common/utils';
+
+export enum BrowserIssue {
+  upgrade = 'upgrade_chrome_firefox',
+  audioRecording = 'audio_recording_not_supported'
+}
 
 @Component({
   selector: 'app-supported-browsers-dialog',
   templateUrl: './supported-browsers-dialog.component.html'
 })
 export class SupportedBrowsersDialogComponent {
-  constructor() {}
+  constructor(@Inject(MDC_DIALOG_DATA) public data: BrowserIssue) {}
 
   get browserLinks() {
     return browserLinks();
+  }
+
+  get audioRecordingNotSupported(): boolean {
+    return this.data === BrowserIssue.audioRecording && isIosDevice();
+  }
+
+  get iosRecordingNotSupported(): boolean {
+    return this.data === BrowserIssue.audioRecording && !isIosDevice();
+  }
+
+  get upgradeChromeFirefox(): boolean {
+    return this.data === BrowserIssue.upgrade && !isIosDevice();
+  }
+
+  get upgradeSafari(): boolean {
+    return this.data === BrowserIssue.upgrade && isIosDevice();
   }
 }
