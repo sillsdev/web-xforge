fe1b7ec27ca934e457dd8504c174026aeb95b4cc
nigel-wells <nigel-wells@hotmail.com>
2021-03-11 11:52:21 +1300

    SF-1201 - Allow inviting users from the translate app - Created a new translate observer role - Added role selection to the share component for admins - Added share button to the translate app for admins

--- src/SIL.XForge.Scripture/ClientApp/src/app/core/models/sf-project-role-info.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/core/models/sf-project-role-info.ts
@@ -1,15 +1,23 @@
-import { SFProjectRole } from 'realtime-server/lib/scriptureforge/models/sf-project-role';
+import { hasTranslateRole, SFProjectRole } from 'realtime-server/lib/scriptureforge/models/sf-project-role';
 import { ProjectRoleInfo } from 'xforge-common/models/project-role-info';
 
 export const SF_PROJECT_ROLES: ProjectRoleInfo[] = [
-  { role: SFProjectRole.ParatextAdministrator, displayName: 'Administrator' },
-  { role: SFProjectRole.ParatextTranslator, displayName: 'Translator' },
-  { role: SFProjectRole.ParatextConsultant, displayName: 'Consultant/Reviewer/Archivist/Typesetter' },
-  { role: SFProjectRole.ParatextObserver, displayName: 'Observer' },
-  { role: SFProjectRole.Reviewer, displayName: 'Reviewer' },
-  { role: SFProjectRole.CommunityChecker, displayName: 'Community Checker' }
+  { role: SFProjectRole.ParatextAdministrator, displayName: 'Administrator', canBeShared: false },
+  { role: SFProjectRole.ParatextTranslator, displayName: 'Translator', canBeShared: false },
+  {
+    role: SFProjectRole.ParatextConsultant,
+    displayName: 'Consultant/Reviewer/Archivist/Typesetter',
+    canBeShared: false
+  },
+  { role: SFProjectRole.ParatextObserver, displayName: 'Observer', canBeShared: false },
+  { role: SFProjectRole.Reviewer, displayName: 'Reviewer', canBeShared: true },
+  { role: SFProjectRole.CommunityChecker, displayName: 'Community Checker', canBeShared: true },
+  { role: SFProjectRole.Observer, displayName: 'View Translation', canBeShared: true }
 ];
 
 export function canAccessTranslateApp(role?: SFProjectRole) {
-  return role != null && role !== SFProjectRole.CommunityChecker;
+  return hasTranslateRole(role);
 }
+
+export const SF_DEFAULT_SHARE_ROLE: SFProjectRole = SFProjectRole.CommunityChecker;
+export const SF_DEFAULT_TRANSLATE_SHARE_ROLE: SFProjectRole = SFProjectRole.Observer;
