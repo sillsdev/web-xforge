d203415ec4494496f2d2ff6affcaad02e1400e98
Raymond Luong <raymond_luong@sil.org>
2021-03-17 16:41:43 -0600

    SF-1194 Show Paratext note icon in translate app

--- src/SIL.XForge.Scripture/Models/ParatextNote.cs
+++ src/SIL.XForge.Scripture/Models/ParatextNote.cs
@@ -8,7 +8,6 @@ namespace SIL.XForge.Scripture.Models
         public string ThreadId { get; set; }
         public string ParatextUser { get; set; }
         public string Content { get; set; }
-        public string Language { get; set; }
         public string ExtUserId { get; set; }
         public int VersionNumber { get; set; }
         public bool Deleted { get; set; }
