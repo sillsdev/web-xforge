8749c92da69ccb2f57017464025d9761209c4f18
nigel-wells <nigel-wells@hotmail.com>
2021-03-23 17:03:00 +1300

    SF-1194 - Show Paratext note icon in translate app - Initial commit

---
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/note-dialog/note-dialog.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NoteDialogComponent } from './note-dialog.component';
+
+describe('NoteDialogComponent', () => {
+  let component: NoteDialogComponent;
+  let fixture: ComponentFixture<NoteDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NoteDialogComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NoteDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
