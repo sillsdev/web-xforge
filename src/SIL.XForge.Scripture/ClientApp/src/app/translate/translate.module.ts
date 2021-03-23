05cceb91035afd2a62b7b023440bc71630f5061f
nigel-wells <nigel-wells@hotmail.com>
2021-03-23 17:03:00 +1300

    SF-1194 - Show Paratext note icon in translate app - Initial commit

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/translate.module.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/translate.module.ts
@@ -4,13 +4,20 @@ import { TranslocoModule } from '@ngneat/transloco';
 import { UICommonModule } from 'xforge-common/ui-common.module';
 import { SharedModule } from '../shared/shared.module';
 import { EditorComponent } from './editor/editor.component';
+import { NoteDialogComponent } from './editor/note-dialog/note-dialog.component';
 import { SuggestionsSettingsDialogComponent } from './editor/suggestions-settings-dialog.component';
 import { SuggestionsComponent } from './editor/suggestions.component';
 import { TranslateOverviewComponent } from './translate-overview/translate-overview.component';
 import { TranslateRoutingModule } from './translate-routing.module';
 
 @NgModule({
-  declarations: [EditorComponent, SuggestionsComponent, TranslateOverviewComponent, SuggestionsSettingsDialogComponent],
+  declarations: [
+    EditorComponent,
+    SuggestionsComponent,
+    TranslateOverviewComponent,
+    SuggestionsSettingsDialogComponent,
+    NoteDialogComponent
+  ],
   imports: [TranslateRoutingModule, CommonModule, SharedModule, UICommonModule, TranslocoModule]
 })
 export class TranslateModule {}
