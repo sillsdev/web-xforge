79aa4e98c5c0bacdaf4e253bb3ee52fb49853983
nigel-wells <nigel-wells@hotmail.com>
2021-03-24 14:50:55 +1300

    SF-1238 - Unauthorized error after 2 hours - Adjusted http intercept handler to ensure the user is authenticated before adding access token to the header

--- src/SIL.XForge.Scripture/ClientApp/src/xforge-common/auth-http-interceptor.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/auth-http-interceptor.ts
@@ -1,6 +1,6 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable, Injector } from '@angular/core';
-import { Observable } from 'rxjs';
+import { from, Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
 const AUTH_APIS = ['paratext-api', 'machine-api', 'command-api'];
@@ -15,14 +15,19 @@ export class AuthHttpInterceptor implements HttpInterceptor {
     if (AUTH_APIS.every(api => !req.url.startsWith(api))) {
       return next.handle(req);
     }
+    return from(this.handle(req, next));
+  }
 
+  async handle(req: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
     if (this.authService == null) {
       this.authService = this.injector.get<AuthService>(AuthService);
     }
-
+    // Make sure the user is authenticated with a valid access token
+    await this.authService.isAuthenticated();
+    // Add access token to the request header
     const authReq = req.clone({
       headers: req.headers.set('Authorization', 'Bearer ' + this.authService.accessToken)
     });
-    return next.handle(authReq);
+    return next.handle(authReq).toPromise();
   }
 }
