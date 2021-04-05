b6e8082bb3d609adab5bd3f5909f7513b18bc5d7
Raymond Luong <raymond_luong@sil.org>
2021-04-05 15:14:38 -0600

    SF-1231 Handle when using a share link while offline
    * separate out scenarios when activating projects route
    * if offline show a dialog message
    * navigate to the last project upon closing the dialog

--- src/SIL.XForge.Scripture/ClientApp/src/app/project/project.component.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/project/project.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { ActivatedRoute, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { TranslocoService } from '@ngneat/transloco';
+import { SystemRole } from 'realtime-server/lib/common/models/system-role';
+import { User } from 'realtime-server/lib/common/models/user';
 import { CheckingShareLevel } from 'realtime-server/lib/scriptureforge/models/checking-config';
 import { SFProject } from 'realtime-server/lib/scriptureforge/models/sf-project';
 import { SFProjectRole } from 'realtime-server/lib/scriptureforge/models/sf-project-role';
@@ -8,10 +10,12 @@ import {
   getSFProjectUserConfigDocId,
   SFProjectUserConfig
 } from 'realtime-server/lib/scriptureforge/models/sf-project-user-config';
-import { of } from 'rxjs';
+import { BehaviorSubject, of } from 'rxjs';
 import { anything, deepEqual, mock, verify, when } from 'ts-mockito';
 import { CommandError, CommandErrorCode } from 'xforge-common/command.service';
+import { UserDoc } from 'xforge-common/models/user-doc';
 import { NoticeService } from 'xforge-common/notice.service';
+import { PwaService } from 'xforge-common/pwa.service';
 import { TestRealtimeModule } from 'xforge-common/test-realtime.module';
 import { TestRealtimeService } from 'xforge-common/test-realtime.service';
 import { configureTestingModule } from 'xforge-common/test-utils';
@@ -29,6 +33,7 @@ const mockedRouter = mock(Router);
 const mockedSFProjectService = mock(SFProjectService);
 const mockedNoticeService = mock(NoticeService);
 const mockedTranslocoService = mock(TranslocoService);
+const mockedPwaService = mock(PwaService);
 
 describe('ProjectComponent', () => {
   configureTestingModule(() => ({
@@ -40,81 +45,88 @@ describe('ProjectComponent', () => {
       { provide: Router, useMock: mockedRouter },
       { provide: SFProjectService, useMock: mockedSFProjectService },
       { provide: NoticeService, useMock: mockedNoticeService },
-      { provide: TranslocoService, useMock: mockedTranslocoService }
+      { provide: TranslocoService, useMock: mockedTranslocoService },
+      { provide: PwaService, useMock: mockedPwaService }
     ]
   }));
 
   it('navigate to last text', fakeAsync(() => {
     const env = new TestEnvironment();
-    env.setProjectData({ selectedTask: 'translate', selectedBooknum: 41 });
+    env.setProjectData({ selectedTask: 'translate', selectedBooknum: 41, memberProjects: ['project01'] });
     env.fixture.detectChanges();
     tick();
 
     verify(mockedSFProjectService.onlineCheckLinkSharing('project01')).never();
-    verify(mockedRouter.navigate(deepEqual(['./', 'translate', 'MRK']), anything())).once();
+    verify(mockedRouter.navigate(deepEqual(['projects', 'project01', 'translate', 'MRK']), anything())).once();
     expect().nothing();
   }));
 
   it('navigate to first text when no last selected text', fakeAsync(() => {
     const env = new TestEnvironment();
-    env.setProjectData();
+    env.setProjectData({ memberProjects: ['project01'] });
     env.fixture.detectChanges();
     tick();
 
     verify(mockedSFProjectService.onlineCheckLinkSharing('project01')).never();
-    verify(mockedRouter.navigate(deepEqual(['./', 'translate', 'MAT']), anything())).once();
+    verify(mockedRouter.navigate(deepEqual(['projects', 'project01', 'translate', 'MAT']), anything())).once();
     expect().nothing();
   }));
 
   it('navigate to checking tool if a checker and no last selected text', fakeAsync(() => {
     const env = new TestEnvironment();
-    env.setProjectData({ role: SFProjectRole.CommunityChecker });
+    env.setProjectData({ role: SFProjectRole.CommunityChecker, memberProjects: ['project01'] });
     env.fixture.detectChanges();
     tick();
 
     verify(mockedSFProjectService.onlineCheckLinkSharing('project01')).never();
-    verify(mockedRouter.navigate(deepEqual(['./', 'checking', 'ALL']), anything())).once();
+    verify(mockedRouter.navigate(deepEqual(['projects', 'project01', 'checking', 'ALL']), anything())).once();
     expect().nothing();
   }));
 
   it('navigates to last checking question if question stored but bookNum is null', fakeAsync(() => {
     const env = new TestEnvironment();
-    env.setProjectData({ selectedTask: 'checking' });
+    env.setProjectData({ selectedTask: 'checking', memberProjects: ['project01'] });
     env.fixture.detectChanges();
     tick();
 
-    verify(mockedRouter.navigate(deepEqual(['./', 'checking', 'ALL']), anything())).once();
+    verify(mockedRouter.navigate(deepEqual(['projects', 'project01', 'checking', 'ALL']), anything())).once();
     expect().nothing();
   }));
 
   it('navigates to last checking book if the last book was saved', fakeAsync(() => {
     const env = new TestEnvironment();
-    env.setProjectData({ selectedTask: 'checking', selectedBooknum: 41 });
+    env.setProjectData({ selectedTask: 'checking', selectedBooknum: 41, memberProjects: ['project01'] });
     env.fixture.detectChanges();
     tick();
 
-    verify(mockedRouter.navigate(deepEqual(['./', 'checking', 'MRK']), anything())).once();
+    verify(mockedRouter.navigate(deepEqual(['projects', 'project01', 'checking', 'MRK']), anything())).once();
     expect().nothing();
   }));
 
   it('navigate to overview when no texts', fakeAsync(() => {
     const env = new TestEnvironment();
-    env.setProjectData({ hasTexts: false });
+    env.setProjectData({ hasTexts: false, memberProjects: ['project01'] });
     env.fixture.detectChanges();
     tick();
 
     verify(mockedSFProjectService.onlineCheckLinkSharing('project01')).never();
-    verify(mockedRouter.navigate(deepEqual(['./', 'translate']), anything())).once();
+    verify(mockedRouter.navigate(deepEqual(['projects', 'project01', 'translate']), anything())).once();
     expect().nothing();
   }));
 
   it('if checking is disabled, navigate to translate app, even if last location was in checking app', fakeAsync(() => {
     const env = new TestEnvironment();
-    env.setProjectData({ selectedTask: 'checking', selectedBooknum: 41, hasTexts: true, checkingEnabled: false });
+    env.setProjectData({
+      selectedTask: 'checking',
+      selectedBooknum: 41,
+      hasTexts: true,
+      checkingEnabled: false,
+      memberProjects: ['project01']
+    });
     env.fixture.detectChanges();
     tick();
 
-    verify(mockedRouter.navigate(deepEqual(['./', 'translate', 'MAT']), anything())).once();
+    verify(mockedRouter.navigate(deepEqual(['projects', 'project01', 'translate', 'MAT']), anything())).once();
     expect().nothing();
   }));
 
@@ -128,18 +140,6 @@ describe('ProjectComponent', () => {
     expect().nothing();
   }));
 
-  it('check sharing link', fakeAsync(() => {
-    const env = new TestEnvironment();
-    env.setProjectData({ selectedTask: 'translate', selectedBooknum: 41 });
-    env.setLinkSharing(true);
-    env.fixture.detectChanges();
-    tick();
-
-    verify(mockedSFProjectService.onlineCheckLinkSharing('project01', undefined)).once();
-    verify(mockedRouter.navigate(deepEqual(['./', 'translate', 'MRK']), anything())).once();
-    expect().nothing();
-  }));
-
   it('check sharing link passes shareKey', fakeAsync(() => {
     const env = new TestEnvironment();
     env.setProjectData({ selectedTask: 'translate', selectedBooknum: 41 });
@@ -148,6 +148,39 @@ describe('ProjectComponent', () => {
     tick();
 
     verify(mockedSFProjectService.onlineCheckLinkSharing('project01', 'secret123')).once();
+    expect().nothing();
+  }));
+
+  it('check sharing link skipped offline', fakeAsync(() => {
+    when(mockedNoticeService.showMessageDialog(anything())).thenResolve();
+    const env = new TestEnvironment();
+    env.onlineStatus = false;
+    env.setProjectData({
+      selectedTask: 'translate',
+      selectedBooknum: 40,
+      memberProjects: ['project01'],
+      projectId: 'project02'
+    });
+    env.setLinkSharing(true, 'secret123');
+    env.fixture.detectChanges();
+    tick();
+    verify(mockedSFProjectService.onlineCheckLinkSharing(anything(), anything())).never();
+    verify(mockedNoticeService.showMessageDialog(anything())).once();
+    verify(mockedRouter.navigate(deepEqual(['projects', 'project01', 'translate', 'MAT']), anything())).once();
+    expect().nothing();
+  }));
+
+  it('check sharing link skipped offline and user not on project', fakeAsync(() => {
+    when(mockedNoticeService.showMessageDialog(anything())).thenResolve();
+    const env = new TestEnvironment();
+    env.onlineStatus = false;
+    env.setProjectData({ selectedTask: 'translate', projectId: 'project02' });
+    env.setLinkSharing(true, 'secret123');
+    env.fixture.detectChanges();
+    tick();
+    verify(mockedSFProjectService.onlineCheckLinkSharing(anything(), anything())).never();
+    verify(mockedNoticeService.showMessageDialog(anything())).once();
+    verify(mockedRouter.navigateByUrl('/projects', anything())).once();
     expect().nothing();
   }));
 
@@ -187,7 +220,7 @@ describe('ProjectComponent', () => {
     when(mockedSFProjectService.onlineCheckLinkSharing(projectId, undefined)).thenReject(
       new CommandError(CommandErrorCode.Forbidden, 'Forbidden')
     );
-    env.setProjectData({ selectedTask: 'translate', selectedBooknum: 41 });
+    env.setProjectData({ selectedTask: 'translate', selectedBooknum: 41, memberProjects: [projectId] });
     env.setLinkSharing(true);
     env.fixture.detectChanges();
     tick();
@@ -200,8 +233,8 @@ describe('ProjectComponent', () => {
 class TestEnvironment {
   readonly component: ProjectComponent;
   readonly fixture: ComponentFixture<ProjectComponent>;
-
   readonly realtimeService: TestRealtimeService = TestBed.inject<TestRealtimeService>(TestRealtimeService);
+  private readonly isOnline: BehaviorSubject<boolean>;
 
   constructor() {
     when(mockedActivatedRoute.params).thenReturn(of({ projectId: 'project01' }));
@@ -209,6 +242,10 @@ class TestEnvironment {
     snapshot.queryParams = { sharing: 'true' };
     when(mockedActivatedRoute.snapshot).thenReturn(snapshot);
     when(mockedUserService.currentUserId).thenReturn('user01');
+    when(mockedUserService.currentProjectId).thenReturn('project01');
+    when(mockedUserService.getCurrentUser()).thenCall(() =>
+      this.realtimeService.subscribe(UserDoc.COLLECTION, 'user01')
+    );
     when(mockedSFProjectService.onlineCheckLinkSharing('project01')).thenResolve();
     when(mockedSFProjectService.onlineCheckLinkSharing('project01', anything())).thenResolve();
     when(mockedNoticeService.showMessageDialog(anything())).thenResolve();
@@ -222,21 +259,32 @@ class TestEnvironment {
       this.realtimeService.subscribe(SFProjectDoc.COLLECTION, 'project01')
     );
     when(mockedTranslocoService.translate<string>(anything())).thenReturn('The project link is invalid.');
+    this.isOnline = new BehaviorSubject<boolean>(true);
+    when(mockedPwaService.onlineStatus).thenReturn(this.isOnline.asObservable());
     this.setLinkSharing(false);
 
     this.fixture = TestBed.createComponent(ProjectComponent);
     this.component = this.fixture.componentInstance;
   }
 
+  set onlineStatus(hasConnection: boolean) {
+    this.isOnline.next(hasConnection);
+  }
+
   setProjectData(
     args: {
+      projectId?: string;
       hasTexts?: boolean;
       selectedTask?: string;
       selectedBooknum?: number;
       role?: SFProjectRole;
       checkingEnabled?: boolean;
+      memberProjects?: string[];
     } = {}
   ): void {
+    if (args.projectId != null) {
+      when(mockedActivatedRoute.params).thenReturn(of({ projectId: args.projectId }));
+    }
     this.realtimeService.addSnapshot<SFProjectUserConfig>(SFProjectUserConfigDoc.COLLECTION, {
       id: getSFProjectUserConfigDocId('project01', 'user01'),
       data: {
@@ -291,7 +339,24 @@ class TestEnvironment {
                 }
               ]
             : [],
-        userRoles: { user01: args.role == null ? SFProjectRole.ParatextTranslator : args.role }
+        userRoles:
+          args.memberProjects == null
+            ? {}
+            : { user01: args.role == null ? SFProjectRole.ParatextTranslator : args.role }
+      }
+    });
+
+    this.realtimeService.addSnapshot<User>(UserDoc.COLLECTION, {
+      id: 'user01',
+      data: {
+        name: 'User 01',
+        email: 'user1@example.com',
+        role: SystemRole.User,
+        isDisplayNameConfirmed: true,
+        avatarUrl: '',
+        authId: 'auth01',
+        displayName: 'User 01',
+        sites: { sf: { projects: args.memberProjects ?? [] } }
       }
     });
   }
