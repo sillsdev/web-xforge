0ba6b7ffca529a756fdeacef7f15789958f3798c
marksvc <marksvc@gmail.com>
2021-04-13 16:23:17 -0600

    SF-1256 Hint to user about incognito or other migration help (#1015)
    * SF-1256 Hint to user about incognito or other migration help
    
    - Directing to community site rather than the email address instead or
    also, since the email address isn't working at present.
    
    * Fix internationalization of beta migration dialog (#1016)
    
    Co-authored-by: Nathaniel Paulus <nateowami@gmail.com>

--- src/SIL.XForge.Scripture/ClientApp/src/xforge-common/beta-migration/beta-migration-dialog/beta-migration-dialog.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/beta-migration/beta-migration-dialog/beta-migration-dialog.component.ts
@@ -3,6 +3,7 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { interval } from 'rxjs';
 import { takeWhile } from 'rxjs/operators';
 import { BetaMigrationMessage } from 'xforge-common/beta-migration/beta-migration.component';
+import { I18nService, TextAroundTemplate } from 'xforge-common/i18n.service';
 import { environment } from '../../../environments/environment';
 
 @Component({
@@ -18,7 +19,7 @@ export class BetaMigrationDialogComponent {
   private hasLoaded: boolean = false;
   private message: BetaMigrationMessage = { message: 'loading', progress: 0 };
 
-  constructor(private sanitizer: DomSanitizer) {
+  constructor(private sanitizer: DomSanitizer, private readonly i18n: I18nService) {
     this.betaUrl = this.migrationUrl;
     window.addEventListener('message', event => {
       if (event.origin !== environment.betaUrl) {
@@ -48,6 +49,10 @@ export class BetaMigrationDialogComponent {
 
   get progress(): number {
     return this.message.progress;
+  }
+
+  get recourseMessage(): TextAroundTemplate | undefined {
+    return this.i18n.translateTextAroundTemplateTags('beta_migration.recourse');
   }
 
   private get migrationUrl(): SafeResourceUrl {
