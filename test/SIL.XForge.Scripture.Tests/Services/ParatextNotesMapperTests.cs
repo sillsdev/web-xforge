fb0dba704530c38f5f9166e67ac4d81ee68d2b1c
Peter Chapman <peter@conglomo.co.nz>
2021-05-25 12:27:08 +1200

    Added more cancellation support and unit tests

--- test/SIL.XForge.Scripture.Tests/Services/ParatextNotesMapperTests.cs
+++ test/SIL.XForge.Scripture.Tests/Services/ParatextNotesMapperTests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using Microsoft.Extensions.Localization;
@@ -418,7 +419,7 @@ namespace SIL.XForge.Scripture.Services
             public async Task InitMapperAsync(bool includeSyncUsers, bool twoPtUsersOnProject)
             {
                 await Mapper.InitAsync(UserSecrets.Get("user01"), ProjectSecret(includeSyncUsers),
-                    ParatextUsersOnProject(twoPtUsersOnProject), "paratextId");
+                    ParatextUsersOnProject(twoPtUsersOnProject), "paratextId", CancellationToken.None);
             }
 
             public void AddData(string answerSyncUserId1, string answerSyncUserId2, string commentSyncUserId1,
@@ -491,7 +492,8 @@ namespace SIL.XForge.Scripture.Services
                 ptUserRoles["ptuser01"] = "pt_administrator";
                 if (twoPtUserOnProject)
                     ptUserRoles["ptuser03"] = "pt_translator";
-                ParatextService.GetProjectRolesAsync(Arg.Any<UserSecret>(), Arg.Any<string>()).Returns(ptUserRoles);
+                ParatextService.GetProjectRolesAsync(Arg.Any<UserSecret>(), Arg.Any<string>(),
+                    Arg.Any<CancellationToken>()).Returns(ptUserRoles);
             }
 
             private static SFProjectSecret ProjectSecret(bool includeSyncUsers)
