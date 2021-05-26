9f785cbffdbd4ee5ae981db64104b00c2ce0b40d
Peter Chapman <peter@conglomo.co.nz>
2021-05-26 18:09:23 +1200

    Implemented job deletion on sync cancellation

--- src/SIL.XForge.Scripture/Models/Sync.cs
+++ src/SIL.XForge.Scripture/Models/Sync.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace SIL.XForge.Scripture.Models
 {
@@ -14,5 +15,15 @@ namespace SIL.XForge.Scripture.Models
         /// </summary>
         public string SyncedToRepositoryVersion { get; set; }
         public bool? DataInSync { get; set; }
+        /// <summary>
+        /// The queued or active Hangfire Job Ids for the project.
+        /// </summary>
+        /// <value>
+        /// The Hangfire Job Ids.
+        /// </value>
+        /// <remarks>
+        /// The <see cref="List{T}.Count">Count</see> should correspond to <see cref="QueuedCount" />.
+        /// </remarks>
+        public List<string> JobIds { get; set; } = new List<string>();
     }
 }
