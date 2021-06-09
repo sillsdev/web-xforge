sillsdev/web-xforge

commit a87af728cf12e2dfc2c446038ad8b8513751a86b

Fix warnings when running Angular tests (#1056)
- Navigation triggered outside Angular zone
- Bugsnag.leaveBreadcrumb() was called before Bugsnag.start()
- run timeout outside of ngzone

src/SIL.XForge.Scripture/ClientApp/src/xforge-common/bugsnag.service.ts
@@ -1,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import Bugsnag, { BreadcrumbType, BrowserConfig, Client } from '@bugsnag/js';
+import { NodeConfig } from '@bugsnag/node';
+
+/**
+ * Wrapper around Bugsnag. This injectable service can be mocked to make dependencies of it testable.
+ */
+@Injectable()
+export class BugsnagService {
+  start(apiKeyOrOpts: string | BrowserConfig | NodeConfig): Client {
+    return Bugsnag.start(apiKeyOrOpts);
+  }
+
+  leaveBreadcrumb(message: string, metadata?: { [key: string]: any }, type?: BreadcrumbType): void {
+    Bugsnag.leaveBreadcrumb(message, metadata, type);
+  }
+
+  setUser(id?: string, email?: string, name?: string): void {
+    Bugsnag.setUser(id, email, name);
+  }
+}

src/SIL.XForge.Scripture/ClientApp/src/app/app.component.spec.ts
@@ -21,6 +21,7 @@ import { anyString, anything, instance, mock, verify, when } from 'ts-mockito';
 import { AuthService } from 'xforge-common/auth.service';
 import { AvatarTestingModule } from 'xforge-common/avatar/avatar-testing.module';
 import { BetaMigrationDialogComponent } from 'xforge-common/beta-migration/beta-migration-dialog/beta-migration-dialog.component';
+import { BugsnagService } from 'xforge-common/bugsnag.service';
 import { ErrorReportingService } from 'xforge-common/error-reporting.service';
 import { FileService } from 'xforge-common/file.service';
 import { LocationService } from 'xforge-common/location.service';
@@ -47,6 +48,7 @@ const mockedAuthService = mock(AuthService);
 const mockedUserService = mock(UserService);
 const mockedSFAdminAuthGuard = mock(SFAdminAuthGuard);
 const mockedSFProjectService = mock(SFProjectService);
+const mockedBugsnagService = mock(BugsnagService);
 const mockedCookieService = mock(CookieService);
 const mockedLocationService = mock(LocationService);
 const mockedNoticeService = mock(NoticeService);
@@ -90,6 +92,7 @@ describe('AppComponent', () => {
       { provide: UserService, useMock: mockedUserService },
       { provide: SFAdminAuthGuard, useMock: mockedSFAdminAuthGuard },
       { provide: SFProjectService, useMock: mockedSFProjectService },
+      { provide: BugsnagService, useMock: mockedBugsnagService },
       { provide: CookieService, useMock: mockedCookieService },
       { provide: LocationService, useMock: mockedLocationService },
       { provide: NoticeService, useMock: mockedNoticeService },
@@ -829,7 +832,7 @@ class TestEnvironment {
   }
 
   confirmProjectDeletedDialog() {
-    this.projectDeletedDialogRefAfterClosed$.next('close');
+    this.ngZone.run(() => this.projectDeletedDialogRefAfterClosed$.next('close'));
   }
 
   private addProject(projectId: string, userRoles: { [userRef: string]: string }, texts: TextInfo[]): void {

src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking-overview/checking-overview.component.spec.ts
@@ -1,6 +1,6 @@
 import { MdcDialog, MdcDialogModule, MdcDialogRef } from '@angular-mdc/web/dialog';
 import { Location } from '@angular/common';
-import { DebugElement, NgModule } from '@angular/core';
+import { DebugElement, NgModule, NgZone } from '@angular/core';
 import { ComponentFixture, fakeAsync, flush, TestBed, tick } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
@@ -28,6 +28,7 @@ import { Canon } from 'realtime-server/lib/esm/scriptureforge/scripture-utils/ca
 import { BehaviorSubject, of } from 'rxjs';
 import { anything, instance, mock, resetCalls, verify, when } from 'ts-mockito';
 import { AuthService } from 'xforge-common/auth.service';
+import { BugsnagService } from 'xforge-common/bugsnag.service';
 import { NoticeService } from 'xforge-common/notice.service';
 import { PwaService } from 'xforge-common/pwa.service';
 import { TestRealtimeModule } from 'xforge-common/test-realtime.module';
@@ -55,6 +56,7 @@ const mockedProjectService = mock(SFProjectService);
 const mockedUserService = mock(UserService);
 const mockedAuthService = mock(AuthService);
 const mockedQuestionDialogService = mock(QuestionDialogService);
+const mockedBugsnagService = mock(BugsnagService);
 const mockedCookieService = mock(CookieService);
 const mockedPwaService = mock(PwaService);
 
@@ -76,6 +78,7 @@ describe('CheckingOverviewComponent', () => {
       { provide: UserService, useMock: mockedUserService },
       { provide: AuthService, useMock: mockedAuthService },
       { provide: QuestionDialogService, useMock: mockedQuestionDialogService },
+      { provide: BugsnagService, useMock: mockedBugsnagService },
       { provide: CookieService, useMock: mockedCookieService },
       { provide: PwaService, useMock: mockedPwaService }
     ]
@@ -438,6 +441,7 @@ class TestEnvironment {
   fixture: ComponentFixture<CheckingOverviewComponent>;
   location: Location;
 
+  readonly ngZone: NgZone = TestBed.inject(NgZone);
   readonly mockedAnsweredDialogRef: MdcDialogRef<QuestionAnsweredDialogComponent> = mock(MdcDialogRef);
   readonly realtimeService: TestRealtimeService = TestBed.inject<TestRealtimeService>(TestRealtimeService);
 
@@ -861,8 +865,10 @@ class TestEnvironment {
   }
 
   setCheckingEnabled(isEnabled: boolean): void {
-    const projectDoc = this.realtimeService.get<SFProjectDoc>(SFProjectDoc.COLLECTION, 'project01');
-    projectDoc.submitJson0Op(op => op.set<boolean>(p => p.checkingConfig.checkingEnabled, isEnabled), false);
+    this.ngZone.run(() => {
+      const projectDoc = this.realtimeService.get<SFProjectDoc>(SFProjectDoc.COLLECTION, 'project01');
+      projectDoc.submitJson0Op(op => op.set<boolean>(p => p.checkingConfig.checkingEnabled, isEnabled), false);
+    });
     tick();
     this.fixture.detectChanges();
   }

src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/checking-answers/checking-owner/checking-owner.component.spec.ts
@@ -8,6 +8,7 @@ import { UserProfile } from 'realtime-server/lib/esm/common/models/user';
 import { anything, instance, mock, when } from 'ts-mockito';
 import { AuthService } from 'xforge-common/auth.service';
 import { AvatarTestingModule } from 'xforge-common/avatar/avatar-testing.module';
+import { BugsnagService } from 'xforge-common/bugsnag.service';
 import { UserProfileDoc } from 'xforge-common/models/user-profile-doc';
 import { TestRealtimeModule } from 'xforge-common/test-realtime.module';
 import { TestRealtimeService } from 'xforge-common/test-realtime.service';
@@ -85,6 +86,7 @@ class TestEnvironment {
 
   readonly mockedAuthService = mock(AuthService);
   readonly mockedAvatarService = mock(AvatarService);
+  readonly mockedBugsnagService = mock(BugsnagService);
   readonly mockedCookieService = mock(CookieService);
   readonly mockedTranslocoService = mock(TranslocoService);
   readonly mockedUserService = mock(UserService);
@@ -98,6 +100,7 @@ class TestEnvironment {
       providers: [
         { provide: AuthService, useFactory: () => instance(this.mockedAuthService) },
         { provide: AvatarService, useFactory: () => instance(this.mockedAvatarService) },
+        { provide: BugsnagService, useFactory: () => instance(this.mockedBugsnagService) },
         { provide: CookieService, useFactory: () => instance(this.mockedCookieService) },
         { provide: TranslocoService, useFactory: () => instance(this.mockedTranslocoService) },
         { provide: UserService, useFactory: () => instance(this.mockedUserService) }

src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/checking-audio-combined/checking-audio-combined.component.spec.ts
@@ -1,4 +1,4 @@
-import { DebugElement } from '@angular/core';
+import { DebugElement, NgZone } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { ngfModule } from 'angular-file';
@@ -85,6 +85,7 @@ describe('CheckingAudioCombinedComponent', () => {
 });
 
 class TestEnvironment {
+  readonly ngZone: NgZone = TestBed.inject(NgZone);
   readonly audioFile: string = 'test-audio-player.webm';
   readonly component: CheckingAudioCombinedComponent;
   readonly fixture: ComponentFixture<CheckingAudioCombinedComponent>;
@@ -141,7 +142,7 @@ class TestEnvironment {
   }
 
   async waitForRecorder(ms: number) {
-    await new Promise(resolve => setTimeout(resolve, ms));
+    await new Promise(resolve => this.ngZone.runOutsideAngular(() => setTimeout(resolve, ms)));
     this.fixture.detectChanges();
   }
 }

src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/checking-audio-player/checking-audio-player.component.spec.ts
@@ -1,4 +1,4 @@
-import { Component, DebugElement, ViewChild } from '@angular/core';
+import { Component, DebugElement, NgZone, ViewChild } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { of } from 'rxjs';
@@ -11,12 +11,13 @@ import { AudioStatus, AudioTimePipe, CheckingAudioPlayerComponent } from './chec
 describe('CheckingAudioPlayerComponent', () => {
   const audioFile = 'test-audio-player.webm';
   const audioFileB = 'test-audio-player-b.webm';
+  const playerLoadTimeMs = 1000;
 
   it('should be created', async () => {
     const template =
       '<app-checking-audio-player #player1 id="player1" source="' + audioFile + '"></app-checking-audio-player>';
     const env = new TestEnvironment(template);
-    await env.waitForPlayer(1000);
+    await env.waitForPlayer(playerLoadTimeMs);
     expect(env.component.player1.enabled).toBe(true);
     expect(env.duration).toBe('0:05');
     expect(env.currentTime).toBe('0:00');
@@ -26,7 +27,7 @@ describe('CheckingAudioPlayerComponent', () => {
     const template =
       '<app-checking-audio-player #player1 id="player1" source="' + audioFile + '"></app-checking-audio-player>';
     const env = new TestEnvironment(template);
-    await env.waitForPlayer(1000);
+    await env.waitForPlayer(playerLoadTimeMs);
     env.clickButton(env.playButton(1));
     await env.waitForPlayer(1500);
     env.fixture.detectChanges();
@@ -43,7 +44,7 @@ describe('CheckingAudioPlayerComponent', () => {
       audioFileB +
       '"></app-checking-audio-player>';
     const env = new TestEnvironment(template);
-    await env.waitForPlayer(1000);
+    await env.waitForPlayer(playerLoadTimeMs);
     env.clickButton(env.playButton(1));
     await env.waitForPlayer(500);
     expect(env.component.player1.isPlaying).toBe(true);
@@ -58,7 +59,7 @@ describe('CheckingAudioPlayerComponent', () => {
     const template =
       '<app-checking-audio-player #player1 id="player1" source="' + audioFile + '"></app-checking-audio-player>';
     const env = new TestEnvironment(template);
-    await env.waitForPlayer(1000);
+    await env.waitForPlayer(playerLoadTimeMs);
     expect(env.component.player1.hasSource).toBe(true);
     env.component.player1.source = '';
     expect(env.component.player1.hasSource).toBe(false);
@@ -67,7 +68,7 @@ describe('CheckingAudioPlayerComponent', () => {
   it('it notifies the user when audio is unavailable offline', async () => {
     const template = `<app-checking-audio-player #player1 source="https://"></app-checking-audio-player>`;
     const env = new TestEnvironment(template, false);
-    await env.waitForPlayer(1000);
+    await env.waitForPlayer(playerLoadTimeMs);
     expect(env.component.player1.hasSource).toBe(true);
     expect(env.audioNotAvailableMessage).not.toBeNull();
   });
@@ -75,7 +76,7 @@ describe('CheckingAudioPlayerComponent', () => {
   it('it can play blobs even when offline', async () => {
     const template = `<app-checking-audio-player #player1 source="${audioFile}"></app-checking-audio-player>`;
     const env = new TestEnvironment(template, false);
-    await env.waitForPlayer(1000);
+    await env.waitForPlayer(playerLoadTimeMs);
     expect(env.component.player1.hasSource).toBe(true);
     expect(env.audioNotAvailableMessage).toBeNull();
   });
@@ -83,7 +84,7 @@ describe('CheckingAudioPlayerComponent', () => {
   it('it can play preloaded audio when offline', async () => {
     const template = `<app-checking-audio-player #player1 source="${audioFile}"></app-checking-audio-player>`;
     const env = new TestEnvironment(template, false);
-    await env.waitForPlayer(1000);
+    await env.waitForPlayer(playerLoadTimeMs);
     expect(env.component.player1.hasSource).toBe(true);
     // The browser is online, but the component thinks it is offline. This simulates the scenario where audio data is
     // already loaded, but the browser is offline.
@@ -93,7 +94,7 @@ describe('CheckingAudioPlayerComponent', () => {
   it('show error tooltip if error loading audio while online and the file does not exist', async () => {
     const template = `<app-checking-audio-player #player1 source="audio-file-not-exists.webm"></app-checking-audio-player>`;
     const env = new TestEnvironment(template, true);
-    await env.waitForPlayer(1000);
+    await env.waitForPlayer(playerLoadTimeMs);
     expect(env.audioNotAvailableMessage).not.toBeNull();
     expect(env.audioNotAvailableMessage.query(By.css('#error-load'))).not.toBeNull();
     expect(env.component.player1.audioStatus).toEqual(AudioStatus.Unavailable);
@@ -103,7 +104,7 @@ describe('CheckingAudioPlayerComponent', () => {
   it('show error tooltip if error loading audio while offline and the file does not exist', async () => {
     const template = `<app-checking-audio-player #player1 source="audio-file-not-exists.webm"></app-checking-audio-player>`;
     const env = new TestEnvironment(template, false);
-    await env.waitForPlayer(1000);
+    await env.waitForPlayer(playerLoadTimeMs);
     expect(env.audioNotAvailableMessage).not.toBeNull();
     expect(env.audioNotAvailableMessage.query(By.css('#error-load'))).not.toBeNull();
     expect(env.component.player1.audioStatus).toEqual(AudioStatus.Offline);
@@ -113,7 +114,7 @@ describe('CheckingAudioPlayerComponent', () => {
   it('show error tooltip if error loading audio is unsupported', async () => {
     const template = `<app-checking-audio-player #player1 source="blob://unsupported"></app-checking-audio-player>`;
     const env = new TestEnvironment(template, true);
-    await env.waitForPlayer(1000);
+    await env.waitForPlayer(playerLoadTimeMs);
     expect(env.audioNotAvailableMessage).not.toBeNull();
     expect(env.audioNotAvailableMessage.query(By.css('#error-load'))).not.toBeNull();
     expect(env.component.player1.audioStatus).toEqual(AudioStatus.LocalNotAvailable);
@@ -129,6 +130,7 @@ class HostComponent {
 
 class TestEnvironment {
   readonly mockedPwaService = mock(PwaService);
+  readonly ngZone: NgZone;
 
   fixture: ComponentFixture<HostComponent>;
   component: HostComponent;
@@ -143,6 +145,7 @@ class TestEnvironment {
     when(this.mockedPwaService.onlineStatus).thenReturn(of(isOnline));
 
     TestBed.overrideComponent(HostComponent, { set: { template: template } });
+    this.ngZone = TestBed.inject(NgZone);
     this.fixture = TestBed.createComponent(HostComponent);
     this.component = this.fixture.componentInstance;
     this.fixture.detectChanges();
@@ -184,7 +187,7 @@ class TestEnvironment {
   }
 
   async waitForPlayer(ms: number) {
-    await new Promise(resolve => setTimeout(resolve, ms));
+    await new Promise(resolve => this.ngZone.runOutsideAngular(() => setTimeout(resolve, ms)));
     this.fixture.detectChanges();
   }
 }

src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/checking-audio-recorder/checking-audio-recorder.component.spec.ts
@@ -1,5 +1,5 @@
 import { MdcDialog } from '@angular-mdc/web';
-import { DebugElement } from '@angular/core';
+import { DebugElement, NgZone } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { of } from 'rxjs';
@@ -94,6 +94,7 @@ describe('CheckingAudioRecorderComponent', () => {
 
 class TestEnvironment {
   rejectUserMedia = false;
+  readonly ngZone: NgZone = TestBed.inject(NgZone);
   readonly component: CheckingAudioRecorderComponent;
   readonly fixture: ComponentFixture<CheckingAudioRecorderComponent>;
 
@@ -138,7 +139,7 @@ class TestEnvironment {
   }
 
   async waitForRecorder(ms: number) {
-    await new Promise(resolve => setTimeout(resolve, ms));
+    await new Promise(resolve => this.ngZone.runOutsideAngular(() => setTimeout(resolve, ms)));
     this.fixture.detectChanges();
   }
 }

src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/checking.component.spec.ts
@@ -1,6 +1,6 @@
 import { MdcDialog, MdcDialogRef } from '@angular-mdc/web/dialog';
 import { Location } from '@angular/common';
-import { DebugElement } from '@angular/core';
+import { DebugElement, NgZone } from '@angular/core';
 import { ComponentFixture, fakeAsync, flush, TestBed, tick } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
@@ -33,6 +33,7 @@ import { first } from 'rxjs/operators';
 import { anyString, anything, deepEqual, instance, mock, reset, resetCalls, spy, verify, when } from 'ts-mockito';
 import { AuthService } from 'xforge-common/auth.service';
 import { AvatarTestingModule } from 'xforge-common/avatar/avatar-testing.module';
+import { BugsnagService } from 'xforge-common/bugsnag.service';
 import { FileService } from 'xforge-common/file.service';
 import { createStorageFileData, FileOfflineData, FileType } from 'xforge-common/models/file-offline-data';
 import { Snapshot } from 'xforge-common/models/snapshot';
@@ -82,6 +83,7 @@ const mockedActivatedRoute = mock(ActivatedRoute);
 const mockedMdcDialog = mock(MdcDialog);
 const mockedTextChooserDialogComponent = mock(TextChooserDialogComponent);
 const mockedQuestionDialogService = mock(QuestionDialogService);
+const mockedBugsnagService = mock(BugsnagService);
 const mockedCookieService = mock(CookieService);
 const mockedPwaService = mock(PwaService);
 const mockedFileService = mock(FileService);
@@ -157,6 +159,7 @@ describe('CheckingComponent', () => {
       { provide: MdcDialog, useMock: mockedMdcDialog },
       { provide: TextChooserDialogComponent, useMock: mockedTextChooserDialogComponent },
       { provide: QuestionDialogService, useMock: mockedQuestionDialogService },
+      { provide: BugsnagService, useMock: mockedBugsnagService },
       { provide: CookieService, useMock: mockedCookieService },
       { provide: FileService, useMock: mockedFileService },
       { provide: PwaService, useMock: mockedPwaService }
@@ -223,11 +226,7 @@ describe('CheckingComponent', () => {
       const projectUserConfig = env.component.projectUserConfigDoc!.data!;
       expect(projectUserConfig.selectedTask).toEqual('checking');
       expect(projectUserConfig.selectedQuestionRef).not.toBeNull();
-      env.component.projectDoc!.submitJson0Op(
-        op => op.set<boolean>(p => p.checkingConfig.checkingEnabled, false),
-        false
-      );
-      env.waitForSliderUpdate();
+      env.setCheckingEnabled(false);
       expect(projectUserConfig.selectedTask).toBeUndefined();
       expect(projectUserConfig.selectedQuestionRef).toBeUndefined();
       expect(env.component.projectDoc).toBeUndefined();
@@ -238,11 +237,7 @@ describe('CheckingComponent', () => {
       // User with access to translate app should get redirected there
       const env = new TestEnvironment(OBSERVER_USER, 'ALL');
       env.selectQuestion(1);
-      env.component.projectDoc!.submitJson0Op(
-        op => op.set<boolean>(p => p.checkingConfig.checkingEnabled, false),
-        false
-      );
-      env.waitForSliderUpdate();
+      env.setCheckingEnabled(false);
       expect(env.location.path()).toEqual('/projects/project01/translate/JHN');
       expect(env.component.projectDoc).toBeUndefined();
       expect(env.component.questionDocs.length).toEqual(0);
@@ -1455,6 +1450,7 @@ interface UserInfo {
 class TestEnvironment {
   readonly component: CheckingComponent;
   readonly fixture: ComponentFixture<CheckingComponent>;
+  readonly ngZone: NgZone = TestBed.inject(NgZone);
   readonly realtimeService: TestRealtimeService = TestBed.inject<TestRealtimeService>(TestRealtimeService);
   readonly mockedAnsweredDialogRef: MdcDialogRef<QuestionAnsweredDialogComponent> = mock(MdcDialogRef);
   readonly mockedTextChooserDialogComponent: MdcDialogRef<TextChooserDialogComponent> = mock(MdcDialogRef);
@@ -1765,7 +1761,7 @@ class TestEnvironment {
 
   activateQuestion(dataId: string) {
     const questionDoc = this.getQuestionDoc(dataId);
-    this.component.questionsPanel!.activateQuestion(questionDoc);
+    this.ngZone.run(() => this.component.questionsPanel!.activateQuestion(questionDoc));
     tick();
     this.fixture.detectChanges();
     tick(this.questionReadTimer);
@@ -1949,6 +1945,16 @@ class TestEnvironment {
   segmentHasQuestion(chapter: number, verse: number): boolean {
     const segment = this.getVerse(chapter, verse);
     return segment != null && segment.classList.contains('question-segment');
+  }
+
+  setCheckingEnabled(isEnabled: boolean = true): void {
+    this.ngZone.run(() => {
+      this.component.projectDoc!.submitJson0Op(
+        op => op.set<boolean>(p => p.checkingConfig.checkingEnabled, isEnabled),
+        false
+      );
+    });
+    this.waitForSliderUpdate();
   }
 
   waitForSliderUpdate(): void {

commit 1cd27e9b293c71d8edf0ab4c63cb601eea20a185

Fix injection of BugsnagService
This was broken in a87af728cf12e2dfc2c446038ad8b8513751a86b

src/SIL.XForge.Scripture/ClientApp/src/xforge-common/bugsnag.service.ts
@@ -5,7 +5,9 @@ import { NodeConfig } from '@bugsnag/node';
 /**
  * Wrapper around Bugsnag. This injectable service can be mocked to make dependencies of it testable.
  */
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class BugsnagService {
   start(apiKeyOrOpts: string | BrowserConfig | NodeConfig): Client {
     return Bugsnag.start(apiKeyOrOpts);