e2cf45e850c59f6b3bccadef69eec19d10a5baa1
Nathaniel Paulus <nateowami@gmail.com>
2021-06-14 16:42:08 -0400

    Show invitee role on users page

--- src/SIL.XForge.Scripture/Models/InviteeStatus.cs
+++ src/SIL.XForge.Scripture/Models/InviteeStatus.cs
@@ -3,6 +3,7 @@ namespace SIL.XForge.Scripture.Models
     public class InviteeStatus
     {
         public string Email { get; set; }
+        public string Role { get; set; }
         public bool Expired { get; set; }
     }
 }
