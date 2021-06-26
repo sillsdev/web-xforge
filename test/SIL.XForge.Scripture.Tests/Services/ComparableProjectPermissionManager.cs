6d2f1385138df1dc7ad4a8d9341b9ef35d2e4689
Peter Chapman <peter@conglomo.co.nz>
2021-06-26 14:10:09 +1200

    ScrText objects are now disposed correctly

---
+++ test/SIL.XForge.Scripture.Tests/Services/ComparableProjectPermissionManager.cs
@@ -0,0 +1,32 @@
+using System;
+using Paratext.Data;
+using Paratext.Data.Users;
+using PtxUtils;
+
+namespace SIL.XForge.Scripture.Services
+{
+    /// <summary>
+    /// This is an implementation of the ProjectPermissionManager that allows comparison of permission managers.
+    /// </summary>
+    /// <seealso cref="ProjectPermissionManager" />
+    /// <seealso cref="IEquatable{ComparableProjectPermissionManager}" />
+    public class ComparableProjectPermissionManager
+        : ProjectPermissionManager, IEquatable<ComparableProjectPermissionManager>
+    {
+        public ComparableProjectPermissionManager(ScrText scrText)
+            : base(scrText)
+        {
+        }
+
+        /// <summary>
+        /// Gets the XML data.
+        /// </summary>
+        /// <remarks>
+        /// This is based on the code for <see cref="PermissionManager.Clone" />.
+        /// </remarks>
+        public string XmlData => Memento.ToXmlString(Data, false, true);
+
+        public bool Equals(ComparableProjectPermissionManager other)
+            => this.XmlData == other?.XmlData;
+    }
+}
