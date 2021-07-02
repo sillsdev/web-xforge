d4f5f075585caa453074331b0a45c8660471a128
Ira Hopkinson <ira_hopkinson@sil.org>
2021-07-02 08:01:10 +0700

    - add AuthGuard tests

---
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/auth.guard.spec.ts
@@ -0,0 +1,107 @@
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { anything, instance, mock, resetCalls, verify, when } from 'ts-mockito';
+import { AuthGuard } from './auth.guard';
+import { AuthService } from './auth.service';
+import { LocationService } from './location.service';
+
+const mockedAuthService = mock(AuthService);
+const mockedLocationService = mock(LocationService);
+const mockedActivatedRouteSnapshot = mock(ActivatedRouteSnapshot);
+const mockedRouterStateSnapshot = mock(RouterStateSnapshot);
+
+describe('AuthGuard', () => {
+  beforeEach(() => {
+    resetCalls(mockedAuthService);
+  });
+
+  it('should do nothing if already logged in', () => {
+    // url test pattern: https://scriptureforge.org/projects/<projectId>
+    const authGuard = new AuthGuard(instance(mockedAuthService), instance(mockedLocationService));
+    expect(authGuard).toBeDefined();
+    when(mockedAuthService.isLoggedIn).thenResolve(true);
+    when(mockedActivatedRouteSnapshot.queryParams).thenReturn({});
+
+    const canActivate$ = authGuard.canActivate(
+      instance(mockedActivatedRouteSnapshot),
+      instance(mockedRouterStateSnapshot)
+    );
+
+    canActivate$.subscribe(canActivate => {
+      expect(canActivate).toBe(true);
+      verify(mockedAuthService.logIn(anything(), anything(), anything())).never();
+    });
+  });
+
+  it('should call login when not logged in', () => {
+    // url test pattern: https://scriptureforge.org/projects/<projectId>
+    const authGuard = new AuthGuard(instance(mockedAuthService), instance(mockedLocationService));
+    expect(authGuard).toBeDefined();
+    when(mockedAuthService.isLoggedIn).thenResolve(false);
+    when(mockedActivatedRouteSnapshot.queryParams).thenReturn({});
+
+    const canActivate$ = authGuard.canActivate(
+      instance(mockedActivatedRouteSnapshot),
+      instance(mockedRouterStateSnapshot)
+    );
+
+    canActivate$.subscribe(canActivate => {
+      expect(canActivate).toBe(false);
+      verify(mockedAuthService.logIn(anything(), false, undefined)).once();
+    });
+  });
+
+  it('should call login when not logged in and sharing', () => {
+    // url test pattern: https://scriptureforge.org/projects/<projectId>?sharing=true&shareKey=<shareKey>
+    const authGuard = new AuthGuard(instance(mockedAuthService), instance(mockedLocationService));
+    expect(authGuard).toBeDefined();
+    when(mockedAuthService.isLoggedIn).thenResolve(false);
+    when(mockedActivatedRouteSnapshot.queryParams).thenReturn({ sharing: 'true', 'sign-up': 'false' });
+
+    const canActivate$ = authGuard.canActivate(
+      instance(mockedActivatedRouteSnapshot),
+      instance(mockedRouterStateSnapshot)
+    );
+
+    canActivate$.subscribe(canActivate => {
+      expect(canActivate).toBe(false);
+      verify(mockedAuthService.logIn(anything(), true, undefined)).once();
+    });
+  });
+
+  it('should call login when not logged in and signing up', () => {
+    // url test pattern: https://scriptureforge.org/projects/<projectId>?sign-up=true
+    const authGuard = new AuthGuard(instance(mockedAuthService), instance(mockedLocationService));
+    expect(authGuard).toBeDefined();
+    when(mockedAuthService.isLoggedIn).thenResolve(false);
+    when(mockedActivatedRouteSnapshot.queryParams).thenReturn({ sharing: '', 'sign-up': 'true' });
+
+    const canActivate$ = authGuard.canActivate(
+      instance(mockedActivatedRouteSnapshot),
+      instance(mockedRouterStateSnapshot)
+    );
+
+    canActivate$.subscribe(canActivate => {
+      expect(canActivate).toBe(false);
+      verify(mockedAuthService.logIn(anything(), true, undefined)).once();
+    });
+  });
+
+  it('should call login when not logged in with locale', () => {
+    // url test pattern: https://scriptureforge.org/projects/<projectId>?locale=es
+    const authGuard = new AuthGuard(instance(mockedAuthService), instance(mockedLocationService));
+    expect(authGuard).toBeDefined();
+    when(mockedAuthService.isLoggedIn).thenResolve(false);
+    const locale = 'es';
+    when(mockedActivatedRouteSnapshot.queryParams).thenReturn({ locale });
+
+    const canActivate$ = authGuard.canActivate(
+      instance(mockedActivatedRouteSnapshot),
+      instance(mockedRouterStateSnapshot)
+    );
+
+    canActivate$.subscribe(canActivate => {
+      expect(canActivate).toBe(false);
+      verify(mockedAuthService.logIn(anything(), false, locale)).once();
+    });
+  });
+});
