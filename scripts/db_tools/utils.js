76453fe37d343192cf8b6fc7a69c0140c7024a11
Nathaniel Paulus <Nateowami@users.noreply.github.com>
2021-09-28 00:30:35 -0400

    Add script to delete doc and restore to past version (#1150)

--- scripts/db_tools/utils.js
+++ scripts/db_tools/utils.js
@@ -1,13 +1,92 @@
+/**
+ * @param {string} package The name of the package to import
+ */
 function requireFromRealTimeServer(package) {
   return require('../../src/RealtimeServer/node_modules/' + package);
 }
 
+/**
+ * @param {ShareDB.Doc} doc The doc to fetch
+ */
+function fetchDoc(doc) {
+  return new Promise((resolve, reject) => {
+    doc.fetch(err => {
+      if (err != null) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+/**
+ * @param {ShareDB.Doc} doc The document to submit the op to
+ * @param {Object} op The op to submit
+ */
+function submitDocOp(doc, op) {
+  return new Promise((resolve, reject) => {
+    doc.submitOp(op, undefined, err => {
+      if (err != null) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+/**
+ * @param {ShareDB.Doc} doc The doc to delete
+ */
+function deleteDoc(doc) {
+  return new Promise((resolve, reject) => {
+    doc.del(undefined, err => {
+      if (err != null) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+/**
+ * @param {ShareDB.Doc} doc The document to create (The document object needs to already exist; this function just
+ * writes it to ShareDB)
+ * @param {object} data The data of the document (actual contents as opposed to metadata)
+ * @param {string} type Type of the document (e.g. 'richtext')
+ */
+function createDoc(doc, data, type) {
+  return new Promise((resolve, reject) => {
+    doc.create(data, type, undefined, err => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+/**
+ * @param {ShareDB.Connection} conn The ShareDB connection
+ * @param {string} collection The collection to fetch the doc from
+ * @param {string} docId The id of the doc
+ * @param {number} version Version number of the snapshot to fetch
+ */
 function fetchSnapshotByVersion(conn, collection, docId, version) {
   return new Promise((resolve, reject) => {
     conn.fetchSnapshot(collection, docId, version, (err, snapshot) => (err != null ? reject(err) : resolve(snapshot)));
   });
 }
 
+/**
+ * @param {ShareDB.Connection} conn The ShareDB connection
+ * @param {string} collection The collection to fetch the doc from
+ * @param {string} docId The id of the doc
+ * @param {number} time The timestamp of the desired snapshot, in milliseconds since Unix Epoch
+ */
 function fetchSnapshotByTimestamp(conn, collection, docId, time) {
   return new Promise((resolve, reject) => {
     conn.fetchSnapshotByTimestamp(collection, docId, time, (err, snapshot) =>
@@ -16,10 +95,16 @@ function fetchSnapshotByTimestamp(conn, collection, docId, time) {
   });
 }
 
+/**
+ * @param {Object[]} ops List of operations, where an operation can be any object for the purposes of this function
+ * (though in practice the definition of an operation should be much more limited; this function is just flexible).
+ */
 function visualizeOps(ops) {
   const result = ops
     .map(op => {
-      if (typeof op.insert === 'string') {
+      if (typeof op.insert === 'undefined') {
+        return '[ invalid op ' + JSON.stringify(op) + ' ]';
+      } else if (typeof op.insert === 'string') {
         return op.insert;
       } else if (op.insert.verse) {
         return op.insert.verse.number + ' ';
@@ -48,6 +133,10 @@ const liveConfig = {
 
 module.exports = {
   requireFromRealTimeServer,
+  fetchDoc,
+  submitDocOp,
+  deleteDoc,
+  createDoc,
   fetchSnapshotByVersion,
   fetchSnapshotByTimestamp,
   visualizeOps,
