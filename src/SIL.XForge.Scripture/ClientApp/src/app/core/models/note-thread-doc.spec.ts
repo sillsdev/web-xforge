c5548277dcb740976f947830a002055f78ed4064
nigel-wells <nigel-wells@hotmail.com>
2021-11-16 14:45:28 +1300

    - Fixed tests

--- src/SIL.XForge.Scripture/ClientApp/src/app/core/models/note-thread-doc.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/core/models/note-thread-doc.spec.ts
@@ -1,6 +1,6 @@
 import { mock } from 'ts-mockito';
 import { TestRealtimeService } from 'xforge-common/test-realtime.service';
-import { NoteThread } from 'realtime-server/lib/esm/scriptureforge/models/note-thread';
+import { NoteStatus, NoteThread } from 'realtime-server/lib/esm/scriptureforge/models/note-thread';
 import { TestBed } from '@angular/core/testing';
 import { configureTestingModule } from 'xforge-common/test-utils';
 import { TestRealtimeModule } from 'xforge-common/test-realtime.module';
@@ -21,7 +21,7 @@ describe('NoteThreadDoc', () => {
     env = new TestEnvironment();
   });
 
-  it('produce valid alternative note icon', async () => {
+  it('should use specified icon instead of default', async () => {
     const noteThread: NoteThread = {
       originalContextBefore: '',
       originalContextAfter: '',
@@ -32,7 +32,7 @@ describe('NoteThreadDoc', () => {
       ownerRef: 'user01',
       projectRef: 'project01',
       tagIcon: 'flag02',
-      resolved: false,
+      status: NoteStatus.Todo,
       verseRef: { bookNum: 40, chapterNum: 1, verseNum: 1 }
     };
     const noteThreadDoc = await env.setupDoc(noteThread);
@@ -43,7 +43,7 @@ describe('NoteThreadDoc', () => {
     expect(noteThreadDoc.icon).toEqual(expectedIcon);
   });
 
-  it('produce valid alternative note icon', async () => {
+  it('should use default to do icon if none specified', async () => {
     const noteThread: NoteThread = {
       originalContextBefore: '',
       originalContextAfter: '',
@@ -54,7 +54,7 @@ describe('NoteThreadDoc', () => {
       ownerRef: 'user01',
       projectRef: 'project01',
       tagIcon: '',
-      resolved: false,
+      status: NoteStatus.Todo,
       verseRef: { bookNum: 40, chapterNum: 1, verseNum: 1 }
     };
     const noteThreadDoc = await env.setupDoc(noteThread);
@@ -65,7 +65,34 @@ describe('NoteThreadDoc', () => {
     expect(noteThreadDoc.icon).toEqual(expectedIcon);
   });
 
-  it('produce valid alternative note icon', async () => {
+  it('should use resolved icon', async () => {
+    const noteThread: NoteThread = {
+      originalContextBefore: '',
+      originalContextAfter: '',
+      originalSelectedText: '',
+      position: { start: 0, length: 1 },
+      dataId: 'thread01',
+      notes: [],
+      ownerRef: 'user01',
+      projectRef: 'project01',
+      tagIcon: '',
+      status: NoteStatus.Todo,
+      verseRef: { bookNum: 40, chapterNum: 1, verseNum: 1 }
+    };
+    const noteThreadDoc = await env.setupDoc(noteThread);
+    const expectedIcon: NoteThreadIcon = {
+      cssVar: '--icon-file: url(/assets/icons/TagIcons/01flag1.png);',
+      url: '/assets/icons/TagIcons/01flag1.png'
+    };
+    const expectedIconResolved: NoteThreadIcon = {
+      cssVar: '--icon-file: url(/assets/icons/TagIcons/01flag5.png);',
+      url: '/assets/icons/TagIcons/01flag5.png'
+    };
+    expect(noteThreadDoc.icon).toEqual(expectedIcon);
+    expect(noteThreadDoc.iconResolved).toEqual(expectedIconResolved);
+  });
+
+  it('should use the last icon specified in a threads note list based on date ', async () => {
     const noteThread: NoteThread = {
       originalContextBefore: '',
       originalContextAfter: '',
@@ -73,7 +100,7 @@ describe('NoteThreadDoc', () => {
       ownerRef: 'user01',
       projectRef: 'project01',
       tagIcon: '',
-      resolved: false,
+      status: NoteStatus.Todo,
       verseRef: { bookNum: 40, chapterNum: 1, verseNum: 1 },
       position: { start: 0, length: 1 },
       dataId: 'thread01',
@@ -84,6 +111,7 @@ describe('NoteThreadDoc', () => {
           content: 'note content',
           deleted: false,
           tagIcon: 'flag2',
+          status: NoteStatus.Todo,
           ownerRef: 'user01',
           extUserId: 'user01',
           dateCreated: '2021-11-10T12:00:00',
@@ -95,6 +123,7 @@ describe('NoteThreadDoc', () => {
           content: 'note content',
           deleted: false,
           tagIcon: 'flag3',
+          status: NoteStatus.Todo,
           ownerRef: 'user01',
           extUserId: 'user01',
           dateCreated: '2021-11-10T13:00:00', // Note out of order to test it still renders last
@@ -106,6 +135,7 @@ describe('NoteThreadDoc', () => {
           content: 'note content',
           deleted: false,
           tagIcon: 'flag4',
+          status: NoteStatus.Todo,
           ownerRef: 'user01',
           extUserId: 'user01',
           dateCreated: '2021-11-10T12:30:00',
