22778337ba7ae4c43b899e47f96aaf5fabcdf199
MarkS <marksvc@gmail.com>
2021-11-19 13:52:25 -0700

    utils: add find-problem-cids
    - SF has handled character styling in a way that can lead to
    unexpected character formatting markers ending and beginning, as
    described in SF-1435. This script attempts to spot situations where
    this may have happened.
    - This script is derived from text-doc-history.js.
    - This script is written in TypeScript, and so also introduced are
    utils.d.ts, tsconfig.json, package.json, and package-lock.json.
    - The ws and @types/ws package versions are set to match what I have
    in RealtimeServer.

---
+++ scripts/db_tools/utils.d.ts
@@ -0,0 +1,19 @@
+// Type definitions for utils
+//
+// Copyright 2021 SIL International. MIT License.
+
+import { Snapshot } from 'sharedb';
+import { Connection } from 'sharedb/lib/client';
+
+export function visualizeOps(ops: any, showAttributes: boolean): void;
+export function fetchSnapshotByVersion(
+  conn: Connection,
+  collection: string,
+  docId: string,
+  version: number
+): Promise<Snapshot>;
+export function colored(colorCode: number, textToColor: string): string;
+export var databaseConfigs: Map<string, { dbLocation: string; wsConnectionString: string }>;
+export var devConfig: { dbLocation: string; wsConnectionString: string };
+export function useColor(ifUseColor: boolean): void;
+export var colors: any;
