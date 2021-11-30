e36e0f559bb2aac91aa41b430287ba63f3e0b2e3
Ira Hopkinson <ira_hopkinson@sil.org>
2021-11-30 11:18:45 +0700

    - Obsolete the API method to fix lgtm-com

--- src/SIL.XForge.Scripture/Controllers/SFProjectsRpcController.cs
+++ src/SIL.XForge.Scripture/Controllers/SFProjectsRpcController.cs
@@ -362,7 +362,7 @@ namespace SIL.XForge.Scripture.Controllers
             }
         }
 
-        /// To be removed soon. Only here for clients still running a front end that still calls it.
+        [Obsolete("To be removed soon. Only here for clients still running a front end that still calls it.")]
         public async Task<IRpcMethodResult> HasTransceleratorQuestions(string projectId)
         {
             try
