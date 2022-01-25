0d7de5d46b2a6c0995b8842533116c58b8402d1b
MarkS <marksvc@gmail.com>
2022-01-25 14:15:55 -0700

    utils: fetch text docs

--- scripts/db_tools/show-projects.ts
+++ scripts/db_tools/show-projects.ts
@@ -1,14 +1,17 @@
 #!./node_modules/.bin/ts-node
 //
-// List projects
+// Show project metadata and data
 //
 // Copyright 2021 SIL International. MIT License.
 //
-// Display metadata about projects.
+// Display data and metadata about projects.
 //
 // This script needs ts-node and must be run from the containing directory. Setup: npm ci
-// Usage example: ./list-projects.ts
-// More info: ./list-projects.ts --help
+// Usage info: ./show-projects.ts --help
+// Examples:
+//   Show a list of projects and their Ids: ./show-projects.ts
+//   Show a list of books in each project: ./show-project.ts --books
+//   Show a textdoc: ./show-projects.ts --proj ABC --book RUT --chapter 1 --text --no-color > out.json
 
 import { MongoClient, Db, Collection } from 'mongodb';
 import WebSocket from 'ws';
@@ -23,11 +26,13 @@ type ProgArgs = {
   server: string;
   books: boolean;
   chapters: boolean;
+  text: boolean;
   proj: string | undefined;
   sfId: string | undefined;
   ptId: string | undefined;
   book: string | undefined;
   bookNum: number | undefined;
+  chapter: number | undefined;
 };
 type BookMetadata = Map<string, { bookNumber: number; chaptersInProj: number[] }>;
 
@@ -37,11 +42,13 @@ class ProjectInquirer {
   connectionConfig: ConnectionSettings | undefined;
   showBooks: boolean = false;
   showChapters: boolean = false;
+  showChapterText: boolean = false;
   limitProj: string | undefined;
   limitSFId: string | undefined;
   limitPTId: string | undefined;
   limitBook: string | undefined;
   limitbookNum: number | undefined;
+  limitChapter: number | undefined;
 
   constructor() {
     this.processArguments();
@@ -71,6 +78,11 @@ class ProjectInquirer {
         default: false,
         description: 'include chapters in metadata (implies --books)'
       })
+      .option('text', {
+        type: 'boolean',
+        default: false,
+        description: 'include chapter text in output (implies --chapters)'
+      })
       .option('proj', {
         type: 'string',
         description: 'Limit to project shortName (eg ABC)'
@@ -91,6 +103,10 @@ class ProjectInquirer {
         type: 'number',
         description: 'Limit book list to book number (eg 1 for GEN)'
       })
+      .option('chapter', {
+        type: 'number',
+        description: 'Limit chapter list to chapter number (eg 1)'
+      })
       .strict()
       .parseSync();
 
@@ -98,13 +114,15 @@ class ProjectInquirer {
     useColor(shouldUseColor);
     this.server = args.server;
     this.connectionConfig = databaseConfigs.get(this.server);
-    this.showBooks = args.books || args.chapters;
-    this.showChapters = args.chapters;
+    this.showBooks = args.books || args.chapters || args.text;
+    this.showChapters = args.chapters || args.text;
+    this.showChapterText = args.text;
     this.limitProj = args.proj;
     this.limitSFId = args.sfId;
     this.limitPTId = args.ptId;
     this.limitBook = args.book;
     this.limitbookNum = args.bookNum;
+    this.limitChapter = args.chapter;
   }
 
   getProjectBookMetadata(projectDoc: any): BookMetadata {
@@ -118,6 +136,18 @@ class ProjectInquirer {
       bookMetadata.set(bookAbbr, { bookNumber, chaptersInProj });
     }
     return bookMetadata;
+  }
+
+  getChapterText(textsInDB: any[], projectId: string, bookAbbr: string, chapter: number): string {
+    const lookupId = `${projectId}:${bookAbbr}:${chapter}:target`;
+    const chapterTexts: any[] = textsInDB.filter((item: any) => item._id === lookupId);
+    if (chapterTexts.length < 1) {
+      throw new Error(`Could not find expected chapter text for ${lookupId}.`);
+    }
+    if (chapterTexts.length > 1) {
+      throw new Error(`Expected 1 matching chapter text, found ${chapterTexts.length} for ${lookupId}.`);
+    }
+    return chapterTexts[0];
   }
 
   async main() {
@@ -134,6 +164,8 @@ class ProjectInquirer {
     });
     try {
       const db: Db = client.db();
+      const textsCollection: Collection<any> = db.collection('texts');
+      const textsInDB: any[] = await textsCollection.find().toArray();
       const projectCollection: Collection<any> = db.collection('sf_projects');
       let projectsInDB: any[] = await projectCollection.find().toArray();
       if (this.limitProj != null) {
@@ -150,27 +182,50 @@ class ProjectInquirer {
         return;
       }
       projectsInDB.forEach((projDoc: any) => metadata.set(projDoc._id, projDoc));
-      metadata.forEach((projDoc: any, sfId: string) => {
+      metadata.forEach(async (projDoc: any, sfId: string) => {
         const books: BookMetadata = this.getProjectBookMetadata(projDoc);
         let booksToExamine: string[] = Array.from(books.keys());
         if (this.limitBook) {
           booksToExamine = booksToExamine.filter(item => item === this.limitBook);
         }
-        if (this.limitbookNum) {
+        if (this.limitbookNum != null) {
           booksToExamine = booksToExamine.filter(item => books.get(item)?.bookNumber === this.limitbookNum);
         }
-        const bookList: string = booksToExamine
-          .map((bookAbbr: string) => {
+        let optionalBooksDisplay: string = '';
+        if (this.showChapterText) {
+          for (const bookAbbr of booksToExamine) {
             const bookItem = books.get(bookAbbr);
             const bookNum: number = bookItem!.bookNumber;
-            const chapters: string = bookItem!.chaptersInProj.join(' ');
-            const chaptersDisplay: string = ` (${chapters})`;
-            return `${colored(colors.darkGrey, `[${bookNum}]`)}${colored(colors.orange, bookAbbr)}${
-              this.showChapters ? chaptersDisplay : ''
-            }`;
-          })
-          .join(' ');
-        const optionalBooksDisplay: string = this.showBooks ? `; ${bookList}` : '';
+            let chapters: number[] = bookItem!.chaptersInProj;
+            if (this.limitChapter != null) {
+              chapters = chapters.filter((chapNum: number) => chapNum === this.limitChapter);
+            }
+            for (const chapNum of chapters) {
+              const chapText = JSON.stringify(this.getChapterText(textsInDB, sfId, bookAbbr, chapNum), null, '  ');
+              optionalBooksDisplay = `${optionalBooksDisplay}\n${sfId} ${colored(
+                colors.darkGrey,
+                `[${bookNum}]`
+              )}${colored(colors.orange, bookAbbr)} ${chapNum}\n${chapText}`;
+            }
+          }
+        } else {
+          const bookList: string = booksToExamine
+            .map((bookAbbr: string) => {
+              const bookItem = books.get(bookAbbr);
+              const bookNum: number = bookItem!.bookNumber;
+              let chapters: number[] = bookItem!.chaptersInProj;
+              if (this.limitChapter != null) {
+                chapters = chapters.filter((chapNum: number) => chapNum === this.limitChapter);
+              }
+              const chapterList: string = chapters.join(' ');
+              const chaptersDisplay: string = ` (${chapterList})`;
+              return `${colored(colors.darkGrey, `[${bookNum}]`)}${colored(colors.orange, bookAbbr)}${
+                this.showChapters ? chaptersDisplay : ''
+              }`;
+            })
+            .join(' ');
+          optionalBooksDisplay = this.showBooks ? `; ${bookList}` : '';
+        }
         console.log(
           `${colored(colors.yellow, sfId)}; ${projDoc.paratextId}; ${colored(colors.lightBlue, projDoc.shortName)}; ${
             projDoc.name
