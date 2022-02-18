e679c734501c28a75ce48e46ed6d7b9d440df8b4
Raymond Luong <raymond_luong@sil.org>
2022-02-18 11:58:50 -0700

    Fix checking app type issues and improve class comments

--- src/SIL.XForge.Scripture/ClientApp/src/app/core/models/sf-project-profile-doc.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/core/models/sf-project-profile-doc.ts
@@ -5,6 +5,7 @@ import {
 } from 'realtime-server/lib/esm/scriptureforge/models/sf-project';
 import { SFProjectBaseDoc } from './sf-project-base-doc';
 
+// Real-time document for the data of an sf project that all project members can access
 export class SFProjectProfileDoc extends SFProjectBaseDoc<SFProjectProfile> {
   static readonly COLLECTION = SF_PROJECT_PROFILES_COLLECTION;
   static readonly INDEX_PATHS = SF_PROJECT_PROFILES_INDEX_PATHS;
