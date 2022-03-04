68dd9c0c4d8a42f0900dd8b2abac921e952b5340
Ira Hopkinson <ira_hopkinson@sil.org>
2022-03-04 13:45:39 +0700

    - replace @angular-mdc/web/card
    - replace @angular-mdc/web/card with @angular/material/card
    - fix unit tests

--- src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking-overview/checking-overview.component.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking-overview/checking-overview.component.spec.ts
@@ -860,7 +860,7 @@ class TestEnvironment {
   }
 
   get likePanel(): DebugElement {
-    return this.fixture.debugElement.query(By.css('.reviewer-panels .card .card-content-like'));
+    return this.fixture.debugElement.query(By.css('.reviewer-panels .card-content-like'));
   }
 
   set onlineStatus(isOnline: boolean) {
