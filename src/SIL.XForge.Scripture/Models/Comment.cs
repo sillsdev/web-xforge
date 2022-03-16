e189612595c81ea94a4308cac772c8ab16d5787c
MarkS <marksvc@gmail.com>
2022-03-16 16:49:26 -0600

    Improve sync tests, comment
    commit-id:09c05404

--- src/SIL.XForge.Scripture/Models/Comment.cs
+++ src/SIL.XForge.Scripture/Models/Comment.cs
@@ -7,6 +7,10 @@ namespace SIL.XForge.Scripture.Models
     {
         public string DataId { get; set; }
         public string OwnerRef { get; set; }
+        /// <summary>
+        /// The OpaqueUserId of a ParatextUserProfile. It is used to correlate comments between PT and SF. It may refer
+        /// to a SF user who synchronized the comment.
+        /// </summary>
         public string SyncUserRef { get; set; }
         public string Text { get; set; }
         public DateTime DateModified { get; set; }
