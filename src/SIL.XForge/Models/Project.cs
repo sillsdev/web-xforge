84d5b20a272cb81f0ff3b8e50dfc75d028d0ea6a
MarkS <marksvc@gmail.com>
2022-03-29 12:04:29 -0600

    Comment on Project.UserRoles
    commit-id:7ddf825f

--- src/SIL.XForge/Models/Project.cs
+++ src/SIL.XForge/Models/Project.cs
@@ -5,6 +5,9 @@ namespace SIL.XForge.Models
     public abstract class Project : Json0Snapshot
     {
         public string Name { get; set; }
+        /// <summary>
+        /// Dictionary of SF user id to role.
+        /// </summary>
         public Dictionary<string, string> UserRoles { get; set; } = new Dictionary<string, string>();
         public Dictionary<string, string[]> UserPermissions { get; set; } = new Dictionary<string, string[]>();
         public bool SyncDisabled { get; set; }
