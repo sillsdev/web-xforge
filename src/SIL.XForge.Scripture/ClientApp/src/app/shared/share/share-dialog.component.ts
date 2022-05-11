sillsdev/web-xforge

commit cb494c620519e0bcc6313166558976db71b38f11

SF-1557 Fix share dialog menus appearing behind dialog (#1342)
* This was caused by an interaction between Angular Material and Angular
MDC. The mix of a MDC dialog and a Material mat-select within it caused
the menu to appear behind the dialog. This problem only manifests itself
if a Material dialog was opened prior to this dialog being opened.

src/SIL.XForge.Scripture/ClientApp/src/app/shared/share/share-dialog.component.scss
@@ -1,6 +1,3 @@
-mdc-dialog-container {
-  width: 100%;
-  > mdc-dialog-surface {
-    width: 100%;
-  }
+.mat-dialog-content {
+  width: 30em;
 }

src/SIL.XForge.Scripture/ClientApp/src/app/shared/share/share-dialog.component.ts
@@ -1,5 +1,5 @@
-import { MDC_DIALOG_DATA } from '@angular-mdc/web/dialog';
 import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { SFProjectRole } from 'realtime-server/lib/esm/scriptureforge/models/sf-project-role';
 
 export interface ShareDialogData {
@@ -12,5 +12,5 @@ export interface ShareDialogData {
   styleUrls: ['./share-dialog.component.scss']
 })
 export class ShareDialogComponent {
-  constructor(@Inject(MDC_DIALOG_DATA) public readonly data: ShareDialogData) {}
+  constructor(@Inject(MAT_DIALOG_DATA) public readonly data: ShareDialogData) {}
 }

src/SIL.XForge.Scripture/ClientApp/src/app/shared/share/share-dialog.component.html
@@ -1,19 +1,12 @@
 <ng-container *transloco="let t; read: 'share_dialog'">
-  <mdc-dialog>
-    <mdc-dialog-container>
-      <mdc-dialog-surface class="dialog-size">
-        <mdc-dialog-title>{{ t("share_with_others") }}</mdc-dialog-title>
-        <mdc-dialog-content>
-          <app-share-control
-            class="share-dialog"
-            [projectId]="data.projectId"
-            [defaultRole]="data.defaultRole"
-          ></app-share-control>
-        </mdc-dialog-content>
-        <mdc-dialog-actions align="end">
-          <button id="close-btn" mdcDialogButton type="button" mdcDialogAction="close">{{ t("done") }}</button>
-        </mdc-dialog-actions>
-      </mdc-dialog-surface>
-    </mdc-dialog-container>
-  </mdc-dialog>
+  <h1 mat-dialog-title>{{ t("share_with_others") }}</h1>
+  <app-share-control
+    mat-dialog-content
+    class="share-dialog"
+    [projectId]="data.projectId"
+    [defaultRole]="data.defaultRole"
+  ></app-share-control>
+  <div mat-dialog-actions align="end">
+    <button id="close-btn" mat-flat-button mat-dialog-close>{{ t("done") }}</button>
+  </div>
 </ng-container>

src/SIL.XForge.Scripture/ClientApp/src/app/shared/share/share.component.ts
@@ -1,5 +1,5 @@
-import { MdcDialog } from '@angular-mdc/web/dialog';
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { SFProjectRole } from 'realtime-server/lib/esm/scriptureforge/models/sf-project-role';
 import { map } from 'rxjs/operators';
@@ -15,7 +15,7 @@ export class ShareComponent implements OnInit {
 
   private projectId?: string;
 
-  constructor(private readonly dialog: MdcDialog, private readonly activatedRoute: ActivatedRoute) {}
+  constructor(private readonly dialog: MatDialog, private readonly activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.activatedRoute.params.pipe(map(params => params['projectId'] as string)).subscribe(async projectId => {

commit 33ab78ee5488aa48dc88884a3109c01a6a96f11a

SF-1557b Prevent scrollbar in share dialog on mobile
This was broken in #1342 "SF-1557 Fix share dialog menus appearing
behind dialog" (cb494c620519e0bcc6313166558976db71b38f11)

src/SIL.XForge.Scripture/ClientApp/src/app/shared/share/share-dialog.component.scss
@@ -1,3 +1,7 @@
-.mat-dialog-content {
+// Note: ViewEncapsulation is disabled for this file, so all styles can apply to any element globally
+
+.share-dialog-component-panel {
   width: 30em;
+  // Angular Material sets max-width on the element itself, so this is the only way to override in CSS
+  max-width: 90vw !important;
 }

src/SIL.XForge.Scripture/ClientApp/src/app/shared/share/share-dialog.component.ts
@@ -1,5 +1,5 @@
-import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, Inject, ViewEncapsulation } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { SFProjectRole } from 'realtime-server/lib/esm/scriptureforge/models/sf-project-role';
 
 export interface ShareDialogData {
@@ -9,8 +9,14 @@ export interface ShareDialogData {
 
 @Component({
   templateUrl: './share-dialog.component.html',
-  styleUrls: ['./share-dialog.component.scss']
+  styleUrls: ['./share-dialog.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ShareDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public readonly data: ShareDialogData) {}
+  constructor(
+    readonly dialogRef: MatDialogRef<ShareDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public readonly data: ShareDialogData
+  ) {
+    dialogRef.addPanelClass('share-dialog-component-panel');
+  }
 }