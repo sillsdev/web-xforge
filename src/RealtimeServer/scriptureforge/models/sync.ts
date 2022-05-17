1e8458413d387e7dee3984ea4fe92170b9e3d7bd
MarkS <marksvc@gmail.com>
2022-05-17 16:39:30 -0600

    model: add missing sync.syncedToRepositoryVersion
    It is present on Sync.cs.
    
    commit-id:ac568990

--- src/RealtimeServer/scriptureforge/models/sync.ts
+++ src/RealtimeServer/scriptureforge/models/sync.ts
@@ -3,6 +3,7 @@ export interface Sync {
   percentCompleted?: number;
   lastSyncSuccessful?: boolean;
   dateLastSuccessfulSync?: string;
+  syncedToRepositoryVersion?: string;
   /** Indicates if PT project data from the last send/receive operation was incorporated into the SF project docs */
   dataInSync?: boolean;
 }
