sillsdev/web-xforge

commit 2bcff6fdbbaace80d7ad5251a41a118263a92ee9

SF-1516c Optimize checking questions component (#1301)
- Add changes$ observable to RealtimeDoc and RealtimeDocAdapter
- Add docs$ observable to RealtimeQuery
- Make checking questions component use OnPush change detection strategy

src/SIL.XForge.Scripture/ClientApp/src/xforge-common/models/realtime-query.ts
@@ -1,5 +1,5 @@
 import arrayDiff, { InsertDiff, MoveDiff, RemoveDiff } from 'arraydiff';
-import { Observable, Subject, Subscription } from 'rxjs';
+import { BehaviorSubject, Observable, Subject, Subscription } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { RealtimeQueryAdapter } from '../realtime-remote-store';
 import { RealtimeService } from '../realtime.service';
@@ -19,6 +19,7 @@ export class RealtimeQuery<T extends RealtimeDoc = RealtimeDoc> {
   private readonly _ready$ = new Subject<void>();
   private readonly docSubscriptions = new Map<string, Subscription>();
   private readonly _remoteDocChanges$ = new Subject<any>();
+  private readonly _docs$ = new BehaviorSubject<T[]>([]);
 
   constructor(private readonly realtimeService: RealtimeService, public readonly adapter: RealtimeQueryAdapter) {
     this.adapter.ready$.pipe(takeUntil(this.unsubscribe$)).subscribe(() => this.onReady());
@@ -37,6 +38,14 @@ export class RealtimeQuery<T extends RealtimeDoc = RealtimeDoc> {
 
   get docs(): Readonly<T[]> {
     return this._docs;
+  }
+
+  /**
+   * Observable for the docs that match the query. Emits whenever a doc is added or removed from the results, or one of
+   * the docs is modified.
+   */
+  get docs$(): Observable<Readonly<T[]>> {
+    return this._docs$;
   }
 
   get count(): number {
@@ -171,8 +180,11 @@ export class RealtimeQuery<T extends RealtimeDoc = RealtimeDoc> {
     }
     this._unpagedCount = unpagedCount;
 
-    if (emitRemoteChanges && changed && this.adapter.ready) {
-      this._remoteChanges$.next();
+    if (this.adapter.ready) {
+      this._docs$.next(this._docs);
+      if (changed && emitRemoteChanges) {
+        this._remoteChanges$.next();
+      }
     }
   }
 
@@ -185,6 +197,7 @@ export class RealtimeQuery<T extends RealtimeDoc = RealtimeDoc> {
       newDocs.push(newDoc);
       const docSubscription = newDoc.remoteChanges$.subscribe(() => {
         this._remoteDocChanges$.next(newDoc);
+        this._docs$.next(this._docs);
       });
       this.docSubscriptions.set(newDoc.id, docSubscription);
     }

src/SIL.XForge.Scripture/ClientApp/src/app/app.component.ts
@@ -35,6 +35,7 @@ import { UserService } from 'xforge-common/user.service';
 import { issuesEmailTemplate, supportedBrowser } from 'xforge-common/utils';
 import { version } from '../../../version.json';
 import { environment } from '../environments/environment';
+import { QuestionDoc } from './core/models/question-doc';
 import { SFProjectProfileDoc } from './core/models/sf-project-profile-doc';
 import { canAccessTranslateApp } from './core/models/sf-project-role-info';
 import { SFProjectService } from './core/sf-project.service';
@@ -80,7 +81,7 @@ export class AppComponent extends DataLoadingComponent implements OnInit, OnDest
   private selectedProjectDeleteSub?: Subscription;
   private removedFromProjectSub?: Subscription;
   private _isDrawerPermanent: boolean = true;
-  private readonly questionCountQueries = new Map<number, RealtimeQuery>();
+  private readonly questionCountQueries = new Map<number, RealtimeQuery<QuestionDoc>>();
 
   constructor(
     private readonly router: Router,
@@ -533,7 +534,7 @@ export class AppComponent extends DataLoadingComponent implements OnInit, OnDest
       );
     }
     await Promise.all(promises);
-    this.questionCountQueries.forEach((query: RealtimeQuery, bookNum: number) => {
+    this.questionCountQueries.forEach((query: RealtimeQuery<QuestionDoc>, bookNum: number) => {
       this.subscribe(merge(query.remoteChanges$, query.localChanges$, query.ready$), () => {
         if (this.selectedProjectDoc == null) {
           return;

src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/checking-questions/checking-questions.component.ts
@@ -1,45 +1,96 @@
 import { MdcList, MdcListItem } from '@angular-mdc/web';
-import { Component, EventEmitter, Input, Output, ViewChild, ViewChildren } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+  ViewChildren
+} from '@angular/core';
 import sortBy from 'lodash-es/sortBy';
 import { Operation } from 'realtime-server/lib/esm/common/models/project-rights';
 import { Answer } from 'realtime-server/lib/esm/scriptureforge/models/answer';
 import { Comment } from 'realtime-server/lib/esm/scriptureforge/models/comment';
 import { SFProjectProfile } from 'realtime-server/lib/esm/scriptureforge/models/sf-project';
 import { SFProjectDomain, SF_PROJECT_RIGHTS } from 'realtime-server/lib/esm/scriptureforge/models/sf-project-rights';
-import { Subject } from 'rxjs';
+import { Observable, Subject, Subscription } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 import { SubscriptionDisposable } from 'xforge-common/subscription-disposable';
 import { UserService } from 'xforge-common/user.service';
+import { SFProjectUserConfig } from 'realtime-server/lib/esm/scriptureforge/models/sf-project-user-config';
+import { SFProjectProfileDoc } from '../../../core/models/sf-project-profile-doc';
 import { QuestionDoc } from '../../../core/models/question-doc';
 import { SFProjectUserConfigDoc } from '../../../core/models/sf-project-user-config-doc';
 import { TranslationEngineService } from '../../../core/translation-engine.service';
 import { CheckingUtils } from '../../checking.utils';
 
+// For performance reasons, this component uses the OnPush change detection strategy rather than the default change
+// detection strategy. This means when change detection runs, this component will be skipped during change detection
+// until:
+// 1. One of the @Input values is no longer equal to its previous value. Primitives are compared by value, objects by
+// reference. So an array that has elements added or an observable that has emitted a value is not changed.
+// 2. An event originates from the component or its children
+// 3. The component calls changeDetector.markForCheck()
+// There are a few other things that can trigger it. See the Angular documentation for details.
+// Calling markForCheck() does not trigger change detection like detectChanges(). It merely indicates that at the next
+// time change detection is run, this component should be checked for changes instead of being skipped.
+
 @Component({
   selector: 'app-checking-questions',
   templateUrl: './checking-questions.component.html',
-  styleUrls: ['./checking-questions.component.scss']
+  styleUrls: ['./checking-questions.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CheckingQuestionsComponent extends SubscriptionDisposable {
-  @Input() project?: SFProjectProfile;
-  @Input() projectUserConfigDoc?: SFProjectUserConfigDoc;
   @Input() isAllBooksShown: boolean = false;
   @Output() update = new EventEmitter<QuestionDoc>();
   @Output() changed = new EventEmitter<QuestionDoc>();
-  _questionDocs: Readonly<QuestionDoc[]> = [];
+  questionDocs: Readonly<QuestionDoc[]> = [];
   activeQuestionDoc?: QuestionDoc;
   activeQuestionDoc$ = new Subject<QuestionDoc>();
   @ViewChild(MdcList, { static: true }) mdcList!: MdcList;
 
+  private project?: SFProjectProfile;
+  private _projectUserConfigDoc?: SFProjectUserConfigDoc;
+
+  private projectProfileDocChangesSubscription?: Subscription;
+  private projectUserConfigDocChangesSubscription?: Subscription;
+  private questionDocsSubscription?: Subscription;
+
   constructor(
     private readonly userService: UserService,
-    private readonly translationEngineService: TranslationEngineService
+    private readonly translationEngineService: TranslationEngineService,
+    private readonly changeDetector: ChangeDetectorRef
   ) {
     super();
     // Only mark as read if it has been viewed for a set period of time and not an accidental click
     this.subscribe(this.activeQuestionDoc$.pipe(debounceTime(2000)), questionDoc => {
       this.updateElementsRead(questionDoc);
     });
+  }
+
+  @Input()
+  set projectProfileDoc(projectProfileDoc: SFProjectProfileDoc | undefined) {
+    this.projectProfileDocChangesSubscription?.unsubscribe();
+    this.project = projectProfileDoc?.data;
+    if (projectProfileDoc != null) {
+      this.projectProfileDocChangesSubscription = this.subscribe(projectProfileDoc?.changes$, () => {
+        this.changeDetector.markForCheck();
+      });
+    }
+  }
+
+  @Input()
+  set projectUserConfigDoc(projectUserConfigDoc: SFProjectUserConfigDoc | undefined) {
+    this.projectUserConfigDocChangesSubscription?.unsubscribe();
+    this._projectUserConfigDoc = projectUserConfigDoc;
+    if (projectUserConfigDoc != null) {
+      this.projectUserConfigDocChangesSubscription = this.subscribe(projectUserConfigDoc.changes$, () => {
+        this.changeDetector.markForCheck();
+      });
+    }
   }
 
   get activeQuestionBook(): number | undefined {
@@ -62,17 +113,18 @@ export class CheckingQuestionsComponent extends SubscriptionDisposable {
     return this.questionDocs.findIndex(question => question.id === activeQuestionDocId);
   }
 
-  get questionDocs(): Readonly<QuestionDoc[]> {
-    return this._questionDocs;
-  }
-
-  @Input() set questionDocs(questionDocs: Readonly<QuestionDoc[]>) {
-    if (questionDocs.length > 0) {
-      this.activateStoredQuestion(questionDocs);
-    } else {
-      this.activeQuestionDoc = undefined;
-    }
-    this._questionDocs = questionDocs;
+  @Input()
+  set questionDocs$(questionDocs$: Observable<Readonly<QuestionDoc[]>>) {
+    this.questionDocsSubscription?.unsubscribe();
+    this.questionDocsSubscription = this.subscribe(questionDocs$, docs => {
+      if (docs.length > 0) {
+        this.activateStoredQuestion(docs);
+      } else {
+        this.activeQuestionDoc = undefined;
+      }
+      this.questionDocs = docs;
+      this.changeDetector.markForCheck();
+    });
   }
 
   // When the list of questions is hidden it has display: none applied, which prevents scrolling to the active question
@@ -134,6 +186,7 @@ export class CheckingQuestionsComponent extends SubscriptionDisposable {
     }
     return unread;
   }
+
   /**
    * Activates the question that a user has most recently viewed if available
    */
@@ -144,8 +197,8 @@ export class CheckingQuestionsComponent extends SubscriptionDisposable {
       activeQuestionDocId = this.activeQuestionDoc.id;
     }
     if (activeQuestionDocId == null || !questionDocs.some(question => question.id === activeQuestionDocId)) {
-      if (this.projectUserConfigDoc != null && this.projectUserConfigDoc.data != null) {
-        const lastQuestionDocId = this.projectUserConfigDoc.data.selectedQuestionRef;
+      if (this._projectUserConfigDoc?.data != null) {
+        const lastQuestionDocId = this._projectUserConfigDoc.data.selectedQuestionRef;
         if (lastQuestionDocId != null) {
           questionToActivate = questionDocs.find(qd => qd.id === lastQuestionDocId);
         }
@@ -161,11 +214,11 @@ export class CheckingQuestionsComponent extends SubscriptionDisposable {
   }
 
   updateElementsRead(questionDoc: QuestionDoc): void {
-    if (this.projectUserConfigDoc == null) {
+    if (this._projectUserConfigDoc == null) {
       return;
     }
 
-    this.projectUserConfigDoc
+    this._projectUserConfigDoc
       .submitJson0Op(op => {
         if (questionDoc != null && questionDoc.data != null && !this.hasUserReadQuestion(questionDoc)) {
           op.add(puc => puc.questionRefsRead, questionDoc.data.dataId);
@@ -209,24 +262,17 @@ export class CheckingQuestionsComponent extends SubscriptionDisposable {
   }
 
   hasUserReadQuestion(questionDoc: QuestionDoc): boolean {
-    return (
-      this.projectUserConfigDoc != null &&
-      CheckingUtils.hasUserReadQuestion(questionDoc.data, this.projectUserConfigDoc.data)
-    );
+    return CheckingUtils.hasUserReadQuestion(questionDoc.data, this._projectUserConfigDoc?.data);
   }
 
   hasUserReadAnswer(answer: Answer): boolean {
-    return this.projectUserConfigDoc != null && this.projectUserConfigDoc.data != null
-      ? this.projectUserConfigDoc.data.answerRefsRead.includes(answer.dataId) ||
-          this.projectUserConfigDoc.data.ownerRef === answer.ownerRef
-      : false;
+    const config: Readonly<SFProjectUserConfig> | undefined = this._projectUserConfigDoc?.data;
+    return config != null && (config.answerRefsRead.includes(answer.dataId) || config.ownerRef === answer.ownerRef);
   }
 
   hasUserReadComment(comment: Comment): boolean {
-    return this.projectUserConfigDoc != null && this.projectUserConfigDoc.data != null
-      ? this.projectUserConfigDoc.data.commentRefsRead.includes(comment.dataId) ||
-          this.projectUserConfigDoc.data.ownerRef === comment.ownerRef
-      : false;
+    const config: Readonly<SFProjectUserConfig> | undefined = this._projectUserConfigDoc?.data;
+    return config != null && (config.commentRefsRead.includes(comment.dataId) || config.ownerRef === comment.ownerRef);
   }
 
   nextQuestion(): void {
@@ -270,7 +316,7 @@ export class CheckingQuestionsComponent extends SubscriptionDisposable {
   }
 
   private async storeMostRecentQuestion(bookNum: number): Promise<void> {
-    if (this.projectUserConfigDoc != null && this.projectUserConfigDoc.data != null) {
+    if (this._projectUserConfigDoc != null && this._projectUserConfigDoc.data != null) {
       const activeQuestionDoc = this.activeQuestionDoc;
       if (activeQuestionDoc != null && activeQuestionDoc.data != null) {
         if (
@@ -279,11 +325,11 @@ export class CheckingQuestionsComponent extends SubscriptionDisposable {
           this.project.translateConfig.source !== undefined
         ) {
           await this.translationEngineService.trainSelectedSegment(
-            this.projectUserConfigDoc.data,
+            this._projectUserConfigDoc.data,
             this.project.translateConfig.source.projectRef
           );
         }
-        await this.projectUserConfigDoc.submitJson0Op(op => {
+        await this._projectUserConfigDoc.submitJson0Op(op => {
           op.set<string>(puc => puc.selectedTask!, 'checking');
           op.set(puc => puc.selectedQuestionRef!, activeQuestionDoc.id);
           this.isAllBooksShown ? op.unset(puc => puc.selectedBookNum!) : op.set(puc => puc.selectedBookNum!, bookNum);

src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/checking.component.html
@@ -23,8 +23,8 @@
                   (changed)="questionChanged($event)"
                   [visible]="isDrawerPermanent || isExpanded"
                   [projectUserConfigDoc]="projectUserConfigDoc"
-                  [project]="projectDoc?.data"
-                  [questionDocs]="questionDocs"
+                  [projectProfileDoc]="projectDoc"
+                  [questionDocs$]="questionDocs$"
                   [isAllBooksShown]="showAllBooks"
                 ></app-checking-questions>
               </div>

src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/checking.component.ts
@@ -15,7 +15,7 @@ import { TextInfo } from 'realtime-server/lib/esm/scriptureforge/models/text-inf
 import { toVerseRef } from 'realtime-server/lib/esm/scriptureforge/models/verse-ref-data';
 import { Canon } from 'realtime-server/lib/esm/scriptureforge/scripture-utils/canon';
 import { VerseRef } from 'realtime-server/lib/esm/scriptureforge/scripture-utils/verse-ref';
-import { merge, Subscription } from 'rxjs';
+import { merge, NEVER, Observable, Subscription } from 'rxjs';
 import { DataLoadingComponent } from 'xforge-common/data-loading-component';
 import { I18nService } from 'xforge-common/i18n.service';
 import { FileType } from 'xforge-common/models/file-offline-data';
@@ -172,6 +172,10 @@ export class CheckingComponent extends DataLoadingComponent implements OnInit, O
 
   get questionDocs(): Readonly<QuestionDoc[]> {
     return this.questionsQuery?.docs || [];
+  }
+
+  get questionDocs$(): Observable<Readonly<QuestionDoc[]>> {
+    return this.questionsQuery?.docs$ || NEVER;
   }
 
   get textsByBookId(): TextsByBookId {

src/SIL.XForge.Scripture/ClientApp/src/xforge-common/memory-realtime-remote-store.ts
@@ -63,6 +63,7 @@ export class MemoryRealtimeDocAdapter implements RealtimeDocAdapter {
   readonly pendingOps: any[] = [];
   subscribed: boolean = false;
   version: number = -1;
+  readonly changes$ = new Subject<any>();
   readonly remoteChanges$ = new Subject<any>();
   readonly create$ = new Subject<void>();
   readonly delete$ = new Subject<void>();
@@ -116,6 +117,7 @@ export class MemoryRealtimeDocAdapter implements RealtimeDocAdapter {
     }
     this.data = this.type.apply(this.data, op);
     this.version++;
+    this.emitChange(op);
     if (!source) {
       this.emitRemoteChange(op);
     }
@@ -144,6 +146,10 @@ export class MemoryRealtimeDocAdapter implements RealtimeDocAdapter {
 
   destroy(): Promise<void> {
     return Promise.resolve();
+  }
+
+  emitChange(op?: any): void {
+    this.changes$.next(op);
   }
 
   emitRemoteChange(op?: any): void {

src/SIL.XForge.Scripture/ClientApp/src/xforge-common/models/realtime-doc.ts
@@ -80,6 +80,10 @@ export abstract class RealtimeDoc<T = any, Ops = any> {
     return this.adapter.remoteChanges$;
   }
 
+  get changes$(): Observable<Ops> {
+    return this.adapter.changes$;
+  }
+
   /**
    * Subscribes to remote changes for the realtime data.
    * For this record, update the RealtimeDoc cache, if any, from IndexedDB.

src/SIL.XForge.Scripture/ClientApp/src/xforge-common/realtime-remote-store.ts
@@ -32,6 +32,8 @@ export interface RealtimeDocAdapter {
   readonly create$: Observable<void>;
   readonly delete$: Observable<void>;
   /** Fires when there are changes to underlying data. */
+  readonly changes$: Observable<any>;
+  /** Fires when there are changes to underlying data that came from a different client. */
   readonly remoteChanges$: Observable<any>;
 
   create(data: any, type?: string): Promise<void>;

commit 4f0d85bd03eb6c1e1e6194f197153a5b619b3609

SF-1593 Fix checking questions being unavailable offline
This was broken in #1301 when optimizing the checking component.

src/SIL.XForge.Scripture/ClientApp/src/xforge-common/models/realtime-query.ts
@@ -180,11 +180,9 @@ export class RealtimeQuery<T extends RealtimeDoc = RealtimeDoc> {
     }
     this._unpagedCount = unpagedCount;
 
-    if (this.adapter.ready) {
-      this._docs$.next(this._docs);
-      if (changed && emitRemoteChanges) {
-        this._remoteChanges$.next();
-      }
+    this._docs$.next(this._docs);
+    if (changed && this.adapter.ready && emitRemoteChanges) {
+      this._remoteChanges$.next();
     }
   }
 