0a5b326d03559efab288c3548edcab80eb690156
Nathaniel Paulus <Nateowami@users.noreply.github.com>
2022-06-10 11:44:42 -0500

    SF-1591 Prevent error when switching to book with fewer chapters (#1390)
    - Previously, switching to a book with n chapters caused an error if the
    currently active chapter was n+1 or greater.
    - This was solved by switching the chapter select from Angular MDC to
    Angular Material.

--- src/SIL.XForge.Scripture/ClientApp/src/app/shared/chapter-nav/chapter-nav.component.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/chapter-nav/chapter-nav.component.spec.ts
@@ -1,29 +1,33 @@
-import { MdcSelect } from '@angular-mdc/web';
-import { Component, DebugElement, ViewChild } from '@angular/core';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { Component, ViewChild } from '@angular/core';
 import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { mock, when } from 'ts-mockito';
 import { I18nService } from 'xforge-common/i18n.service';
 import { configureTestingModule, TestTranslocoModule } from 'xforge-common/test-utils';
 import { UICommonModule } from 'xforge-common/ui-common.module';
+import { MatSelectHarness } from '@angular/material/select/testing';
 import { ChapterNavComponent } from './chapter-nav.component';
 
 const mockedI18nService = mock(I18nService);
 describe('ChapterNavComponent', () => {
   configureTestingModule(() => ({
-    imports: [TestTranslocoModule, UICommonModule],
+    imports: [TestTranslocoModule, UICommonModule, NoopAnimationsModule],
     declarations: [ChapterNavComponent, ChapterNavHostComponent],
     providers: [{ provide: I18nService, useMock: mockedI18nService }]
   }));
 
   it('should open to a book and chapter', fakeAsync(() => {
     const env = new TestEnvironment({ book: 2, chapter: 2 });
+    env.wait();
     expect(env.component.chapter).toEqual(2);
     expect(env.component.bookName).toBe('Book 2');
   }));
 
   it('should change book', fakeAsync(() => {
     const env = new TestEnvironment();
+    env.wait();
     expect(env.component.chapter).toEqual(1);
     expect(env.component.bookName).toBe('Book 1');
     env.hostComponent.activeBookNum = 2;
@@ -32,22 +36,28 @@ describe('ChapterNavComponent', () => {
     expect(env.component.bookName).toBe('Book 2');
   }));
 
-  it('should change chapter if selected', fakeAsync(() => {
+  it('should change chapter if selected', async () => {
     const env = new TestEnvironment();
-    expect(env.component.chapter).toEqual(1);
+    const select = await env.getSelectHarness();
+    await select.open();
+
     expect(env.hostComponent.hasChangeEmitted).toBe(false);
-    env.changeSelectValue(env.chapterSelect, 'Book 1 2');
-    expect(env.component.chapterString).toEqual('Book 1 2');
+    const options = await select.getOptions();
+    await options[1].click();
+    expect(await select.getValueText()).toBe('Book 1 2');
+    expect(env.component.chapter).toEqual(2);
     expect(env.hostComponent.hasChangeEmitted).toBe(true);
-  }));
+    await select.close();
+    env.cleanUp();
+  });
 });
 
 @Component({
   template: `
     <app-chapter-nav
       [bookNum]="activeBookNum"
-      [(chapter)]="activeChapter"
-      (chapters)="(allChapters)"
+      [chapter]="activeChapter"
+      [chapters]="allChapters"
       (chapterChange)="hasChangeEmitted = true"
     ></app-chapter-nav>
   `
@@ -62,6 +72,7 @@ class ChapterNavHostComponent {
 
 class TestEnvironment {
   readonly fixture: ComponentFixture<ChapterNavHostComponent>;
+  readonly harnessLoader: HarnessLoader;
   readonly hostComponent: ChapterNavHostComponent;
   readonly component: ChapterNavComponent;
 
@@ -69,28 +80,27 @@ class TestEnvironment {
     when(mockedI18nService.localizeBook(1)).thenReturn('Book 1');
     when(mockedI18nService.localizeBook(2)).thenReturn('Book 2');
     this.fixture = TestBed.createComponent(ChapterNavHostComponent);
+    this.harnessLoader = TestbedHarnessEnvironment.loader(this.fixture);
     this.fixture.detectChanges();
     this.hostComponent = this.fixture.componentInstance;
     this.component = this.hostComponent.chapterNavComponent;
 
     this.hostComponent.activeBookNum = start == null ? 1 : start.book;
     this.hostComponent.activeChapter = start == null ? 1 : start.chapter;
-    this.wait();
   }
 
-  get chapterSelect(): DebugElement {
-    return this.fixture.debugElement.query(By.css('.chapter-select'));
-  }
-
-  changeSelectValue(element: DebugElement, value: string): void {
-    const select: MdcSelect = element.componentInstance;
-    select.value = value;
-    this.wait();
+  async getSelectHarness(): Promise<MatSelectHarness> {
+    return await this.harnessLoader.getHarness(MatSelectHarness);
   }
 
   wait(): void {
     tick();
     this.fixture.detectChanges();
     tick();
+  }
+
+  cleanUp(): void {
+    // Hacky way to clean up the overlay container. Only needs to be called for tests that actually open the menu.
+    document.querySelector('.cdk-overlay-container')!.remove();
   }
 }
