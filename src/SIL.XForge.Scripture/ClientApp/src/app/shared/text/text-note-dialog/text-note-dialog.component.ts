6e5ab6406279ba30790d9bcce6f9ce21bd65d000
Nigel Wells <nigel-wells@hotmail.com>
2022-08-08 13:49:50 +1200

    - Defined note types as an enum - Improved type checking for EventTarget

--- src/SIL.XForge.Scripture/ClientApp/src/app/shared/text/text-note-dialog/text-note-dialog.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/text/text-note-dialog/text-note-dialog.component.ts
@@ -2,14 +2,19 @@ import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { TranslocoService } from '@ngneat/transloco';
 
+export enum TextNoteType {
+  Footnote = 'f',
+  EndNote = 'fe',
+  CrossReference = 'x'
+}
+
 export interface NoteDialogData {
-  type: 'f' | 'fe' | 'x';
+  type: TextNoteType;
   text: string;
   isRightToLeft: boolean;
 }
 
 @Component({
-  selector: 'app-text-note-dialog',
   templateUrl: './text-note-dialog.component.html',
   styleUrls: ['./text-note-dialog.component.scss']
 })
@@ -28,14 +33,17 @@ export class TextNoteDialogComponent {
   }
 
   get type(): string {
+    let translateKey = this.data.type.toString();
     switch (this.data.type) {
-      case 'f':
-        return this.translocoService.translate('text_note_dialog.footnote');
-      case 'fe':
-        return this.translocoService.translate('text_note_dialog.end_note');
-      case 'x':
-        return this.translocoService.translate('text_note_dialog.cross_reference');
+      case TextNoteType.Footnote:
+        translateKey = 'footnote';
+        break;
+      case TextNoteType.EndNote:
+        translateKey = 'end_note';
+        break;
+      case TextNoteType.CrossReference:
+        translateKey = 'cross_reference';
     }
-    return this.data.type;
+    return this.translocoService.translate(`text_note_dialog.${translateKey}`);
   }
 }
