ac505c79fa9b6637d268589fccd82bdcc45acfbc
Peter Chapman <peter@conglomo.co.nz>
2022-08-12 20:25:13 +1200

    Added status, error, and prequisite sync metrics

---
+++ src/SIL.XForge.Scripture/Models/SyncStatus.cs
@@ -0,0 +1,14 @@
+namespace SIL.XForge.Scripture.Models
+{
+    /// <summary>
+    /// The status of a sync for the <see cref="SyncMetrics"/>.
+    /// </summary>
+    public enum SyncStatus
+    {
+        Queued,
+        Running,
+        Successful,
+        Cancelled,
+        Failed,
+    }
+}
