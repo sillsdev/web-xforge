ae727fb55ba11d94ff35a708c1072cdaf7372713
Peter Chapman <peter@conglomo.co.nz>
2022-08-13 10:33:33 +1200

    Added more basic metrics

--- src/SIL.XForge.Scripture/Models/SyncMetrics.cs
+++ src/SIL.XForge.Scripture/Models/SyncMetrics.cs
@@ -8,6 +8,7 @@ namespace SIL.XForge.Scripture.Models
     /// </summary>
     public class SyncMetrics : IIdentifiable
     {
+        // Sync Details
         public DateTime? DateFinished { get; set; }
         public DateTime DateQueued { get; set; }
         public DateTime? DateStarted { get; set; }
@@ -17,5 +18,10 @@ namespace SIL.XForge.Scripture.Models
         public string ProjectRef { get; set; }
         public SyncStatus Status { get; set; }
         public string UserRef { get; set; }
+
+        // Sync Statistics
+        public bool RepositoryBackupCreated { get; set; }
+        public bool RepositoryRestoredFromBackup { get; set; }
+        public int UsersRemoved { get; set; }
     }
 }
