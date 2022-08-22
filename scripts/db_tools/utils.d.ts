b42516501be61bbe4efb63d610e04d50cd1d79f9
MarkS <marksvc@gmail.com>
2022-08-22 16:47:14 -0600

    tool: add deleteDoc to TS API for scripts
    commit-id:cc1d4c2c

--- scripts/db_tools/utils.d.ts
+++ scripts/db_tools/utils.d.ts
@@ -12,6 +12,7 @@ export function fetchSnapshotByVersion(
   version: number
 ): Promise<Snapshot>;
 export function fetchDoc(doc: Doc): Promise<void>;
+export function deleteDoc(doc: Doc): Promise<void>;
 export function submitDocOp(doc: Doc, op: any): Promise<void>;
 export function colored(colorCode: number, textToColor: string): string;
 export type ConnectionSettings = { dbLocation: string; wsConnectionString: string; origin: string };
