acc7efb59c3b122ec8ec604810764706d5b508ae
Raymond Luong <raymond_luong@sil.org>
2022-09-14 13:37:31 -0600

    Return undefined if the user closes the dialog

--- src/RealtimeServer/scriptureforge/models/note.ts
+++ src/RealtimeServer/scriptureforge/models/note.ts
@@ -7,6 +7,10 @@ export interface Note extends Comment {
   threadId: string;
   type: string;
   conflictType: string;
+  /**
+   * Allows Paratext to keep a record of the SF user who created the note.
+   * At the moment, this is the same as the owner ref.
+   */
   extUserId: string;
   deleted: boolean;
   status: NoteStatus;
