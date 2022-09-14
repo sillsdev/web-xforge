15489c3cec85a39005d8810ad0a95ae23ac458af
Nathaniel Paulus <nathaniel_paulus@sil.org>
2022-09-14 13:43:07 -0400

    Revise Angular 13 upgrade PR

--- src/SIL.XForge.Scripture/ClientApp/src/utils.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/utils.ts
@@ -3,7 +3,7 @@ export function isObj(value: unknown): value is {} {
 }
 
 export function hasProp<X, Y extends PropertyKey>(value: X, property: Y): value is X & Record<Y, unknown> {
-  return Object.prototype.hasOwnProperty.call(value, property);
+  return isObj(value) && Object.prototype.hasOwnProperty.call(value, property);
 }
 
 export function hasFunctionProp<X, Y extends PropertyKey>(value: X, property: Y): value is X & Record<Y, Function> {
