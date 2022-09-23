83f3f002c8c6f4fda3a6b3be581d2f9e5d4c479b
Peter Chapman <peter@conglomo.co.nz>
2022-09-24 10:48:33 +1200

    Added machine configuration, started on API client

---
+++ src/SIL.XForge/Configuration/MachineOptions.cs
@@ -0,0 +1,11 @@
+namespace SIL.XForge.Configuration
+{
+    public class MachineOptions
+    {
+        public string ApiServer { get; set; }
+        public string Audience { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string TokenUrl { get; set; }
+    }
+}
