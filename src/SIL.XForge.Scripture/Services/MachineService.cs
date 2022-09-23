01e485bfecc893d291bc2ff99d6237f0000b473e
Peter Chapman <peter@conglomo.co.nz>
2022-09-24 10:48:33 +1200

    Added machine configuration, started on API client

--- src/SIL.XForge.Scripture/Services/MachineService.cs
+++ src/SIL.XForge.Scripture/Services/MachineService.cs
@@ -1,21 +1,60 @@
+using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using SIL.Machine.WebApi.Services;
+using SIL.ObjectModel;
+using SIL.XForge.Configuration;
 using SIL.XForge.Realtime;
 using SIL.XForge.Scripture.Models;
 using SIL.XForge.Services;
 using MachineProject = SIL.Machine.WebApi.Models.Project;
 
+#nullable enable
+
 namespace SIL.XForge.Scripture.Services
 {
-    public class MachineService : IMachineService
+    public class MachineService : DisposableBase, IMachineService
     {
         private readonly IEngineService _engineService;
+        private readonly HttpClientHandler _httpClientHandler;
+        private readonly HttpClient? _machineClient;
         private readonly IRealtimeService _realtimeService;
 
-        public MachineService(IEngineService engineService, IRealtimeService realtimeService)
+        public MachineService(
+            IEngineService engineService,
+            IWebHostEnvironment env,
+            ILogger<MachineService> logger,
+            IOptions<MachineOptions> machineOptions,
+            IRealtimeService realtimeService
+        )
         {
             _engineService = engineService;
             _realtimeService = realtimeService;
+            _httpClientHandler = new HttpClientHandler();
+
+            // Verify that we have the required machine options
+            if (
+                string.IsNullOrWhiteSpace(machineOptions.Value.ApiServer)
+                || string.IsNullOrWhiteSpace(machineOptions.Value.Audience)
+                || string.IsNullOrWhiteSpace(machineOptions.Value.ClientId)
+                || string.IsNullOrWhiteSpace(machineOptions.Value.ClientSecret)
+                || string.IsNullOrWhiteSpace(machineOptions.Value.TokenUrl)
+            )
+            {
+                logger.LogWarning("Machine API not configured - using in-memory instance.");
+                return;
+            }
+
+            // Setup the HTTP Client
+            _machineClient = new HttpClient(_httpClientHandler);
+            if (env.IsDevelopment() || env.IsEnvironment("Testing"))
+            {
+                _httpClientHandler.ServerCertificateCustomValidationCallback =
+                    HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            }
         }
 
         public async Task AddProjectAsync(string curUserId, string projectId)
@@ -53,6 +92,12 @@ namespace SIL.XForge.Scripture.Services
             await _engineService.RemoveProjectAsync(projectId);
 
             // TODO: Remove the project from the Machine API
+        }
+
+        protected override void DisposeManagedResources()
+        {
+            _machineClient?.Dispose();
+            _httpClientHandler.Dispose();
         }
     }
 }
