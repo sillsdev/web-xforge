e5543a0c02b9d7df49ca5f36107b6f9a95e037ac
Nigel Wells <nigel-wells@hotmail.com>
2022-09-27 14:25:15 +1300

    - Removed commented code - Refactored test for setting browser and websocket online status

--- src/SIL.XForge.Scripture/ClientApp/src/xforge-common/pwa.service.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/pwa.service.ts
@@ -39,7 +39,6 @@ export class PwaService extends SubscriptionDisposable {
       // we have a valid web socket connection OR
       //    the web socket hasn't yet had a chance to connect i.e. (null) when the app first loads
       this.appOnlineStatus.next(this.windowOnLineStatus.getValue() && this.webSocketStatus.getValue() !== false);
-      // this.appOnlineStatus.next(this.windowOnLineStatus.getValue());
     });
   }
 
