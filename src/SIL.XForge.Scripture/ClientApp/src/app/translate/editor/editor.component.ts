2a832d95c2e24d8bd77701ecb2cc8bd1b839cf66
Nigel Wells <nigel-wells@hotmail.com>
2022-10-03 14:13:12 +1300

    SF-1727 - Make presence avatars clickable and scroll to position in the document - Clicking avatar in the toolbar will trigger a scroll to the segment of that viewer

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/editor.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/editor.component.ts
@@ -1567,4 +1567,8 @@ export class EditorComponent extends DataLoadingComponent implements OnDestroy, 
     const otherBounds = this.source.editor.selection.getBounds(otherRange.index);
     this.source.editor.scrollingContainer.scrollTop += otherBounds.top - thisBounds.top;
   }
+
+  viewerClicked(viewer: MultiCursorViewer) {
+    this.target!.scrollToViewer(viewer);
+  }
 }
