db899499d3c119f1ee216154765a84b98b984984
Peter Chapman <peter@conglomo.co.nz>
2022-10-08 10:58:29 +1300

    Added Corpus upload of a Paratext backup zip file

--- src/SIL.XForge.Scripture/Services/IMachineCorporaService.cs
+++ src/SIL.XForge.Scripture/Services/IMachineCorporaService.cs
@@ -1,9 +1,24 @@
+#nullable enable
+
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SIL.XForge.Scripture.Services
 {
     public interface IMachineCorporaService
     {
-        Task<string> AddCorporaAsync(string name);
+        Task<string> AddCorpusAsync(string name, CancellationToken cancellationToken);
+        Task<bool> AddCorpusToTranslationEngineAsync(
+            string translationEngineId,
+            string corpusId,
+            bool pretranslate,
+            CancellationToken cancellationToken
+        );
+        Task<string> UploadParatextCorpusAsync(
+            string corpusId,
+            string languageTag,
+            string path,
+            CancellationToken cancellationToken
+        );
     }
 }
