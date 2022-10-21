5f4656f571bfa001115e858197068d1bc72ee24d
Peter Chapman <peter@conglomo.co.nz>
2022-10-22 11:18:13 +1300

    Fixed bugs where invalid data sent to Machine API

--- src/SIL.XForge.Scripture/Services/IMachineProjectService.cs
+++ src/SIL.XForge.Scripture/Services/IMachineProjectService.cs
@@ -8,8 +8,13 @@ namespace SIL.XForge.Scripture.Services
     public interface IMachineProjectService
     {
         Task AddProjectAsync(string curUserId, string projectId, CancellationToken cancellationToken);
-        Task BuildProjectAsync(string curUserId, string projectId, CancellationToken cancellationToken);
+        Task BuildProjectAsync(
+            string curUserId,
+            string projectId,
+            bool trainInMemoryEngine,
+            CancellationToken cancellationToken
+        );
         Task RemoveProjectAsync(string curUserId, string projectId, CancellationToken cancellationToken);
-        Task SyncProjectCorporaAsync(string curUserId, string projectId, CancellationToken cancellationToken);
+        Task<bool> SyncProjectCorporaAsync(string curUserId, string projectId, CancellationToken cancellationToken);
     }
 }
