2271d543f46df59edc614c7eff34cbf84c6fa8bb
Nathaniel Paulus <nathaniel_paulus@sil.org>
2022-11-01 13:47:19 -0400

    SF-1617 Allow importing question from TSV file (#1562)

--- src/SIL.XForge.Scripture/ClientApp/src/xforge-common/csv-service.service.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/csv-service.service.ts
@@ -6,6 +6,12 @@ import Papa from 'papaparse';
 })
 export class CsvService {
   parse(file: File): Promise<string[][]> {
-    return new Promise(resolve => Papa.parse(file, { complete: result => resolve(result.data as string[][]) }));
+    // Papa Parse can auto-detect delimiters, but only for files with 10+ lines, and will fall back to comma if it can't
+    // auto detect the delimiter. We can work around this by specifying the delimiter when the file extension is TSV.
+    const delimiter = /.tsv$/i.test(file.name) ? '\t' : undefined;
+
+    return new Promise(resolve =>
+      Papa.parse(file, { delimiter, complete: result => resolve(result.data as string[][]) })
+    );
   }
 }
