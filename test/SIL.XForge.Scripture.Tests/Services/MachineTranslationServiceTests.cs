9b21476d101a64f491127f3ba23da89a65172283
Peter Chapman <peter@conglomo.co.nz>
2022-11-21 09:13:00 +1300

    Added translation C# API endpoints

---
+++ test/SIL.XForge.Scripture.Tests/Services/MachineTranslationServiceTests.cs
@@ -0,0 +1,334 @@
+using System.Net;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using SIL.Machine.WebApi;
+
+namespace SIL.XForge.Scripture.Services
+{
+    [TestFixture]
+    public class MachineTranslationServiceTests
+    {
+        private static readonly string TranslationEngine01 = "translationEngine01";
+
+        [Test]
+        public async Task GetWordGraphAsync_Success()
+        {
+            // Set up a mock Machine API
+            var segment = new[] { "Test", "Data" };
+            string response =
+                $@"{{
+                    ""initialStateScore"": -91.43696,
+                    ""finalStates"": [
+                    2
+                    ],
+                    ""arcs"": [
+                    {{
+                      ""prevState"": 0,
+                      ""nextState"": 1,
+                      ""score"": -9.8753,
+                      ""words"": [
+                        ""{segment.First().ToLower()}""
+                      ],
+                      ""confidences"": [
+                        0.9980028
+                      ],
+                      ""sourceSegmentRange"": {{
+                        ""start"": 0,
+                        ""end"": 1
+                      }},
+                      ""alignment"": [
+                        {{
+                          ""sourceIndex"": 0,
+                          ""targetIndex"": 0
+                        }}
+                      ],
+                      ""sources"": [
+                        ""Smt""
+                      ]
+                    }},
+                    {{
+                      ""prevState"": 1,
+                      ""nextState"": 2,
+                      ""score"": 41.6542,
+                      ""words"": [
+                        ""{segment.Last().ToLower()}""
+                      ],
+                      ""confidences"": [
+                        0.0012210013
+                      ],
+                      ""sourceSegmentRange"": {{
+                        ""start"": 1,
+                        ""end"": 2
+                      }},
+                      ""alignment"": [
+                        {{
+                          ""sourceIndex"": 0,
+                          ""targetIndex"": 0
+                        }}
+                      ],
+                      ""sources"": [
+                        ""None""
+                      ]
+                    }}
+                    ]
+                }}";
+            var handler = new MockHttpMessageHandler(response, HttpStatusCode.OK);
+            var httpClient = new HttpClient(handler) { BaseAddress = new Uri("http://localhost") };
+
+            // Set up test environment
+            var env = new TestEnvironment(httpClient);
+
+            // SUT
+            WordGraphDto actual = await env.Service.GetWordGraphAsync(
+                TranslationEngine01,
+                segment,
+                CancellationToken.None
+            );
+
+            Assert.AreEqual(1, handler.NumberOfCalls);
+            Assert.AreEqual(segment.Length, actual.Arcs.Length);
+            Assert.AreEqual(segment.First().ToLower(), actual.Arcs.First().Words.First().ToLower());
+            Assert.AreEqual(segment.Last().ToLower(), actual.Arcs.Last().Words.First().ToLower());
+        }
+
+        [Test]
+        public void GetWordGraphAsync_NoPermission()
+        {
+            // Set up a mock Machine API
+            string response = string.Empty;
+            var handler = new MockHttpMessageHandler(response, HttpStatusCode.Forbidden);
+            var httpClient = new HttpClient(handler) { BaseAddress = new Uri("http://localhost") };
+
+            // Set up test environment
+            var env = new TestEnvironment(httpClient);
+            var segment = new[] { "Test", "Data" };
+
+            // SUT
+            Assert.ThrowsAsync<HttpRequestException>(
+                () => env.Service.GetWordGraphAsync(TranslationEngine01, segment, CancellationToken.None)
+            );
+        }
+
+        [Test]
+        public async Task TrainSegmentAsync_Success()
+        {
+            // Set up a mock Machine API
+            string response = string.Empty;
+            var handler = new MockHttpMessageHandler(response, HttpStatusCode.OK);
+            var httpClient = new HttpClient(handler) { BaseAddress = new Uri("http://localhost") };
+
+            // Set up test environment
+            var env = new TestEnvironment(httpClient);
+            var segmentPairDto = new SegmentPairDto
+            {
+                SentenceStart = true,
+                SourceSegment = new[] { "Test", "Data" },
+                TargetSegment = new[] { "Test", "Data" },
+            };
+
+            // SUT
+            await env.Service.TrainSegmentAsync(TranslationEngine01, segmentPairDto, CancellationToken.None);
+
+            Assert.AreEqual(1, handler.NumberOfCalls);
+        }
+
+        [Test]
+        public void TrainSegmentAsync_NoPermission()
+        {
+            // Set up a mock Machine API
+            string response = string.Empty;
+            var handler = new MockHttpMessageHandler(response, HttpStatusCode.Forbidden);
+            var httpClient = new HttpClient(handler) { BaseAddress = new Uri("http://localhost") };
+
+            // Set up test environment
+            var env = new TestEnvironment(httpClient);
+            var segmentPairDto = new SegmentPairDto
+            {
+                SentenceStart = true,
+                SourceSegment = new[] { "Test", "Data" },
+                TargetSegment = new[] { "Test", "Data" },
+            };
+
+            // SUT
+            Assert.ThrowsAsync<HttpRequestException>(
+                () => env.Service.TrainSegmentAsync(TranslationEngine01, segmentPairDto, CancellationToken.None)
+            );
+        }
+
+        [Test]
+        public async Task TranslateAsync_Success()
+        {
+            // Set up a mock Machine API
+            var segment = new[] { "Test", "Data" };
+            string response =
+                $@"{{
+                    ""target"": [
+                      ""{segment.First()}"",
+                      ""{segment.Last()}""
+                    ],
+                    ""confidences"": [
+                      0.97835666,
+                      0.9998312
+                    ],
+                    ""sources"": [
+                      ""Smt"",
+                      ""Smt""
+                    ],
+                    ""alignment"": [
+                    {{
+                      ""sourceIndex"": 0,
+                      ""targetIndex"": 0
+                    }},
+                    {{
+                      ""sourceIndex"": 1,
+                      ""targetIndex"": 1
+                    }}
+                    ],
+                    ""phrases"": [
+                    {{
+                      ""sourceSegmentRange"": {{
+                        ""start"": 0,
+                        ""end"": 2
+                      }},
+                      ""targetSegmentCut"": 2,
+                      ""confidence"": 0.9783566679198871
+                    }}
+                    ]
+                }}";
+            var handler = new MockHttpMessageHandler(response, HttpStatusCode.OK);
+            var httpClient = new HttpClient(handler) { BaseAddress = new Uri("http://localhost") };
+
+            // Set up test environment
+            var env = new TestEnvironment(httpClient);
+
+            // SUT
+            TranslationResultDto actual = await env.Service.TranslateAsync(
+                TranslationEngine01,
+                segment,
+                CancellationToken.None
+            );
+
+            Assert.AreEqual(1, handler.NumberOfCalls);
+            Assert.AreEqual(segment.Length, actual.Target.Length);
+            Assert.AreEqual(segment.First(), actual.Target.First());
+            Assert.AreEqual(segment.Last(), actual.Target.Last());
+        }
+
+        [Test]
+        public void TranslateAsync_NoPermission()
+        {
+            // Set up a mock Machine API
+            string response = string.Empty;
+            var handler = new MockHttpMessageHandler(response, HttpStatusCode.Forbidden);
+            var httpClient = new HttpClient(handler) { BaseAddress = new Uri("http://localhost") };
+
+            // Set up test environment
+            var env = new TestEnvironment(httpClient);
+            var segment = new[] { "Test", "Data" };
+
+            // SUT
+            Assert.ThrowsAsync<HttpRequestException>(
+                () => env.Service.TranslateAsync(TranslationEngine01, segment, CancellationToken.None)
+            );
+        }
+
+        [Test]
+        public async Task TranslateNAsync_Success()
+        {
+            // Set up a mock Machine API
+            var segment = new[] { "Test", "Data" };
+            string response =
+                $@"[
+                    {{
+                      ""target"": [
+                        ""{segment.First()}"",
+                        ""{segment.Last()}""
+                      ],
+                      ""confidences"": [
+                        0.97835666,
+                        0.9998312
+                      ],
+                      ""sources"": [
+                        ""Smt"",
+                        ""Smt""
+                      ],
+                      ""alignment"": [
+                        {{
+                          ""sourceIndex"": 0,
+                          ""targetIndex"": 0
+                        }},
+                        {{
+                          ""sourceIndex"": 1,
+                          ""targetIndex"": 1
+                        }}
+                      ],
+                      ""phrases"": [
+                        {{
+                          ""sourceSegmentRange"": {{
+                            ""start"": 0,
+                            ""end"": 2
+                          }},
+                          ""targetSegmentCut"": 2,
+                          ""confidence"": 0.9783566679198871
+                        }}
+                      ]
+                    }}
+                ]";
+            var handler = new MockHttpMessageHandler(response, HttpStatusCode.OK);
+            var httpClient = new HttpClient(handler) { BaseAddress = new Uri("http://localhost") };
+
+            // Set up test environment
+            var env = new TestEnvironment(httpClient);
+
+            // SUT
+            TranslationResultDto[] actual = await env.Service.TranslateNAsync(
+                TranslationEngine01,
+                n: 2,
+                segment,
+                CancellationToken.None
+            );
+
+            Assert.AreEqual(1, handler.NumberOfCalls);
+            Assert.AreEqual(segment.Length, actual.First().Target.Length);
+            Assert.AreEqual(segment.First(), actual.First().Target.First());
+            Assert.AreEqual(segment.Last(), actual.First().Target.Last());
+        }
+
+        [Test]
+        public void TranslateNAsync_NoPermission()
+        {
+            // Set up a mock Machine API
+            string response = string.Empty;
+            var handler = new MockHttpMessageHandler(response, HttpStatusCode.Forbidden);
+            var httpClient = new HttpClient(handler) { BaseAddress = new Uri("http://localhost") };
+
+            // Set up test environment
+            var env = new TestEnvironment(httpClient);
+            var segment = new[] { "Test", "Data" };
+
+            // SUT
+            Assert.ThrowsAsync<HttpRequestException>(
+                () => env.Service.TranslateNAsync(TranslationEngine01, n: 2, segment, CancellationToken.None)
+            );
+        }
+
+        private class TestEnvironment
+        {
+            public TestEnvironment(HttpClient? httpClient = default)
+            {
+                var httpClientFactory = Substitute.For<IHttpClientFactory>();
+                httpClientFactory.CreateClient(Arg.Any<string>()).Returns(httpClient);
+                var logger = new MockLogger<MachineTranslationService>();
+
+                Service = new MachineTranslationService(httpClientFactory, logger);
+            }
+
+            public MachineTranslationService Service { get; }
+        }
+    }
+}
