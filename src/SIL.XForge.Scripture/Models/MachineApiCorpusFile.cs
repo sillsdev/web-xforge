2bda4e2f61b09d1c6e9a2dd12a1bfa309722c675
Peter Chapman <peter@conglomo.co.nz>
2022-11-21 14:57:06 +1300

    Bug fixes, code clean up, and refactoring

--- src/SIL.XForge.Scripture/Models/MachineApiCorpusFile.cs
+++ src/SIL.XForge.Scripture/Models/MachineApiCorpusFile.cs
@@ -3,6 +3,9 @@ namespace SIL.XForge.Scripture.Models
     /// <summary>
     /// The details of a corpus file from the Machine API.
     /// </summary>
+    /// <remarks>
+    /// TODO: When Machine >= 2.5.12, change any code that uses this to use DataFileDto
+    /// </remarks>
     public class MachineApiCorpusFile
     {
         public string Id { get; set; } = string.Empty;
