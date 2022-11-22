64fbca2b9d6641b8a2afdc9bfaba530a255887cb
Peter Chapman <peter@conglomo.co.nz>
2022-11-23 09:51:51 +1300

    Code refactoring & added GetTranslationEngineAsync

--- src/SIL.XForge.Scripture/Services/MachineTranslationService.cs
+++ src/SIL.XForge.Scripture/Services/MachineTranslationService.cs
@@ -5,6 +5,7 @@ using System.Net.Http.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using SIL.Machine.WebApi;
+using SIL.XForge.Scripture.Models;
 
 namespace SIL.XForge.Scripture.Services
 {
@@ -16,6 +17,71 @@ namespace SIL.XForge.Scripture.Services
             : base(httpClientFactory)
         {
             _exceptionHandler = exceptionHandler;
+        }
+
+        public async Task<string> CreateTranslationEngineAsync(
+            string name,
+            string sourceLanguageTag,
+            string targetLanguageTag,
+            CancellationToken cancellationToken
+        )
+        {
+            // TODO: When Machine >= 2.5.12, change the anonymous object to TranslationEngineConfigDto
+            const string requestUri = "translation-engines";
+            using var response = await MachineClient.PostAsJsonAsync(
+                requestUri,
+                new { name, sourceLanguageTag, targetLanguageTag, type = "SmtTransfer" },
+                cancellationToken
+            );
+            await _exceptionHandler.EnsureSuccessStatusCode(response);
+
+            try
+            {
+                var translationEngine = await ReadAnonymousObjectFromJsonAsync(
+                    response,
+                    new { id = string.Empty },
+                    Options,
+                    cancellationToken
+                );
+                return translationEngine?.id ?? string.Empty;
+            }
+            catch (Exception e)
+            {
+                throw new HttpRequestException(await ExceptionHandler.CreateHttpRequestErrorMessage(response), e);
+            }
+        }
+
+        public async Task DeleteTranslationEngineAsync(string translationEngineId, CancellationToken cancellationToken)
+        {
+            // Delete the translation engine from the Machine API
+            ValidateId(translationEngineId);
+            string requestUri = $"translation-engines/{translationEngineId}";
+            using var response = await MachineClient.DeleteAsync(requestUri, cancellationToken);
+            await _exceptionHandler.EnsureSuccessStatusCode(response);
+        }
+
+        public async Task<MachineApiTranslationEngine> GetTranslationEngineAsync(
+            string translationEngineId,
+            CancellationToken cancellationToken
+        )
+        {
+            ValidateId(translationEngineId);
+
+            string requestUri = $"translation-engines/{translationEngineId}";
+            using var response = await MachineClient.GetAsync(requestUri, cancellationToken);
+            await _exceptionHandler.EnsureSuccessStatusCode(response);
+
+            // Return the translation engine
+            try
+            {
+                return (
+                    await response.Content.ReadFromJsonAsync<MachineApiTranslationEngine>(Options, cancellationToken)
+                )!;
+            }
+            catch (Exception e)
+            {
+                throw new HttpRequestException(await ExceptionHandler.CreateHttpRequestErrorMessage(response), e);
+            }
         }
 
         public async Task<WordGraphDto> GetWordGraphAsync(
