b21cea6c307746c83d27393addc10adbc5d670b8
Nigel Wells <nigel-wells@hotmail.com>
2022-11-25 10:14:53 +1300

    - Added test to ensure hasUpdate emits an expected event

--- src/SIL.XForge.Scripture/ClientApp/src/xforge-common/pwa.service.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/pwa.service.spec.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { fakeAsync, flush, tick } from '@angular/core/testing';
-import { SwUpdate } from '@angular/service-worker';
-import { instance, mock, verify } from 'ts-mockito';
+import { SwUpdate, VersionEvent, VersionReadyEvent } from '@angular/service-worker';
+import { instance, mock, verify, when } from 'ts-mockito';
+import { Subject } from 'rxjs';
 import { LocationService } from './location.service';
 import { PWA_CHECK_FOR_UPDATES, PwaService } from './pwa.service';
 
@@ -98,11 +99,24 @@ describe('PwaService', () => {
     expect().nothing();
     env.dispose();
   }));
+
+  it('hasUpdate should emit on VERSION_READY', () => {
+    const env = new TestEnvironment();
+    let isVersionReady = false;
+    env.pwaService.hasUpdate.subscribe((event: VersionReadyEvent) => {
+      expect(event.type).toEqual('VERSION_READY');
+      isVersionReady = true;
+    });
+    env.triggerVersionEvent('VERSION_READY');
+    expect(isVersionReady).toEqual(true);
+    env.dispose();
+  });
 });
 
 class TestEnvironment {
   readonly pwaService: PwaService;
   private navigatorOnline: boolean = true;
+  private versionUpdates$: Subject<VersionEvent> = new Subject<VersionEvent>();
 
   constructor() {
     this.pwaService = new PwaService(
@@ -111,6 +125,7 @@ class TestEnvironment {
       instance(mockedLocationService)
     );
     spyOnProperty(window.navigator, 'onLine').and.returnValue(this.navigatorOnline);
+    when(mockedSwUpdate.versionUpdates).thenReturn(this.versionUpdates$);
   }
 
   set onlineStatus(status: boolean) {
@@ -120,5 +135,9 @@ class TestEnvironment {
 
   dispose() {
     this.pwaService.dispose();
+  }
+
+  triggerVersionEvent(type: 'VERSION_DETECTED' | 'VERSION_READY' | 'VERSION_INSTALLATION_FAILED') {
+    this.versionUpdates$.next({ currentVersion: {}, latestVersion: {}, type } as VersionEvent);
   }
 }
