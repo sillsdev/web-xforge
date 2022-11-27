978e7a0f9f8eaf019b42f6ba31f47f683e823621
Peter Chapman <peter@conglomo.co.nz>
2022-11-28 12:52:52 +1300

    Removed unnecessary BuildProjectAsync parameter

--- src/SIL.XForge.Scripture/Services/IMachineProjectService.cs
+++ src/SIL.XForge.Scripture/Services/IMachineProjectService.cs
@@ -6,12 +6,7 @@ namespace SIL.XForge.Scripture.Services
     public interface IMachineProjectService
     {
         Task AddProjectAsync(string curUserId, string projectId, CancellationToken cancellationToken);
-        Task BuildProjectAsync(
-            string curUserId,
-            string projectId,
-            bool trainInMemoryEngine,
-            CancellationToken cancellationToken
-        );
+        Task BuildProjectAsync(string curUserId, string projectId, CancellationToken cancellationToken);
         Task RemoveProjectAsync(string curUserId, string projectId, CancellationToken cancellationToken);
         Task<bool> SyncProjectCorporaAsync(string curUserId, string projectId, CancellationToken cancellationToken);
     }
