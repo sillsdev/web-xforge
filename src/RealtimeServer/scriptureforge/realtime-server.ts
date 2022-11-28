a95278758c74b560f82e82a1a747f79b13d44664
Raymond Luong <raymond_luong@sil.org>
2022-11-28 12:56:29 -0700

    Backend parameter

--- src/RealtimeServer/scriptureforge/realtime-server.ts
+++ src/RealtimeServer/scriptureforge/realtime-server.ts
@@ -3,6 +3,7 @@ import { RealtimeServer } from '../common/realtime-server';
 import { SchemaVersionRepository } from '../common/schema-version-repository';
 import { DocService } from '../common/services/doc-service';
 import { UserService } from '../common/services/user-service';
+import { NOTE_THREAD_COLLECTION, SF_NOTE_THREAD_PREFIX } from './models/note-thread';
 import { SF_PROJECTS_COLLECTION } from './models/sf-project';
 import { NoteThreadService } from './services/note-thread-service';
 import { QuestionService } from './services/question-service';
@@ -25,6 +26,12 @@ const SF_DOC_SERVICES: DocService[] = [
 class SFRealtimeServer extends RealtimeServer {
   constructor(siteId: string, migrationsDisabled: boolean, db: ShareDB.DB, schemaVersions: SchemaVersionRepository) {
     super(siteId, migrationsDisabled, SF_DOC_SERVICES, SF_PROJECTS_COLLECTION, db, schemaVersions);
+    this.use('query', (context: ShareDB.middleware.QueryContext, next: (err?: any) => void) => {
+      if (context.collection === NOTE_THREAD_COLLECTION) {
+        context.query = { ...context.query, ...{ dataId: { $regex: SF_NOTE_THREAD_PREFIX } } };
+      }
+      next();
+    });
   }
 }
 
