29b6fc89fcef2c6e7205ab9e772a86db3004c5f9
Peter Chapman <peter@conglomo.co.nz>
2022-11-29 11:05:17 +1300

    Both Machine instances are run at the same time

--- test/SIL.XForge.Scripture.Tests/Services/MachineProjectServiceTests.cs
+++ test/SIL.XForge.Scripture.Tests/Services/MachineProjectServiceTests.cs
@@ -36,12 +36,7 @@ namespace SIL.XForge.Scripture.Services
             // Set up test environment
             var env = new TestEnvironment();
             env.MachineTranslationService
-                .CreateTranslationEngineAsync(
-                    Project01,
-                    Arg.Any<string>(),
-                    Arg.Any<string>(),
-                    Arg.Any<CancellationToken>()
-                )
+                .CreateTranslationEngineAsync(Project01, Arg.Any<string>(), Arg.Any<string>(), CancellationToken.None)
                 .Returns(Task.FromResult(TranslationEngine01));
 
             // SUT
@@ -79,12 +74,7 @@ namespace SIL.XForge.Scripture.Services
             var env = new TestEnvironment();
             env.FeatureManager.IsEnabledAsync(FeatureFlags.MachineInMemory).Returns(Task.FromResult(false));
             env.MachineTranslationService
-                .CreateTranslationEngineAsync(
-                    Project01,
-                    Arg.Any<string>(),
-                    Arg.Any<string>(),
-                    Arg.Any<CancellationToken>()
-                )
+                .CreateTranslationEngineAsync(Project01, Arg.Any<string>(), Arg.Any<string>(), CancellationToken.None)
                 .Returns(Task.FromResult(TranslationEngine01));
 
             // SUT
