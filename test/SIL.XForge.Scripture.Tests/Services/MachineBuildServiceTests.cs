e528b9fd5e65028647e5c6741cd2f446d74e7222
Peter Chapman <peter@conglomo.co.nz>
2022-11-29 16:00:04 +1300

    Added support to get build by ID

--- test/SIL.XForge.Scripture.Tests/Services/MachineBuildServiceTests.cs
+++ test/SIL.XForge.Scripture.Tests/Services/MachineBuildServiceTests.cs
@@ -13,6 +13,7 @@ namespace SIL.XForge.Scripture.Services
     [TestFixture]
     public class MachineBuildServiceTests
     {
+        private const string Build01 = "build01";
         private const string TranslationEngine01 = "translationEngine01";
 
         [Test]
@@ -50,15 +51,14 @@ namespace SIL.XForge.Scripture.Services
         }
 
         [Test]
-        public async Task GetCurrentBuildAsync_Success()
+        public async Task GetBuildAsync_Success()
         {
             // Set up a mock Machine API
-            string buildId = "633fdb281a2e7ac760f7193a";
             string state = "Active";
             string message = "Finalizing";
             int revision = 553;
             double percentCompleted = 0.95;
-            string href = $"/translation-engines/{TranslationEngine01}/builds/{buildId}";
+            string href = $"/translation-engines/{TranslationEngine01}/builds/{Build01}";
             string response =
                 @$"{{
                     ""revision"": {revision},
@@ -70,11 +70,162 @@ namespace SIL.XForge.Scripture.Services
                     ""percentCompleted"": {percentCompleted},
                     ""message"": ""{message}"",
                     ""state"": ""{state}"",
-                    ""id"": ""{buildId}"",
+                    ""id"": ""{Build01}"",
                     ""href"": ""{href}""
                     }}";
             var handler = new MockHttpMessageHandler(response, HttpStatusCode.OK);
             var httpClient = TestEnvironment.CreateHttpClient(handler);
+
+            // Set up test environment
+            var env = new TestEnvironment(httpClient);
+
+            // SUT
+            BuildDto? actual = await env.Service.GetBuildAsync(
+                TranslationEngine01,
+                Build01,
+                minRevision: null,
+                CancellationToken.None
+            );
+
+            Assert.NotNull(actual);
+            Assert.AreEqual(Build01, actual.Id);
+            Assert.AreEqual(percentCompleted, actual.PercentCompleted);
+            Assert.AreEqual(message, actual.Message);
+            Assert.AreEqual(revision, actual.Revision);
+            Assert.AreEqual(state.ToUpperInvariant(), actual.State);
+            Assert.AreEqual(1, handler.NumberOfCalls);
+        }
+
+        [Test]
+        public async Task GetBuildAsync_MinRevision()
+        {
+            // Set up a mock Machine API
+            int minRevision = 553;
+            string state = "Active";
+            string message = "Finalizing";
+            int revision = minRevision + 1;
+            double percentCompleted = 0.95;
+            string response =
+                @$"{{
+                    ""revision"": {revision},
+                    ""parent"": {{
+                        ""id"": ""{TranslationEngine01}"",
+                        ""href"": ""/translation-engines/{TranslationEngine01}""
+                    }},
+                    ""step"": 540,
+                    ""percentCompleted"": {percentCompleted},
+                    ""message"": ""{message}"",
+                    ""state"": ""{state}"",
+                    ""id"": ""{Build01}"",
+                    ""href"": ""/translation-engines/{TranslationEngine01}/builds/{Build01}""
+                    }}";
+            var handler = new MockHttpMessageHandler(response, HttpStatusCode.OK);
+            var httpClient = new HttpClient(handler) { BaseAddress = new Uri("http://localhost") };
+
+            // Set up test environment
+            var env = new TestEnvironment(httpClient);
+
+            // SUT
+            BuildDto? actual = await env.Service.GetBuildAsync(
+                TranslationEngine01,
+                Build01,
+                minRevision,
+                CancellationToken.None
+            );
+
+            Assert.NotNull(actual);
+            Assert.AreEqual(Build01, actual.Id);
+            Assert.AreEqual(percentCompleted, actual.PercentCompleted);
+            Assert.AreEqual(message, actual.Message);
+            Assert.AreEqual(revision, actual.Revision);
+            Assert.AreEqual(state.ToUpperInvariant(), actual.State);
+            Assert.AreEqual(1, handler.NumberOfCalls);
+        }
+
+        [Test]
+        public void GetBuildAsync_BuildEnded()
+        {
+            // Set up a mock Machine API
+            string response = string.Empty;
+            var handler = new MockHttpMessageHandler(response, HttpStatusCode.NoContent);
+            var httpClient = new HttpClient(handler) { BaseAddress = new Uri("http://localhost") };
+
+            // Set up test environment
+            var env = new TestEnvironment(httpClient);
+
+            // SUT
+            Assert.ThrowsAsync<DataNotFoundException>(
+                () => env.Service.GetBuildAsync(TranslationEngine01, Build01, minRevision: 0, CancellationToken.None)
+            );
+
+            Assert.AreEqual(1, handler.NumberOfCalls);
+        }
+
+        [Test]
+        public async Task GetBuildAsync_NoBuildRunning()
+        {
+            // Set up a mock Machine API
+            string response = string.Empty;
+            var handler = new MockHttpMessageHandler(response, HttpStatusCode.RequestTimeout);
+            var httpClient = new HttpClient(handler) { BaseAddress = new Uri("http://localhost") };
+
+            // Set up test environment
+            var env = new TestEnvironment(httpClient);
+
+            // SUT
+            BuildDto? actual = await env.Service.GetBuildAsync(
+                TranslationEngine01,
+                Build01,
+                minRevision: 0,
+                CancellationToken.None
+            );
+
+            Assert.Null(actual);
+            Assert.AreEqual(1, handler.NumberOfCalls);
+        }
+
+        [Test]
+        public void GetBuildAsync_NoPermission()
+        {
+            // Set up a mock Machine API
+            string response = string.Empty;
+            var handler = new MockHttpMessageHandler(response, HttpStatusCode.Forbidden);
+            var httpClient = new HttpClient(handler) { BaseAddress = new Uri("http://localhost") };
+
+            // Set up test environment
+            var env = new TestEnvironment(httpClient);
+
+            // SUT
+            Assert.ThrowsAsync<HttpRequestException>(
+                () => env.Service.GetBuildAsync(TranslationEngine01, Build01, null, CancellationToken.None)
+            );
+        }
+
+        [Test]
+        public async Task GetCurrentBuildAsync_Success()
+        {
+            // Set up a mock Machine API
+            string state = "Active";
+            string message = "Finalizing";
+            int revision = 553;
+            double percentCompleted = 0.95;
+            string href = $"/translation-engines/{TranslationEngine01}/builds/{Build01}";
+            string response =
+                @$"{{
+                    ""revision"": {revision},
+                    ""parent"": {{
+                        ""id"": ""{TranslationEngine01}"",
+                        ""href"": ""/translation-engines/{TranslationEngine01}""
+                    }},
+                    ""step"": 540,
+                    ""percentCompleted"": {percentCompleted},
+                    ""message"": ""{message}"",
+                    ""state"": ""{state}"",
+                    ""id"": ""{Build01}"",
+                    ""href"": ""{href}""
+                    }}";
+            var handler = new MockHttpMessageHandler(response, HttpStatusCode.OK);
+            var httpClient = new HttpClient(handler) { BaseAddress = new Uri("http://localhost") };
 
             // Set up test environment
             var env = new TestEnvironment(httpClient);
@@ -87,7 +238,7 @@ namespace SIL.XForge.Scripture.Services
             );
 
             Assert.NotNull(actual);
-            Assert.AreEqual(buildId, actual.Id);
+            Assert.AreEqual(Build01, actual.Id);
             Assert.AreEqual(href, actual.Href);
             Assert.AreEqual(percentCompleted, actual.PercentCompleted);
             Assert.AreEqual(message, actual.Message);
@@ -101,12 +252,11 @@ namespace SIL.XForge.Scripture.Services
         {
             // Set up a mock Machine API
             int minRevision = 553;
-            string buildId = "633fdb281a2e7ac760f7193a";
             string state = "Active";
             string message = "Finalizing";
             int revision = minRevision + 1;
             double percentCompleted = 0.95;
-            string href = $"/translation-engines/{TranslationEngine01}/builds/{buildId}";
+            string href = $"/translation-engines/{TranslationEngine01}/builds/{Build01}";
             string response =
                 @$"{{
                     ""revision"": {revision},
@@ -118,7 +268,7 @@ namespace SIL.XForge.Scripture.Services
                     ""percentCompleted"": {percentCompleted},
                     ""message"": ""{message}"",
                     ""state"": ""{state}"",
-                    ""id"": ""{buildId}"",
+                    ""id"": ""{Build01}"",
                     ""href"": ""{href}""
                     }}";
             var handler = new MockHttpMessageHandler(response, HttpStatusCode.OK);
@@ -135,7 +285,7 @@ namespace SIL.XForge.Scripture.Services
             );
 
             Assert.NotNull(actual);
-            Assert.AreEqual(buildId, actual.Id);
+            Assert.AreEqual(Build01, actual.Id);
             Assert.AreEqual(href, actual.Href);
             Assert.AreEqual(percentCompleted, actual.PercentCompleted);
             Assert.AreEqual(message, actual.Message);
@@ -206,10 +356,9 @@ namespace SIL.XForge.Scripture.Services
         public async Task StartBuildAsync_Success()
         {
             // Set up a mock Machine API
-            string buildId = "633fdb281a2e7ac760f7193a";
             string state = "Pending";
             int revision = 2;
-            string href = $"/translation-engines/{TranslationEngine01}/builds/{buildId}";
+            string href = $"/translation-engines/{TranslationEngine01}/builds/{Build01}";
             string response =
                 @$"{{
                     ""revision"": {revision},
@@ -219,7 +368,7 @@ namespace SIL.XForge.Scripture.Services
                     }},
                     ""step"": 1,
                     ""state"": ""{state}"",
-                    ""id"": ""{buildId}"",
+                    ""id"": ""{Build01}"",
                     ""href"": ""{href}""
                     }}";
             var handler = new MockHttpMessageHandler(response, HttpStatusCode.OK);
@@ -231,7 +380,7 @@ namespace SIL.XForge.Scripture.Services
             // SUT
             BuildDto actual = await env.Service.StartBuildAsync(TranslationEngine01, CancellationToken.None);
 
-            Assert.AreEqual(buildId, actual.Id);
+            Assert.AreEqual(Build01, actual.Id);
             Assert.AreEqual(href, actual.Href);
             Assert.Zero(actual.PercentCompleted);
             Assert.Null(actual.Message);
