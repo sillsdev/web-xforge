928ccedc27a6425de41434f0251446dfcebf3d4e
Peter Chapman <peter@conglomo.co.nz>
2022-11-29 16:00:04 +1300

    Added support to get build by ID

--- src/SIL.XForge.Scripture/Models/MachineApi.cs
+++ src/SIL.XForge.Scripture/Models/MachineApi.cs
@@ -4,13 +4,19 @@ namespace SIL.XForge.Scripture.Models
     {
         public const string Namespace = "machine-api/v2";
         public const string StartBuild = "translation/builds";
-        public const string GetBuild = "translation/builds/{reference}:{projectId}";
+        public const string GetBuild = "translation/builds/{reference}:{projectId}.{buildId?}";
         public const string GetEngine = "translation/engines/project:{projectId}";
         public const string GetWordGraph = "translation/engines/project:{projectId}/actions/getWordGraph";
         public const string TrainSegment = "translation/engines/project:{projectId}/actions/trainSegment";
 
-        public static string GetBuildHref(string projectId) =>
-            $"{Namespace}/{GetBuild.Replace("{projectId}", projectId).Replace("{reference}", "id")}";
+        public static string GetBuildHref(string projectId, string buildId)
+        {
+            string buildHref = GetBuild
+                .Replace("{projectId}", projectId)
+                .Replace("{buildId?}", buildId)
+                .Replace("{reference}", "id");
+            return $"{Namespace}/{buildHref}";
+        }
 
         public static string GetEngineHref(string projectId) =>
             $"{Namespace}/{GetEngine.Replace("{projectId}", projectId)}";
