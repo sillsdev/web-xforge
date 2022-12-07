2dedbd0d9a223c5d8c191a66924f627a616620cf
Peter Chapman <peter@conglomo.co.nz>
2022-12-08 10:32:00 +1300

    Updated Jering, removed pragmas in JsonService

--- src/SIL.XForge/Realtime/RealtimeJsonService.cs
+++ src/SIL.XForge/Realtime/RealtimeJsonService.cs
@@ -14,30 +14,26 @@ namespace SIL.XForge.Realtime
     /// </summary>
     public class RealtimeJsonService : IJsonService
     {
-        private static readonly JsonSerializer _serializer = new JsonSerializer
+        private static readonly JsonSerializer Serializer = new JsonSerializer
         {
             ContractResolver = new CamelCasePropertyNamesContractResolver(),
-            NullValueHandling = NullValueHandling.Ignore
+            NullValueHandling = NullValueHandling.Ignore,
         };
 
         /// <inheritdoc />
-#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
-        public async ValueTask<T> DeserializeAsync<T>(Stream stream, CancellationToken cancellationToken = default)
-#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+        public ValueTask<T> DeserializeAsync<T>(Stream stream, CancellationToken cancellationToken = default)
         {
             using StreamReader sr = new StreamReader(stream, System.Text.Encoding.Default, true, 1024, true);
             using JsonTextReader reader = new JsonTextReader(sr);
-            return _serializer.Deserialize<T>(reader);
+            return ValueTask.FromResult(Serializer.Deserialize<T>(reader));
         }
 
         /// <inheritdoc />
-#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
         public async Task SerializeAsync<T>(Stream stream, T value, CancellationToken cancellationToken = default)
-#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
         {
-            using StreamWriter sw = new StreamWriter(stream, System.Text.Encoding.Default, 1024, true);
+            await using StreamWriter sw = new StreamWriter(stream, System.Text.Encoding.Default, 1024, true);
             using JsonWriter writer = new JsonTextWriter(sw);
-            _serializer.Serialize(writer, value);
+            Serializer.Serialize(writer, value);
         }
     }
 }
