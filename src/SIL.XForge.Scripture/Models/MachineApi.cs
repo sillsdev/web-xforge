17351aee819f266c09ae6813197f16bc7b5490be
Peter Chapman <peter@conglomo.co.nz>
2022-12-14 11:05:40 +1300

    Fixes from code review

--- src/SIL.XForge.Scripture/Models/MachineApi.cs
+++ src/SIL.XForge.Scripture/Models/MachineApi.cs
@@ -1,26 +1,30 @@
 namespace SIL.XForge.Scripture.Models
 {
+    /// <summary>
+    /// Machine API URL strings and helper methods.
+    /// </summary>
     public static class MachineApi
     {
         public const string Namespace = "machine-api/v2";
         public const string StartBuild = "translation/builds";
-        public const string GetBuild = "translation/builds/{reference}:{projectId}.{buildId?}";
-        public const string GetEngine = "translation/engines/project:{projectId}";
-        public const string GetWordGraph = "translation/engines/project:{projectId}/actions/getWordGraph";
-        public const string TrainSegment = "translation/engines/project:{projectId}/actions/trainSegment";
-        public const string Translate = "translation/engines/project:{projectId}/actions/translate";
-        public const string TranslateN = "translation/engines/project:{projectId}/actions/translate/{n}";
+        public const string GetBuild = "translation/builds/{locatorType}:{sfProjectId}.{buildId?}";
+        public const string GetEngine = "translation/engines/project:{sfProjectId}";
+        public const string GetWordGraph = "translation/engines/project:{sfProjectId}/actions/getWordGraph";
+        public const string TrainSegment = "translation/engines/project:{sfProjectId}/actions/trainSegment";
+        public const string Translate = "translation/engines/project:{sfProjectId}/actions/translate";
+        public const string TranslateN = "translation/engines/project:{sfProjectId}/actions/translate/{n}";
 
-        public static string GetBuildHref(string projectId, string buildId)
+        public static string GetBuildHref(string sfProjectId, string buildId)
         {
+            // The {locatorType} parameter is required to maintain compatibility with the v1 machine-api and machine.js
             string buildHref = GetBuild
-                .Replace("{projectId}", projectId)
+                .Replace("{sfProjectId}", sfProjectId)
                 .Replace("{buildId?}", buildId)
-                .Replace("{reference}", "id");
+                .Replace("{locatorType}", "id");
             return $"{Namespace}/{buildHref}";
         }
 
-        public static string GetEngineHref(string projectId) =>
-            $"{Namespace}/{GetEngine.Replace("{projectId}", projectId)}";
+        public static string GetEngineHref(string sfProjectId) =>
+            $"{Namespace}/{GetEngine.Replace("{sfProjectId}", sfProjectId)}";
     }
 }
