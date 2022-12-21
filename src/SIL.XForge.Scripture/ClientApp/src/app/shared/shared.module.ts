1a211158ad7406df1ce95b4ec914dce5a78f9ba4
Nigel Wells <nigel-wells@hotmail.com>
2022-12-21 16:27:51 +1300

    - Created new notice component - Add ability to reserve a share key so that a new one can be generated for recipients - Adjusted wording for link sharing to be relative to the type of link being shared

--- src/SIL.XForge.Scripture/ClientApp/src/app/shared/shared.module.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/shared.module.ts
@@ -8,9 +8,11 @@ import { ShareControlComponent } from './share/share-control.component';
 import { ShareDialogComponent } from './share/share-dialog.component';
 import { ShareButtonComponent } from './share/share-button.component';
 import { TextComponent } from './text/text.component';
+import { NoticeComponent } from './notice/notice.component';
 
 const componentExports = [
   ChapterNavComponent,
+  NoticeComponent,
   ShareButtonComponent,
   ShareControlComponent,
   ShareDialogComponent,
