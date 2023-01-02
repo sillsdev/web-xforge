a09684c00b34566a0171f45a53de6b79a74fb159
Peter Chapman <peter@conglomo.co.nz>
2023-01-03 09:23:32 +1300

    ProgressState bug fix

--- src/SIL.XForge.Scripture/Models/ProgressState.cs
+++ src/SIL.XForge.Scripture/Models/ProgressState.cs
@@ -2,8 +2,8 @@ namespace SIL.XForge.Scripture.Models
 {
     public class ProgressState
     {
-        public static ProgressState Completed = new ProgressState { ProgressValue = 100.0 };
-        public static ProgressState NotStarted = new ProgressState { ProgressValue = 0.0 };
+        public static readonly ProgressState Completed = new ProgressState { ProgressValue = 1.0 };
+        public static readonly ProgressState NotStarted = new ProgressState { ProgressValue = 0.0 };
         public string? ProgressString { get; set; }
         public double ProgressValue { get; set; }
     }
