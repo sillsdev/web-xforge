0af1843463f17d679b78df6f6dd38b154e7cd2db
Nigel Wells <nigel-wells@hotmail.com>
2023-01-11 16:55:28 +1300

    - Translation changes - Added outline option to notice component - Removed unused variables - Show copy link as primary when share API is not available - Fix for copying link as a non-admin

--- src/SIL.XForge/Models/ShareKey.cs
+++ src/SIL.XForge/Models/ShareKey.cs
@@ -10,6 +10,11 @@ namespace SIL.XForge.Models
         public string ShareLinkType { get; set; }
         public string ProjectRole { get; set; }
         public string? RecipientUserId { get; set; }
+
+        /// <summary>
+        /// Determines if a one time, recipient only, share link has been shared which requires the key to be reserved
+        /// only for that link. Any new one time, recipient only, share links required will generate a new share key
+        /// </summary>
         public bool? Reserved { get; set; }
     }
 }
