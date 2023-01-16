36e58330a9f892e8cbb2e4a4b8e38ee33c1ba13f
Peter Chapman <peter@conglomo.co.nz>
2023-01-16 14:52:24 +1300

    Moved training progress translation suggestions

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/editor.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/editor.component.ts
@@ -924,10 +924,10 @@ export class EditorComponent extends DataLoadingComponent implements OnDestroy, 
           complete: async () => {
             // training completed successfully
             if (this.trainingProgressClosed) {
-              this.noticeService.show(translate('editor.training_completed_successfully'));
+              this.noticeService.show(translate('training_progress.training_completed_successfully'));
               this.trainingProgressClosed = false;
             } else {
-              this.trainingMessage = translate('editor.completed_successfully');
+              this.trainingMessage = translate('training_progress.completed_successfully');
               this.trainingCompletedTimeout = setTimeout(() => {
                 this.showTrainingProgress = false;
                 this.trainingCompletedTimeout = undefined;
