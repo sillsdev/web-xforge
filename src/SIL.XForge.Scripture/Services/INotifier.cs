10c4f63a1cf250da3925ac71a12a0ee2109a5611
Peter Chapman <peter@conglomo.co.nz>
2023-01-23 09:35:01 +1300

    SF-1844 Use SignalR for Sync Progress (#1654)
    * Added basic backend SignalR implementation
    
    * Added basic signalR client for sync progress
    
    * SignalR notifications are now project specific
    
    * ProgressState Cleanup
    
    * Added project notification service, updated tests
    
    * ProgressState bug fix
    
    * Improved sync progress reporting
    
    * Indeterminate progress is now only shown at start
    
    * Changes from Code Review

---
+++ src/SIL.XForge.Scripture/Services/INotifier.cs
@@ -0,0 +1,11 @@
+using System.Threading.Tasks;
+using SIL.XForge.Scripture.Models;
+
+namespace SIL.XForge.Scripture.Services
+{
+    public interface INotifier
+    {
+        Task NotifySyncProgress(string sfProjectId, ProgressState progressState);
+        Task SubscribeToProject(string projectId);
+    }
+}
