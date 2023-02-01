b8db3586b05292b0bbff4692aa460c0c21ea23ae
Raymond Luong <raymond_luong@sil.org>
2023-02-01 13:57:10 -0700

    SF-1852 Hide conflict notes from SF

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/note-dialog/note-dialog.component.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/note-dialog/note-dialog.component.spec.ts
@@ -146,7 +146,7 @@ describe('NoteDialogComponent', () => {
     });
   }));
 
-  it('conflict preamble is transformed', fakeAsync(() => {
+  xit('conflict preamble is transformed', fakeAsync(() => {
     env = new TestEnvironment({ noteThread: TestEnvironment.getNoteThread() });
     const conflictNote = env.component.notesToDisplay[0];
     conflictNote.type = NoteType.Conflict;
@@ -192,7 +192,7 @@ describe('NoteDialogComponent', () => {
       .toEqual(unexpectedContent);
   }));
 
-  it('isConflictNote detects', fakeAsync(() => {
+  xit('isConflictNote detects', fakeAsync(() => {
     env = new TestEnvironment({ noteThread: TestEnvironment.getNoteThread() });
     // A non-conflict note is detected.
     env.component.notesToDisplay[0].type = NoteType.Normal;
@@ -305,7 +305,7 @@ describe('NoteDialogComponent', () => {
     expect(reattachNote.querySelector('img')?.getAttribute('title')).toEqual('Note reattached');
   }));
 
-  it('conflict note displays accepted and rejected changes', fakeAsync(() => {
+  xit('conflict note displays accepted and rejected changes', fakeAsync(() => {
     // Make a conflict note to be displayed.
     const noteThread: NoteThread = {
       originalContextBefore: '',
