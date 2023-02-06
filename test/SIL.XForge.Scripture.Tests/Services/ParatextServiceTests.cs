eff48c33193116ebc58b9351f010a89ac89f61ad
Peter Chapman <peter@conglomo.co.nz>
2023-02-07 07:25:58 +1300

    Minor cleanups and CSharpier fix

--- test/SIL.XForge.Scripture.Tests/Services/ParatextServiceTests.cs
+++ test/SIL.XForge.Scripture.Tests/Services/ParatextServiceTests.cs
@@ -1166,8 +1166,7 @@ namespace SIL.XForge.Scripture.Services
         {
             var env = new TestEnvironment();
             IEnumerable<NoteThreadChange> changes = await env.PrepareChangeOnSingleCommentAsync(
-                (Paratext.Data.ProjectComments.Comment comment) =>
-                {
+                (Paratext.Data.ProjectComments.Comment comment) => {
                     // Not modifying comment.
                 }
             );
