d7f6da910fc73390dae121c04b23bd46d8415f37
MarkS <marksvc@gmail.com>
2023-02-15 14:57:26 -0700

    tool: allow running manipulate-sharedb.ts from another dir
    commit-id:952d2bce

--- scripts/db_tools/manipulate-sharedb.ts
+++ scripts/db_tools/manipulate-sharedb.ts
@@ -1,4 +1,5 @@
-#!./node_modules/.bin/ts-node
+#!/usr/bin/env -S bash -c '"$(dirname "$0")"/node_modules/.bin/ts-node "$(dirname "$0")/$(basename "$0")" "$@"'
+// The above causes the local ts-node to be used even if run from another directory. Setup: npm i
 //
 // Manipulate sharedb
 //
