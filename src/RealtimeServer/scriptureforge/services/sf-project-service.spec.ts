0ce2b094a3531ef4d338e2fd0aa3b5033db701f6
Nathaniel Paulus <nathaniel_paulus@sil.org>
2023-02-15 17:24:00 -0500

    Fix project migration tests to prevent new migrations from being run (#1707)

--- src/RealtimeServer/scriptureforge/services/sf-project-service.spec.ts
+++ src/RealtimeServer/scriptureforge/services/sf-project-service.spec.ts
@@ -1,6 +1,7 @@
 import ShareDB from 'sharedb';
 import ShareDBMingo from 'sharedb-mingo-memory';
 import { instance, mock } from 'ts-mockito';
+import { SF_PROJECT_MIGRATIONS } from '../services/sf-project-migrations';
 import { SF_PROJECT_PROFILES_COLLECTION, SF_PROJECTS_COLLECTION, SFProject } from '../models/sf-project';
 import { RealtimeServer } from '../../common/realtime-server';
 import { SchemaVersionRepository } from '../../common/schema-version-repository';
