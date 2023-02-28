44d20537e1795758d697b56098d28c0065ecbeea
marksvc <marksvc@gmail.com>
2023-02-28 13:57:24 -0700

    SF-1849 Remove secondary linked account paratextId and tokens (#1710)
    When a password- or google-authenticated user Connects a project, they
    first need to log in with Paratext. This creates a new auth0 account,
    that is linked/merged into the original auth0 account. The original SF
    account then has a paratextId and Paratext tokens set.
    
    In certain instances, another SF account is created for the new auth0
    account as well. This new SF account also gains the paratextId and
    Paratext tokens. This SF account can not be logged into because the
    auth0 id on the account is for the new auth0 account that was
    linked/merged into the original auth0 account. But the new SF account
    now causes a problem because two SF accounts have the same paratextId.
    
    It looks as though there may have been some unfortunate timing for the
    problem instance, where auth0 calls pushAuthUserProfile and SF creates
    an account for the new auth0, before auth.service handleOnlineAuth
    finishes a call to linkParatextAccount.
    
    This secondary SF user can probably be safely deleted in
    LinkParatextAccountAsync(). We know the secondary account has only
    been logged into once, and so would not have other associated data.
    However, because of the difficulty in clearly identifying the problem
    at hand, I am removing paratextId and the Paratext tokens, and leaving
    the user doc and user_secret doc. If we find that the solution at hand
    is in fact a fix for the problem, we can change the code so it deletes
    the secondary SF user and user_secret docs.

--- src/SIL.XForge/Models/User.cs
+++ src/SIL.XForge/Models/User.cs
@@ -8,10 +8,19 @@ public class User : Json0Snapshot
     public string Email { get; set; }
     public string Role { get; set; }
     public string AvatarUrl { get; set; }
+
+    /// <summary>PT user id, as determined from auth0 profile, from authenticating with Paratext.</summary>
     public string ParatextId { get; set; }
     public string DisplayName { get; set; }
     public bool IsDisplayNameConfirmed { get; set; }
     public string InterfaceLanguage { get; set; }
+
+    /// <summary>
+    /// The id of the user as understood by auth0.
+    /// This is the user_id string received from auth0 when the user first logged in. It is not indicative of what
+    /// authentication method the user is currently using. So it might be `auth0|abc1234` even if the user
+    /// is authenticating with Paratext.
+    /// </summary>
     public string AuthId { get; set; }
     public Dictionary<string, Site> Sites { get; set; } = new Dictionary<string, Site>();
 }
