sillsdev/web-xforge

commit ce97a921bb006604897071c5de44c8caf9e7d940

SF-1906 Separate migration into distinct subprocess (#1735)

src/SIL.XForge.Scripture/Migrator.cs
@@ -1,0 +1,39 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace SIL.XForge.Scripture;
+
+public static class Migrator
+{
+    public static void RunMigrations(string environment)
+    {
+        using var process = new Process();
+        process.StartInfo.FileName = "node";
+        string srcDir = Directory.GetParent(Directory.GetCurrentDirectory()).ToString();
+        string migratorPath = Path.Combine(srcDir, "RealtimeServer", "lib", "cjs", "scriptureforge", "migrator.js");
+        process.StartInfo.ArgumentList.Add(migratorPath);
+        process.StartInfo.ArgumentList.Add(environment);
+        string location = System.Reflection.Assembly.GetEntryAssembly().Location;
+        process.StartInfo.ArgumentList.Add(FileVersionInfo.GetVersionInfo(location).ProductVersion);
+        process.StartInfo.RedirectStandardOutput = true;
+
+        var startTime = DateTime.Now;
+        Console.WriteLine($"[{startTime:o}] Starting migrator");
+
+        process.Start();
+        while (!process.StandardOutput.EndOfStream)
+        {
+            Console.WriteLine(process.StandardOutput.ReadLine());
+        }
+        process.WaitForExit();
+
+        var exitCode = process.ExitCode;
+        Console.WriteLine($"[{process.ExitTime:o}] Migrator exited with code {exitCode}");
+        Console.WriteLine($"Total time running migrator was {process.ExitTime - startTime}");
+        if (exitCode != 0)
+        {
+            Environment.Exit(exitCode);
+        }
+    }
+}

src/RealtimeServer/common/index.ts
@@ -58,8 +58,10 @@ async function startServer(options: RealtimeServerOptions): Promise<void> {
   ShareDB.logger.setMethods({ warn: reportError, error: reportError });
 
   try {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const RealtimeServerType: RealtimeServerConstructor = require(`../${options.appModuleName}/realtime-server`);
+    const RealtimeServerType: RealtimeServerConstructor =
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      require(`../${options.appModuleName}/realtime-server`).default;
+
     const DBType = MetadataDB(ShareDBMongo);
     const client = await MongoClient.connect(options.connectionString);
     const db = client.db();
@@ -70,7 +72,6 @@ async function startServer(options: RealtimeServerOptions): Promise<void> {
       new SchemaVersionRepository(db)
     );
     await server.createIndexes(db);
-    await server.migrateIfNecessary();
 
     streamListener = new WebSocketStreamListener(
       options.audience,

src/RealtimeServer/common/realtime-server.ts
@@ -246,6 +246,7 @@ export class RealtimeServer extends ShareDB {
         $limit: limit
       });
       while (query.results.length > 0) {
+        console.log(`Migrating ${docService.collection}: ${skip + 1} to ${skip + query.results.length}`);
         let docVersion = curVersion;
         while (docVersion < version) {
           docVersion++;

src/RealtimeServer/scriptureforge/migrator.ts
@@ -1,0 +1,66 @@
+import fs from 'fs';
+import { MongoClient } from 'mongodb';
+import ShareDB from 'sharedb';
+import ShareDBMongo from 'sharedb-mongo';
+import { ExceptionReporter } from '../common/exception-reporter';
+import { MetadataDB } from '../common/metadata-db';
+import { SchemaVersionRepository } from '../common/schema-version-repository';
+import SFRealtimeServer from './realtime-server';
+import '../common/diagnostics';
+
+console.log(`Migrator has been invoked with ${JSON.stringify(process.argv.slice(2))}`);
+
+if (process.argv.length !== 4) {
+  console.error('Usage: node migrator.js <release stage> <version>');
+  process.exit(1);
+}
+const stage = process.argv[2];
+const version = process.argv[3];
+
+if (/\d+\.\d+\.\d+/.test(version) === false) {
+  console.error('Version must be in the form of x.y.z');
+  process.exit(1);
+}
+
+const baseSettings = JSON.parse(fs.readFileSync('appsettings.json', 'utf8'));
+const stageSettings = JSON.parse(fs.readFileSync(`appsettings.${stage}.json`, 'utf8'));
+
+const bugsnagApiKey = stageSettings.Bugsnag.ApiKey ?? baseSettings.Bugsnag.ApiKey;
+const bugsnagReleaseStage = stageSettings.Bugsnag.ReleaseStage ?? baseSettings.Bugsnag.ReleaseStage;
+const dataAccessConnectionString =
+  stageSettings.DataAccess.ConnectionString ?? baseSettings.DataAccess.ConnectionString;
+const siteId = stageSettings.Site.Id ?? baseSettings.Site.Id;
+
+const exceptionReporter = new ExceptionReporter(bugsnagApiKey, bugsnagReleaseStage, version);
+function reportError(...args: unknown[]): void {
+  console.error('Error from ShareDB server: ', ...args);
+  exceptionReporter.report(args.toString());
+}
+
+// ShareDB sometimes reports errors as warnings
+ShareDB.logger.setMethods({ warn: reportError, error: reportError });
+
+async function runMigrations() {
+  const DBType = MetadataDB(ShareDBMongo);
+  let maybeClient: MongoClient | undefined;
+  let server: SFRealtimeServer | undefined;
+
+  try {
+    const client = await MongoClient.connect(`${dataAccessConnectionString}/xforge`);
+    maybeClient = client;
+    const db = client.db();
+    server = new SFRealtimeServer(
+      siteId,
+      false,
+      new DBType(callback => callback(null, client)),
+      new SchemaVersionRepository(db)
+    );
+    await server.createIndexes(db);
+    await server.migrateIfNecessary();
+  } finally {
+    server?.close();
+    maybeClient?.close();
+  }
+}
+
+runMigrations();

src/RealtimeServer/scriptureforge/realtime-server.ts
@@ -26,7 +26,7 @@ const SF_DOC_SERVICES: DocService[] = [
 /**
  * This class represents the SF real-time server.
  */
-class SFRealtimeServer extends RealtimeServer {
+export default class SFRealtimeServer extends RealtimeServer {
   constructor(siteId: string, migrationsDisabled: boolean, db: ShareDB.DB, schemaVersions: SchemaVersionRepository) {
     super(siteId, migrationsDisabled, SF_DOC_SERVICES, SF_PROJECTS_COLLECTION, db, schemaVersions);
     this.use('query', (context: ShareDB.middleware.QueryContext, next: (err?: any) => void) => {
@@ -52,5 +52,3 @@ class SFRealtimeServer extends RealtimeServer {
     });
   }
 }
-
-export = SFRealtimeServer;

src/SIL.XForge.Scripture/Program.cs
@@ -22,6 +22,8 @@ public class Program
             .AddJsonFile($"hosting.{environment}.json", true, true)
             .Build();
 
+        Migrator.RunMigrations(environment);
+
         return builder
             .ConfigureAppConfiguration(
                 (context, config) =>

commit e6ec180fbcc3fc79f0a556856f0e361ba06b7188

SF-1906b Fix migrator path in production environments
Fixes failure introduced in ce97a921bb006604897071c5de44c8caf9e7d940

src/SIL.XForge.Scripture/Migrator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 
 namespace SIL.XForge.Scripture;
 
@@ -8,20 +9,29 @@ public static class Migrator
 {
     public static void RunMigrations(string environment)
     {
-        using var process = new Process();
-        process.StartInfo.FileName = "node";
-        string srcDir = Directory.GetParent(Directory.GetCurrentDirectory()).ToString();
-        string migratorPath = Path.Combine(srcDir, "RealtimeServer", "lib", "cjs", "scriptureforge", "migrator.js");
-        process.StartInfo.ArgumentList.Add(migratorPath);
-        process.StartInfo.ArgumentList.Add(environment);
-        string location = System.Reflection.Assembly.GetEntryAssembly().Location;
-        process.StartInfo.ArgumentList.Add(FileVersionInfo.GetVersionInfo(location).ProductVersion);
-        process.StartInfo.RedirectStandardOutput = true;
+        string version = FileVersionInfo.GetVersionInfo(Assembly.GetEntryAssembly().Location).ProductVersion;
+        string projectPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        string migratorPath = Path.Combine(
+            projectPath,
+            "RealtimeServer",
+            "lib",
+            "cjs",
+            "scriptureforge",
+            "migrator.js"
+        );
+
+        var startInfo = new ProcessStartInfo
+        {
+            FileName = "node",
+            RedirectStandardOutput = true,
+            ArgumentList = { migratorPath, environment, version }
+        };
 
         var startTime = DateTime.Now;
         Console.WriteLine($"[{startTime:o}] Starting migrator");
 
-        process.Start();
+        using var process = Process.Start(startInfo);
         while (!process.StandardOutput.EndOfStream)
         {
             Console.WriteLine(process.StandardOutput.ReadLine());
@@ -33,7 +43,7 @@ public static class Migrator
         Console.WriteLine($"Total time running migrator was {process.ExitTime - startTime}");
         if (exitCode != 0)
         {
-            Environment.Exit(exitCode);
+            throw new Exception($"Migrator exited with code {exitCode}");
         }
     }
 }