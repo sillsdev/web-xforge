7e3b376bfefbabf72627d635ca466fb35afaa8b6
Nigel Wells <nigel-wells@hotmail.com>
2023-03-14 14:01:23 +1300

    SF-1712 - Transparent authentication - Expanded the join component to allow non-authenticated users to create a user and join the project - Created an anonymous backend service - Updated to the latest version of auth0-spa-js - Generate a cookie for transparent authenticated users to store credentials - Added a login state to the auth service allowing the app to respond to changes in the state of user becoming logged in after initial load

---
+++ src/SIL.XForge.Scripture/Models/AnonymousShareKeyResponse.cs
@@ -0,0 +1,8 @@
+namespace SIL.XForge.Scripture.Models;
+
+public class AnonymousShareKeyResponse
+{
+    public string ProjectName { get; set; }
+    public string Role { get; set; }
+    public string ShareKey { get; set; }
+}
