f91929b4de4b624c2de48cb2d08708044272aa87
MarkS <marksvc@gmail.com>
2023-03-22 15:59:50 -0600

    SF-1444 Preserve important whitespace when get text
    Getting book USX from Paratext and then using XDocument.Parse()
    produces an XDocument for further processing. As implemented,
    sometimes important whitespace can be lost when doing this, such as
    the space between `<char>In</char> <char>the</char>`.
    
    Manually pre-process the whitespace, and ask XDocument.Parse() to
    preserve the whitespace it is given.
    
    The migrator seeks to correct existing data in SF. Migrator Startup.cs
    and Program.cs files can be compared to files in
    ../MachineApiMiration/.
    
    launch.json and tasks.json: Rather than be able to create a new
    launcher and builder for each migrator, just leave an existing
    migration entry that can be easily adapted to a migration being worked
    on.
    
    trigger-resources-need-sync.ts can be compared to
    manipulate-sharedb.ts.
    
    commit-id:e0dd8a3d

---
+++ scripts/db_tools/trigger-resources-need-sync.ts
@@ -0,0 +1,95 @@
+#!/usr/bin/env -S bash -c '"$(dirname "$0")"/node_modules/.bin/ts-node "$(dirname "$0")/$(basename "$0")" "$@"'
+// The above causes the local ts-node to be used even if run from another directory. Setup: npm i
+
+import * as RichText from 'rich-text';
+import { Db, MongoClient } from 'mongodb';
+import ShareDB from 'sharedb';
+import { Connection, Doc } from 'sharedb/lib/client';
+import WebSocket from 'ws';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import { ConnectionSettings, createWS, databaseConfigs, submitDocOp } from './utils';
+import { SFProject } from '../../src/RealtimeServer/scriptureforge/models/sf-project';
+
+type ProgArgs = {
+  server: string;
+};
+
+class Program {
+  server: string | undefined;
+  connectionConfig: ConnectionSettings | undefined;
+
+  constructor() {
+    this.processArguments();
+  }
+
+  processArguments() {
+    const args: ProgArgs = yargs(hideBin(process.argv))
+      .option('server', {
+        type: 'string',
+        choices: ['dev', 'qa', 'live'],
+        default: 'dev',
+        requiresArg: true,
+        description: 'server to connect to'
+      })
+      .strict()
+      .parseSync();
+
+    this.server = args.server;
+    this.connectionConfig = databaseConfigs.get(this.server);
+  }
+
+  /** Run the lambda with a connection to the db. */
+  async withDB(activity: (conn: Connection, db: Db) => Promise<void>): Promise<void> {
+    if (this.connectionConfig == null) {
+      throw new Error('null connection config');
+    }
+    console.log(`Connecting to ${this.server}.`);
+    const ws: WebSocket = createWS(this.connectionConfig);
+    const conn: Connection = new Connection(ws);
+
+    const client: MongoClient = await MongoClient.connect(this.connectionConfig.dbLocation);
+    try {
+      const db: Db = client.db();
+      await activity(conn, db);
+    } finally {
+      client.close();
+      (conn as any).close();
+    }
+  }
+
+  async main() {
+    ShareDB.types.register(RichText.type);
+    await this.withDB(async (conn: Connection, db: Db) => {
+      // This script marks all resources so they need to be synchronized.
+
+      const SF_PROJECTS_COLLECTION = 'sf_projects';
+      await new Promise<void>((resolve, reject) => {
+        conn.createFetchQuery(
+          SF_PROJECTS_COLLECTION,
+          { resourceConfig: { $exists: true } },
+          {},
+          async (err, results: Doc[]) => {
+            if (err) {
+              console.error('Error fetching documents:', err);
+              return;
+            }
+
+            console.log(`Fetched ${results.length} documents.`);
+            for (const projDoc of results) {
+              const proj: SFProject = projDoc.data;
+              console.log(`${proj.shortName} had resourceConfig.revision ${proj.resourceConfig?.revision}`);
+              if (proj.resourceConfig != null) {
+                await submitDocOp(projDoc, { p: ['resourceConfig', 'revision'], od: proj.resourceConfig, oi: -1 });
+              }
+            }
+            resolve();
+          }
+        );
+      });
+    });
+  }
+}
+
+const program = new Program();
+program.main();
---
+++ src/Migrations/WhitespaceRestoreMigration/Program.cs
@@ -0,0 +1,159 @@
+using System.Net;
+using System.Reflection;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SIL.XForge.Scripture.Services;
+
+namespace WhitespaceRestoreMigration;
+
+/// <summary>
+/// SF-1444 fixes a bug where whitespace is lost when importing PT data into SF. But before the bug was fixed,
+/// past data may have been imported with spaces lost. This migrator is to detect and fix those situations, where
+/// possible, by re-writing the data in SF to retain those spaces.
+///
+/// Scripture Forge will need to not be running in order to run this migrator, which starts the realtime server.
+/// Usage: cd bin/Debug/net6.0; ASPNETCORE_ENVIRONMENT=Development ./WhitespaceRestoreMigration read-mode ../../../../../SIL.XForge.Scripture
+/// Usage on server: Shut down the SF service, and then run:
+///   export SF_APP_DIR=/path/to/sf/app; sudo --user $(stat --format='%G' "${SF_APP_DIR}") ASPNETCORE_ENVIRONMENT=DevelopmentORStagingORProduction time ./WhitespaceRestoreMigration read-mode "${SF_APP_DIR}" |& tee ~/output-WhitespaceRestoreMigration-$(date '+%Y%m%d-%H%M%S').txt
+/// </summary>
+public class Program
+{
+    /// <summary>
+    /// Gets a value indicating whether to disable realtime server migrations.
+    /// </summary>
+    /// <value>
+    ///   <c>true</c> if realtime server migrations are disabled; otherwise, <c>false</c>.
+    /// </value>
+    /// <remarks>
+    /// This exists for the <see cref="Startup" /> class to retrieve this value.
+    /// </remarks>
+    public static bool RealtimeServerMigrationsDisabled { get; private set; }
+
+    /// <summary>
+    /// Defines the entry point of the application.
+    /// </summary>
+    /// <param name="args">Application arguments.</param>
+    public static async Task Main(string[] args)
+    {
+        string usage = "Arg 1 must be 'read-mode' or 'write-mode'. Arg 2 must be the path to the SF app dir.";
+        if (args.Length < 2)
+            throw new Exception($"Error: Missing required arguments. {usage}");
+        bool writeMode;
+        if (args[0] == "read-mode")
+            writeMode = false;
+        else if (args[0] == "write-mode")
+            writeMode = true;
+        else
+            throw new Exception($"Error: {usage}");
+        string sfAppDir = args[1];
+
+        RealtimeServerMigrationsDisabled = !writeMode;
+        string? aspNetCoreEnv = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        if (aspNetCoreEnv == null)
+            throw new Exception($"Refusing to run unless ASPNETCORE_ENVIRONMENT is specified.");
+        Log($"Operating as ASPNETCORE_ENVIRONMENT {aspNetCoreEnv}.");
+
+        // Space-delimited set of SF project ids, to optionally specify a subset of projects to process.
+        string? sfProjectIdsSubset = Environment.GetEnvironmentVariable("SF_PROJECT_SET");
+        // Space-delimited set of projects and users, to optionally specify that a specific user be used when
+        // processing a given project. Specify each project and user pair as the SF project id, colon, SF user id.
+        string? sfUserRequestValues = Environment.GetEnvironmentVariable("SF_PROJECT_USERS");
+
+        Log($"Running in {(writeMode ? "write-mode. An SF instance had better not also be running!" : "read-mode.")}");
+
+        // Set up the web host, and start the realtime server
+        Log("Starting Scripture Forge Server...");
+        Directory.SetCurrentDirectory(sfAppDir);
+        IWebHostBuilder builder = CreateWebHostBuilder(args);
+        IWebHost webHost = builder.Build();
+        try
+        {
+            await webHost.StartAsync();
+        }
+        catch (HttpRequestException)
+        {
+            Log(
+                "There was an error starting the program before getting to the migration"
+                    + " part. Maybe the SF server is running and needs shut down? Rethrowing."
+            );
+            throw;
+        }
+
+        // Get the project IDs and user IDs, if they were set in the environment variables
+        string[] sfProjectIdsToMigrate = sfProjectIdsSubset?.Split(' ') ?? Array.Empty<string>();
+        Dictionary<string, string> sfUsersToUse = new Dictionary<string, string>();
+        foreach (string sfUserRequest in sfUserRequestValues?.Split(' ') ?? Array.Empty<string>())
+        {
+            string[] request = sfUserRequest.Split(':');
+            sfUsersToUse.Add(request[0], request[1]);
+        }
+
+        // Migrate projects
+        var migrator = webHost.Services.GetService<Migrator>();
+        bool success = await migrator!.MigrateAsync(writeMode, sfProjectIdsToMigrate, sfUsersToUse);
+
+        // Stop the web host and realtime server
+        Log("Stopping Scripture Forge Server...");
+        await webHost.StopAsync();
+
+        Log(success ? "Finished Migration." : "Migration Failed.");
+    }
+
+    internal static void Log(string message)
+    {
+        string when = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        Console.WriteLine($"{when} Migration: {message}");
+    }
+
+    private static IWebHostBuilder CreateWebHostBuilder(string[] args)
+    {
+        IWebHostBuilder builder = WebHost.CreateDefaultBuilder(args);
+
+        // Secrets to connect to PT web API are associated with the SIL.XForge.Scripture assembly
+        var sfAssembly = Assembly.GetAssembly(typeof(ParatextService));
+
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .AddUserSecrets(sfAssembly)
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .Build();
+
+        // Random, big number.
+        int migratorDotnetPort = 39570;
+
+        return builder
+            .ConfigureAppConfiguration(
+                (context, config) =>
+                {
+                    IWebHostEnvironment env = context.HostingEnvironment;
+                    if (env.IsDevelopment() || env.IsEnvironment("Testing"))
+                    {
+                        config.AddJsonFile("appsettings.user.json", true);
+                    }
+                    else
+                    {
+                        config.AddJsonFile("secrets.json", true, true);
+                    }
+
+                    if (env.IsEnvironment("Testing"))
+                    {
+                        Assembly appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
+                        config.AddUserSecrets(appAssembly, true);
+                    }
+
+                    config.AddEnvironmentVariables();
+                }
+            )
+            .UseConfiguration(configuration)
+            .ConfigureKestrel(
+                options =>
+                    // Listen to a different port than the default. Then it won't have a
+                    // conflict if SF is running. And specific, rather than just available, to
+                    // make sure we really won't be handling user requests.
+                    options.Listen(IPAddress.Loopback, migratorDotnetPort)
+            )
+            .UseStartup<Startup>();
+    }
+}
