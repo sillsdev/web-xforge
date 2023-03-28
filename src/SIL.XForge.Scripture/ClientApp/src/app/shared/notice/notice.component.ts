6ade7991183669c507a0962590226b5fa4b18744
Nathaniel Paulus <nathaniel_paulus@sil.org>
2023-03-27 23:09:23 -0400

    Upgrade notice component to use ng-content

--- src/SIL.XForge.Scripture/ClientApp/src/app/shared/notice/notice.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/notice/notice.component.ts
@@ -1,6 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { I18nKey } from 'xforge-common/i18n.service';
-import { TranslocoService } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-notice',
@@ -9,15 +7,8 @@ import { TranslocoService } from '@ngneat/transloco';
 })
 export class NoticeComponent {
   @Input() icon?: string;
-  @Input() text?: I18nKey;
   @Input() type: 'normal' | 'warning' | 'error' = 'normal';
   @Input() outline: boolean = false;
 
-  constructor(private readonly transloco: TranslocoService) {}
-  get notice(): string {
-    if (this.text == null) {
-      return '';
-    }
-    return this.transloco.translate(this.text);
-  }
+  constructor() {}
 }
