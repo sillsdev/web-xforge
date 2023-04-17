fbc33c7c02a02741f2e0887c23968db0d1b416e3
Nigel Wells <nigel-wells@hotmail.com>
2023-04-17 16:11:33 +1200

    - Set hover state to login text - Set default values for new classes - Added HttpPost routes back in - Fix to join form state check

--- src/SIL.XForge.Scripture/Models/GenerateAccountRequest.cs
+++ src/SIL.XForge.Scripture/Models/GenerateAccountRequest.cs
@@ -2,7 +2,7 @@ namespace SIL.XForge.Scripture.Models;
 
 public class GenerateAccountRequest
 {
-    public string ShareKey { get; set; }
-    public string DisplayName { get; set; }
-    public string Language { get; set; }
+    public string ShareKey { get; set; } = string.Empty;
+    public string DisplayName { get; set; } = string.Empty;
+    public string Language { get; set; } = string.Empty;
 }
