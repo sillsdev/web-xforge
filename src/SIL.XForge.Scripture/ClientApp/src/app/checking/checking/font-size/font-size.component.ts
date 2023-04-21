3cd482a2169ce72e71db4c98a3fc0d38bb95791e
Nathaniel Paulus <nathaniel_paulus@sil.org>
2023-04-21 11:32:11 -0400

    Add font size stories

--- src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/font-size/font-size.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/font-size/font-size.component.ts
@@ -8,7 +8,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 export class FontSizeComponent implements OnInit {
   @Input() min: number = 1;
   @Input() max: number = 3;
-  @Output() apply: EventEmitter<string> = new EventEmitter<string>();
+  @Output() apply = new EventEmitter<string>();
   fontSize: number = 1;
 
   constructor() {}
@@ -18,19 +18,19 @@ export class FontSizeComponent implements OnInit {
       this.fontSize = this.min;
     }
     if (this.min > this.max) {
-      throw new RangeError('min (' + this.min + ') can not be larger than max (' + this.max + ')');
+      throw new RangeError(`min (${this.min}) can not be larger than max (${this.max})`);
     } else if (this.max < this.fontSize) {
-      throw new RangeError('max (' + this.max + ') can not be less than font size (' + this.fontSize + ')');
+      throw new RangeError(`max (${this.max}) can not be less than font size (${this.fontSize})`);
     } else {
       this.applySize();
     }
   }
 
-  applySize() {
+  applySize(): void {
     this.apply.emit(this.fontSize + 'rem');
   }
 
-  decreaseFontSize() {
+  decreaseFontSize(): void {
     this.fontSize = this.fontSize - 0.1;
     if (this.fontSize < this.min) {
       this.fontSize = this.min;
@@ -38,7 +38,7 @@ export class FontSizeComponent implements OnInit {
     this.applySize();
   }
 
-  increaseFontSize() {
+  increaseFontSize(): void {
     this.fontSize = this.fontSize + 0.1;
     if (this.fontSize > this.max) {
       this.fontSize = this.max;
