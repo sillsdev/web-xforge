236e2ea02810e6b2ffe5a8ea1cf4e00d999f416b
Nathaniel Paulus <nathaniel_paulus@sil.org>
2023-05-16 10:36:23 -0400

    SF-2004 Make appAutofocus directive work with non-MDC components (#1834)

--- src/SIL.XForge.Scripture/ClientApp/src/xforge-common/autofocus.directive.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/autofocus.directive.ts
@@ -1,22 +1,24 @@
-import { MdcTextarea, MdcTextField } from '@angular-mdc/web/textfield';
-import { AfterViewInit, Directive, Host, Optional, Self } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef } from '@angular/core';
 
 /**
- * Auto focuses MdcTextField and MdcTextarea. HTML autofocus attribute does not work for dynamically generated content.
+ * Auto focuses text inputs and textarea. HTML autofocus attribute does not work for dynamically generated content.
  */
 @Directive({
   selector: '[appAutofocus]'
 })
 export class AutofocusDirective implements AfterViewInit {
-  private component: { focus: () => void };
+  constructor(private readonly elementRef: ElementRef<HTMLElement>) {}
 
-  // Angular allows injecting the component or element but doesn't have a good way to handle components of variable type
-  // See https://github.com/angular/angular/issues/8277#issuecomment-323678013 for this workaround
-  constructor(@Host() @Self() @Optional() textField: MdcTextField, @Host() @Self() @Optional() textArea: MdcTextarea) {
-    this.component = (textField || textArea)!;
-  }
+  ngAfterViewInit(): void {
+    const hostElement: HTMLElement = this.elementRef.nativeElement;
 
-  ngAfterViewInit() {
-    setTimeout(() => this.component.focus(), 0);
+    const input =
+      hostElement instanceof HTMLInputElement || hostElement instanceof HTMLTextAreaElement
+        ? hostElement
+        : hostElement.querySelector('input, textarea');
+
+    if (input instanceof HTMLElement) {
+      setTimeout(() => input.focus());
+    }
   }
 }
