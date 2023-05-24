007df2c31a537ebe0a1c92395d458e93f567a7bd
siltomato <colin_moore@sil.org>
2023-05-23 21:11:45 -0400

    SF-2010 Create generic dialog launcher for material dialogs.

--- src/SIL.XForge.Scripture/ClientApp/src/xforge-common/system-administration/sa-delete-dialog.stories.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/system-administration/sa-delete-dialog.stories.ts
@@ -1,45 +1,22 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { MatDialog, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
-import { Meta, StoryFn } from '@storybook/angular';
-import { SystemRole } from 'realtime-server/lib/esm/common/models/system-role';
-import { MatButtonModule } from '@angular/material/button';
+import { Meta } from '@storybook/angular';
+import { MatDialogLaunchComponent, matDialogStory } from '../../../.storybook/util/mat-dialog-launch';
 import { AvatarTestingModule } from '../avatar/avatar-testing.module';
 import { SaDeleteDialogComponent } from './sa-delete-dialog.component';
-
-@Component({ template: '' })
-export class MatDialogLaunchComponent implements OnInit {
-  constructor(private dialog: MatDialog, @Inject(MAT_DIALOG_DATA) public data: any) {}
-
-  public ngOnInit(): void {
-    this.dialog.open(SaDeleteDialogComponent, this.data);
-  }
-}
 
 export default {
   title: 'System Admin',
   component: MatDialogLaunchComponent
 } as Meta;
 
-export const DeleteUserDialog: StoryFn = args => ({
-  moduleMetadata: {
-    imports: [MatButtonModule, MatDialogModule, AvatarTestingModule],
-    declarations: [SaDeleteDialogComponent],
-    providers: [{ provide: MAT_DIALOG_DATA, useValue: { data: args.data } }]
-  },
-  props: args
+export const DeleteUserDialog = matDialogStory(SaDeleteDialogComponent, {
+  imports: [AvatarTestingModule]
 });
 
 DeleteUserDialog.args = {
   data: {
     user: {
       name: 'Billy T James',
-      displayName: 'Billy T James',
-      isDisplayNameConfirmed: true,
-      email: 'user01@example.com',
-      avatarUrl: '',
-      authId: 'auth01',
-      role: SystemRole.User,
-      sites: {}
+      email: 'user01@example.com'
     }
   }
 };
