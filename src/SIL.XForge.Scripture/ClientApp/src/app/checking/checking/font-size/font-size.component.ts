b9a6913e19fbb5215de49630bf8b07699767686a
siltomato <colin_moore@sil.org>
2023-05-24 00:52:13 -0400

    SF-2015 Convert font size component to material.

--- src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/font-size/font-size.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/font-size/font-size.component.ts
@@ -8,41 +8,55 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 export class FontSizeComponent implements OnInit {
   @Input() min: number = 1;
   @Input() max: number = 3;
+  @Input() step: number = 0.1;
+  @Input() initial: number = 1;
   @Output() apply = new EventEmitter<string>();
-  fontSize: number = 1;
+
+  fontSize!: number;
 
   constructor() {}
 
   ngOnInit(): void {
-    if (this.fontSize < this.min) {
-      this.fontSize = this.min;
-    }
     if (this.min > this.max) {
       throw new RangeError(`min (${this.min}) can not be larger than max (${this.max})`);
-    } else if (this.max < this.fontSize) {
-      throw new RangeError(`max (${this.max}) can not be less than font size (${this.fontSize})`);
-    } else {
-      this.applySize();
     }
+
+    this.fontSize = this.initial;
+    this.enforceBounds();
+    this.applySize();
   }
 
   applySize(): void {
     this.apply.emit(this.fontSize + 'rem');
   }
 
-  decreaseFontSize(): void {
-    this.fontSize = this.fontSize - 0.1;
+  enforceBounds(): void {
     if (this.fontSize < this.min) {
       this.fontSize = this.min;
     }
-    this.applySize();
-  }
-
-  increaseFontSize(): void {
-    this.fontSize = this.fontSize + 0.1;
     if (this.fontSize > this.max) {
       this.fontSize = this.max;
     }
+  }
+
+  adjustFontSize($event: Event, direction: '-' | '+'): void {
+    switch (direction) {
+      case '-':
+        this.fontSize -= this.step;
+        break;
+      case '+':
+        this.fontSize += this.step;
+        break;
+    }
+    this.enforceBounds();
     this.applySize();
+
+    // Ensure focus removed from element if disabled (firefox doesn't)
+    if (this.fontSize === this.min) {
+      ($event.target as HTMLElement).closest('button')?.blur();
+    }
+
+    // Allows menu to stay open
+    $event.stopPropagation();
   }
 }
