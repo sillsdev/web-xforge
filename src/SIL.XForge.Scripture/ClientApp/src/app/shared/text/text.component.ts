779d15f3dcd58cd7d4c73f48364105bb42b33d54
siltomato <colin_moore@sil.org>
2023-05-26 13:44:53 -0400

    SF-1981 Fix editor highlights incorrect source segment

--- src/SIL.XForge.Scripture/ClientApp/src/app/shared/text/text.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/text/text.component.ts
@@ -1297,10 +1297,21 @@ export class TextComponent extends SubscriptionDisposable implements AfterViewIn
     }
 
     if (!this.viewModel.hasSegmentRange(segmentRef)) {
-      const verseParts: string[] = segmentRef.split('_');
-      const verseRef: VerseRef = new VerseRef(this.id?.bookNum, verseParts[1], verseParts[2]);
-      const correspondingSegments: string[] = this.getVerseSegments(verseRef);
-      if (correspondingSegments.length === 0) {
+      let resetSegment = true;
+
+      // If verse segment ref has no exact match, check for segments that fall within a verse reference
+      if (segmentRef.startsWith('verse')) {
+        const [_, chapterNum, verseNum] = segmentRef.split('_');
+        const verseRef: VerseRef = new VerseRef(this.id?.bookNum, chapterNum, verseNum);
+        const correspondingSegments: string[] = this.getVerseSegments(verseRef);
+
+        if (correspondingSegments.length > 0) {
+          segmentRef = correspondingSegments[0];
+          resetSegment = false;
+        }
+      }
+
+      if (resetSegment) {
         if (this._segment != null && this.highlightSegment) {
           this.clearHighlight();
         }
@@ -1308,7 +1319,6 @@ export class TextComponent extends SubscriptionDisposable implements AfterViewIn
         this.segmentRefChange.emit();
         return true;
       }
-      segmentRef = correspondingSegments[0];
     }
 
     if (focus) {
