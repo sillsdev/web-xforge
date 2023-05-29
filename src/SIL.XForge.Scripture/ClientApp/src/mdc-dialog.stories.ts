e3e5450d44ec82ec324bb85a03c3db0597f53893
Joseph Myers <wjosephmyers@gmail.com>
2023-05-29 09:09:49 +0530

    Moved the supported browser stories into the dialog file and deleted the old (broken) dialog stories

--- src/SIL.XForge.Scripture/ClientApp/src/mdc-dialog.stories.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/mdc-dialog.stories.ts
@@ -1,8 +1,4 @@
 import { Meta } from '@storybook/angular';
-import {
-  BrowserIssue,
-  SupportedBrowsersDialogComponent
-} from 'xforge-common/supported-browsers-dialog/supported-browsers-dialog.component';
 import { ErrorAlert, ErrorComponent } from 'xforge-common/error/error.component';
 import {
   ImportQuestionsConfirmationDialogComponent,
@@ -21,12 +17,6 @@ export default meta;
 export const QuestionAnswered = mdcDialogStory(QuestionAnsweredDialogComponent);
 
 export const ProjectDeletedDialog = mdcDialogStory(ProjectDeletedDialogComponent);
-
-export const UnsupportedBrowser = mdcDialogStory(SupportedBrowsersDialogComponent);
-UnsupportedBrowser.args = { data: BrowserIssue.Upgrade };
-
-export const UnsupportedBrowserAudioRecording = mdcDialogStory(SupportedBrowsersDialogComponent);
-UnsupportedBrowserAudioRecording.args = { data: BrowserIssue.AudioRecording };
 
 export const ImportQuestionsConfirmation = mdcDialogStory(ImportQuestionsConfirmationDialogComponent);
 const confirmationData: ImportQuestionsConfirmationDialogData = {
