d1b791423ba239bf643267f61a9e6e62cc3ee029
Joseph Myers <wjosephmyers@gmail.com>
2023-05-29 09:09:49 +0530

    Moved the supported browser stories into the dialog file and deleted the old (broken) dialog stories

--- src/SIL.XForge.Scripture/ClientApp/src/mat-dialog.stories.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/mat-dialog.stories.ts
@@ -1,4 +1,8 @@
 import { Meta } from '@storybook/angular';
+import {
+  BrowserIssue,
+  SupportedBrowsersDialogComponent
+} from 'xforge-common/supported-browsers-dialog/supported-browsers-dialog.component';
 import { DeleteProjectDialogComponent } from './app/settings/delete-project-dialog/delete-project-dialog.component';
 import { MatDialogLaunchComponent, matDialogStory } from '.storybook/story-utils';
 
@@ -10,3 +14,9 @@ export default meta;
 
 export const DeleteProjectDialog = matDialogStory(DeleteProjectDialogComponent);
 DeleteProjectDialog.args = { data: { name: 'My Project' } };
+
+export const BrowserUnsupportedDialog = matDialogStory(SupportedBrowsersDialogComponent);
+BrowserUnsupportedDialog.args = { data: BrowserIssue.Upgrade };
+
+export const AudioUnsupportedDialog = matDialogStory(SupportedBrowsersDialogComponent);
+AudioUnsupportedDialog.args = { data: BrowserIssue.AudioRecording };
