9e8c3744e66b7aefd9bbf55d4c1bec94a4e724c8
josephmyers <43733878+josephmyers@users.noreply.github.com>
2023-05-30 04:42:10 +0530

    SF-2017 Convert suggestions component to Material (#1856)

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/suggestions.stories.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/suggestions.stories.ts
@@ -1,4 +1,6 @@
 import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+import { within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 import { CommonModule } from '@angular/common';
 import { UICommonModule } from 'xforge-common/ui-common.module';
 import { SuggestionsComponent } from './suggestions.component';
@@ -24,6 +26,12 @@ export const Default: Story = {
       { words: ['there', 'was', 'dearth', 'in', 'the', 'earth'], confidence: 0.29 },
       { words: ['there', 'was', 'a', 'famine', 'in', 'the', 'land'], confidence: 0.21 }
     ]
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const options: HTMLElement[] = canvas.getAllByRole('option');
+    expect(options[0].getAttribute('aria-selected')).toBe('true');
+    expect(options[1].getAttribute('aria-selected')).toBe('false');
   }
 };
 
