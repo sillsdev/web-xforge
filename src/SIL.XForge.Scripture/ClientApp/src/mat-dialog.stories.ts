c157429578c1083047243c4728571c9d3ee9881f
Joseph Myers <wjosephmyers@gmail.com>
2023-05-30 12:07:47 +0530

    Moved Question already answered dialog story

--- src/SIL.XForge.Scripture/ClientApp/src/mat-dialog.stories.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/mat-dialog.stories.ts
@@ -1,4 +1,5 @@
 import { Meta } from '@storybook/angular';
+import { QuestionAnsweredDialogComponent } from './app/checking/question-answered-dialog/question-answered-dialog.component';
 import { DeleteProjectDialogComponent } from './app/settings/delete-project-dialog/delete-project-dialog.component';
 import { MatDialogLaunchComponent, matDialogStory } from '.storybook/story-utils';
 
@@ -7,6 +8,8 @@ const meta: Meta = {
   component: MatDialogLaunchComponent
 };
 export default meta;
+
+export const QuestionAnswered = matDialogStory(QuestionAnsweredDialogComponent);
 
 export const DeleteProjectDialog = matDialogStory(DeleteProjectDialogComponent);
 DeleteProjectDialog.args = { data: { name: 'My Project' } };
