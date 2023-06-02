7fc054d65734f6d7ce01775003a88c36c125dd2b
siltomato <colin_moore@sil.org>
2023-06-01 21:39:29 -0400

    SF-2015 Add stories to include open menu states

--- src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/font-size/font-size.stories.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/font-size/font-size.stories.ts
@@ -1,16 +1,72 @@
 import { CommonModule } from '@angular/common';
-import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+import { AngularRenderer, componentWrapperDecorator, Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+import { expect } from '@storybook/jest';
+import { userEvent } from '@storybook/testing-library';
+import { PlayFunction, PlayFunctionContext } from '@storybook/types';
 import { UICommonModule } from 'xforge-common/ui-common.module';
 import { FontSizeComponent } from './font-size.component';
 
-const meta: Meta<FontSizeComponent> = {
-  title: 'Utility/FontSizeComponent',
+export default {
+  title: 'Utility/Font Size Adjuster',
   component: FontSizeComponent,
-  decorators: [moduleMetadata({ imports: [CommonModule, UICommonModule] })]
-};
-
-export default meta;
+  decorators: [
+    moduleMetadata({
+      imports: [CommonModule, UICommonModule]
+    }),
+    componentWrapperDecorator(story => `<div style="margin:10px 20px">${story}</div>`)
+  ],
+  args: {
+    min: 1,
+    max: 3,
+    initial: 1
+  }
+} as Meta<FontSizeComponent>;
 
 type Story = StoryObj<FontSizeComponent>;
 
-export const Default: Story = {};
+export const Inactive: Story = {};
+
+export const OpenMenu: Story = {
+  play: () => {
+    const menuTrigger: HTMLElement | null = document.body.querySelector('.font-size-menu-trigger');
+
+    expect(menuTrigger).not.toBeNull();
+    userEvent.click(menuTrigger!);
+  }
+};
+
+export const IncreaseFontAllowed: Story = {
+  args: { initial: 1 },
+  play: playForAdjustFontButton('Increase')
+};
+
+export const IncreaseFontNotAllowed: Story = {
+  ...IncreaseFontAllowed,
+  args: { initial: 3 }
+};
+
+export const DecreaseFontAllowed: Story = {
+  args: { initial: 3 },
+  play: playForAdjustFontButton('Decrease')
+};
+
+export const DecreaseFontNotAllowed: Story = {
+  ...DecreaseFontAllowed,
+  args: { initial: 1 }
+};
+
+/**
+ * Return 'play' function to open menu and click the specified button.
+ */
+function playForAdjustFontButton(which: 'Increase' | 'Decrease'): PlayFunction<AngularRenderer, FontSizeComponent> {
+  return (context: PlayFunctionContext<AngularRenderer, FontSizeComponent>) => {
+    OpenMenu.play?.(context);
+
+    const adjustFontButton: HTMLElement | null = document.body.querySelector(
+      `.font-size-menu  button[title="${which} font size"]`
+    );
+
+    expect(adjustFontButton).not.toBeNull();
+    userEvent.click(adjustFontButton!, undefined, { skipPointerEventsCheck: true });
+  };
+}
