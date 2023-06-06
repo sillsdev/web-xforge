2237efc527f1459a090db83e859ef9f106589379
Raymond Luong <raymond_luong@sil.org>
2023-06-06 09:12:30 -0600

    SF-1896 Scroll FAB button with the editor (#1850)

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/editor.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/editor.component.ts
@@ -176,6 +176,7 @@ export class EditorComponent extends DataLoadingComponent implements OnDestroy, 
   private toggleNoteThreadSub?: Subscription;
   private shouldNoteThreadsRespondToEdits: boolean = false;
   private commenterSelectedVerseRef?: VerseRef;
+  private scrollSubscription?: Subscription;
 
   constructor(
     private readonly activatedRoute: ActivatedRoute,
@@ -647,6 +648,9 @@ export class EditorComponent extends DataLoadingComponent implements OnDestroy, 
         this.source.setSegment(this.target.segmentRef);
         this.syncScroll();
       }
+      if (segment == null || !VERSE_REGEX.test(segment.ref)) {
+        this.resetInsertNoteFab(true);
+      }
 
       this.insertSuggestionEnd = -1;
       this.onStartTranslating();
@@ -761,6 +765,9 @@ export class EditorComponent extends DataLoadingComponent implements OnDestroy, 
         this.targetLoaded = true;
         this.toggleNoteThreadVerseRefs$.next();
         this.shouldNoteThreadsRespondToEdits = true;
+        if (this.target?.editor != null) {
+          this.subscribeScroll(this.target.editor);
+        }
         break;
     }
     if ((!this.hasSource || this.sourceLoaded) && this.targetLoaded) {
@@ -1472,7 +1479,8 @@ export class EditorComponent extends DataLoadingComponent implements OnDestroy, 
       const targetRect: DOMRect | undefined = this.targetContainer?.nativeElement.getBoundingClientRect();
       if (targetRect != null) {
         const adjustment: number = this.isTargetRightToLeft ? 20 : -60;
-        const leftCoordinate: number = (this.isTargetRightToLeft ? targetRect.left : targetRect.right) + adjustment;
+        const leftCoordinate: number =
+          (this.isTargetRightToLeft ? targetRect.left : targetRect.right - targetRect.left) + adjustment;
         this.insertNoteFabLeft = `${leftCoordinate}px`;
       }
     }, 10);
@@ -1621,11 +1629,12 @@ export class EditorComponent extends DataLoadingComponent implements OnDestroy, 
     }
   }
 
-  private positionInsertNoteFab(segmentElement: Element): void {
-    if (this.insertNoteFab == null || this.target == null || !this.isInsertNoteFabEnabled) return;
-    const selection: RangeStatic | null | undefined = this.target.editor?.getSelection();
+  private positionInsertNoteFab(): void {
+    if (this.insertNoteFab == null || this.target?.editor == null || !this.isInsertNoteFabEnabled) return;
+    const selection: RangeStatic | null | undefined = this.target.editor.getSelection();
     if (selection != null) {
-      this.insertNoteFab.nativeElement.style.top = `${segmentElement.getBoundingClientRect().top}px`;
+      this.insertNoteFab.nativeElement.style.top = `${this.target.selectionBoundsTop}px`;
+      this.insertNoteFab.nativeElement.style.marginTop = `-${this.target.scrollPosition}px`;
     } else {
       // hide the insert note FAB when the user clicks outside of the editor
       this.showInsertNoteFab = false;
@@ -1653,7 +1662,7 @@ export class EditorComponent extends DataLoadingComponent implements OnDestroy, 
       }
       if (allowToggleVerseSelection) {
         this.showInsertNoteFab = this.target.toggleVerseSelection(verseRef);
-        this.positionInsertNoteFab(segmentElement);
+        this.positionInsertNoteFab();
       }
     } else {
       this.showInsertNoteFab = false;
@@ -1936,6 +1945,27 @@ export class EditorComponent extends DataLoadingComponent implements OnDestroy, 
       newScrollTop += sourceBottomPosition;
     }
     this.source.editor.scrollingContainer.scrollTop = newScrollTop;
+  }
+
+  private subscribeScroll(editor: Quill): void {
+    this.scrollSubscription?.unsubscribe();
+    this.scrollSubscription = this.subscribe(fromEvent(editor.root, 'scroll'), () => {
+      if (this.insertNoteFab == null || this.target == null || this.targetContainer == null) return;
+      const bounds: DOMRect = this.targetContainer.nativeElement.getBoundingClientRect();
+      const fabHeight = 40;
+      const editorMargin = 5;
+
+      // bound the FAB to the top of the editor
+      let scrollTop: number = Math.min(this.target.selectionBoundsTop - editorMargin, editor.root.scrollTop);
+      // bound the FAB to the bottom of the editor
+      const targetContainerBottom: number = bounds.bottom - bounds.top - fabHeight - editorMargin;
+      const minScroll: number = Math.max(this.target.selectionBoundsTop - targetContainerBottom, 0);
+      if (scrollTop < minScroll) {
+        scrollTop = minScroll;
+      }
+
+      this.insertNoteFab.nativeElement.style.marginTop = `-${scrollTop}px`;
+    });
   }
 
   onViewerClicked(viewer: MultiCursorViewer): void {
