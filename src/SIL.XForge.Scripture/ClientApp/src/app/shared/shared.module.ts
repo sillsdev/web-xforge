b7baca061e89eeb3f8d85f18077ae120bf35e78c
Joseph Myers <wjosephmyers@gmail.com>
2023-06-14 11:32:56 +0530

    Refactored blue info icon from Checking Audio into its own component and used it in Settings.
    This replaces my own implementation with what we're already doing elsewhere.
    
    Also, for Settings, I removed some extra spacing at the top of Translate text, to conform its spacing to the other sections. And changed the blue link text to match the blue info.

--- src/SIL.XForge.Scripture/ClientApp/src/app/shared/shared.module.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/shared.module.ts
@@ -3,17 +3,19 @@ import { NgModule } from '@angular/core';
 import { TranslocoModule } from '@ngneat/transloco';
 import { QuillModule } from 'ngx-quill';
 import { UICommonModule } from 'xforge-common/ui-common.module';
+import { BookChapterChooserComponent } from './book-chapter-chooser/book-chapter-chooser.component';
 import { ChapterNavComponent } from './chapter-nav/chapter-nav.component';
+import { InfoComponent } from './info/info.component';
+import { NoticeComponent } from './notice/notice.component';
+import { ShareButtonComponent } from './share/share-button.component';
 import { ShareControlComponent } from './share/share-control.component';
 import { ShareDialogComponent } from './share/share-dialog.component';
-import { ShareButtonComponent } from './share/share-button.component';
 import { TextComponent } from './text/text.component';
-import { NoticeComponent } from './notice/notice.component';
-import { BookChapterChooserComponent } from './book-chapter-chooser/book-chapter-chooser.component';
 
 const componentExports = [
   BookChapterChooserComponent,
   ChapterNavComponent,
+  InfoComponent,
   NoticeComponent,
   ShareButtonComponent,
   ShareControlComponent,
