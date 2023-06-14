493267402d5db58ae5e9929dccc514a522fde80a
Joseph Myers <wjosephmyers@gmail.com>
2023-06-14 11:32:56 +0530

    Refactored blue info icon from Checking Audio into its own component and used it in Settings.
    This replaces my own implementation with what we're already doing elsewhere.
    
    Also, for Settings, I removed some extra spacing at the top of Translate text, to conform its spacing to the other sections. And changed the blue link text to match the blue info.

---
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/info/info.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-info',
+  templateUrl: './info.component.html',
+  styleUrls: ['./info.component.scss']
+})
+export class InfoComponent {
+  @Input() text: string = '';
+
+  constructor() {}
+}
