e71f2e035d1e9fa86fe191922b9ad8640b0c7e98
Peter Chapman <peter@conglomo.co.nz>
2023-06-22 08:41:24 +1200

    Added Web API Documentation

--- src/SIL.XForge.Scripture/Controllers/LanguageController.cs
+++ src/SIL.XForge.Scripture/Controllers/LanguageController.cs
@@ -5,10 +5,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SIL.XForge.Scripture.Controllers;
 
+/// <summary>
+/// Provides methods to allow users to configure their language and culture settings.
+/// </summary>
 [Route("language-api")]
 [ApiController]
 public class LanguageController : ControllerBase
 {
+    /// <summary>
+    /// Set the user's language to the specified culture, then redirect.
+    /// </summary>
+    /// <param name="culture">The culture to change the culture to.</param>
+    /// <param name="returnUrl">The URL to redirect to upon success.</param>
+    /// <exception cref="ArgumentException">The culture or returnUrl parameters were empty.</exception>
+    /// <response code="200">The culture was updated successfully.</response>
+    /// <response code="400">The culture or returnUrl parameters were empty.</response>
     [HttpPost]
     public IActionResult SetLanguage([FromForm] string culture, [FromForm] string returnUrl)
     {
