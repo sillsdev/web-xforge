1e43f44e41972e54fdabefe07e258315cf1098ea
siltomato <colin_moore@sil.org>
2023-06-21 00:29:59 -0400

    SF-2045 Prototype back translation draft ui

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/translate-routing.module.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/translate-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TranslateAuthGuard } from '../shared/project-router.guard';
 import { EditorComponent } from './editor/editor.component';
+import { GenerateDraftComponent } from './generate-draft/generate-draft.component';
 import { TranslateOverviewComponent } from './translate-overview/translate-overview.component';
 
 const routes: Routes = [
   { path: 'projects/:projectId/translate/:bookId', component: EditorComponent, canActivate: [TranslateAuthGuard] },
-  { path: 'projects/:projectId/translate', component: TranslateOverviewComponent, canActivate: [TranslateAuthGuard] }
+  { path: 'projects/:projectId/translate', component: TranslateOverviewComponent, canActivate: [TranslateAuthGuard] },
+  { path: 'projects/:projectId/generate-draft', component: GenerateDraftComponent, canActivate: [TranslateAuthGuard] }
 ];
 
 @NgModule({
