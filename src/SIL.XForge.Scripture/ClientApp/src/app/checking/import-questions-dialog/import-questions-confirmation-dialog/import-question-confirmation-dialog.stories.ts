e224d34a2a11cdb7995b7c0fcfb9b5186f9fe2c9
Joseph Myers <wjosephmyers@gmail.com>
2023-06-26 15:39:36 +0530

    Import Questions confirmation dialog to Material
    This was a little more effort than expected, since Material doesn't have built-in selection support. Some functionality, particularly in styling, needed to be re-added manually to match MDC.
    
    Also moved the dialog story to its own file and deleted mdc-dialog-stories, since that was its only story.

---
+++ src/SIL.XForge.Scripture/ClientApp/src/app/checking/import-questions-dialog/import-questions-confirmation-dialog/import-question-confirmation-dialog.stories.ts
@@ -0,0 +1,23 @@
+import { Meta } from '@storybook/angular';
+import {
+  ImportQuestionsConfirmationDialogComponent,
+  ImportQuestionsConfirmationDialogData
+} from './import-question-confirmation-dialog.component';
+import { MatDialogLaunchComponent, matDialogStory } from '.storybook/story-utils';
+
+const meta: Meta = {
+  title: 'Import/Import Questions',
+  component: MatDialogLaunchComponent
+};
+export default meta;
+
+export const ImportQuestionsConfirmation = matDialogStory(ImportQuestionsConfirmationDialogComponent);
+const confirmationData: ImportQuestionsConfirmationDialogData = {
+  questions: [
+    { before: 'Question before', after: 'Question after', answerCount: 2, checked: true },
+    { before: 'Second question', after: 'Second question (edited)', answerCount: 1, checked: false },
+    { before: 'Third question', after: 'A much better wording of the question', answerCount: 1, checked: true },
+    { before: 'Question #4', after: 'Fourth question (edited)', answerCount: 1, checked: true }
+  ]
+};
+ImportQuestionsConfirmation.args = { data: confirmationData };
