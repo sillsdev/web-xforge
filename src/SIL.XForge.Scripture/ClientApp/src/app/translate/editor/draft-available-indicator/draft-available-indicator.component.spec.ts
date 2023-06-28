900c2f476b9a88fc1abd95500c00747d44db401c
siltomato <colin_moore@sil.org>
2023-06-28 19:39:11 -0400

    SF-2045 Fix some breaking tests

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/draft-available-indicator/draft-available-indicator.component.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/draft-available-indicator/draft-available-indicator.component.spec.ts
@@ -1,14 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { mock } from 'ts-mockito';
+import { DialogService } from 'xforge-common/dialog.service';
 import { DraftAvailableIndicatorComponent } from './draft-available-indicator.component';
 
 describe('DraftAvailableIndicatorComponent', () => {
   let component: DraftAvailableIndicatorComponent;
   let fixture: ComponentFixture<DraftAvailableIndicatorComponent>;
 
+  const mockedDialogService = mock(DialogService);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [DraftAvailableIndicatorComponent]
+      declarations: [DraftAvailableIndicatorComponent],
+      providers: [{ provide: DialogService, useValue: mockedDialogService }]
     }).compileComponents();
 
     fixture = TestBed.createComponent(DraftAvailableIndicatorComponent);
--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/generate-draft/draft-generation.service.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/generate-draft/draft-generation.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClient } from 'src/app/machine-api/http-client';
+import { mock } from 'ts-mockito';
 import { DraftGenerationService } from './draft-generation.service';
+
+const mockedHttpClient = mock(HttpClient);
 
 describe('DraftGenerationService', () => {
   let service: DraftGenerationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [{ provide: HttpClient, useValue: mockedHttpClient }]
+    });
     service = TestBed.inject(DraftGenerationService);
   });
 
