961697edfcff88f1b99b4215c9c20a1df06df971
siltomato <colin_moore@sil.org>
2023-06-28 19:39:11 -0400

    SF-2045 Fix some breaking tests

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/generate-draft/generate-draft.component.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/generate-draft/generate-draft.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClient } from 'src/app/machine-api/http-client';
+import { mock } from 'ts-mockito';
+import { ActivatedProjectService } from 'xforge-common/activated-project.service';
+import { DialogService } from 'xforge-common/dialog.service';
 import { GenerateDraftComponent } from './generate-draft.component';
+
+const mockedHttpClient = mock(HttpClient);
+const mockedActivatedProjectService = mock(ActivatedProjectService);
+const mockedDialogService = mock(DialogService);
 
 describe('GenerateDraftComponent', () => {
   let component: GenerateDraftComponent;
@@ -8,7 +15,12 @@ describe('GenerateDraftComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [GenerateDraftComponent]
+      declarations: [GenerateDraftComponent],
+      providers: [
+        { provide: HttpClient, useValue: mockedHttpClient },
+        { provide: ActivatedProjectService, useValue: mockedActivatedProjectService },
+        { provide: DialogService, useValue: mockedDialogService }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(GenerateDraftComponent);
