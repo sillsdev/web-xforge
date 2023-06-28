d52d9b5be2e548d9ea2aa3580d5e134efdab2721
siltomato <colin_moore@sil.org>
2023-06-28 19:39:11 -0400

    SF-2045 Fix some breaking tests

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/generate-draft/draft-viewer/draft-viewer.component.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/generate-draft/draft-viewer/draft-viewer.component.spec.ts
@@ -1,6 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
+import { SFProjectService } from 'src/app/core/sf-project.service';
+import { HttpClient } from 'src/app/machine-api/http-client';
+import { mock } from 'ts-mockito';
+import { ActivatedProjectService } from 'xforge-common/activated-project.service';
+import { I18nService } from 'xforge-common/i18n.service';
 import { DraftViewerComponent } from './draft-viewer.component';
+
+const mockedHttpClient = mock(HttpClient);
+const mockedActivatedProjectService = mock(ActivatedProjectService);
+const mockedSFProjectService = mock(SFProjectService);
+const mockedActivatedRoute = mock(ActivatedRoute);
+const mockedI18nService = mock(I18nService);
 
 describe('DraftViewerComponent', () => {
   let component: DraftViewerComponent;
@@ -8,7 +19,14 @@ describe('DraftViewerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [DraftViewerComponent]
+      declarations: [DraftViewerComponent],
+      providers: [
+        { provide: HttpClient, useValue: mockedHttpClient },
+        { provide: ActivatedProjectService, useValue: mockedActivatedProjectService },
+        { provide: SFProjectService, useValue: mockedSFProjectService },
+        { provide: ActivatedRoute, useValue: mockedActivatedRoute },
+        { provide: I18nService, useValue: mockedI18nService }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(DraftViewerComponent);
