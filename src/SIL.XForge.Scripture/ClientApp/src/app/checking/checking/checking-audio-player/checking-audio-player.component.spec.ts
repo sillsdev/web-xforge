f6a99d0cd0b24798f3c98a45983f1f0cdfefe4fc
Joseph Myers <wjosephmyers@gmail.com>
2023-06-28 14:01:45 +0530

    Fixed a couple bugs found by unit tests.
    The first was in audioStatus due to an order of operations error. Any time the audio player had a value it was return Unavailable. It was resolving the ?? and then the ?, when it needs to only resolve the ? when the inner status is null.
    
    The second was simply allowing empty source set operations to unset the audio player. This matches previous behavior. Previously we depended on an extra hasSource property. Now, just check if the audio property is defined.

--- src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/checking-audio-player/checking-audio-player.component.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/checking-audio-player/checking-audio-player.component.spec.ts
@@ -3,14 +3,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { of } from 'rxjs';
 import { InfoComponent } from 'src/app/shared/info/info.component';
+import { AudioStatus } from 'src/app/shared/audio-player';
 import { instance, mock, when } from 'ts-mockito';
 import { I18nService } from 'xforge-common/i18n.service';
 import { PwaService } from 'xforge-common/pwa.service';
 import { TestTranslocoModule } from 'xforge-common/test-utils';
 import { UICommonModule } from 'xforge-common/ui-common.module';
-import { AudioStatus, AudioTimePipe, CheckingAudioPlayerComponent } from './checking-audio-player.component';
+import { AudioTimePipe, CheckingAudioPlayerComponent } from './checking-audio-player.component';
 
-describe('CheckingAudioPlayerComponent', () => {
+fdescribe('CheckingAudioPlayerComponent', () => {
   const audioFile = 'test-audio-player.webm';
   const audioFileB = 'test-audio-player-b.webm';
   const playerLoadTimeMs = 1000;
@@ -49,12 +50,12 @@ describe('CheckingAudioPlayerComponent', () => {
     await env.waitForPlayer(playerLoadTimeMs);
     env.clickButton(env.playButton(1));
     await env.waitForPlayer(500);
-    expect(env.component.player1.isPlaying).toBe(true);
+    expect(env.component.player1.audio?.isPlaying).toBe(true);
     env.clickButton(env.playButton(2));
     await env.waitForPlayer(500);
-    expect(env.component.player1.isPlaying).toBe(false);
+    expect(env.component.player1.audio?.isPlaying).toBe(false);
     env.clickButton(env.pauseButton(2));
-    expect(env.component.player2.isPlaying).toBe(false);
+    expect(env.component.player2.audio?.isPlaying).toBe(false);
   });
 
   it('disables the audio player when audio is reset', async () => {
@@ -62,16 +63,16 @@ describe('CheckingAudioPlayerComponent', () => {
       '<app-checking-audio-player #player1 id="player1" source="' + audioFile + '"></app-checking-audio-player>';
     const env = new TestEnvironment(template);
     await env.waitForPlayer(playerLoadTimeMs);
-    expect(env.component.player1.hasSource).toBe(true);
+    expect(env.component.player1.audio).not.toBeUndefined();
     env.component.player1.source = '';
-    expect(env.component.player1.hasSource).toBe(false);
+    expect(env.component.player1.audio).toBeUndefined();
   });
 
   it('it notifies the user when audio is unavailable offline', async () => {
     const template = `<app-checking-audio-player #player1 source="https://"></app-checking-audio-player>`;
     const env = new TestEnvironment(template, false);
     await env.waitForPlayer(playerLoadTimeMs);
-    expect(env.component.player1.hasSource).toBe(true);
+    expect(env.component.player1.audio).not.toBeUndefined();
     expect(env.audioNotAvailableMessage).not.toBeNull();
   });
 
@@ -79,7 +80,7 @@ describe('CheckingAudioPlayerComponent', () => {
     const template = `<app-checking-audio-player #player1 source="${audioFile}"></app-checking-audio-player>`;
     const env = new TestEnvironment(template, false);
     await env.waitForPlayer(playerLoadTimeMs);
-    expect(env.component.player1.hasSource).toBe(true);
+    expect(env.component.player1.audio).not.toBeUndefined();
     expect(env.audioNotAvailableMessage).toBeNull();
   });
 
@@ -87,7 +88,7 @@ describe('CheckingAudioPlayerComponent', () => {
     const template = `<app-checking-audio-player #player1 source="${audioFile}"></app-checking-audio-player>`;
     const env = new TestEnvironment(template, false);
     await env.waitForPlayer(playerLoadTimeMs);
-    expect(env.component.player1.hasSource).toBe(true);
+    expect(env.component.player1.audio?.isAudioAvailable).toBe(true);
     // The browser is online, but the component thinks it is offline. This simulates the scenario where audio data is
     // already loaded, but the browser is offline.
     expect(env.audioNotAvailableMessage).toBeNull();
@@ -194,7 +195,7 @@ class TestEnvironment {
     this.fixture.detectChanges();
   }
 
-  async waitForPlayer(ms: number) {
+  async waitForPlayer(ms: number): Promise<void> {
     await new Promise(resolve => this.ngZone.runOutsideAngular(() => setTimeout(resolve, ms)));
     this.fixture.detectChanges();
   }
