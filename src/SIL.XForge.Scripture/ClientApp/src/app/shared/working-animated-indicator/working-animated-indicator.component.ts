42618db68851a701ea95eec8ff9d3a4cdc2d2b12
siltomato <colin_moore@sil.org>
2023-06-30 10:28:15 -0400

    SF-2045 Prototype back translation draft ui

---
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/working-animated-indicator/working-animated-indicator.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-working-animated-indicator',
+  templateUrl: './working-animated-indicator.component.html',
+  styleUrls: ['./working-animated-indicator.component.scss']
+})
+export class WorkingAnimatedIndicatorComponent {}
