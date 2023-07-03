5e92c2317c20683b1b20df08917f6c9ab94831f6
siltomato <colin_moore@sil.org>
2023-07-03 11:44:29 -0400

    SF-2045 Add unit tests

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/generate-draft/draft-viewer/draft-viewer.component.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/generate-draft/draft-viewer/draft-viewer.component.spec.ts
@@ -1,40 +1,336 @@
+import { QueryList } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs';
+import { SFProjectProfileDoc } from 'src/app/core/models/sf-project-profile-doc';
+import { Delta } from 'src/app/core/models/text-doc';
 import { SFProjectService } from 'src/app/core/sf-project.service';
-import { HttpClient } from 'src/app/machine-api/http-client';
-import { mock } from 'ts-mockito';
+import { TextComponent } from 'src/app/shared/text/text.component';
 import { ActivatedProjectService } from 'xforge-common/activated-project.service';
 import { I18nService } from 'xforge-common/i18n.service';
+import { DraftGenerationService, DraftSegmentMap } from '../draft-generation.service';
 import { DraftViewerComponent } from './draft-viewer.component';
-
-const mockedHttpClient = mock(HttpClient);
-const mockedActivatedProjectService = mock(ActivatedProjectService);
-const mockedSFProjectService = mock(SFProjectService);
-const mockedActivatedRoute = mock(ActivatedRoute);
-const mockedI18nService = mock(I18nService);
+import { DraftViewerService } from './draft-viewer.service';
 
 describe('DraftViewerComponent', () => {
   let component: DraftViewerComponent;
   let fixture: ComponentFixture<DraftViewerComponent>;
+  let mockDraftGenerationService: jasmine.SpyObj<DraftGenerationService>;
+  let mockDraftViewerService: jasmine.SpyObj<DraftViewerService>;
+  let mockActivatedProjectService: jasmine.SpyObj<ActivatedProjectService>;
+  let mockProjectService: jasmine.SpyObj<SFProjectService>;
+  let mockActivatedRoute: jasmine.SpyObj<ActivatedRoute>;
+  let mockRouter: jasmine.SpyObj<Router>;
+  let mockI18nService: jasmine.SpyObj<I18nService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [DraftViewerComponent],
+  beforeEach(() => {
+    mockDraftGenerationService = jasmine.createSpyObj('DraftGenerationService', ['getGeneratedDraft']);
+    mockDraftViewerService = jasmine.createSpyObj('DraftViewerService', ['hasDraftOps', 'toDraftOps']);
+    mockActivatedProjectService = jasmine.createSpyObj('ActivatedProjectService', ['get projectId', 'get projectDoc'], {
+      projectId: 'testProjectId',
+      projectDoc: {
+        data: {
+          translateConfig: {
+            source: {
+              projectRef: 'sourceProjectId'
+            }
+          }
+        }
+      }
+    });
+    mockProjectService = jasmine.createSpyObj('SFProjectService', ['getProfile']);
+    mockActivatedRoute = jasmine.createSpyObj('ActivatedRoute', ['paramMap']);
+    mockRouter = jasmine.createSpyObj('Router', ['navigateByUrl']);
+    mockI18nService = jasmine.createSpyObj('I18nService', []);
+
+    TestBed.configureTestingModule({
+      declarations: [DraftViewerComponent, TextComponent],
       providers: [
-        { provide: HttpClient, useValue: mockedHttpClient },
-        { provide: ActivatedProjectService, useValue: mockedActivatedProjectService },
-        { provide: SFProjectService, useValue: mockedSFProjectService },
-        { provide: ActivatedRoute, useValue: mockedActivatedRoute },
-        { provide: I18nService, useValue: mockedI18nService }
+        { provide: DraftGenerationService, useValue: mockDraftGenerationService },
+        { provide: DraftViewerService, useValue: mockDraftViewerService },
+        { provide: ActivatedProjectService, useValue: mockActivatedProjectService },
+        { provide: SFProjectService, useValue: mockProjectService },
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        { provide: Router, useValue: mockRouter },
+        { provide: I18nService, useValue: mockI18nService }
       ]
     }).compileComponents();
-
     fixture = TestBed.createComponent(DraftViewerComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should initialize component correctly', () => {
+    mockProjectService.getProfile.and.returnValue(Promise.resolve({ data: {} } as SFProjectProfileDoc));
+    component.ngOnInit();
+    expect(component.targetProjectId).toEqual('targetProjectId');
+    expect(component.sourceProjectId).toEqual('sourceProjectId');
+    expect(mockProjectService.getProfile).toHaveBeenCalledWith('sourceProjectId');
   });
+
+  it('should populate draft text correctly', () => {
+    component.currentBook = 1;
+    component.currentChapter = 1;
+    component.targetEditor = jasmine.createSpyObj('TextComponent', ['editor', 'loaded'], {
+      editor: jasmine.createSpyObj('QuillEditor', ['getContents', 'setContents'])
+    });
+    component.sourceEditor = jasmine.createSpyObj('TextComponent', ['loaded']);
+    component.targetEditorQueryList = jasmine.createSpyObj('QueryList', ['changes']);
+    spyOnProperty(component.targetEditorQueryList, 'changes', 'get').and.returnValue(of({}));
+    spyOn(component.targetEditor.editor!, 'getContents').and.returnValue(delta_no_verse_2);
+    mockDraftGenerationService.getGeneratedDraft.and.returnValue(of(draftSegmentMap));
+    component.populateDraftText();
+    expect(mockDraftGenerationService.getGeneratedDraft).toHaveBeenCalledWith('targetProjectId', 1, 1);
+    expect(mockDraftViewerService.hasDraftOps).toHaveBeenCalled();
+    expect(mockDraftViewerService.toDraftOps).toHaveBeenCalled();
+    expect(component.targetEditor.editor!.setContents).toHaveBeenCalledWith(delta_verse_2_suggested);
+  });
+
+  it('should apply draft correctly', () => {
+    component.targetEditor = jasmine.createSpyObj('TextComponent', ['editor', 'loaded'], {
+      editor: jasmine.createSpyObj('QuillEditor', ['getContents', 'setContents'])
+    });
+    spyOn(component.targetEditor.editor!, 'getContents').and.returnValue(delta_verse_2_suggested);
+    component.applyDraft();
+    expect(component.targetEditor.editor!.enable).toHaveBeenCalledWith(true);
+    expect(component.targetEditor.editor!.setContents).toHaveBeenCalledWith(delta_verse_2_accepted);
+    expect(component.targetEditor.editor!.disable).toHaveBeenCalled();
+    expect(component.isDraftApplied).toBeTrue();
+  });
+
+  it('should clean draft ops correctly', () => {
+    const cleanedOps = component.cleanDraftOps(delta_verse_2_suggested.ops!);
+    expect(cleanedOps).toEqual(delta_verse_2_accepted.ops!);
+  });
+
+  it('should navigate to the correct URL for editing the book/chapter', () => {
+    const routerSpy = spyOn(mockRouter, 'navigateByUrl');
+    component.currentBook = 1;
+    component.currentChapter = 2;
+    component.targetProjectId = '123';
+
+    component.editChapter();
+
+    expect(routerSpy).toHaveBeenCalledWith('/projects/123/translate/GEN/2');
+  });
+
+  it('should navigate to the correct URL for the given book and chapter', () => {
+    const routerSpy = spyOn(mockRouter, 'navigateByUrl');
+    const book = 1;
+    const chapter = 2;
+    component.targetProjectId = '123';
+
+    component.navigateBookChapter(book, chapter);
+
+    expect(routerSpy).toHaveBeenCalledWith('/projects/123/translate/GEN/2');
+  });
+
+  const draftSegmentMap: DraftSegmentMap = {
+    verse_1_1: 'This is verse 1.',
+    verse_1_2: 'This is verse 2.',
+    verse_1_3: 'This is verse 3.'
+  };
+
+  const delta_no_verse_2 = new Delta([
+    {
+      insert: {
+        chapter: {
+          number: '1',
+          style: 'c'
+        }
+      }
+    },
+    {
+      attributes: {
+        'para-contents': true
+      },
+      insert: {
+        verse: {
+          number: '1',
+          style: 'v'
+        }
+      }
+    },
+    {
+      attributes: {
+        segment: 'verse_1_1',
+        'para-contents': true
+      },
+      insert: {
+        blank: true
+      }
+    },
+    {
+      attributes: {
+        'para-contents': true
+      },
+      insert: {
+        verse: {
+          number: '2',
+          style: 'v'
+        }
+      }
+    },
+    {
+      attributes: {
+        segment: 'verse_1_2',
+        'para-contents': true
+      },
+      insert: {
+        blank: true
+      }
+    },
+    {
+      attributes: {
+        'para-contents': true
+      },
+      insert: {
+        verse: {
+          number: '3',
+          style: 'v'
+        }
+      }
+    },
+    {
+      attributes: {
+        segment: 'verse_1_3',
+        'para-contents': true
+      },
+      insert: 'And God said '
+    }
+  ]);
+
+  const delta_verse_2_suggested = new Delta([
+    {
+      insert: {
+        chapter: {
+          number: '1',
+          style: 'c'
+        }
+      }
+    },
+    {
+      attributes: {
+        'para-contents': true
+      },
+      insert: {
+        verse: {
+          number: '1',
+          style: 'v'
+        }
+      }
+    },
+    {
+      attributes: {
+        segment: 'verse_1_1',
+        'para-contents': true
+      },
+      insert: {
+        blank: true
+      }
+    },
+    {
+      attributes: {
+        'para-contents': true
+      },
+      insert: {
+        verse: {
+          number: '2',
+          style: 'v'
+        }
+      }
+    },
+    {
+      attributes: {
+        segment: 'verse_1_2',
+        'para-contents': true,
+        draft: true
+      },
+      insert: 'This is verse 2. '
+    },
+    {
+      attributes: {
+        'para-contents': true
+      },
+      insert: {
+        verse: {
+          number: '3',
+          style: 'v'
+        }
+      }
+    },
+    {
+      attributes: {
+        segment: 'verse_1_3',
+        'para-contents': true
+      },
+      insert: 'And God said '
+    }
+  ]);
+
+  const delta_verse_2_accepted = new Delta([
+    {
+      insert: {
+        chapter: {
+          number: '1',
+          style: 'c'
+        }
+      }
+    },
+    {
+      attributes: {
+        'para-contents': true
+      },
+      insert: {
+        verse: {
+          number: '1',
+          style: 'v'
+        }
+      }
+    },
+    {
+      attributes: {
+        segment: 'verse_1_1',
+        'para-contents': true
+      },
+      insert: {
+        blank: true
+      }
+    },
+    {
+      attributes: {
+        'para-contents': true
+      },
+      insert: {
+        verse: {
+          number: '2',
+          style: 'v'
+        }
+      }
+    },
+    {
+      attributes: {
+        segment: 'verse_1_2',
+        'para-contents': true
+      },
+      insert: 'This is verse 2. '
+    },
+    {
+      attributes: {
+        'para-contents': true
+      },
+      insert: {
+        verse: {
+          number: '3',
+          style: 'v'
+        }
+      }
+    },
+    {
+      attributes: {
+        segment: 'verse_1_3',
+        'para-contents': true
+      },
+      insert: 'And God said '
+    }
+  ]);
 });
--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/generate-draft/generate-draft.component.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/generate-draft/generate-draft.component.spec.ts
@@ -1,34 +1,182 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClient } from 'src/app/machine-api/http-client';
-import { mock } from 'ts-mockito';
+import { MatDialog } from '@angular/material/dialog';
+import { of } from 'rxjs';
+import { BuildDto } from 'src/app/machine-api/build-dto';
+import { BuildStates } from 'src/app/machine-api/build-states';
 import { ActivatedProjectService } from 'xforge-common/activated-project.service';
 import { DialogService } from 'xforge-common/dialog.service';
+import { DraftGenerationService } from './draft-generation.service';
 import { GenerateDraftComponent } from './generate-draft.component';
-
-const mockedHttpClient = mock(HttpClient);
-const mockedActivatedProjectService = mock(ActivatedProjectService);
-const mockedDialogService = mock(DialogService);
 
 describe('GenerateDraftComponent', () => {
   let component: GenerateDraftComponent;
-  let fixture: ComponentFixture<GenerateDraftComponent>;
+  let matDialog: jasmine.SpyObj<MatDialog>;
+  let dialogService: jasmine.SpyObj<DialogService>;
+  let draftGenerationService: jasmine.SpyObj<DraftGenerationService>;
+  let activatedProjectService: jasmine.SpyObj<ActivatedProjectService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [GenerateDraftComponent],
-      providers: [
-        { provide: HttpClient, useValue: mockedHttpClient },
-        { provide: ActivatedProjectService, useValue: mockedActivatedProjectService },
-        { provide: DialogService, useValue: mockedDialogService }
-      ]
-    }).compileComponents();
+  const buildDto: BuildDto = {
+    id: 'testId',
+    href: 'testHref',
+    revision: 0,
+    engine: {
+      id: 'testEngineId',
+      href: 'testEngineHref'
+    },
+    percentCompleted: 0,
+    message: '',
+    state: BuildStates.Queued
+  };
 
-    fixture = TestBed.createComponent(GenerateDraftComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    matDialog = jasmine.createSpyObj('MatDialog', ['cancelAll']);
+    dialogService = jasmine.createSpyObj('DialogService', ['confirm']);
+    draftGenerationService = jasmine.createSpyObj('DraftGenerationService', [
+      'startBuild',
+      'cancelBuild',
+      'getBuildProgress'
+    ]);
+    activatedProjectService = jasmine.createSpyObj('ActivatedProjectService', [''], { projectId: 'testProjectId' });
+    component = new GenerateDraftComponent(matDialog, dialogService, activatedProjectService, draftGenerationService, [
+      BuildStates.Active,
+      BuildStates.Pending,
+      BuildStates.Queued
+    ]);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('ngOnInit', () => {
+    it('should subscribe to build progress', () => {
+      draftGenerationService.getBuildProgress.and.returnValue(of(buildDto));
+      component.ngOnInit();
+      expect(draftGenerationService.getBuildProgress).toHaveBeenCalledWith(activatedProjectService.projectId!);
+      expect(component.draftJob).toEqual(buildDto);
+    });
+  });
+
+  describe('generateDraft', () => {
+    it('should start the draft build', () => {
+      component.generateDraft();
+      expect(draftGenerationService.startBuild).toHaveBeenCalledWith('testProjectId');
+    });
+  });
+
+  describe('cancel', () => {
+    it('should cancel the draft build if user confirms "cancel" dialog', async () => {
+      const job: BuildDto = { ...buildDto, state: BuildStates.Active };
+      component.draftJob = job;
+      dialogService.confirm.and.returnValue(Promise.resolve(true));
+      await component.cancel();
+      expect(dialogService.confirm).toHaveBeenCalled();
+      expect(draftGenerationService.cancelBuild).toHaveBeenCalledWith('testProjectId');
+    });
+    it('should not cancel the draft build if user exits "cancel" dialog', async () => {
+      const job: BuildDto = { ...buildDto, state: BuildStates.Active };
+      component.draftJob = job;
+      dialogService.confirm.and.returnValue(Promise.resolve(false));
+      await component.cancel();
+      expect(dialogService.confirm).toHaveBeenCalled();
+      expect(draftGenerationService.cancelBuild).not.toHaveBeenCalled();
+    });
+    it('should cancel the draft build without dialog if the build state is not active', async () => {
+      const job: BuildDto = { ...buildDto, state: BuildStates.Queued };
+      component.draftJob = job;
+      await component.cancel();
+      expect(dialogService.confirm).not.toHaveBeenCalled();
+      expect(draftGenerationService.cancelBuild).toHaveBeenCalledWith('testProjectId');
+    });
+  });
+
+  describe('isDraftInProgress', () => {
+    it('should return true if the draft build is in progress', () => {
+      component.draftJob = { state: BuildStates.Active } as BuildDto;
+      expect(component.isDraftInProgress()).toBe(true);
+      component.draftJob = { state: BuildStates.Pending } as BuildDto;
+      expect(component.isDraftInProgress()).toBe(true);
+      component.draftJob = { state: BuildStates.Queued } as BuildDto;
+      expect(component.isDraftInProgress()).toBe(true);
+    });
+    it('should return false if the draft build is not in progress', () => {
+      component.draftJob = { state: BuildStates.Completed } as BuildDto;
+      expect(component.isDraftInProgress()).toBe(false);
+      component.draftJob = { state: BuildStates.Canceled } as BuildDto;
+      expect(component.isDraftInProgress()).toBe(false);
+      component.draftJob = { state: BuildStates.Faulted } as BuildDto;
+      expect(component.isDraftInProgress()).toBe(false);
+    });
+  });
+
+  describe('isDraftQueued', () => {
+    it('should return true if the draft build is queued', () => {
+      component.draftJob = { state: BuildStates.Queued } as BuildDto;
+      expect(component.isDraftQueued()).toBe(true);
+      component.draftJob = { state: BuildStates.Pending } as BuildDto;
+      expect(component.isDraftQueued()).toBe(true);
+    });
+    it('should return false if the draft build is not queued', () => {
+      component.draftJob = { state: BuildStates.Active } as BuildDto;
+      expect(component.isDraftQueued()).toBe(false);
+      component.draftJob = { state: BuildStates.Canceled } as BuildDto;
+      expect(component.isDraftQueued()).toBe(false);
+      component.draftJob = { state: BuildStates.Completed } as BuildDto;
+      expect(component.isDraftQueued()).toBe(false);
+      component.draftJob = { state: BuildStates.Faulted } as BuildDto;
+      expect(component.isDraftQueued()).toBe(false);
+    });
+  });
+
+  describe('isDraftActive', () => {
+    it('should return true if the draft build is active', () => {
+      component.draftJob = { state: BuildStates.Active } as BuildDto;
+      expect(component.isDraftActive()).toBe(true);
+    });
+    it('should return false if the draft build is not active', () => {
+      component.draftJob = { state: BuildStates.Completed } as BuildDto;
+      expect(component.isDraftActive()).toBe(false);
+      component.draftJob = { state: BuildStates.Canceled } as BuildDto;
+      expect(component.isDraftActive()).toBe(false);
+      component.draftJob = { state: BuildStates.Faulted } as BuildDto;
+      expect(component.isDraftActive()).toBe(false);
+      component.draftJob = { state: BuildStates.Pending } as BuildDto;
+      expect(component.isDraftActive()).toBe(false);
+      component.draftJob = { state: BuildStates.Queued } as BuildDto;
+      expect(component.isDraftActive()).toBe(false);
+    });
+  });
+
+  describe('isDraftComplete', () => {
+    it('should return true if the draft build is complete', () => {
+      component.draftJob = { state: BuildStates.Completed } as BuildDto;
+      expect(component.isDraftComplete()).toBe(true);
+    });
+    it('should return false if the draft build is not complete', () => {
+      component.draftJob = { state: BuildStates.Active } as BuildDto;
+      expect(component.isDraftComplete()).toBe(false);
+      component.draftJob = { state: BuildStates.Canceled } as BuildDto;
+      expect(component.isDraftComplete()).toBe(false);
+      component.draftJob = { state: BuildStates.Faulted } as BuildDto;
+      expect(component.isDraftComplete()).toBe(false);
+      component.draftJob = { state: BuildStates.Pending } as BuildDto;
+      expect(component.isDraftComplete()).toBe(false);
+      component.draftJob = { state: BuildStates.Queued } as BuildDto;
+      expect(component.isDraftComplete()).toBe(false);
+    });
+  });
+
+  describe('canCancel', () => {
+    it('should return true if the draft build is in progress', () => {
+      component.draftJob = { state: BuildStates.Active } as BuildDto;
+      expect(component.isDraftInProgress()).toBe(true);
+      component.draftJob = { state: BuildStates.Pending } as BuildDto;
+      expect(component.isDraftInProgress()).toBe(true);
+      component.draftJob = { state: BuildStates.Queued } as BuildDto;
+      expect(component.isDraftInProgress()).toBe(true);
+    });
+    it('should return false if the draft build is not in progress', () => {
+      component.draftJob = { state: BuildStates.Completed } as BuildDto;
+      expect(component.isDraftInProgress()).toBe(false);
+      component.draftJob = { state: BuildStates.Canceled } as BuildDto;
+      expect(component.isDraftInProgress()).toBe(false);
+      component.draftJob = { state: BuildStates.Faulted } as BuildDto;
+      expect(component.isDraftInProgress()).toBe(false);
+    });
   });
 });
