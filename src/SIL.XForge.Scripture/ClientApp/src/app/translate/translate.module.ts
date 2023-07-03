6703a5803cff06ddd7db1c585df6b0874ae2c44a
siltomato <colin_moore@sil.org>
2023-07-03 11:44:29 -0400

    SF-2045 Add unit tests

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/translate.module.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/translate.module.ts
@@ -15,6 +15,8 @@ import { TranslateRoutingModule } from './translate-routing.module';
 import { GenerateDraftComponent } from './generate-draft/generate-draft.component';
 import { DraftViewerComponent } from './generate-draft/draft-viewer/draft-viewer.component';
 import { DraftAvailableIndicatorComponent } from './editor/draft-available-indicator/draft-available-indicator.component';
+import { DraftGenerationService } from './generate-draft/draft-generation.service';
+import { MockDraftGenerationService } from './generate-draft/draft-generation.mock.service';
 
 @NgModule({
   declarations: [
@@ -29,6 +31,10 @@ import { DraftAvailableIndicatorComponent } from './editor/draft-available-indic
     DraftViewerComponent,
     DraftAvailableIndicatorComponent
   ],
-  imports: [TranslateRoutingModule, CommonModule, SharedModule, UICommonModule, XForgeCommonModule, TranslocoModule]
+  imports: [TranslateRoutingModule, CommonModule, SharedModule, UICommonModule, XForgeCommonModule, TranslocoModule],
+  providers: [
+    // TODO - remove once machine api pretranslation endpoints are present
+    { provide: DraftGenerationService, useClass: MockDraftGenerationService }
+  ]
 })
 export class TranslateModule {}
