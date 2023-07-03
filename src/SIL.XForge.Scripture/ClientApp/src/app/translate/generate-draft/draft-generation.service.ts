a85a651125c6dd53f9120aafe2cd98635496b2b1
siltomato <colin_moore@sil.org>
2023-07-03 11:44:29 -0400

    SF-2045 Add unit tests

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/generate-draft/draft-generation.service.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/generate-draft/draft-generation.service.ts
@@ -1,69 +1,24 @@
-import { Inject, Injectable, InjectionToken } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { reduce } from 'lodash-es';
 import { VerseRef } from 'realtime-server/lib/esm/scriptureforge/scripture-utils/verse-ref';
-import { BehaviorSubject, Observable, of, Subscription, timer } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { delay, map, repeat, shareReplay, switchMap, takeWhile } from 'rxjs/operators';
 import { BuildStates } from 'src/app/machine-api/build-states';
 import { HttpClient } from 'src/app/machine-api/http-client';
 import { BuildDto } from '../../machine-api/build-dto';
-import { PreTranslation, PreTranslationData, samplePreTranslations } from './pretranslation';
-
-/**
- * Dictionary of segmentRef -> verse.
- */
-export interface DraftSegmentMap {
-  [segmentRefId: string]: string;
-}
-
-/**
- * Configuration options for DraftGenerationService.
- */
-export interface DraftGenerationServiceOptions {
-  /**
-   * Polling frequency in milliseconds.
-   */
-  pollRate: number;
-}
-
-/**
- * Configuration options for DraftGenerationService.
- */
-export const DRAFT_GENERATION_SERVICE_OPTIONS = new InjectionToken<DraftGenerationServiceOptions>(
-  'DRAFT_GENERATION_SERVICE_OPTIONS',
-  {
-    providedIn: 'root',
-    factory: () => ({
-      pollRate: 5000
-    })
-  }
-);
-
-/**
- * Build states for builds that are under way.  A new build should not be started if current build
- * is in one of these states.
- */
-export const ACTIVE_BUILD_STATES = new InjectionToken<BuildStates[]>('ACTIVE_BUILD_STATES', {
-  providedIn: 'root',
-  factory: () => [BuildStates.Active, BuildStates.Pending, BuildStates.Queued]
-});
+import {
+  ACTIVE_BUILD_STATES,
+  DraftGenerationServiceOptions,
+  DraftSegmentMap,
+  DRAFT_GENERATION_SERVICE_OPTIONS,
+  PreTranslation,
+  PreTranslationData
+} from './draft-generation';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DraftGenerationService {
-  // Mock build progress
-  private readonly job$ = new BehaviorSubject<BuildDto | undefined>(undefined);
-  private timerSub?: Subscription;
-  private readonly initialJobState: BuildDto = {
-    id: '',
-    href: '',
-    engine: { id: '', href: '' },
-    revision: 0,
-    state: BuildStates.Queued,
-    percentCompleted: 0,
-    message: ''
-  };
-
   constructor(
     private readonly httpClient: HttpClient,
     @Inject(ACTIVE_BUILD_STATES) private readonly activeBuildStates: BuildStates[],
@@ -76,14 +31,12 @@ export class DraftGenerationService {
    * @returns A hot observable 'BuildDto' describing the state and progress of the build job.
    */
   pollBuildProgress(projectId: string): Observable<BuildDto | undefined> {
-    // return this.getBuildProgress(projectId).pipe(
-    //   repeat(),
-    //   delay(this.options.pollRate),
-    //   takeWhile(job => this.activeBuildStates.includes(job?.state as BuildStates), true),
-    //   shareReplay(1)
-    // );
-
-    return this.job$;
+    return this.getBuildProgress(projectId).pipe(
+      repeat(),
+      delay(this.options.pollRate),
+      takeWhile(job => this.activeBuildStates.includes(job?.state as BuildStates), true),
+      shareReplay(1)
+    );
   }
 
   /**
@@ -92,16 +45,14 @@ export class DraftGenerationService {
    * @returns An observable 'BuildDto' describing the state and progress of the build job.
    */
   getBuildProgress(projectId: string): Observable<BuildDto | undefined> {
-    // return this.httpClient.get<BuildDto>(`translation/builds/id:${projectId}?pretranslate=true`).pipe(
-    //   map(res => {
-    //     if (res.data?.state === BuildStates.Faulted) {
-    //       throw new Error('Error occurred during build: ' + res.data.message);
-    //     }
-    //     return res.data;
-    //   })
-    // );
-
-    return this.job$;
+    return this.httpClient.get<BuildDto>(`translation/builds/id:${projectId}?pretranslate=true`).pipe(
+      map(res => {
+        if (res.data?.state === BuildStates.Faulted) {
+          throw new Error('Error occurred during build: ' + res.data.message);
+        }
+        return res.data;
+      })
+    );
   }
 
   /**
@@ -110,19 +61,15 @@ export class DraftGenerationService {
    * @returns An observable 'BuildDto' describing the state and progress of the build job.
    */
   startBuild(projectId: string): Observable<BuildDto | undefined> {
-    // return this.getBuildProgress(projectId).pipe(
-    //   switchMap((job?: BuildDto) => this.activeBuildStates.includes(job?.state as BuildStates)
-    //     ? of(job)
-    //     : this.httpClient
-    //       .post<BuildDto>(`translation/pretranslations`, JSON.stringify(projectId))
-    //       .pipe(map(res => res.data))
-    //   )
-    // );
-
-    if (!this.activeBuildStates.includes(this.job$.value?.state as BuildStates)) {
-      this.startGeneration();
-    }
-    return this.job$;
+    return this.getBuildProgress(projectId).pipe(
+      switchMap((job?: BuildDto) =>
+        this.activeBuildStates.includes(job?.state as BuildStates)
+          ? of(job)
+          : this.httpClient
+              .post<BuildDto>(`translation/pretranslations`, JSON.stringify(projectId))
+              .pipe(map(res => res.data))
+      )
+    );
   }
 
   /**
@@ -131,13 +78,9 @@ export class DraftGenerationService {
    * @returns An observable 'BuildDto' describing the state and progress of the build job.
    */
   cancelBuild(projectId: string): Observable<BuildDto | undefined> {
-    // return this.httpClient
-    //   .post<BuildDto>(`translation/pretranslations/cancel`, JSON.stringify(projectId))
-    //   .pipe(map(res => res.data));
-
-    this.job$.next({ ...this.initialJobState, state: BuildStates.Canceled });
-    this.timerSub?.unsubscribe();
-    return this.job$;
+    return this.httpClient
+      .post<BuildDto>(`translation/pretranslations/cancel`, JSON.stringify(projectId))
+      .pipe(map(res => res.data));
   }
 
   /**
@@ -148,15 +91,9 @@ export class DraftGenerationService {
    * @returns An observable dictionary of segmentRef -> verse.
    */
   getGeneratedDraft(projectId: string, book: number, chapter: number): Observable<DraftSegmentMap> {
-    // return this.httpClient
-    //   .get<PreTranslationData>(`translation/engines/project:${projectId}/actions/preTranslate/${book}_${chapter}`)
-    //   .pipe(
-    //     map(res => (res.data && this.toDraftSegmentMap(res.data.preTranslations)) ?? {}),
-    //   );
-
-    return of({
-      preTranslations: samplePreTranslations[`${book}_${chapter}`]
-    }).pipe(map((data: PreTranslationData) => this.toDraftSegmentMap(data.preTranslations)));
+    return this.httpClient
+      .get<PreTranslationData>(`translation/engines/project:${projectId}/actions/preTranslate/${book}_${chapter}`)
+      .pipe(map(res => (res.data && this.toDraftSegmentMap(res.data.preTranslations)) ?? {}));
   }
 
   /**
@@ -175,45 +112,5 @@ export class DraftGenerationService {
       },
       {}
     );
-  }
-
-  // Mock generation
-  private startGeneration(): void {
-    const interval = 100;
-    const duration = 10000;
-    const pendingAfter = duration / 4;
-    const activeAfter = (duration / 4) * 2;
-    const generationTimer$ = timer(0, interval).pipe(
-      takeWhile(x => interval * x <= duration) // Inclusive of last emission
-    );
-
-    this.job$.next({ ...this.initialJobState });
-
-    this.timerSub = generationTimer$.subscribe((intervalNum: number) => {
-      const elapsed = intervalNum * interval;
-      const newStatus: BuildDto = { ...(this.job$.value ?? this.initialJobState) };
-
-      if (elapsed >= pendingAfter) {
-        newStatus.state = BuildStates.Pending;
-      }
-
-      if (elapsed >= activeAfter) {
-        newStatus.state = BuildStates.Active;
-      }
-
-      if (elapsed >= duration) {
-        newStatus.state = BuildStates.Completed;
-      }
-
-      if (newStatus.state === BuildStates.Active) {
-        newStatus.percentCompleted = (elapsed / duration) * 100;
-      }
-
-      // console.log('elapsed', elapsed);
-      // console.log('percentCompleted', newStatus.percentCompleted);
-      // console.log('state', newStatus.state);
-
-      this.job$.next(newStatus);
-    });
   }
 }
