f56664b1089f9b6041d103a3da0861051ac2fdf3
siltomato <colin_moore@sil.org>
2023-07-03 11:44:29 -0400

    SF-2045 Add unit tests

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/generate-draft/draft-viewer/draft-viewer.service.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/generate-draft/draft-viewer/draft-viewer.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-
+import { DeltaOperation } from 'rich-text';
 import { DraftViewerService } from './draft-viewer.service';
 
 describe('DraftViewerService', () => {
@@ -10,7 +10,50 @@ describe('DraftViewerService', () => {
     service = TestBed.inject(DraftViewerService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('hasDraftOps', () => {
+    it('should return false if draft is empty', () => {
+      const draft = {};
+      const targetOps: DeltaOperation[] = [];
+      const result = service.hasDraftOps(draft, targetOps);
+      expect(result).toBeFalse();
+    });
+
+    it('should return false if all target ops have existing translation', () => {
+      const draft = { segment1: 'pretranslation1' };
+      const targetOps = [{ insert: 'existing translation', attributes: { segment: 'segment1' } }];
+      const result = service.hasDraftOps(draft, targetOps);
+      expect(result).toBeFalse();
+    });
+
+    it('should return true if there is a target op without existing translation', () => {
+      const draft = { segment1: 'pretranslation1' };
+      const targetOps = [{ insert: '', attributes: { segment: 'segment1' } }];
+      const result = service.hasDraftOps(draft, targetOps);
+      expect(result).toBeTrue();
+    });
+  });
+
+  describe('toDraftOps', () => {
+    it('should return target ops as is if draft is empty', () => {
+      const draft = {};
+      const targetOps = [{ insert: 'existing translation', attributes: { segment: 'segment1' } }];
+      const result = service.toDraftOps(draft, targetOps);
+      expect(result).toEqual(targetOps);
+    });
+
+    it('should return target ops with draft pretranslation copied', () => {
+      const draft = { segment1: 'pretranslation1' };
+      const targetOps = [{ insert: '', attributes: { segment: 'segment1' } }];
+      const expectedResult = [{ insert: 'pretranslation1', attributes: { segment: 'segment1', draft: true } }];
+      const result = service.toDraftOps(draft, targetOps);
+      expect(result).toEqual(expectedResult);
+    });
+
+    it('should preserve existing translation if available', () => {
+      const draft = { segment1: 'pretranslation1' };
+      const targetOps = [{ insert: 'existing translation', attributes: { segment: 'segment1' } }];
+      const result = service.toDraftOps(draft, targetOps);
+      expect(result).toEqual(targetOps);
+    });
   });
 });
