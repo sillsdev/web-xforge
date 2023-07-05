8d74e0ba6e7cbe86282236843809cc03916cc727
Peter Chapman <peter@conglomo.co.nz>
2023-07-06 11:57:27 +1200

    Add API to Get History from ShareDB and Paratext

--- test/SIL.XForge.Scripture.Tests/Services/ParatextServiceTests.cs
+++ test/SIL.XForge.Scripture.Tests/Services/ParatextServiceTests.cs
@@ -3924,6 +3924,7 @@ public class ParatextServiceTests
             MockRestClientFactory = Substitute.For<ISFRestClientFactory>();
             MockGuidService = Substitute.For<IGuidService>();
             MockRegistryHttpClient = Substitute.For<HttpClient>();
+            var mockDeltaUsxMapper = Substitute.For<IDeltaUsxMapper>();
 
             DateTime aSecondAgo = DateTime.Now - TimeSpan.FromSeconds(1);
             string accessToken1 = TokenHelper.CreateAccessToken(
@@ -3992,7 +3993,8 @@ public class ParatextServiceTests
                 MockInternetSharedRepositorySourceProvider,
                 MockGuidService,
                 MockRestClientFactory,
-                MockHgWrapper
+                MockHgWrapper,
+                mockDeltaUsxMapper
             )
             {
                 ScrTextCollection = MockScrTextCollection,
