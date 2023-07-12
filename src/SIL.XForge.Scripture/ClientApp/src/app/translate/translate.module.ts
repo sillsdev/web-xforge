955f94d8565806bbfe8f5b5c6fb3233e6e232d31
siltomato <colin_moore@sil.org>
2023-07-12 10:45:57 -0400

    SF-2045 Remove provider for mock machine api http client

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/translate.module.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/translate.module.ts
@@ -4,7 +4,8 @@ import { TranslocoModule } from '@ngneat/transloco';
 import { UICommonModule } from 'xforge-common/ui-common.module';
 import { XForgeCommonModule } from 'xforge-common/xforge-common.module';
 import { SharedModule } from '../shared/shared.module';
-import { HttpClient } from '../machine-api/http-client';
+import { DraftGenerationComponent } from './draft-generation/draft-generation.component';
+import { DraftViewerComponent } from './draft-generation/draft-viewer/draft-viewer.component';
 import { EditorComponent } from './editor/editor.component';
 import { MultiViewerComponent } from './editor/multi-viewer/multi-viewer.component';
 import { NoteDialogComponent } from './editor/note-dialog/note-dialog.component';
@@ -13,10 +14,6 @@ import { SuggestionsComponent } from './editor/suggestions.component';
 import { TrainingProgressComponent } from './training-progress/training-progress.component';
 import { TranslateOverviewComponent } from './translate-overview/translate-overview.component';
 import { TranslateRoutingModule } from './translate-routing.module';
-import { DraftGenerationComponent } from './draft-generation/draft-generation.component';
-import { DraftViewerComponent } from './draft-generation/draft-viewer/draft-viewer.component';
-import { MockPreTranslationHttpClient } from './draft-generation/mock-pretranslation-machine-api';
-import { DRAFT_GENERATION_SERVICE_OPTIONS } from './draft-generation/draft-generation';
 
 @NgModule({
   declarations: [
@@ -30,11 +27,6 @@ import { DRAFT_GENERATION_SERVICE_OPTIONS } from './draft-generation/draft-gener
     DraftGenerationComponent,
     DraftViewerComponent
   ],
-  imports: [TranslateRoutingModule, CommonModule, SharedModule, UICommonModule, XForgeCommonModule, TranslocoModule],
-  providers: [
-    // TODO: Remove these two lines. They are for testing back translation with mock service.
-    { provide: HttpClient, useClass: MockPreTranslationHttpClient },
-    { provide: DRAFT_GENERATION_SERVICE_OPTIONS, useValue: { pollRate: 1000 } }
-  ]
+  imports: [TranslateRoutingModule, CommonModule, SharedModule, UICommonModule, XForgeCommonModule, TranslocoModule]
 })
 export class TranslateModule {}
