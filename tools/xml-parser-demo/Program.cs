f31f204cc0ddc1bf35588c63af126bb13bf88519
Raymond Luong <raymond_luong@sil.org>
2023-07-12 15:55:57 -0600

    Search each notes files for problem state

--- tools/xml-parser-demo/Program.cs
+++ tools/xml-parser-demo/Program.cs
@@ -2,9 +2,14 @@ using System.Text;
 using System.Diagnostics;
 using System.Xml.Serialization;
 using Paratext.Data.ProjectComments;
+using System.Xml.Linq;
+using System.Text.RegularExpressions;
 
 public class Program
 {
+    string machineName = "";
+    string projectDir = "";
+    string projectRootDir = "";
     public static void Timing()
     {
         // Warm up on a small file
@@ -30,8 +35,9 @@ public class Program
 
     public static async Task Main()
     {
+        var program = new Program();
         List<string> projectDirs = new();
-        projectDirs.Add("SFP");
+        projectDirs.Add(program.projectDir);
 
         // For each project:
         //   List all commits on notes xml files since 2023-06-21
@@ -41,28 +47,73 @@ public class Program
         //       Fetch a copy of the before and after of the notes file (before and after the commit)
         //       Show how did SF meaningfully change the content.
 
-        await ProcessProjectsAsync(projectDirs);
+        await program.ProcessProjectsAsync(projectDirs);
     }
 
-    private static async Task ProcessProjectsAsync(List<string> projectDirs)
+    private async Task ProcessProjectsAsync(List<string> projectDirs)
     {
         foreach (string projectDir in projectDirs)
             await ProcessProjectAsync(projectDir);
     }
 
-    private static async Task ProcessProjectAsync(string projectDir)
+    private async Task ProcessProjectAsync(string projectDir)
     {
-        string commitListBlob = await RunCommand(
+        string repoDir = $"{projectRootDir}/{projectDir}/target";
+        string commitListBlob = await RunCommandAsync(
             "hg",
-            "log --no-merges --date >2023-06-21 --template CommitId:{node}\nDate:{date|date}\nDesc:{desc}\nFiles:{files%'{file}\n'}\n",
-            $"/home/vagrant/note-corruption/{projectDir}/target"
+            // "log --no-merges --date >2023-06-21 --template CommitId:{node}\tDate:{date|date}",
+            "log --no-merges --date >2023-06-21 --template CommitId:{node}---Date:{date|date}---Desc:{desc}---Files:{files%'{file}\n'}-----",
+            repoDir
         );
         Console.WriteLine(commitListBlob);
         List<CommitData> commitData = CommitParser.Parse(commitListBlob);
-        Console.WriteLine(commitData[0]);
+        foreach (var data in commitData)
+        {
+            data.RepoDir = repoDir;
+        }
+        // Console.WriteLine(commitData[0]);
+        IEnumerable<CommitData> relevantCommits = commitData.Where(c => c.MachineName == machineName).Where(c => c.Files.Any(f => Regex.Match(f, "notes.*.xml", RegexOptions.IgnoreCase).Length > 0));
+
+        foreach (CommitData commit in relevantCommits)
+        {
+            foreach (string notesFile in commit.Files.Where(f => Regex.Match(f, "notes.*.xml", RegexOptions.IgnoreCase).Length > 0))
+            {
+                await ProcessNotesFileAsync(commit, notesFile);
+            }
+        }
+        // For each notes file
+
+
+        // Get a snapshot of before and after
     }
 
-    private static async Task<string> RunCommand(string program, string arguments, string workingDirectory)
+    private static async Task ProcessNotesFileAsync(CommitData data, string filename)
+    {
+        string fileBefore = await FetchFileAsync(data.CommitId + '^', filename, data.RepoDir);
+        string fileAfter = await FetchFileAsync(data.CommitId, filename, data.RepoDir);
+
+        if (!string.IsNullOrEmpty(fileBefore) && !string.IsNullOrEmpty(fileAfter))
+        {
+            XDocument beforeDoc = XDocument.Parse(fileBefore);
+            XDocument afterDoc = XDocument.Parse(fileAfter);
+            string beforeDocStr = beforeDoc.ToString();
+            string afterDocStr = afterDoc.ToString();
+
+            bool isEqual = beforeDocStr.Equals(afterDocStr);
+            if (!isEqual)
+            {
+                // Console.WriteLine("*********** problem detected *****************");
+                Console.WriteLine($"{data.RepoDir} {data.CommitId} {filename}");
+            }
+        }
+    }
+
+    private static async Task<string> FetchFileAsync(string commitId, string filename, string path)
+    {
+        return await RunCommandAsync("hg", $"cat -r {commitId} \"{filename}\"", path);
+    }
+
+    private static async Task<string> RunCommandAsync(string program, string arguments, string workingDirectory)
     {
         StringBuilder output = new();
         StringBuilder error = new();
@@ -91,10 +142,9 @@ public class Program
     {
         public string CommitId { get; set; }
         public DateTime Date { get; set; }
-        public string Comment { get; set; }
         public string MachineName { get; set; }
-        public string ApplicationVersion { get; set; }
         public List<string> Files { get; set; }
+        public string RepoDir { get; set; }
     }
 
     public class CommitParser
@@ -102,44 +152,46 @@ public class Program
         public static List<CommitData> Parse(string data)
         {
             var commits = new List<CommitData>();
-            var lines = data.Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            var setOfCommits = data.Split(new[] { "-----" }, StringSplitOptions.RemoveEmptyEntries);
             var currentCommit = new CommitData();
 
-            foreach (var line in lines)
+            foreach (var commitChunk in setOfCommits)
             {
-                var parts = line.Split(':');
-                if (parts.Length != 2)
-                    continue;
-
-                var key = parts[0].Trim();
-                var value = parts[1].Trim();
-
-                switch (key)
+                var commitChunkPart = commitChunk.Split(new[] { "---" }, System.StringSplitOptions.RemoveEmptyEntries);
+                foreach (var part in commitChunkPart)
                 {
-                    case "CommitId":
-                        currentCommit.CommitId = value;
-                        break;
-                    case "Date":
-                        currentCommit.Date = DateTime.Parse(value);
-                        break;
-                    case "Comment":
-                        currentCommit.Comment = value;
-                        break;
-                    case "MachineName":
-                        currentCommit.MachineName = value;
-                        break;
-                    case "ApplicationVersion":
-                        currentCommit.ApplicationVersion = value;
-                        break;
-                    case "Files":
-                        currentCommit.Files = value.Split(',').Select(f => f.Trim()).ToList();
-                        break;
-                    case "":
-                        // Blank line indicates the end of a commit, add it to the list
-                        commits.Add(currentCommit);
-                        currentCommit = new CommitData();
-                        break;
+                    var parts = part.Split(':');
+                    if (parts.Length != 2)
+                        continue;
+
+                    var key = parts[0].Trim();
+                    var value = parts[1].Trim();
+
+                    switch (key)
+                    {
+                        case "CommitId":
+                            currentCommit.CommitId = value;
+                            break;
+                        case "Date":
+                            currentCommit.Date = DateTime.Parse(value);
+                            break;
+                        case "Desc":
+                            XDocument doc = XDocument.Parse(value);
+                            var machineName = doc.Descendants("MachineName").SingleOrDefault();
+                            currentCommit.MachineName = machineName.Value;
+                            break;
+                        case "Files":
+                            currentCommit.Files = value.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries).Select(f => f.Trim()).ToList();
+                            break;
+                        case "":
+                            // Blank line indicates the end of a commit, add it to the list
+                            // commits.Add(currentCommit);
+                            // currentCommit = new CommitData();
+                            break;
+                    }
                 }
+                commits.Add(currentCommit);
+                currentCommit = new CommitData();
             }
 
             return commits;
