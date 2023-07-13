9eb0110132f569c0e15c9fea36f52bbee41b324f
Joseph Myers <wjosephmyers@gmail.com>
2023-07-13 09:07:12 +0530

    Question plays audio and relevant scripture audio.
    There's still a lot of work to be done, particularly with UX. This completes the happy path (with stubbed data) and serves as the groundwork for the rest of the effort.
    
    The single button audio player now renders custom content, which will aid in reuse. It still needs error state work. It now inherits from the new base class AudioPlayerBaseComponent, which I introduced as a common layer for any audio-loading component. The main feature of the single button player is its ability to dynamically use either AudioPlayer or AudioSegmentPlayer, based on given optional Inputs. If it receives start and end values, it will create and use the latter.
    
    AudioSegmentPlayer is a subclass of AudioPlayer. I debated over various design patterns, rather than traditional inheritance, but extending the original seemed the best fit. The subclass is small, and it changes the behavior of its parent, rather than adding behavior. The child class accepts a full audio file, like its parent, but plays only the given range from it. All its properties, e.g. seek and currentTime, are based off the range and not the full audio file.
    
    Much work left to be done in error cases, UX, and testing.

--- src/SIL.XForge.Scripture/ClientApp/src/app/checking-question/checking-question.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/checking-question/checking-question.component.ts
@@ -1,39 +1,84 @@
-import { Component, Input } from '@angular/core';
-import { toVerseRef, VerseRefData } from 'realtime-server/lib/esm/scriptureforge/models/verse-ref-data';
+import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
+import { VerseRef } from '@sillsdev/scripture';
+import { getTextAudioId, TextAudio } from 'realtime-server/lib/esm/scriptureforge/models/text-audio';
+import {
+  toStartAndEndVerseRefs,
+  toVerseRef,
+  VerseRefData
+} from 'realtime-server/lib/esm/scriptureforge/models/verse-ref-data';
 import { I18nService } from 'xforge-common/i18n.service';
+import { SingleButtonAudioPlayerComponent } from '../checking/checking/single-button-audio-player/single-button-audio-player.component';
 import { QuestionDoc } from '../core/models/question-doc';
+import { SFProjectService } from '../core/sf-project.service';
 
 @Component({
   selector: 'app-checking-question',
   templateUrl: './checking-question.component.html',
   styleUrls: ['./checking-question.component.scss']
 })
-export class CheckingQuestionComponent {
-  @Input() questionDoc?: QuestionDoc;
+export class CheckingQuestionComponent implements AfterViewInit {
+  private _scriptureAudio?: TextAudio;
 
-  constructor(private readonly i18n: I18nService) {}
+  @Input() questionDoc?: QuestionDoc;
+  @ViewChild('questionAudio') questionAudio?: SingleButtonAudioPlayerComponent;
+  @ViewChild('scriptureAudio') scriptureAudio?: SingleButtonAudioPlayerComponent;
+
+  constructor(private readonly projectService: SFProjectService, private readonly i18n: I18nService) {}
+
+  ngAfterViewInit(): void {
+    const projectId = this.questionDoc!.data!.projectRef;
+    const audioId = getTextAudioId(
+      projectId,
+      this.questionDoc!.data!.verseRef!.bookNum,
+      this.questionDoc!.data!.verseRef!.chapterNum
+    );
+
+    this.projectService.queryAudioText(projectId).then(audioQuery => {
+      this._scriptureAudio = audioQuery.docs.find(t => t.id === audioId)?.data;
+    });
+  }
 
   get referenceForDisplay(): string {
     const verseRefData: VerseRefData | undefined = this.questionDoc?.data?.verseRef;
     return verseRefData ? this.i18n.localizeReference(toVerseRef(verseRefData)) : '';
   }
 
+  get scriptureAudioUrl(): string | undefined {
+    return this._scriptureAudio?.audioUrl;
+  }
+
+  get scriptureAudioStart(): number | undefined {
+    return this._scriptureAudio?.timings.find(t => t.textRef === 'v' + this.startVerse.toString())?.from;
+  }
+
+  get scriptureAudioEnd(): number | undefined {
+    return this._scriptureAudio?.timings.find(t => t.textRef === 'v' + this.endVerse.toString())?.to;
+  }
+
   get questionText(): string {
     return this.questionDoc?.data?.text ?? '';
   }
 
-  get audioUrl(): string | undefined {
-    // TODO (scripture audio) implement stub
-    return '';
+  get questionAudioUrl(): string | undefined {
+    return this.questionDoc?.data?.audioUrl;
   }
 
   playScripture(): void {
-    // TODO (scripture audio) implement stub
-    alert('Not implemented yet');
+    this.scriptureAudio?.audio?.isPlaying ? this.scriptureAudio?.stop() : this.scriptureAudio?.play();
   }
 
   playQuestion(): void {
-    // TODO (scripture audio) implement stub
-    alert('Not implemented yet');
+    this.questionAudio?.audio?.isPlaying ? this.questionAudio?.stop() : this.questionAudio?.play();
+  }
+
+  private get startVerse(): number {
+    const verseRefData: VerseRefData | undefined = this.questionDoc?.data?.verseRef;
+    return verseRefData ? toVerseRef(verseRefData).verseNum : 0;
+  }
+
+  private get endVerse(): number {
+    const verseRefData: VerseRefData | undefined = this.questionDoc?.data?.verseRef;
+    const end: VerseRef | undefined = verseRefData && toStartAndEndVerseRefs(verseRefData).endVerseRef;
+    return end ? end.verseNum : this.startVerse;
   }
 }
--- src/SIL.XForge.Scripture/ClientApp/src/app/shared/audio/audio-player.component/audio-player.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/audio/audio-player.component/audio-player.component.ts
@@ -1,53 +1,22 @@
-import { Component, Input, Pipe, PipeTransform } from '@angular/core';
+import { Component, Pipe, PipeTransform } from '@angular/core';
 import { MatSliderChange } from '@angular/material/slider';
-import { BehaviorSubject } from 'rxjs';
-import { AudioPlayer, AudioStatus } from 'src/app/shared/audio/audio-player';
 import { I18nService } from 'xforge-common/i18n.service';
 import { PwaService } from 'xforge-common/pwa.service';
-import { SubscriptionDisposable } from 'xforge-common/subscription-disposable';
+import { AudioPlayerBaseComponent } from '../audio-player-base.component/audio-player-base.component';
 
 @Component({
   selector: 'app-audio-player',
   templateUrl: './audio-player.component.html',
   styleUrls: ['./audio-player.component.scss']
 })
-export class AudioPlayerComponent extends SubscriptionDisposable {
-  readonly isAudioAvailable$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-  private _audio: AudioPlayer | undefined;
-
-  constructor(private readonly pwaService: PwaService, readonly i18n: I18nService) {
-    super();
-
-    this.subscribe(this.isAudioAvailable$, () => {
-      this.audio?.setSeek(0);
-    });
-  }
-
-  get audio(): AudioPlayer | undefined {
-    return this._audio;
+//todo rename to slider
+export class AudioPlayerComponent extends AudioPlayerBaseComponent {
+  constructor(pwaService: PwaService, readonly i18n: I18nService) {
+    super(pwaService);
   }
 
   get duration(): number {
     return this.audio?.duration ?? 0;
-  }
-
-  get audioStatus(): AudioStatus {
-    return this.audio?.status$.value ?? (this.pwaService.isOnline ? AudioStatus.Unavailable : AudioStatus.Offline);
-  }
-
-  @Input() set source(source: string | undefined) {
-    this.isAudioAvailable$.next(false);
-    this.audio?.dispose();
-    if (source != null && source !== '') {
-      this._audio = new AudioPlayer(source, this.pwaService);
-      this.subscribe(this._audio.status$, newVal => {
-        if (newVal === AudioStatus.Available) {
-          this.isAudioAvailable$.next(true);
-        }
-      });
-    } else {
-      this._audio = undefined;
-    }
   }
 
   get seek(): number {
