3ff5d5103f77ce6b677b0fc98c6fbde4d49d99ab
Joseph Myers <wjosephmyers@gmail.com>
2023-07-13 10:52:45 +0530

    Moved AudioTimePipe to its own file
    And removed duplicate

---
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/audio/audio-time-pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'audioTime' })
+export class AudioTimePipe implements PipeTransform {
+  transform(seconds: number, ..._args: any[]): string {
+    const minutesString = Math.floor(seconds / 60);
+    seconds = Math.floor(seconds % 60);
+    const secondsString = seconds >= 10 ? seconds : '0' + seconds;
+    return minutesString + ':' + secondsString;
+  }
+}
