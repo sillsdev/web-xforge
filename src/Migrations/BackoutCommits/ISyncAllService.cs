1ce3389af3ce62af075dc9d63168b09dfb3d82fc
Raymond Luong <raymond_luong@sil.org>
2023-07-20 16:17:01 -0600

    Add service to sync projects after backing out commits

---
+++ src/Migrations/BackoutCommits/ISyncAllService.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BackoutCommits;
+
+/// <summary>
+/// Interface for service that can initiate a synchronization of all SF projects.
+/// </summary>
+public interface ISyncAllService
+{
+    Task SynchronizeAllProjectsAsync(
+        bool doSynchronizations,
+        ISet<string> sfProjectIdsToSynchronize = null,
+        IDictionary<string, string> sfAdminsToUse = null
+    );
+}
