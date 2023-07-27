24075ecd6a54643be485f4d1b96b1478df15ed07
siltomato <colin_moore@sil.org>
2023-07-27 00:23:55 -0400

    SF-2045 Improve 'has draft' detection for non-blank object 'insert' op

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/draft-viewer/draft-viewer.service.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/draft-viewer/draft-viewer.service.spec.ts
@@ -15,22 +15,27 @@ describe('DraftViewerService', () => {
     it('should return false if draft is empty', () => {
       const draft: DraftSegmentMap = {};
       const targetOps: DeltaOperation[] = [];
-      const result: boolean = service.hasDraftOps(draft, targetOps);
-      expect(result).toBeFalse();
+      expect(service.hasDraftOps(draft, targetOps)).toBeFalse();
     });
 
     it('should return false if all target ops have existing translation', () => {
       const draft: DraftSegmentMap = { verse_1_1: 'In the beginning' };
       const targetOps: DeltaOperation[] = [{ insert: 'existing translation', attributes: { segment: 'verse_1_1' } }];
-      const result: boolean = service.hasDraftOps(draft, targetOps);
-      expect(result).toBeFalse();
+      expect(service.hasDraftOps(draft, targetOps)).toBeFalse();
+    });
+
+    it('should return false for ops with insert object that is not { blank: true}', () => {
+      const draft: DraftSegmentMap = { verse_1_1: 'In the beginning' };
+      const targetOps: DeltaOperation[] = [
+        { insert: { 'note-thread-embed': {} }, attributes: { segment: 'verse_1_1' } }
+      ];
+      expect(service.hasDraftOps(draft, targetOps)).toBeFalse();
     });
 
     it('should return true if there is a target op without existing translation', () => {
       const draft: DraftSegmentMap = { verse_1_1: 'In the beginning' };
       const targetOps: DeltaOperation[] = [{ insert: '', attributes: { segment: 'verse_1_1' } }];
-      const result: boolean = service.hasDraftOps(draft, targetOps);
-      expect(result).toBeTrue();
+      expect(service.hasDraftOps(draft, targetOps)).toBeTrue();
     });
   });
 
@@ -38,8 +43,7 @@ describe('DraftViewerService', () => {
     it('should return target ops as-is if draft is empty', () => {
       const draft: DraftSegmentMap = {};
       const targetOps: DeltaOperation[] = [{ insert: 'existing translation', attributes: { segment: 'verse_1_1' } }];
-      const result: DeltaOperation[] = service.toDraftOps(draft, targetOps);
-      expect(result).toEqual(targetOps);
+      expect(service.toDraftOps(draft, targetOps)).toEqual(targetOps);
     });
 
     it('should return target ops with draft pretranslation copied', () => {
@@ -52,15 +56,13 @@ describe('DraftViewerService', () => {
         { insert: 'In the beginning', attributes: { segment: 'verse_1_1', draft: true } },
         { insert: 'Existing verse 2', attributes: { segment: 'verse_1_2' } }
       ];
-      const result: DeltaOperation[] = service.toDraftOps(draft, targetOps);
-      expect(result).toEqual(expectedResult);
+      expect(service.toDraftOps(draft, targetOps)).toEqual(expectedResult);
     });
 
     it('should preserve existing translation if available', () => {
       const draft: DraftSegmentMap = { verse_1_1: 'In the beginning' };
       const targetOps: DeltaOperation[] = [{ insert: 'existing translation', attributes: { segment: 'verse_1_1' } }];
-      const result: DeltaOperation[] = service.toDraftOps(draft, targetOps);
-      expect(result).toEqual(targetOps);
+      expect(service.toDraftOps(draft, targetOps)).toEqual(targetOps);
     });
   });
 });
