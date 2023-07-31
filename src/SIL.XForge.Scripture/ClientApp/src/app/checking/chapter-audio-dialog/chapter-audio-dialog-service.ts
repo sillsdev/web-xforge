65a20c8cc6e8d789d75f8773015bed7773fc0435
Nigel Wells <nigel-wells@hotmail.com>
2023-07-31 13:20:11 +1200

    - Remove redundant textdoc - Fix correct IDs assigned to file uploads - Create a dialog service to handle uploads independent of a specific component - Initial setup for tests

---
+++ src/SIL.XForge.Scripture/ClientApp/src/app/checking/chapter-audio-dialog/chapter-audio-dialog-service.ts
@@ -0,0 +1,35 @@
+import { DialogService } from 'xforge-common/dialog.service';
+import { MatDialogConfig } from '@angular/material/dialog';
+import { Injectable } from '@angular/core';
+import { SFProjectService } from '../../core/sf-project.service';
+import {
+  ChapterAudioDialogComponent,
+  ChapterAudioDialogData,
+  ChapterAudioDialogResult
+} from './chapter-audio-dialog.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChapterAudioDialogService {
+  constructor(private readonly dialogService: DialogService, private readonly projectService: SFProjectService) {}
+
+  async openDialog(config: ChapterAudioDialogData): Promise<void> {
+    const dialogConfig: MatDialogConfig<ChapterAudioDialogData> = {
+      data: config,
+      width: '500px'
+    };
+    const dialogRef = this.dialogService.openMatDialog(ChapterAudioDialogComponent, dialogConfig);
+    const result: ChapterAudioDialogResult | 'close' | undefined = await dialogRef.afterClosed().toPromise();
+    if (result == null || result === 'close') {
+      return;
+    }
+    await this.projectService.onlineCreateAudioTimingData(
+      config.projectId,
+      result.book,
+      result.chapter,
+      result.timingData,
+      result.audioUrl
+    );
+  }
+}
