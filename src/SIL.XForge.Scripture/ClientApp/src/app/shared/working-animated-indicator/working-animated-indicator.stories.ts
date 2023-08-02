132678ce139f543e7642a9fa212c8adad95ac860
siltomato <colin_moore@sil.org>
2023-08-02 14:53:37 -0400

    SF-2045 Remove spec in favor of storybook story

---
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/working-animated-indicator/working-animated-indicator.stories.ts
@@ -0,0 +1,36 @@
+import { MatIconModule } from '@angular/material/icon';
+import { componentWrapperDecorator, Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+import { WorkingAnimatedIndicatorComponent } from './working-animated-indicator.component';
+
+export default {
+  title: 'Utility/Working Animated Indicator',
+  component: WorkingAnimatedIndicatorComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [MatIconModule]
+    }),
+    componentWrapperDecorator(
+      (story: string) => `
+        <div style="font-size: {{ (storyProps$ | async).fontSize }}px">
+        ${story}
+        </div>
+      `
+    )
+  ],
+  argTypes: {
+    fontSize: {
+      control: {
+        type: 'range',
+        min: 12,
+        max: 48
+      }
+    }
+  },
+  args: {
+    fontSize: 16
+  }
+} as Meta<WorkingAnimatedIndicatorComponent>;
+
+type Story = StoryObj<WorkingAnimatedIndicatorComponent>;
+
+export const Default: Story = {};
