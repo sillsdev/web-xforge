d2ddc695b2adee1876034a39c7d2d286a1ec9f7d
Joseph Myers <wjosephmyers@gmail.com>
2023-08-09 15:41:35 +0700

    PR comments

--- src/SIL.XForge.Scripture/Models/AudioTiming.cs
+++ src/SIL.XForge.Scripture/Models/AudioTiming.cs
@@ -5,4 +5,6 @@ public class AudioTiming
     public string TextRef { get; set; }
     public double From { get; set; }
     public double To { get; set; }
+
+    public override string ToString() => $"{TextRef}: {From} - {To}";
 }
