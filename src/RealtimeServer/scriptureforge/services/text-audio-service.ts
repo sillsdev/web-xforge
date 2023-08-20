7b5a7bf2399ae7095917132a66f11e0e2136eaa1
Peter Chapman <peter@conglomo.co.nz>
2023-08-21 09:30:31 +1200

    Added Validation Schema for Chapter Audio

--- src/RealtimeServer/scriptureforge/services/text-audio-service.ts
+++ src/RealtimeServer/scriptureforge/services/text-audio-service.ts
@@ -1,3 +1,4 @@
+import { ValidationSchema } from '../../common/models/validation-schema';
 import { ProjectDomainConfig } from '../../common/services/project-data-service';
 import { SFProjectDomain } from '../models/sf-project-rights';
 import { TextAudio, TEXT_AUDIO_COLLECTION, TEXT_AUDIO_INDEX_PATHS } from '../models/text-audio';
@@ -11,6 +12,46 @@ export class TextAudioService extends SFProjectDataService<TextAudio> {
   readonly collection = TEXT_AUDIO_COLLECTION;
 
   protected readonly indexPaths = TEXT_AUDIO_INDEX_PATHS;
+  readonly validationSchema: ValidationSchema = {
+    bsonType: SFProjectDataService.validationSchema.bsonType,
+    required: SFProjectDataService.validationSchema.required,
+    properties: {
+      ...SFProjectDataService.validationSchema.properties,
+      _id: {
+        bsonType: 'string',
+        pattern: '^[0-9a-f]+:[0-9A-Z]+:[0-9]+:target$'
+      },
+      dataId: {
+        bsonType: 'string'
+      },
+      mimeType: {
+        bsonType: 'string'
+      },
+      audioUrl: {
+        bsonType: 'string'
+      },
+      timings: {
+        bsonType: 'array',
+        items: {
+          bsonType: 'object',
+          required: ['textRef', 'from', 'to'],
+          properties: {
+            textRef: {
+              bsonType: 'string'
+            },
+            from: {
+              bsonType: 'number'
+            },
+            to: {
+              bsonType: 'number'
+            }
+          },
+          additionalProperties: false
+        }
+      }
+    },
+    additionalProperties: false
+  };
 
   constructor() {
     super(TEXT_AUDIO_MIGRATIONS);
