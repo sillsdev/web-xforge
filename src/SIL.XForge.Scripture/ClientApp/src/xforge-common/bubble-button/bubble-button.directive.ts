4b6196e4b832d130163ffa9a610c2bcdd4e75632
siltomato <colin_moore@sil.org>
2023-08-21 22:29:56 -0400

    SF-2193 Add directive for button click bubbles

---
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/bubble-button/bubble-button.directive.ts
@@ -0,0 +1,45 @@
+/* eslint-disable @angular-eslint/directive-selector */
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+
+/**
+ * Directive to add bubble animation to a button. Inspired by https://codepen.io/nourabusoud/pen/ypZzMM,
+ * but modified to use an inner `<span>` to attach the pseudo elements to so as not to conflict
+ * with other styles or components that use `::before` or `::after` such as Angular Material components.
+ */
+@Directive({
+  selector: '[sfBubbleButton]'
+})
+export class BubbleButtonDirective implements OnInit {
+  cssInnerSpanStyleClass = 'sf-bubble-button-elements';
+  cssButtonStyleClass = 'sf-bubble-button';
+  cssButtonAnimationClass = 'sf-bubble-animate';
+
+  constructor(private readonly el: ElementRef, private readonly renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    const hostElement = this.el.nativeElement;
+    const innerSpan = this.renderer.createElement('span');
+
+    // Add inner span to host element
+    this.renderer.addClass(innerSpan, this.cssInnerSpanStyleClass);
+    this.renderer.appendChild(hostElement, innerSpan);
+
+    // Add class and click listener to host element
+    this.renderer.addClass(hostElement, this.cssButtonStyleClass);
+    this.renderer.listen(hostElement, 'click', () => {
+      // Add animation class to inner span
+      this.addAnimationClass(innerSpan);
+    });
+  }
+
+  // Adds animation class to the element and removes it after animation is complete
+  addAnimationClass(el: any): void {
+    // Reset animation
+    el.classList.remove(this.cssButtonAnimationClass);
+
+    // Timeout needed to restart animation
+    setTimeout(() => {
+      el.classList.add(this.cssButtonAnimationClass);
+    }, 10);
+  }
+}
---
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/bubble-button/bubble-button.stories.ts
@@ -0,0 +1,66 @@
+import '!style-loader!css-loader!sass-loader!./bubble-button.scss';
+import { MatButtonModule } from '@angular/material/button';
+import { componentWrapperDecorator, Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+import { BubbleButtonDirective } from './bubble-button.directive';
+
+export default {
+  title: 'Misc/Bubble Button',
+
+  decorators: [
+    moduleMetadata({
+      imports: [MatButtonModule],
+      declarations: [BubbleButtonDirective]
+    }),
+    componentWrapperDecorator(
+      story => `
+      <div style="display:flex; justify-content:center; padding:100px 0">
+        ${story}
+      </div>
+      `
+    )
+  ]
+} as Meta;
+
+type Story = StoryObj;
+
+export const MatRaisedButton: Story = {
+  render: () => ({
+    template: `<button mat-raised-button sfBubbleButton color="primary">Mat raised button</button>`
+  })
+};
+
+export const MatFlatButton: Story = {
+  render: () => ({
+    template: `<button mat-flat-button sfBubbleButton color="primary">Mat flat button</button>`
+  })
+};
+
+export const MatStrokedButton: Story = {
+  render: () => ({
+    template: `<button mat-stroked-button sfBubbleButton color="primary">Mat stroked button</button>`
+  })
+};
+
+export const MatButton: Story = {
+  render: () => ({
+    template: `<button mat-button sfBubbleButton color="primary">Mat button</button>`
+  })
+};
+
+export const VanillaButton: Story = {
+  render: () => ({
+    template: `<button sfBubbleButton color="primary">Vanilla button</button>`
+  })
+};
+
+export const LongTextButton: Story = {
+  render: () => ({
+    template: `<button mat-flat-button sfBubbleButton color="primary">This mat flat button has long text</button>`
+  })
+};
+
+export const ShortTextButton: Story = {
+  render: () => ({
+    template: `<button mat-flat-button sfBubbleButton color="primary">Ok</button>`
+  })
+};
