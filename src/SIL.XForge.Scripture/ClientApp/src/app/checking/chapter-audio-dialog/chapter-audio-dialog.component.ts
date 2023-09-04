91e75355837b751e8573a0c998c013af50deb8e5
Joseph Myers <wjosephmyers@gmail.com>
2023-09-04 15:37:09 +0700

    Added error check for chapters property

--- src/SIL.XForge.Scripture/ClientApp/src/app/checking/chapter-audio-dialog/chapter-audio-dialog.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/checking/chapter-audio-dialog/chapter-audio-dialog.component.ts
@@ -116,7 +116,7 @@ export class ChapterAudioDialogComponent {
   }
 
   get chapters(): number[] {
-    if (this.book === 0) {
+    if (this.book === 0 || this.book === undefined) {
       return [];
     }
     const bookAbbr: string = Canon.bookNumberToId(this.book);
