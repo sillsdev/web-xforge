ebd9ae507048950ea8c8faba8c599e1a4acb60fa
MarkS <marksvc@gmail.com>
2023-09-06 16:56:05 -0600

    Shrink Scripture audio area up if hide text
    In situations where we hide the text, intending just for the audio
    player to be used, make the text+audio area have a
    size of the height needed by the audio player, to just show
    the audio player. The audio area can still be made smaller to move it
    up out of the way.
    
    In the case where we do not hide the text, and whether or not there is
    audio, still we show the question area as a smaller area near the
    bottom of the screen, with the Scripture text area using most of the
    available space.
    
    Rename to clarify meaning of AudioStatus.Initializing.
    
    AudioPlayerBaseComponent is first instantiated with a null `source`,
    and later given the real one (or not). Make it start out with a timing
    slider to make it start out with a likely height so the
    CheckingComponent doesn't need to wait for APBC to build itself, to
    know its height.
    
    Remove close button from audio player when hide-text, which would
    leave user with no audio player and no Scripture text.

--- src/SIL.XForge.Scripture/ClientApp/src/app/shared/audio/audio-player-base/audio-player-base.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/audio/audio-player-base/audio-player-base.component.ts
@@ -9,15 +9,11 @@ import { AudioPlayer, AudioStatus } from '../audio-player';
 })
 export abstract class AudioPlayerBaseComponent extends SubscriptionDisposable implements OnDestroy {
   readonly isAudioAvailable$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-  private _isAudioInitComplete: boolean = false;
+  hasProblem: boolean = false;
   private _audio: AudioPlayer | undefined;
 
   constructor(protected readonly onlineStatusService: OnlineStatusService) {
     super();
-
-    this.subscribe(this.isAudioAvailable$, () => {
-      this.audio?.setSeek(0);
-    });
   }
 
   get audio(): AudioPlayer | undefined {
@@ -29,10 +25,13 @@ export abstract class AudioPlayerBaseComponent extends SubscriptionDisposable im
     if (this.audio !== undefined) {
       this.subscribe(this.audio.status$, newVal => {
         if (newVal === AudioStatus.Available) {
+          this.audio?.setSeek(0);
+          this.hasProblem = false;
           this.isAudioAvailable$.next(true);
         }
-        if (newVal !== AudioStatus.Init) {
-          this._isAudioInitComplete = true;
+        if (this._audio?.hasErrorState) {
+          this.hasProblem = true;
+          this.isAudioAvailable$.next(false);
         }
       });
     }
@@ -42,10 +41,6 @@ export abstract class AudioPlayerBaseComponent extends SubscriptionDisposable im
     return (
       this.audio?.status$.value ?? (this.onlineStatusService.isOnline ? AudioStatus.Unavailable : AudioStatus.Offline)
     );
-  }
-
-  get isAudioInitComplete(): boolean {
-    return this._isAudioInitComplete;
   }
 
   @Input() set source(source: string | undefined) {
