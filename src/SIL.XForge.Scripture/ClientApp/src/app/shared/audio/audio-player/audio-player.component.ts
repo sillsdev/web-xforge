56a48a31b2894a795d9ef54a5577d989f92d4dc5
Raymond Luong <raymond_luong@sil.org>
2023-09-11 16:33:35 -0600

    Clean up

--- src/SIL.XForge.Scripture/ClientApp/src/app/shared/audio/audio-player/audio-player.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/audio/audio-player/audio-player.component.ts
@@ -22,15 +22,13 @@ export class AudioPlayerComponent extends AudioPlayerBaseComponent {
     return this.audio?.seek ?? 0;
   }
 
-  get direction(): 'ltr' | 'rtl' {
-    return this.i18n.direction;
-  }
-
   onSeek(event: MatSliderChange): void {
     let seek: number | null = event.value;
     if (seek == null) return;
-    if (this.direction === 'rtl') {
-      // The slider is reversed for RTL languages so the seek is the inverse of the value that is emitted
+    if (this.i18n.direction === 'rtl') {
+      // It appears that a bug in @angular/material@14.x prevents the slider from working in RTL environments.
+      // The workaround is to flip the slider over the y-axis (so it appears LTR). But to allow seeking to work,
+      // the seek value is set to the inverse of the value that is emitted from the slider.
       seek = 100 - seek;
     }
     this.audio?.setSeek(seek);
