96595674a1610dfc186b659f5a38bf924af608f3
Nathaniel Paulus <nathaniel_paulus@sil.org>
2023-09-13 11:53:16 -0400

    Remove MDC from dialog service (#2057)

--- src/SIL.XForge.Scripture/ClientApp/src/xforge-common/dialog.service.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/dialog.service.ts
@@ -1,6 +1,5 @@
-import { MdcDialog, MdcDialogConfig, MdcDialogRef } from '@angular-mdc/web';
 import { ComponentType } from '@angular/cdk/portal';
-import { Injectable, TemplateRef } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { MatDialog, MatDialogConfig, MatDialogRef } from '@angular/material/dialog';
 import { Observable } from 'rxjs';
 import {
@@ -14,18 +13,7 @@ import { I18nKey, I18nService } from './i18n.service';
   providedIn: 'root'
 })
 export class DialogService {
-  constructor(
-    private readonly i18n: I18nService,
-    private readonly mdcDialog: MdcDialog,
-    private readonly matDialog: MatDialog
-  ) {}
-
-  openMdcDialog<T, D = any, R = any>(
-    componentOrTemplateRef: ComponentType<T> | TemplateRef<T>,
-    config?: MdcDialogConfig<D>
-  ): MdcDialogRef<T, R> {
-    return this.mdcDialog.open(componentOrTemplateRef, config);
-  }
+  constructor(private readonly i18n: I18nService, private readonly matDialog: MatDialog) {}
 
   openMatDialog<T, D = any, R = any>(component: ComponentType<T>, config?: MatDialogConfig<D>): MatDialogRef<T, R> {
     return this.matDialog.open(component, { direction: this.i18n.direction, ...(config ?? {}) });
@@ -79,7 +67,7 @@ export class DialogService {
   }
 
   get openDialogCount(): number {
-    return this.mdcDialog.openDialogs.length + this.matDialog.openDialogs.length;
+    return this.matDialog.openDialogs.length;
   }
 
   /**
