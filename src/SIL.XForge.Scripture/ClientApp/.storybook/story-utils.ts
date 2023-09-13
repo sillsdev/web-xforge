5d832c1a241f987a6045b7b05f4aff67bda06e47
Nathaniel Paulus <nathaniel_paulus@sil.org>
2023-09-13 11:36:44 -0400

    Remove unused MDC imports from story-utils.ts (#2056)

--- src/SIL.XForge.Scripture/ClientApp/.storybook/story-utils.ts
+++ src/SIL.XForge.Scripture/ClientApp/.storybook/story-utils.ts
@@ -1,4 +1,3 @@
-import { MdcDialog, MDC_DIALOG_DATA } from '@angular-mdc/web';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
 import { Component, Inject, InjectionToken, OnInit, Provider } from '@angular/core';
@@ -19,28 +18,6 @@ export function getOverlay(element: HTMLElement): HTMLElement {
 }
 
 export const COMPONENT_UNDER_TEST = new InjectionToken<any>('COMPONENT_UNDER_TEST');
-
-@Component({ template: '' })
-export class MdcDialogLaunchComponent implements OnInit {
-  constructor(
-    private dialog: MdcDialog,
-    @Inject(MDC_DIALOG_DATA) public data: any,
-    @Inject(COMPONENT_UNDER_TEST) private component: any
-  ) {}
-
-  public ngOnInit(): void {
-    this.dialog.open(this.component, this.data);
-  }
-}
-
-export function mdcDialogStory(
-  component: any,
-  imports: any[] = [],
-  declarations: any[] = [],
-  providers: Provider[] = []
-): StoryFn {
-  return getDialogStory(imports, component, declarations, providers, MDC_DIALOG_DATA);
-}
 
 @Component({ template: '' })
 export class MatDialogLaunchComponent implements OnInit {
