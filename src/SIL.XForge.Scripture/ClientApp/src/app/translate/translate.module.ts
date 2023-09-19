025a9b90d84876886f3d9feba7adf866a8c7d0cf
siltomato <colin_moore@sil.org>
2023-09-19 11:35:41 -0400

    SF-2045 Mock service providers

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/translate.module.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/translate.module.ts
@@ -4,11 +4,14 @@ import { TranslocoModule } from '@ngneat/transloco';
 import { AngularSplitModule } from 'angular-split';
 import { UICommonModule } from 'xforge-common/ui-common.module';
 import { XForgeCommonModule } from 'xforge-common/xforge-common.module';
+import { HttpClient } from '../machine-api/http-client';
 import { SharedModule } from '../shared/shared.module';
 import { BiblicalTermDialogComponent } from './biblical-terms/biblical-term-dialog.component';
 import { BiblicalTermsComponent } from './biblical-terms/biblical-terms.component';
+import { DRAFT_GENERATION_SERVICE_OPTIONS } from './draft-generation/draft-generation';
 import { DraftGenerationComponent } from './draft-generation/draft-generation.component';
 import { DraftViewerComponent } from './draft-generation/draft-viewer/draft-viewer.component';
+import { MockPreTranslationHttpClient } from './draft-generation/mock-pretranslation-machine-api';
 import { EditorComponent } from './editor/editor.component';
 import { MultiViewerComponent } from './editor/multi-viewer/multi-viewer.component';
 import { NoteDialogComponent } from './editor/note-dialog/note-dialog.component';
@@ -40,6 +43,11 @@ import { TranslateRoutingModule } from './translate-routing.module';
     UICommonModule,
     XForgeCommonModule,
     TranslocoModule
+  ],
+  providers: [
+    // These providers are for testing back translation with mock service
+    { provide: HttpClient, useClass: MockPreTranslationHttpClient },
+    { provide: DRAFT_GENERATION_SERVICE_OPTIONS, useValue: { pollRate: 200 } }
   ]
 })
 export class TranslateModule {}
