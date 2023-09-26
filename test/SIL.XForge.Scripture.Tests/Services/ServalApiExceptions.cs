59f12d83672644d10c68c1777a9eecc6d655d770
Peter Chapman <peter@conglomo.co.nz>
2023-09-27 04:56:13 +1300

    SF-2257 Handle translation engine not yet created error (#2082)

--- test/SIL.XForge.Scripture.Tests/Services/ServalApiExceptions.cs
+++ test/SIL.XForge.Scripture.Tests/Services/ServalApiExceptions.cs
@@ -10,6 +10,15 @@ namespace SIL.XForge.Scripture.Services;
 /// <remarks>These have been constructed based on the information in Swagger.</remarks>
 public static class ServalApiExceptions
 {
+    public static ServalApiException EngineNotBuilt =>
+        new ServalApiException(
+            "The engine needs to be built first",
+            StatusCodes.Status409Conflict,
+            null,
+            new Dictionary<string, IEnumerable<string>>(),
+            null
+        );
+
     public static ServalApiException Forbidden =>
         new ServalApiException(
             "The authenticated client does not own the translation engine.",
