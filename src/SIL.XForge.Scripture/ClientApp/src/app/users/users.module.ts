c25249fcae8b701d1850ec2efdb005cf5a458f2f
Joseph Myers <wjosephmyers@gmail.com>
2023-09-27 15:20:44 +0700

    Introduced role and permissions dialog
    And moved create/edit questions toggle button to it. No new functionality here yet, just the initial move. But soon I will add role changing and scripture audio changing toggle.
    
    Removed help text from top of Users page, as it seemed a little unnecessary, and certainly wordy. We already have links to help pages on this page and the dialog. If that info should go anywhere, it should go there, if it's not already.

--- src/SIL.XForge.Scripture/ClientApp/src/app/users/users.module.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/users/users.module.ts
@@ -5,11 +5,12 @@ import { UICommonModule } from 'xforge-common/ui-common.module';
 import { XForgeCommonModule } from 'xforge-common/xforge-common.module';
 import { SharedModule } from '../shared/shared.module';
 import { CollaboratorsComponent } from './collaborators/collaborators.component';
+import { RolesAndPermissionsComponent } from './roles-and-permissions/roles-and-permissions.component';
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
 
 @NgModule({
-  declarations: [CollaboratorsComponent, UsersComponent],
+  declarations: [CollaboratorsComponent, UsersComponent, RolesAndPermissionsComponent],
   imports: [UsersRoutingModule, CommonModule, SharedModule, UICommonModule, XForgeCommonModule, TranslocoModule]
 })
 export class UsersModule {}
