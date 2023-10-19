e0b5eddd0090595ff917d811596bd6f77e814e53
Raymond Luong <raymond_luong@sil.org>
2023-10-19 09:32:15 -0600

    SF-2229 Add router guard for generate draft page (#2127)
    * hide generate draft menu item for users without text edit permission

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/translate-routing.module.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/translate-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TranslateAuthGuard } from '../shared/project-router.guard';
+import { NmtDraftAuthGuard, TranslateAuthGuard } from '../shared/project-router.guard';
 import { EditorComponent } from './editor/editor.component';
 import { TranslateOverviewComponent } from './translate-overview/translate-overview.component';
 import { DraftViewerComponent } from './draft-generation/draft-viewer/draft-viewer.component';
@@ -17,14 +17,14 @@ const routes: Routes = [
   {
     path: 'projects/:projectId/draft-generation',
     component: DraftGenerationComponent,
-    canActivate: [TranslateAuthGuard]
+    canActivate: [NmtDraftAuthGuard]
   },
   {
     path: 'projects/:projectId/draft-preview/:bookId/:chapter',
     component: DraftViewerComponent,
-    canActivate: [TranslateAuthGuard]
+    canActivate: [NmtDraftAuthGuard]
   },
-  { path: 'projects/:projectId/draft-preview', component: DraftViewerComponent, canActivate: [TranslateAuthGuard] }
+  { path: 'projects/:projectId/draft-preview', component: DraftViewerComponent, canActivate: [NmtDraftAuthGuard] }
 ];
 
 @NgModule({
