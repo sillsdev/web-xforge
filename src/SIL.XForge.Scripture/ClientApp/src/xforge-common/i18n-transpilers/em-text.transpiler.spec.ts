9426fae8a726825874124ea769e1b340b8f1d7a4
siltomato <colin_moore@sil.org>
2023-10-24 11:03:49 -0400

    SF-2153 Add i18n to pre-translation draft UI

---
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/i18n-transpilers/em-text.transpiler.spec.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  provideTranslationMarkupTranspiler,
+  TokenizeResult,
+  TranslationMarkupRendererFactory,
+  TranslocoMarkupModule
+} from 'ngx-transloco-markup';
+import { TestTranslocoModule } from '../test-utils';
+import { EmTextTranspiler } from './em-text.transpiler';
+
+function createTestTranspiler(): EmTextTranspiler {
+  return new EmTextTranspiler(new TranslationMarkupRendererFactory(document));
+}
+
+describe('EmTextTranspiler', () => {
+  describe('tokenize function', () => {
+    it('correctly recognizes the em start and boundaries', () => {
+      const transpiler: EmTextTranspiler = createTestTranspiler();
+
+      const translation: string = 'a[bc][[em]de[/c][/[/em][em]';
+      const expectedTokenOffsets: number[] = [6, 18, 23];
+
+      for (const [offset] of translation.split('').entries()) {
+        const result: TokenizeResult | undefined = transpiler.tokenize(translation, offset);
+
+        if (expectedTokenOffsets.includes(offset)) {
+          expect(result).toBeDefined();
+        } else {
+          expect(result).toBeUndefined();
+        }
+      }
+    });
+  });
+
+  describe('transpile and render in component', () => {
+    let fixture: ComponentFixture<TestComponent>;
+    let component: TestComponent;
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [TestTranslocoModule, TranslocoMarkupModule],
+        providers: [provideTranslationMarkupTranspiler(EmTextTranspiler)],
+        declarations: [TestComponent]
+      });
+
+      fixture = TestBed.createComponent(TestComponent);
+      component = fixture.componentInstance;
+    });
+
+    it('should render the translation with <em> tags', () => {
+      component.translation = 'The [em]rain[/em] in spain [em]falls mainly[/em] on the plain';
+      fixture.detectChanges();
+
+      expect(fixture.nativeElement.firstChild.innerHTML).toBe(
+        'The <em>rain</em> in spain <em>falls mainly</em> on the plain'
+      );
+    });
+  });
+});
+
+@Component({
+  selector: 'app-test',
+  template: `<transloco [key]="translation"></transloco>`
+})
+export class TestComponent {
+  translation: string = '';
+}
