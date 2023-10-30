d870328582cf4dbf25f58959b147374b19e8a434
Joseph Myers <wjosephmyers@gmail.com>
2023-10-30 13:51:32 +0700

    Rename

--- src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/checking.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/checking.component.ts
@@ -30,7 +30,7 @@ import { UserService } from 'xforge-common/user.service';
 import { objectId } from 'xforge-common/utils';
 import { QuestionDoc } from '../../core/models/question-doc';
 import { SFProjectProfileDoc } from '../../core/models/sf-project-profile-doc';
-import { SF_DEFAULT_SHARE_ROLE, canAccessCommunityCheckingApp } from '../../core/models/sf-project-role-info';
+import { SF_DEFAULT_SHARE_ROLE, roleCanAccessCommunityChecking } from '../../core/models/sf-project-role-info';
 import { SFProjectUserConfigDoc } from '../../core/models/sf-project-user-config-doc';
 import { TextAudioDoc } from '../../core/models/text-audio-doc';
 import { TextDocId } from '../../core/models/text-doc';
@@ -536,7 +536,7 @@ export class CheckingComponent extends DataLoadingComponent implements OnInit, A
                   this.onRemovedFromProject();
                 } else if (
                   !this.projectDoc.data.checkingConfig.checkingEnabled ||
-                  !canAccessCommunityCheckingApp(roles[userId] as SFProjectRole)
+                  !roleCanAccessCommunityChecking(roles[userId] as SFProjectRole)
                 ) {
                   const currentBookId =
                     this.questionsList == null || this.questionsList.activeQuestionBook == null
