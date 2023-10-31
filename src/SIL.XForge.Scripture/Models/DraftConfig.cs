e40beb648faec15790845eae70eb002e5faa6238
Peter Chapman <peter@conglomo.co.nz>
2023-10-31 13:15:31 +1300

    Last Selected Books are stored in the Realtime Server

--- src/SIL.XForge.Scripture/Models/DraftConfig.cs
+++ src/SIL.XForge.Scripture/Models/DraftConfig.cs
@@ -1,6 +1,10 @@
+using System.Collections.Generic;
+
 namespace SIL.XForge.Scripture.Models;
 
 public class DraftConfig
 {
     public TranslateSource? AlternateSource { get; set; }
+
+    public IList<int> LastSelectedBooks { get; set; } = new List<int>();
 }
