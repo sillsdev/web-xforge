fbacb831f97a0cb61b2a01e4b46beba594cd34dc
Joseph Myers <wjosephmyers@gmail.com>
2023-10-31 12:39:43 +0700

    Unit tested permissions service

---
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/permissions.service.spec.ts
@@ -0,0 +1,79 @@
+import { TestBed, fakeAsync } from '@angular/core/testing';
+import { SFProjectRole } from 'realtime-server/lib/esm/scriptureforge/models/sf-project-role';
+import { createTestProjectProfile } from 'realtime-server/lib/esm/scriptureforge/models/sf-project-test-data';
+import { SFProjectProfileDoc } from 'src/app/core/models/sf-project-profile-doc';
+import { instance, mock, when } from 'ts-mockito';
+import { PermissionsService } from './permissions.service';
+import { configureTestingModule } from './test-utils';
+import { UserService } from './user.service';
+
+const mockedUserService = mock(UserService);
+const mockedProjectDoc = mock(SFProjectProfileDoc);
+
+describe('PermissionsService', () => {
+  configureTestingModule(() => ({
+    providers: [{ provide: UserService, useMock: mockedUserService }]
+  }));
+
+  it('allows translators to access Translate', fakeAsync(() => {
+    const env = new TestEnvironment();
+    when(mockedUserService.currentUserId).thenReturn('commenter');
+
+    expect(env.service.canAccessTranslate(env.projectDoc)).toBe(true);
+    expect(env.service.canAccessTranslate(env.projectDoc, 'commenter')).toBe(true);
+  }));
+
+  it('allows checkers to access Community Checking if enabled', fakeAsync(() => {
+    const env = new TestEnvironment();
+    when(mockedUserService.currentUserId).thenReturn('checker');
+
+    expect(env.service.canAccessCommunityChecking(env.projectDoc)).toBe(true);
+    expect(env.service.canAccessCommunityChecking(env.projectDoc, 'checker')).toBe(true);
+  }));
+
+  it('allows admins to access both', fakeAsync(() => {
+    const env = new TestEnvironment();
+    when(mockedUserService.currentUserId).thenReturn('projectAdmin');
+
+    expect(env.service.canAccessTranslate(env.projectDoc)).toBe(true);
+    expect(env.service.canAccessTranslate(env.projectDoc, 'projectAdmin')).toBe(true);
+    expect(env.service.canAccessCommunityChecking(env.projectDoc)).toBe(true);
+    expect(env.service.canAccessCommunityChecking(env.projectDoc, 'projectAdmin')).toBe(true);
+  }));
+
+  it('doesnt allow checkers to access Translate', fakeAsync(() => {
+    const env = new TestEnvironment();
+    when(mockedUserService.currentUserId).thenReturn('checker');
+
+    expect(env.service.canAccessTranslate(env.projectDoc)).toBe(false);
+    expect(env.service.canAccessTranslate(env.projectDoc, 'checker')).toBe(false);
+  }));
+
+  it('doesnt allow translators to access Community Checking', fakeAsync(() => {
+    const env = new TestEnvironment();
+    when(mockedUserService.currentUserId).thenReturn('commenter');
+
+    expect(env.service.canAccessCommunityChecking(env.projectDoc)).toBe(false);
+    expect(env.service.canAccessCommunityChecking(env.projectDoc, 'commenter')).toBe(false);
+  }));
+});
+
+class TestEnvironment {
+  readonly service: PermissionsService;
+  readonly projectDoc: SFProjectProfileDoc = instance(mockedProjectDoc);
+
+  constructor(checkingEnabled = true) {
+    this.service = TestBed.inject(PermissionsService);
+    const data = createTestProjectProfile({
+      userRoles: {
+        projectAdmin: SFProjectRole.ParatextAdministrator,
+        checker: SFProjectRole.CommunityChecker,
+        commenter: SFProjectRole.Commenter
+      },
+      checkingConfig: {
+        checkingEnabled: checkingEnabled
+      }
+    });
+    when(mockedProjectDoc.data).thenReturn(data);
+  }
+}
