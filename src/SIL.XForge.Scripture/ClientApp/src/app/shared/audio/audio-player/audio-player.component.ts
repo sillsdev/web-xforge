a933588e7384292568bf62a5a9b1ebe316df2e23
Nathaniel Paulus <nathaniel_paulus@sil.org>
2023-11-21 14:15:48 -0500

    SF-2345 Fix MatSlider in audio player in RTL mode

--- src/SIL.XForge.Scripture/ClientApp/src/app/shared/audio/audio-player/audio-player.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/audio/audio-player/audio-player.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { MatLegacySliderChange as MatSliderChange } from '@angular/material/legacy-slider';
+import { MatSliderDragEvent } from '@angular/material/slider';
 import { I18nService } from 'xforge-common/i18n.service';
 import { OnlineStatusService } from 'xforge-common/online-status.service';
 import { AudioPlayerBaseComponent } from '../audio-player-base/audio-player-base.component';
@@ -26,15 +26,7 @@ export class AudioPlayerComponent extends AudioPlayerBaseComponent {
     return this.audio?.seek ?? 0;
   }
 
-  onSeek(event: MatSliderChange): void {
-    let seek: number | null = event.value;
-    if (seek == null) return;
-    if (this.i18n.direction === 'rtl') {
-      // It appears that a bug in @angular/material@14.x prevents the slider from working in RTL environments.
-      // The workaround is to flip the slider over the y-axis (so it appears LTR). But to allow seeking to work,
-      // the seek value is set to the inverse of the value that is emitted from the slider.
-      seek = 100 - seek;
-    }
-    this.audio?.setSeek(seek);
+  onSeek(event: MatSliderDragEvent): void {
+    this.audio?.setSeek(event.value);
   }
 }
