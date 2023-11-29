9e755b7339f910e9fc55dafe27edc2e757afe509
siltomato <colin_moore@sil.org>
2023-11-28 19:11:35 -0500

    SF-2351 Improve user guidance on draft generation page

--- src/SIL.XForge.Scripture/ClientApp/src/app/shared/working-animated-indicator/working-animated-indicator.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/working-animated-indicator/working-animated-indicator.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-working-animated-indicator',
   templateUrl: './working-animated-indicator.component.html',
-  styleUrls: ['./working-animated-indicator.component.scss']
+  styleUrls: ['./working-animated-indicator.component.scss'],
+  standalone: true,
+  imports: [MatIconModule]
 })
 export class WorkingAnimatedIndicatorComponent {}
