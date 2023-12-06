ab1c4b1565326f98b5479441f4aa7f167729a20d
siltomato <colin_moore@sil.org>
2023-12-06 12:32:24 -0500

    code review changes

---
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/util/string-util.ts
@@ -0,0 +1,16 @@
+/**
+ * Checks if two string arrays have identical content.
+ */
+export function areStringArraysEqual(a: string[], b: string[]): boolean {
+  if (a.length !== b.length) {
+    return false;
+  }
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
