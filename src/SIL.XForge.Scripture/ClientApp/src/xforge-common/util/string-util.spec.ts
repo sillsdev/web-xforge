0af72fce0856ddc913991aab97a19c0a1aa874d0
siltomato <colin_moore@sil.org>
2023-12-06 12:32:24 -0500

    code review changes

---
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/util/string-util.spec.ts
@@ -0,0 +1,19 @@
+import { areStringArraysEqual } from './string-util';
+
+describe('areStringArraysEqual', () => {
+  it('should return true if two empty arrays are compared', () => {
+    expect(areStringArraysEqual([], [])).toBe(true);
+  });
+
+  it('should return true if two identical arrays are compared', () => {
+    expect(areStringArraysEqual(['a', 'b', 'c'], ['a', 'b', 'c'])).toBe(true);
+  });
+
+  it('should return false if two arrays with different lengths are compared', () => {
+    expect(areStringArraysEqual(['a', 'b', 'c'], ['a', 'b'])).toBe(false);
+  });
+
+  it('should return false if two arrays with different elements are compared', () => {
+    expect(areStringArraysEqual(['a', 'b', 'c'], ['a', 'b'])).toBe(false);
+  });
+});
