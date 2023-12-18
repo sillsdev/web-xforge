114c21b5be4bc1b25102f8a91676a38872734935
Callum Law <callum@apexdigital.co.nz>
2023-12-19 10:25:43 +1300

    SF-2405 Improve AudioPlayer loading of short audio files in Chromium

--- src/SIL.XForge.Scripture/ClientApp/src/app/shared/audio/audio-player.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/audio/audio-player.ts
@@ -16,6 +16,7 @@ export enum AudioStatus {
 export class AudioPlayer extends SubscriptionDisposable {
   private static lastPlayedAudio: HTMLAudioElement;
   private audioDataLoaded: boolean = false;
+  private doingAudioDurationCalculation = false;
 
   protected audio: HTMLAudioElement = new Audio();
   // See explanatory comment where this number is used
@@ -30,14 +31,14 @@ export class AudioPlayer extends SubscriptionDisposable {
     // Loaded metadata works best for blobs
     this.audio.addEventListener('loadedmetadata', () => {
       if (isLocalBlobUrl(this.audio.src)) {
-        this.audioIsReady();
+        this.audioIsLoaded();
       }
     });
 
     // Loaded data works best for real files
     this.audio.addEventListener('loadeddata', () => {
       if (!isLocalBlobUrl(this.audio.src)) {
-        this.audioIsReady();
+        this.audioIsLoaded();
       }
     });
 
@@ -57,6 +58,11 @@ export class AudioPlayer extends SubscriptionDisposable {
     });
 
     this.audio.addEventListener('error', () => {
+      // Sometimes the duration calculation within audioIsLoaded will throw an error - we can safely ignore this
+      if (this.doingAudioDurationCalculation) {
+        return;
+      }
+
       if (isLocalBlobUrl(this.audio.src)) {
         this.status$.next(AudioStatus.LocalNotAvailable);
       } else {
@@ -82,12 +88,6 @@ export class AudioPlayer extends SubscriptionDisposable {
     };
 
     this.status$.next(AudioStatus.Initializing);
-    // In Chromium the duration of blobs isn't known even after metadata is loaded
-    // By making it skip to the end the duration becomes available. To do this we have to skip to some point that we
-    // assume is past the end. This number should be large, but numbers as small as 1e16 have been observed to cause
-    // audio playback to skip to the end of the audio when the user presses play in Chromium. Normal audio files will
-    // know the duration once metadata has loaded.
-    this.audio.currentTime = AudioPlayer.ARBITRARILY_LARGE_NUMBER;
     this.audio.src = formatFileSource(FileType.Audio, source);
   }
 
@@ -147,7 +147,7 @@ export class AudioPlayer extends SubscriptionDisposable {
       AudioPlayer.lastPlayedAudio.pause();
     }
 
-    if (!this.audioDataLoaded) {
+    if (!this.audioDataLoaded || this.audio.readyState < 2) {
       this.audio.load();
     }
 
@@ -166,6 +166,31 @@ export class AudioPlayer extends SubscriptionDisposable {
   stop(): void {
     this.pause();
     this.currentTime = 0;
+  }
+
+  private audioIsLoaded(): void {
+    // In Chromium the duration of some audio sources aren't known after loading.
+    // By making it skip to the end the duration becomes available. To do this we have to skip to some point that we
+    // assume is past the end. This number should be large, but numbers as small as 1e16 have been observed to cause
+    // audio playback to skip to the end of the audio when the user presses play in Chromium.
+    if (this.audio.duration === Infinity) {
+      this.doingAudioDurationCalculation = true;
+
+      const audioDurationCalculationFinishedCallback = (): void => {
+        this.doingAudioDurationCalculation = false;
+        this.audio.removeEventListener('seeked', audioDurationCalculationFinishedCallback);
+        this.audio.removeEventListener('error', audioDurationCalculationFinishedCallback);
+
+        this.audioIsReady();
+      };
+
+      this.audio.addEventListener('seeked', audioDurationCalculationFinishedCallback);
+      this.audio.addEventListener('error', audioDurationCalculationFinishedCallback);
+
+      this.audio.currentTime = AudioPlayer.ARBITRARILY_LARGE_NUMBER;
+    } else {
+      this.audioIsReady();
+    }
   }
 
   private audioIsReady(): void {
