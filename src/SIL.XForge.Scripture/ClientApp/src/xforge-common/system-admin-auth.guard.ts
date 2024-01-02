6a987d6079646537272aebbe885626fdf52fceca
Nathaniel Paulus <nathaniel_paulus@sil.org>
2024-01-02 12:54:51 -0500

    Update to Angular 16
    This was done by running
    ./node_modules/.bin/ng update @angular/core@16 @angular/cli@16 ngx-quill@23 @angular-eslint/schematics@16 @angular/pwa@16 --force

--- src/SIL.XForge.Scripture/ClientApp/src/xforge-common/system-admin-auth.guard.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/system-admin-auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { SystemRole } from 'realtime-server/lib/esm/common/models/system-role';
 import { Observable } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
@@ -9,7 +9,7 @@ import { AuthService } from './auth.service';
 @Injectable({
   providedIn: 'root'
 })
-export class SystemAdminAuthGuard implements CanActivate {
+export class SystemAdminAuthGuard {
   constructor(private readonly authGuard: AuthGuard, private readonly authService: AuthService) {}
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
