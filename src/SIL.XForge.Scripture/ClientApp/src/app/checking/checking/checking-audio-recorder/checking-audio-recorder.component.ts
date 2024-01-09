cd9abbea77d4653c009b9a88792aa8f5ef1dca23
Joseph Myers <wjosephmyers@gmail.com>
2024-01-09 14:26:28 +0530

    Refactored CC answer tabs into a single view
    This does not include Select Text. Still working on the design for that. So a couple tests are broken.
    
    Really trying to maximize efficiency of space, with a focus on mobile devices.
    
    Introduced a compact flag on the audio recorder, which the CC answer view uses. The combined control uses the standard version. Both versions have had some modifications made to save space.
    
    Redid some spacing on the question dialog. This will help users better associate the information and forms with the concepts and sections they represent.

--- src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/checking-audio-recorder/checking-audio-recorder.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/checking/checking/checking-audio-recorder/checking-audio-recorder.component.ts
@@ -1,14 +1,15 @@
-import { Component, EventEmitter, Inject, OnDestroy, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Inject, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 import { translate } from '@ngneat/transloco';
 import RecordRTC from 'recordrtc';
 import { NAVIGATOR } from 'xforge-common/browser-globals';
+import { DialogService } from 'xforge-common/dialog.service';
 import { NoticeService } from 'xforge-common/notice.service';
 import {
   BrowserIssue,
   SupportedBrowsersDialogComponent
 } from 'xforge-common/supported-browsers-dialog/supported-browsers-dialog.component';
 import { objectId } from 'xforge-common/utils';
-import { DialogService } from 'xforge-common/dialog.service';
+import { SingleButtonAudioPlayerComponent } from '../single-button-audio-player/single-button-audio-player.component';
 
 export interface AudioAttachment {
   status?: 'denied' | 'processed' | 'recording' | 'reset' | 'stopped' | 'uploaded';
@@ -23,8 +24,10 @@ export interface AudioAttachment {
   styleUrls: ['./checking-audio-recorder.component.scss']
 })
 export class CheckingAudioRecorderComponent implements OnInit, OnDestroy {
+  @ViewChild(SingleButtonAudioPlayerComponent) audioPlayer?: SingleButtonAudioPlayerComponent;
   @Output() status = new EventEmitter<AudioAttachment>();
-  audioUrl: string = '';
+  @Input() audioUrl: string = '';
+  @Input() compact: boolean = false;
   mediaDevicesUnsupported: boolean = false;
   private stream?: MediaStream;
   private recordRTC?: RecordRTC;
@@ -102,6 +105,10 @@ export class CheckingAudioRecorderComponent implements OnInit, OnDestroy {
         }
       });
     });
+  }
+
+  toggleAudio(): void {
+    this.audioPlayer?.playing ? this.audioPlayer?.stop() : this.audioPlayer?.play();
   }
 
   private errorCallback(error: any): void {
