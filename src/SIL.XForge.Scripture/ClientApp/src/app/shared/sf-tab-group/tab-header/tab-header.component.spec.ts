c173c25061f994715217b9a92453ee4272abcdb7
siltomato <colin_moore@sil.org>
2024-01-16 17:03:04 -0500

    SF-2528 Implement tab reorder

--- src/SIL.XForge.Scripture/ClientApp/src/app/shared/sf-tab-group/tab-header/tab-header.component.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/sf-tab-group/tab-header/tab-header.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SFTabsModule } from '../sf-tabs.module';
 import { TabHeaderComponent } from './tab-header.component';
 
-describe('SfTabHeaderComponent', () => {
+describe('TabHeaderComponent', () => {
   let component: TabHeaderComponent;
   let fixture: ComponentFixture<TabHeaderComponent>;
 
@@ -14,6 +14,54 @@ describe('SfTabHeaderComponent', () => {
     fixture = TestBed.createComponent(TabHeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  describe('closeable class binding', () => {
+    it('should have the "closeable" class when closeable is true', () => {
+      component.closeable = true;
+      fixture.detectChanges();
+      const el: HTMLElement = fixture.nativeElement;
+      expect(el.classList.contains('closeable')).toBe(true);
+    });
+
+    it('should not have the "closeable" class when closeable is false', () => {
+      component.closeable = false;
+      fixture.detectChanges();
+      const el: HTMLElement = fixture.nativeElement;
+      expect(el.classList.contains('closeable')).toBe(false);
+    });
+  });
+
+  describe('movable class binding', () => {
+    it('should have the "movable" class when movable is true', () => {
+      component.movable = true;
+      fixture.detectChanges();
+      const el: HTMLElement = fixture.nativeElement;
+      expect(el.classList.contains('movable')).toBe(true);
+    });
+
+    it('should not have the "movable" class when movable is false', () => {
+      component.movable = false;
+      fixture.detectChanges();
+      const el: HTMLElement = fixture.nativeElement;
+      expect(el.classList.contains('movable')).toBe(false);
+    });
+  });
+
+  describe('active class binding', () => {
+    it('should have the "active" class when active is true', () => {
+      component.active = true;
+      fixture.detectChanges();
+      const el: HTMLElement = fixture.nativeElement;
+      expect(el.classList.contains('active')).toBe(true);
+    });
+
+    it('should not have the "active" class when active is false', () => {
+      component.active = false;
+      fixture.detectChanges();
+      const el: HTMLElement = fixture.nativeElement;
+      expect(el.classList.contains('active')).toBe(false);
+    });
   });
 
   it('should emit tabClick event on click', () => {
