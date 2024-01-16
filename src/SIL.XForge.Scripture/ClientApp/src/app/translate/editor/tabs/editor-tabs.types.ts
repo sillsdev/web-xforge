1944fefdd0936b6a82163bf797e427b3ff1ba1d7
siltomato <colin_moore@sil.org>
2024-01-16 17:03:04 -0500

    SF-2439 Implement tabbed editor

---
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/tabs/editor-tabs.types.ts
@@ -0,0 +1,13 @@
+import { TabInfo } from 'src/app/shared/sf-tab-group';
+
+export type EditorTabGroupType = 'source' | 'target';
+
+export const editorTabTypes = ['history', 'draft', 'project-source', 'project'] as const;
+export type EditorTabType = (typeof editorTabTypes)[number];
+
+export interface EditorTabInfo extends TabInfo<EditorTabType> {
+  /**
+   * If set, only a single instance of this tab is allowed with uniqueness determined by tab type and projectId.
+   */
+  unique?: boolean;
+}
