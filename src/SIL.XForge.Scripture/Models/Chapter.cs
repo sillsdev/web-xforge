f85b4079b178acb77407fe9355d16151adec85bf
MarkS <marksvc@gmail.com>
2024-01-18 15:59:57 -0700

    dev: add to comment on Chapter.IsValid

--- src/SIL.XForge.Scripture/Models/Chapter.cs
+++ src/SIL.XForge.Scripture/Models/Chapter.cs
@@ -7,9 +7,11 @@ public class Chapter
     public int Number { get; set; }
     public int LastVerse { get; set; }
 
-    /// <summary>Whether the chapter's USX conforms to the usx-sf SF USX
-    /// schema, which is a subset of the USX schema. If not, it will not
-    /// be editable in SF.</summary>
+    /// <summary>
+    /// Whether the chapter's USX conforms to the usx-sf SF USX schema, which is a subset of the USX schema. And if
+    /// nothing was flagged as invalid by tighter constraints of the DeltaUsxMapper. If a chapter is marked as invalid,
+    /// it will not be editable in SF.
+    /// </summary>
     public bool IsValid { get; set; }
     public Dictionary<string, string> Permissions { get; set; } = new Dictionary<string, string>();
     public bool? HasAudio { get; set; }
