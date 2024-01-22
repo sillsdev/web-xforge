66eabd2553b09141b0e39061c0c6cba256785b46
Peter Chapman <peter@conglomo.co.nz>
2024-01-22 14:09:38 +1300

    Generate ISFTexts from additional files

--- src/SIL.XForge.Scripture/Services/ITrainingDataService.cs
+++ src/SIL.XForge.Scripture/Services/ITrainingDataService.cs
@@ -1,9 +1,20 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
+using SIL.XForge.Scripture.Models;
 
 namespace SIL.XForge.Scripture.Services;
 
 public interface ITrainingDataService
 {
+    Task GetTextsAsync(
+        string userId,
+        string projectId,
+        string[] dataIds,
+        IList<ISFText> sourceTexts,
+        IList<ISFText> targetTexts,
+        CancellationToken cancellationToken
+    );
     Task<Uri> SaveTrainingDataAsync(string userId, string projectId, string dataId, string path);
 }
