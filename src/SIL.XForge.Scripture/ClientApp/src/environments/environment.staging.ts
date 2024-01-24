f1bc033c6d49f2161614444d346e8d2cbff4d521
Peter Chapman <peter@conglomo.co.nz>
2024-01-24 13:10:05 +1300

    Fixed Offline Database

--- src/SIL.XForge.Scripture/ClientApp/src/environments/environment.staging.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/environments/environment.staging.ts
@@ -15,5 +15,5 @@ export const environment = {
   realtimeUrl: '/realtime-api/',
   authDomain: 'dev-sillsdev.auth0.com',
   authClientId: '4eHLjo40mAEGFU6zUxdYjnpnC1K1Ydnj',
-  offlineDBVersion: 7
+  offlineDBVersion: 8
 };
