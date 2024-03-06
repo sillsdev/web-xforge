2cb79755f3d0d500a7affa33404ecbb4f66b66fd
Joseph Myers <wjosephmyers@gmail.com>
2024-03-06 11:33:22 +0700

    Removed my new interfaces in favor of defining the type inline in the component.

--- src/RealtimeServer/scriptureforge/models/dynamic-value.ts
+++
@@ -1,9 +0,0 @@
-import { Value } from './value';
-
-/*
-  This interface indicates a value represented by text,
-  audio, or both.
-*/
-export interface DynamicValue extends Value {
-  audioUrl?: string;
-}
