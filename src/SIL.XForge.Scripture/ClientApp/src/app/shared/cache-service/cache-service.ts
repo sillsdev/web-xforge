f961aa9d1194a00161c81aeaf21655b4ac9922e9
Joseph Myers <wjosephmyers@gmail.com>
2024-03-08 09:05:47 +0700

    Unsubscribe on abort

--- src/SIL.XForge.Scripture/ClientApp/src/app/shared/cache-service/cache-service.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/cache-service/cache-service.ts
@@ -2,16 +2,13 @@ import { EventEmitter, Injectable } from '@angular/core';
 import { SFProjectProfileDoc } from 'src/app/core/models/sf-project-profile-doc';
 import { TextDocId } from 'src/app/core/models/text-doc';
 import { SFProjectService } from 'src/app/core/sf-project.service';
-import { SubscriptionDisposable } from 'xforge-common/subscription-disposable';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CacheService extends SubscriptionDisposable {
+export class CacheService {
   private abortCurrent: EventEmitter<void> = new EventEmitter();
-  constructor(private readonly projectService: SFProjectService) {
-    super();
-  }
+  constructor(private readonly projectService: SFProjectService) {}
 
   async cache(project: SFProjectProfileDoc): Promise<void> {
     this.abortCurrent.emit();
@@ -20,12 +17,15 @@ export class CacheService extends SubscriptionDisposable {
 
   private async loadAllChapters(project: SFProjectProfileDoc): Promise<void> {
     var abort = false;
-    this.abortCurrent.subscribe(() => (abort = true));
+    const sub = this.abortCurrent.subscribe(() => (abort = true));
 
     if (project?.data != null) {
       for (const text of project.data.texts) {
         for (const chapter of text.chapters) {
-          if (abort) return;
+          if (abort) {
+            sub.unsubscribe();
+            return;
+          }
 
           const textDocId = new TextDocId(project.id, text.bookNum, chapter.number, 'target');
           await this.projectService.getText(textDocId);
