c684cb5fbed5a9e2b362f8f1a11aa7b41c6ab332
MarkS <marksvc@gmail.com>
2024-04-05 14:21:40 -0600

    SF-2508 Change project selector to My Projects page
    - Removes project selector drop-down from navigation drawer.
    - Provides user with new page listing their connected and available
    projects.
    - Adds icons to items in user menu.
    
    - start.component is repurposed and renamed to my-projects.component.
    - my-projects.component: When connecting a project, the behaviour is
    now to navigate to the connect project page, and pass the desired PT
    project id as part of the navigation state.
    - connect-project.component:
      - Receives PT project id from navigation state. (Not to be confused
    with the component's `state` field.)
      - A next step is to remove the PT project chooser from the
    component, and move and adjust the
    connect_project.only_paratext_admins_can_start message to
    my-projects.component. For now, the PT project chooser is displayed,
    but starts out as the project specified in my-projects.component.
    - app.component.spec: Removed some tests that no longer are relevant.
    - Make the filename and class name for the router link directive more
    consistent.
    - If user-projects.service projectDocs$ starts with a value that can
    be distinguished from an empty set of projects, it can reduce flicker
    when showing content on my-projects.component when depending on the
    value.
    
    Co-authored-by: Nathaniel Paulus <nathaniel_paulus@sil.org>

--- src/SIL.XForge.Scripture/ClientApp/src/app/connect-project/connect-project.component.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/connect-project/connect-project.component.spec.ts
@@ -152,8 +152,10 @@ describe('ConnectProjectComponent', () => {
     env.clickElement(env.submitButton);
     expect(env.component.paratextIdControl.errors!.required).toBe(true);
 
+    when(mockedParatextService.isParatextProjectInSF(anything())).thenReturn(true);
     env.changeSelectValue(env.projectSelect, 'pt03');
 
+    // The project is already in SF, so do not present settings to configure.
     expect(env.settingsCard).toBeNull();
     env.clickElement(env.submitButton);
 
@@ -366,6 +368,18 @@ describe('ConnectProjectComponent', () => {
     };
     verify(mockedSFProjectService.onlineCreate(deepEqual(settings))).once();
     verify(mockedRouter.navigate(deepEqual(['/projects', 'project01']))).once();
+  }));
+
+  it('knows what pt project id is being asked to connect to', fakeAsync(() => {
+    when(mockedRouter.getCurrentNavigation()).thenReturn({
+      extras: { state: { ptProjectId: 'requested-pt-project-id' } }
+    } as any);
+    const env = new TestEnvironment();
+    env.setupDefaultProjectData();
+    env.waitForProjectsResponse();
+    expect(env.component.state).toEqual('input');
+    expect(env.component.ptProjectId).toEqual('requested-pt-project-id');
+    expect(env.component.paratextIdControl.value).toEqual('requested-pt-project-id');
   }));
 });
 
