74caab6526bad9f727b77bdcb050691f10af9820
Peter Chapman <peter@conglomo.co.nz>
2024-04-09 15:12:33 +1200

    Code review fixes

--- src/SIL.XForge.Scripture/ClientApp/src/xforge-common/url-constants.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/url-constants.ts
@@ -1,4 +1,5 @@
 export const ANONYMOUS_URL = 'anonymous';
 export const COMMAND_API_NAMESPACE = 'command-api';
+export const PARATEXT_API_NAMESPACE = 'paratext-api';
 export const USERS_URL = 'users';
 export const PROJECTS_URL = 'projects';
