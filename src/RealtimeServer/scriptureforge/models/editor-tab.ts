40c009f193cc4e521ce88098ea6a6bdbceb31d8d
siltomato <133386342+siltomato@users.noreply.github.com>
2024-04-17 19:36:34 -0400

    lint

--- src/RealtimeServer/scriptureforge/models/editor-tab.ts
+++ src/RealtimeServer/scriptureforge/models/editor-tab.ts
@@ -1,3 +1,3 @@
 export const editorTabTypes = ['history', 'draft', 'project-source', 'project'] as const;
-export type EditorTabType = (typeof editorTabTypes)[number];
+export type EditorTabType =  typeof editorTabTypes[number];
 export type EditorTabGroupType = 'source' | 'target';
