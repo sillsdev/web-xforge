cc63e639ee2ece8455de2be225dff4ef56afc2a6
Peter Chapman <peter@conglomo.co.nz>
2024-06-18 08:47:03 +1200

    Revert "Make the different language warning non-blocking"
    This reverts commit afd17aa416b0949105706411b4569686971cce25.

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/draft-generation.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/draft-generation.component.ts
@@ -169,6 +169,8 @@ export class DraftGenerationComponent extends DataLoadingComponent implements On
       this.isTargetLanguageSupported &&
       this.isSourceProjectSet &&
       this.isSourceAndTargetDifferent &&
+      this.isSourceAndTrainingSourceLanguageIdentical &&
+      this.isSourceAndAdditionalTrainingSourceLanguageIdentical &&
       this.canAccessDraftSourceIfAvailable(this.source) &&
       (this.isBackTranslationMode || this.isPreTranslationApproved)
     );
