7958fcff59523854bb986ff1dea518657dd1686c
Nathaniel Paulus <nathaniel_paulus@sil.org>
2024-06-21 11:11:32 -0400

    SF-2319a Remove asterisk from required form fields

--- src/SIL.XForge.Scripture/ClientApp/src/app/app.module.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/app.module.ts
@@ -88,7 +88,7 @@ import { UsersModule } from './users/users.module';
     { provide: OverlayContainer, useClass: InAppRootOverlayContainer },
     {
       provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
-      useValue: { appearance: 'outline' }
+      useValue: { appearance: 'outline', hideRequiredMarker: true }
     }
   ],
   bootstrap: [AppComponent]
