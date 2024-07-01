7712fc604c4d1b4e02b941f7002f22aec40238ef
Peter Chapman <peter@conglomo.co.nz>
2024-07-01 13:05:13 +1200

    SF-2802 Update Revision History API to return revision metadata

--- src/SIL.XForge/Realtime/OpMetadata.cs
+++ src/SIL.XForge/Realtime/OpMetadata.cs
@@ -5,7 +5,13 @@ namespace SIL.XForge.Realtime;
 
 public class OpMetadata
 {
+    [JsonProperty("source")]
+    public string? Source { get; set; }
+
     [JsonProperty("ts")]
     [JsonConverter(typeof(IntegerToUtcDateTimeConverter))]
     public DateTime Timestamp { get; set; }
+
+    [JsonProperty("uId")]
+    public string? UserId { get; set; }
 }
