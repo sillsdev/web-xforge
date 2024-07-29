sillsdev/web-xforge

commit 28553e5451ed003cc25d85a505290033f387f5d3

SF-1668 Store the current project id and navigate on login (#1468)
* add currentProjectId property on the Site class
* store the project id on the user's site in the data
* use the stored id when no project id exists in local storage
* modify user service currentProjectId to accept a user parameter
* add user service test
* define current project params as non-optional
* assert current user exists in app component

src/SIL.XForge.Scripture/ClientApp/src/app/app.component.ts
@@ -363,10 +363,11 @@ export class AppComponent extends DataLoadingComponent implements OnInit, OnDest
         // check if the currently selected project has been deleted
         if (
           projectId != null &&
-          projectId === this.userService.currentProjectId &&
+          this.currentUserDoc != null &&
+          projectId === this.userService.currentProjectId(this.currentUserDoc) &&
           (selectedProjectDoc == null || !selectedProjectDoc.isLoaded)
         ) {
-          this.userService.setCurrentProjectId();
+          await this.userService.setCurrentProjectId(this.currentUserDoc, undefined);
           this.navigateToStart();
           return;
         }
@@ -413,8 +414,7 @@ export class AppComponent extends DataLoadingComponent implements OnInit, OnDest
         if (this._projectSelect != null) {
           this._projectSelect.value = this.selectedProjectDoc.id;
         }
-
-        this.userService.setCurrentProjectId(this.selectedProjectDoc.id);
+        await this.userService.setCurrentProjectId(this.currentUserDoc!, this.selectedProjectDoc.id);
 
         this.checkCheckingBookQuestions();
         this.checkDeviceStorage();
@@ -588,8 +588,8 @@ export class AppComponent extends DataLoadingComponent implements OnInit, OnDest
     );
   }
 
-  private showProjectDeletedDialog(): void {
-    this.userService.setCurrentProjectId();
+  private async showProjectDeletedDialog(): Promise<void> {
+    await this.userService.setCurrentProjectId(this.currentUserDoc!, undefined);
     this.projectDeletedDialogRef = this.dialog.open(ProjectDeletedDialogComponent);
     this.projectDeletedDialogRef.afterClosed().subscribe(() => this.navigateToStart());
   }

src/RealtimeServer/common/models/site.ts
@@ -1,3 +1,6 @@
 export interface Site {
+  // The most recent project a user has navigated to, which may be different than the current open project
+  // if a user is logged in on multiple browsers
+  currentProjectId?: string;
   projects: string[];
 }

src/SIL.XForge.Scripture/ClientApp/src/app/app.component.spec.ts
@@ -114,7 +114,7 @@ describe('AppComponent', () => {
     expect(env.isDrawerVisible).toEqual(true);
     expect(env.selectedProjectId).toEqual('project01');
     expect(env.menuLength).toEqual(5);
-    verify(mockedUserService.setCurrentProjectId('project01')).once();
+    verify(mockedUserService.setCurrentProjectId(anything(), 'project01')).once();
   }));
 
   it('navigate to different project', fakeAsync(() => {
@@ -128,7 +128,7 @@ describe('AppComponent', () => {
     expect(env.menuLength).toEqual(5);
     expect(env.component.isCheckingEnabled).toEqual(true);
     expect(env.component.isTranslateEnabled).toEqual(false);
-    verify(mockedUserService.setCurrentProjectId('project02')).once();
+    verify(mockedUserService.setCurrentProjectId(anything(), 'project02')).once();
   }));
 
   it('hide translate tool for community checkers', fakeAsync(() => {
@@ -142,7 +142,7 @@ describe('AppComponent', () => {
     expect(env.menuLength).toEqual(5);
     expect(env.component.isCheckingEnabled).toEqual(true);
     expect(env.component.isTranslateEnabled).toEqual(false);
-    verify(mockedUserService.setCurrentProjectId('project03')).once();
+    verify(mockedUserService.setCurrentProjectId(anything(), 'project03')).once();
 
     // Does not collapse Community Checking item when translate is disabled
     env.selectItem(0);
@@ -190,7 +190,7 @@ describe('AppComponent', () => {
     expect(env.isDrawerVisible).toEqual(true);
     expect(env.selectedProjectId).toEqual('project02');
     expect(env.location.path()).toEqual('/projects/project02');
-    verify(mockedUserService.setCurrentProjectId('project02')).once();
+    verify(mockedUserService.setCurrentProjectId(anything(), 'project02')).once();
   }));
 
   it('connect project', fakeAsync(() => {
@@ -224,7 +224,7 @@ describe('AppComponent', () => {
     // SUT
     env.deleteProject('project01', false);
     verify(mockedMdcDialog.open(ProjectDeletedDialogComponent)).once();
-    verify(mockedUserService.setCurrentProjectId()).once();
+    verify(mockedUserService.setCurrentProjectId(anything(), undefined)).once();
     env.confirmProjectDeletedDialog();
     // Get past setTimeout to navigation
     tick();
@@ -241,7 +241,7 @@ describe('AppComponent', () => {
     env.init();
 
     expect(env.isDrawerVisible).toEqual(false);
-    verify(mockedUserService.setCurrentProjectId()).once();
+    verify(mockedUserService.setCurrentProjectId(anything(), undefined)).once();
     expect(env.location.path()).toEqual('/projects');
   }));
 
@@ -582,7 +582,7 @@ class TestEnvironment {
     when(mockedUserService.getCurrentUser()).thenCall(() =>
       this.realtimeService.subscribe(UserDoc.COLLECTION, 'user01')
     );
-    when(mockedUserService.currentProjectId).thenReturn('project01');
+    when(mockedUserService.currentProjectId(anything())).thenReturn('project01');
     when(mockedSettingsAuthGuard.allowTransition(anything())).thenReturn(this.canSeeSettings$);
     when(mockedSyncAuthGuard.allowTransition(anything())).thenReturn(this.canSync$);
     when(mockedUsersAuthGuard.allowTransition(anything())).thenReturn(this.canSeeUsers$);
@@ -785,7 +785,7 @@ class TestEnvironment {
 
   deleteProject(projectId: string, isLocal: boolean): void {
     if (isLocal) {
-      when(mockedUserService.currentProjectId).thenReturn(undefined);
+      when(mockedUserService.currentProjectId(anything())).thenReturn(undefined);
     }
     this.ngZone.run(() => {
       const projectDoc = this.realtimeService.get(SFProjectProfileDoc.COLLECTION, projectId);

src/SIL.XForge.Scripture/ClientApp/src/app/project/project.component.spec.ts
@@ -262,7 +262,7 @@ class TestEnvironment {
     snapshot.queryParams = { sharing: 'true' };
     when(mockedActivatedRoute.snapshot).thenReturn(snapshot);
     when(mockedUserService.currentUserId).thenReturn('user01');
-    when(mockedUserService.currentProjectId).thenReturn('project01');
+    when(mockedUserService.currentProjectId(anything())).thenReturn('project01');
     when(mockedUserService.getCurrentUser()).thenCall(() =>
       this.realtimeService.subscribe(UserDoc.COLLECTION, 'user01')
     );

src/SIL.XForge.Scripture/ClientApp/src/app/project/project.component.ts
@@ -133,7 +133,8 @@ export class ProjectComponent extends DataLoadingComponent implements OnInit {
   private async showOfflineMessage(): Promise<void> {
     await this.noticeService.showMessageDialog(() => this.transloco.translate('project.please_connect_to_use_link'));
     const userDoc: UserDoc = await this.userService.getCurrentUser();
-    const projectId: string | undefined = selectValidProject(userDoc, this.userService.currentProjectId);
+    const currentProjectId: string | undefined = this.userService.currentProjectId(userDoc);
+    const projectId: string | undefined = selectValidProject(userDoc, currentProjectId);
     if (projectId == null) {
       this.router.navigateByUrl('/projects', { replaceUrl: true });
     } else {

src/SIL.XForge.Scripture/ClientApp/src/app/settings/settings.component.spec.ts
@@ -504,7 +504,7 @@ describe('SettingsComponent', () => {
       env.setDialogResponse(true);
       env.wait();
       env.clickElement(env.deleteProjectButton);
-      verify(mockedUserService.setCurrentProjectId()).once();
+      verify(mockedUserService.setCurrentProjectId(anything(), undefined)).once();
       verify(mockedSFProjectService.onlineDelete(anything())).once();
       expect(env.location.path()).toEqual('/projects');
     }));
@@ -515,7 +515,7 @@ describe('SettingsComponent', () => {
       env.setDialogResponse(false);
       env.wait();
       env.clickElement(env.deleteProjectButton);
-      verify(mockedUserService.setCurrentProjectId()).never();
+      verify(mockedUserService.setCurrentProjectId(anything(), undefined)).never();
       verify(mockedSFProjectService.onlineDelete(anything())).never();
       expect().nothing();
     }));
@@ -536,7 +536,6 @@ class TestEnvironment {
     when(mockedSFProjectService.onlineIsSourceProject('project01')).thenResolve(isSource);
     when(mockedSFProjectService.onlineDelete(anything())).thenResolve();
     when(mockedSFProjectService.onlineUpdateSettings('project01', anything())).thenResolve();
-    when(mockedUserService.currentProjectId).thenReturn('project01');
     when(mockedSFProjectService.get('project01')).thenCall(() =>
       this.realtimeService.subscribe(SFProjectDoc.COLLECTION, 'project01')
     );

src/SIL.XForge.Scripture/ClientApp/src/app/settings/settings.component.ts
@@ -9,6 +9,7 @@ import { map, tap } from 'rxjs/operators';
 import { DataLoadingComponent } from 'xforge-common/data-loading-component';
 import { I18nService, TextAroundTemplate } from 'xforge-common/i18n.service';
 import { ElementState } from 'xforge-common/models/element-state';
+import { UserDoc } from 'xforge-common/models/user-doc';
 import { NoticeService } from 'xforge-common/notice.service';
 import { PwaService } from 'xforge-common/pwa.service';
 import { UserService } from 'xforge-common/user.service';
@@ -201,7 +202,8 @@ export class SettingsComponent extends DataLoadingComponent implements OnInit {
     const dialogRef = this.dialog.open(DeleteProjectDialogComponent, config);
     dialogRef.afterClosed().subscribe(async result => {
       if (result === 'accept') {
-        this.userService.setCurrentProjectId();
+        const user: UserDoc = await this.userService.getCurrentUser();
+        await this.userService.setCurrentProjectId(user, undefined);
         if (this.projectDoc != null) {
           await this.projectService.onlineDelete(this.projectDoc.id);
           this.router.navigateByUrl('/projects', { replaceUrl: true });

src/SIL.XForge.Scripture/ClientApp/src/app/start/start.component.spec.ts
@@ -102,7 +102,7 @@ class TestEnvironment {
   }
 
   setCurrentUserProjectData(projectId?: string, projects: string[] = ['project01', 'project02']): void {
-    when(mockedUserService.currentProjectId).thenReturn(projectId);
+    when(mockedUserService.currentProjectId(anything())).thenReturn(projectId);
 
     this.realtimeService.addSnapshot<User>(UserDoc.COLLECTION, {
       id: 'user01',

commit 0a3927c1f88bda874606c7291fb312c80fe2fdd2

SF-2885 Fix project deleted message being shown after deleting project
This was broken in 28553e5451ed003cc25d85a505290033f387f5d3 when
UserService.currentProjectId was changed from a gettter to a method that
requires an argument, and AppComponent was not correctly updated.

src/SIL.XForge.Scripture/ClientApp/src/app/app.component.ts
@@ -264,7 +264,8 @@ export class AppComponent extends DataLoadingComponent implements OnInit, OnDest
       }
       this.selectedProjectDeleteSub = this._selectedProjectDoc.delete$.subscribe(() => {
         // handle remotely deleted project
-        if (this.userService.currentProjectId != null) {
+        const userDoc = this.currentUserDoc;
+        if (userDoc != null && this.userService.currentProjectId(userDoc) != null) {
           this.showProjectDeletedDialog();
         }
       });