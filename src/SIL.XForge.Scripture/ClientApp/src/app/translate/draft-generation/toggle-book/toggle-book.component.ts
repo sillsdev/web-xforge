dad95d34d6d8012415ee6a2ad771def2cd4f5a5e
Nathaniel Paulus <nathaniel_paulus@sil.org>
2024-08-22 10:55:54 -0400

    Update book toggle component to use background instead of border

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/toggle-book/toggle-book.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/toggle-book/toggle-book.component.ts
@@ -4,6 +4,42 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { TranslocoModule } from '@ngneat/transloco';
 import { I18nService } from '../../../../xforge-common/i18n.service';
 
+interface ButtonColorSpec {
+  hue: number;
+  saturation: number;
+  light: number;
+  dark: number;
+  hoverLight: number;
+  hoverDark: number;
+}
+
+const availableColors: ButtonColorSpec[] = [
+  // some are commented out because they are ugly, or too similar to others
+  { hue: 0 },
+  // { hue: 30, dark: 35 },
+  // { hue: 60, dark: 35 },
+  { hue: 90, dark: 35 },
+  // { hue: 120, dark: 35 },
+  // { hue: 150, dark: 35 },
+  { hue: 180, dark: 35 },
+  { hue: 210 },
+  { hue: 240 },
+  { hue: 270 },
+  { hue: 300 },
+  { hue: 330 }
+].map(spec => {
+  const dark = spec['dark'] ?? 55;
+  const light = spec['light'] ?? Math.round(Math.min(dark * 1.18, 100));
+  return {
+    ...spec,
+    dark,
+    light,
+    hoverLight: spec['hoverLight'] ?? Math.round(Math.min(light * 1.07, 100)),
+    hoverDark: spec['hoverDark'] ?? Math.round(Math.min(dark * 1.07, 100)),
+    saturation: spec['saturation'] ?? 80
+  };
+});
+
 @Component({
   selector: 'app-toggle-book',
   standalone: true,
@@ -19,6 +55,8 @@ export class ToggleBookComponent {
   @Input() book!: number;
   @Input() progress?: number;
   @Input() hues: number[] = [230];
+
+  colorSpec = availableColors[3];
 
   constructor(private readonly i18n: I18nService) {}
 
@@ -56,8 +94,17 @@ export class ToggleBookComponent {
     return `${(this.progress ?? 0) * 100}%`;
   }
 
-  get borderWidthCssValue(): string {
-    return `${this.borderWidth}px`;
+  get progressColorCssValue(): string {
+    return `hsl(${this.colorSpec.hue}, ${this.colorSpec.saturation}%, ${this.colorSpec.dark}%)`;
+  }
+  get progressColorHoverCssValue(): string {
+    return `hsl(${this.colorSpec.hue}, ${this.colorSpec.saturation}%, ${this.colorSpec.hoverDark}%)`;
+  }
+  get progressBgColorCssValue(): string {
+    return `hsl(${this.colorSpec.hue}, ${this.colorSpec.saturation}%, ${this.colorSpec.light}%)`;
+  }
+  get progressHoverBgColorCssValue(): string {
+    return `hsl(${this.colorSpec.hue}, ${this.colorSpec.saturation}%, ${this.colorSpec.hoverLight}%)`;
   }
 
   get progressDescription(): string {
