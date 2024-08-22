828424edd661623e223d4d19e9bd056dd5e2614a
Nigel Wells <nigel-wells@hotmail.com>
2024-08-22 15:44:10 +1200

    * Added in more stories * Positioned tooltips to be below * Removed redundant tooltips * Mocked audio stream

--- src/SIL.XForge.Scripture/ClientApp/src/app/shared/audio-recorder-dialog/audio-recorder-dialog.stories.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/audio-recorder-dialog/audio-recorder-dialog.stories.ts
@@ -1,23 +1,109 @@
-import { Meta } from '@storybook/angular';
+import { Meta, moduleMetadata } from '@storybook/angular';
 import { NoticeService } from 'xforge-common/notice.service';
 import { I18nStoryModule } from 'xforge-common/i18n-story.module';
 import { DialogService } from 'xforge-common/dialog.service';
 import { expect } from '@storybook/jest';
-import { AudioRecorderDialogComponent } from './audio-recorder-dialog.component';
+import { instance, mock, when } from 'ts-mockito';
+import { NAVIGATOR } from 'xforge-common/browser-globals';
+import { userEvent } from '@storybook/testing-library';
+import {
+  AudioAttachment,
+  AudioRecorderDialogComponent,
+  AudioRecorderDialogData
+} from './audio-recorder-dialog.component';
 import {
   MatDialogLaunchComponent,
   matDialogStory,
   MatDialogStoryConfig
 } from '../../../../.storybook/util/mat-dialog-launch';
 
+const mockedNavigator = mock(Navigator);
+
+// Function to create a mock MediaStream with an audio track
+const createMockMediaStream = (): MediaStream => {
+  // Use the MediaStream constructor to simulate a stream with an audio track
+  const audioContext = new window.AudioContext();
+  const oscillator = audioContext.createOscillator();
+  const destination = audioContext.createMediaStreamDestination();
+
+  oscillator.connect(destination);
+  oscillator.start();
+
+  return destination.stream;
+};
+
+interface StoryAppState {
+  audio: AudioAttachment;
+  micAvailable: boolean;
+  micPermission: boolean;
+  countdown: boolean;
+  data: AudioRecorderDialogData;
+}
+
+const defaultArgs: StoryAppState = {
+  audio: {},
+  micAvailable: true,
+  micPermission: true,
+  countdown: false,
+  data: {}
+};
+
 export default {
   title: 'Shared/Audio Recorder',
-  component: MatDialogLaunchComponent
+  component: MatDialogLaunchComponent,
+  argTypes: {
+    audio: {
+      description: 'Existing audio attachment',
+      table: { category: 'Dialog data', type: { summary: 'AudioAttachment' } }
+    },
+    countdown: {
+      control: { type: 'boolean' },
+      description: 'Initiate with a countdown',
+      table: { category: 'Dialog data' }
+    },
+    data: {
+      description: 'Data to pass to the dialog',
+      table: { category: 'Dialog data', type: { summary: 'AudioRecorderDialogData' } }
+    },
+    micAvailable: {
+      control: { type: 'boolean' },
+      description: 'Device has a microphone available for use',
+      table: { category: 'App state' }
+    },
+    micPermission: {
+      control: { type: 'boolean' },
+      description: 'Permission to access the microphone',
+      table: { category: 'App state' }
+    }
+  },
+  decorators: [
+    moduleMetadata({}),
+    (story, context) => {
+      context.args.data = { countdown: context.args.countdown } as AudioRecorderDialogData;
+      when(mockedNavigator.mediaDevices).thenReturn({
+        getUserMedia: (_: MediaStreamConstraints): Promise<MediaStream> => {
+          return context.args.micPermission && context.args.micAvailable
+            ? Promise.resolve(createMockMediaStream())
+            : context.args.micAvailable
+              ? Promise.reject('No microphone')
+              : Promise.reject({ code: DOMException.NOT_FOUND_ERR });
+        }
+      } as MediaDevices);
+      return story();
+    }
+  ],
+  parameters: {
+    controls: {
+      expanded: true,
+      include: Object.keys(defaultArgs)
+    }
+  },
+  args: defaultArgs
 } as Meta;
 
 const dialogStoryConfig: MatDialogStoryConfig = {
   imports: [I18nStoryModule],
-  providers: [DialogService, NoticeService],
+  providers: [DialogService, NoticeService, { provide: NAVIGATOR, useValue: instance(mockedNavigator) }],
   standaloneComponent: true
 };
 
@@ -25,7 +111,7 @@ export const ReadyToRecord = matDialogStory(AudioRecorderDialogComponent, dialog
 
 export const Countdown = matDialogStory(AudioRecorderDialogComponent, dialogStoryConfig);
 Countdown.args = {
-  data: { countdown: true }
+  countdown: true
 };
 Countdown.play = async ({ canvasElement }) => {
   // Countdown should be visible
@@ -44,5 +130,27 @@ Countdown.play = async ({ canvasElement }) => {
 
 export const ExistingAudio = matDialogStory(AudioRecorderDialogComponent, dialogStoryConfig);
 ExistingAudio.args = {
-  data: { audio: { status: 'processed', url: './test-audio-player.webm' } }
+  audio: { status: 'processed', url: './test-audio-player.webm' }
+};
+
+export const PermissionDenied = matDialogStory(AudioRecorderDialogComponent, dialogStoryConfig);
+PermissionDenied.args = {
+  micPermission: false
+};
+PermissionDenied.play = async ({ canvasElement }) => {
+  const recordButton = canvasElement.querySelector('.record');
+  await userEvent.click(recordButton!);
+  const notice = canvasElement.querySelector('simple-snack-bar');
+  expect(notice?.textContent?.includes('Access to your microphone was denied')).toBeTruthy();
+};
+
+export const MicNotFound = matDialogStory(AudioRecorderDialogComponent, dialogStoryConfig);
+MicNotFound.args = {
+  micAvailable: false
+};
+MicNotFound.play = async ({ canvasElement }) => {
+  const recordButton = canvasElement.querySelector('.record');
+  await userEvent.click(recordButton!);
+  const notice = canvasElement.querySelector('simple-snack-bar');
+  expect(notice?.textContent?.includes('No microphone was found')).toBeTruthy();
 };
