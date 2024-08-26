cf90676e365a1d18802b2ab033b59814bc11d30d
Peter Chapman <peter@conglomo.co.nz>
2024-08-27 08:44:44 +1200

    SF-2928 Add download draft as zip to Serval admin area

--- src/SIL.XForge.Scripture/ClientApp/src/app/serval-administration/serval-project.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/serval-administration/serval-project.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Canon } from '@sillsdev/scripture';
 import { saveAs } from 'file-saver';
 import { SFProjectProfile } from 'realtime-server/lib/esm/scriptureforge/models/sf-project';
-import { catchError, lastValueFrom, of, switchMap, throwError } from 'rxjs';
+import { catchError, lastValueFrom, of, Subscription, switchMap, throwError } from 'rxjs';
 import { ActivatedProjectService } from 'xforge-common/activated-project.service';
 import { DataLoadingComponent } from 'xforge-common/data-loading-component';
 import { NoticeService } from 'xforge-common/notice.service';
@@ -15,6 +15,7 @@ import { SFProjectService } from '../core/sf-project.service';
 import { BuildDto } from '../machine-api/build-dto';
 import { NoticeComponent } from '../shared/notice/notice.component';
 import { SharedModule } from '../shared/shared.module';
+import { DraftZipProgress } from '../translate/draft-generation/draft-generation';
 import { DraftGenerationService } from '../translate/draft-generation/draft-generation.service';
 import { ServalAdministrationService } from './serval-administration.service';
 
@@ -44,8 +45,12 @@ export class ServalProjectComponent extends DataLoadingComponent implements OnIn
   trainingFiles: string[] = [];
   translationBooks: string[] = [];
 
+  downloadBooksProgress: number = 0;
+  downloadBooksTotal: number = 0;
+
   draftConfig: Object | undefined;
   lastCompletedBuild: BuildDto | undefined;
+  zipSubscription: Subscription | undefined;
 
   constructor(
     private readonly activatedProjectService: ActivatedProjectService,
@@ -157,7 +162,16 @@ export class ServalProjectComponent extends DataLoadingComponent implements OnIn
   }
 
   async downloadDraft(): Promise<void> {
-    // TBD
+    this.zipSubscription?.unsubscribe();
+    this.zipSubscription = this.draftGenerationService
+      .downloadGeneratedDraftZip(this.activatedProjectService.projectDoc, this.lastCompletedBuild)
+      .subscribe({
+        next: (draftZipProgress: DraftZipProgress) => {
+          this.downloadBooksProgress = draftZipProgress.current;
+          this.downloadBooksTotal = draftZipProgress.total;
+        },
+        error: (error: Error) => this.noticeService.showError(error.message)
+      });
   }
 
   async downloadProject(id: string, fileName: string): Promise<void> {
