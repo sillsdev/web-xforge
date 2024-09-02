b00142536ce4570e7244af18b53e63c215049d7b
Peter Chapman <peter@conglomo.co.nz>
2024-09-02 13:35:58 +1200

    Add Paratext Biblical Terms Unit Tests

--- src/SIL.XForge.Scripture/Models/BiblicalTermsChanges.cs
+++ src/SIL.XForge.Scripture/Models/BiblicalTermsChanges.cs
@@ -5,9 +5,9 @@ namespace SIL.XForge.Scripture.Models;
 /// <summary>
 /// The changes in the Biblical Terms from Paratext
 /// </summary>
-public record BiblicalTermsChanges
+public class BiblicalTermsChanges
 {
-    public List<BiblicalTerm> BiblicalTerms = new List<BiblicalTerm>();
-    public string ErrorMessage { get; set; } = string.Empty;
-    public bool HasRenderings { get; set; }
+    public List<BiblicalTerm> BiblicalTerms { get; } = [];
+    public string ErrorMessage { get; init; } = string.Empty;
+    public bool HasRenderings { get; init; }
 }
