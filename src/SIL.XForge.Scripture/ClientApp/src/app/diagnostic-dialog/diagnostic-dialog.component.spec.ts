db409bd7ba1c49dc7fbb8cc7038187dff022edb2
Kyle Buss <kyle_buss@sil.org>
2024-09-23 08:22:32 -0500

    [WIP] SF-2963 Add a diagnostic overlay for developers to view subscribed doc counts.

---
+++ src/SIL.XForge.Scripture/ClientApp/src/app/diagnostic-dialog/diagnostic-dialog.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DiagnosticDialogComponent } from './diagnostic-dialog.component';
+
+describe('DiagnosticDialogComponent', () => {
+  let component: DiagnosticDialogComponent;
+  let fixture: ComponentFixture<DiagnosticDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DiagnosticDialogComponent]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(DiagnosticDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
