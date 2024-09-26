d3cc13a2ed32f1d6f3ba525ea56dcf81433585b7
siltomato <colin_moore@sil.org>
2024-09-26 14:27:13 -0400

    refactor services

---
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/activated-project-user-config.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { SFProjectUserConfig } from 'realtime-server/lib/esm/scriptureforge/models/sf-project-user-config';
+import { Observable, map, of, shareReplay, startWith, switchMap } from 'rxjs';
+import { SFProjectUserConfigDoc } from '../app/core/models/sf-project-user-config-doc';
+import { SFProjectService } from '../app/core/sf-project.service';
+import { ActivatedProjectService } from './activated-project.service';
+import { UserService } from './user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ActivatedProjectUserConfigService {
+  readonly projectUserConfigDoc$: Observable<SFProjectUserConfigDoc | undefined> =
+    this.activatedProject.projectId$.pipe(
+      switchMap(projectId =>
+        projectId != null ? this.projectService.getUserConfig(projectId, this.userService.currentUserId) : of(undefined)
+      ),
+      switchMap(
+        projectUserConfigDoc =>
+          projectUserConfigDoc?.changes$.pipe(
+            map(() => projectUserConfigDoc),
+
+            startWith(projectUserConfigDoc)
+          ) ?? of(undefined)
+      ),
+      shareReplay(1)
+    );
+
+  readonly projectUserConfig$: Observable<SFProjectUserConfig | undefined> = this.projectUserConfigDoc$.pipe(
+    map(doc => doc?.data)
+  );
+
+  constructor(
+    private readonly activatedProject: ActivatedProjectService,
+    private readonly projectService: SFProjectService,
+    private readonly userService: UserService
+  ) {}
+}
