6d88ecdcfc81adb1748e638fd5ec02ef4974e740
Peter Chapman <peter@conglomo.co.nz>
2024-10-01 12:49:19 +1300

    SF-3011 Clean up razor page formatting and use custom site name

--- src/SIL.XForge.Scripture/Models/RazorPageSettings.cs
+++ src/SIL.XForge.Scripture/Models/RazorPageSettings.cs
@@ -3,6 +3,6 @@ namespace SIL.XForge.Scripture.Models;
 /// <summary>Various settings to be used Razor pages</summary>
 public class RazorPageSettings
 {
-    public string ProductVersion { get; set; }
-    public string BugsnagConfig { get; set; }
+    public string ProductVersion { get; init; } = string.Empty;
+    public string BugsnagConfig { get; init; } = string.Empty;
 }
