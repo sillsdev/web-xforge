13862fc43809d5daae9cf3b266354db31143dbb3
Raymond Luong <raymond_luong@sil.org>
2024-10-07 14:13:18 -0600

    Use Paratext service to store forced usernames

--- src/SIL.XForge/Models/UserSecret.cs
+++ src/SIL.XForge/Models/UserSecret.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-
 namespace SIL.XForge.Models;
 
 /// <summary>
@@ -8,10 +6,6 @@ namespace SIL.XForge.Models;
 /// </summary>
 public class UserSecret : IIdentifiable
 {
-    // Stores a mapping of usernames to the forced usernames for a user secret to allow specifying an
-    // alternate username. This is needed to persist data created by a user prior changing PT names
-    private static Dictionary<string, string> _forcedUsernameMap = [];
-
     /// <summary>
     /// SF user ID of the user that these secrets pertain to. (This is not a different set of IDs for
     /// specifically user secrets.)
@@ -19,16 +13,4 @@ public class UserSecret : IIdentifiable
     public string Id { get; set; }
 
     public Tokens ParatextTokens { get; set; }
-
-    public static string ParatextUsername(string username)
-    {
-        if (_forcedUsernameMap.TryGetValue(username, out string forcedUsername))
-            return forcedUsername;
-        return username;
-    }
-
-    public static void RemoveForcedUsernames() => _forcedUsernameMap.Clear();
-
-    public static bool ForceUsername(string username, string forcedUsername) =>
-        _forcedUsernameMap.TryAdd(username, forcedUsername);
 }
