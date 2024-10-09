0d0f8a5bdb81eb03c7ef78d25d16ecf8408bb2f5
Nathaniel Paulus <nathaniel_paulus@sil.org>
2024-10-09 13:57:44 -0400

    Add doc subscriber counts to diagnostics overlay

--- src/SIL.XForge.Scripture/ClientApp/src/app/shared/diagnostic-overlay/diagnostic-overlay.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/diagnostic-overlay/diagnostic-overlay.component.ts
@@ -31,7 +31,7 @@ export class DiagnosticOverlayComponent {
     readonly noticeService: NoticeService
   ) {}
 
-  get docCountsByCollection(): { [key: string]: number } {
+  get docCountsByCollection(): { [key: string]: { docCount: number; subscriberCount: number } } {
     return this.realtimeService.docsCountByCollection;
   }
 
