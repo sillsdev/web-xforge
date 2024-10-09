c413b4d261059d2a03c421dc964d4dc9f560493c
Nathaniel Paulus <nathaniel_paulus@sil.org>
2024-10-09 11:16:19 -0400

    Deprecate SubscriptionDisposable

--- src/SIL.XForge.Scripture/ClientApp/src/xforge-common/subscription-disposable.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/subscription-disposable.ts
@@ -2,7 +2,11 @@ import { Directive, OnDestroy } from '@angular/core';
 import { Observable, Subject, Subscription } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
-/** Handles Observable unsubscribing */
+/** Handles Observable unsubscribing
+ * @deprecated Angular previously did not have a good way to subscribe to a component's destruction. This class was used
+ * to help handle unsubscribing when a component was destroyed. When possible, use
+ * {@link https://angular.dev/api/core/DestroyRef DestroyRef}.
+ */
 // Decorator required by Angular compiler
 @Directive()
 // eslint-disable-next-line @angular-eslint/directive-class-suffix
