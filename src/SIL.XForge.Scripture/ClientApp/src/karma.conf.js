0ec7d6947333edfb11b6cb1befcd34ca32d4c483
Peter Chapman <peter@conglomo.co.nz>
2024-10-10 14:35:31 +1300

    Add a Safari launcher for Karma

--- src/SIL.XForge.Scripture/ClientApp/src/karma.conf.js
+++ src/SIL.XForge.Scripture/ClientApp/src/karma.conf.js
@@ -23,6 +23,7 @@ module.exports = function (config) {
     require('karma-jasmine'),
     require('karma-chrome-launcher'),
     require('karma-firefox-launcher'),
+    require('karma-safari-launcher'),
     require('karma-jasmine-html-reporter'),
     require('karma-coverage-istanbul-reporter'),
     require('karma-teamcity-reporter'),
