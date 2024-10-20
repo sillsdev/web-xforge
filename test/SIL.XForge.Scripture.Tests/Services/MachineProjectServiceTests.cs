876930ddb4617e19a4eefdc11b48648bbaee3ef7
Peter Chapman <peter@conglomo.co.nz>
2024-10-21 11:50:12 +1300

    Remove SFBiblicalTermsText and MockText

--- test/SIL.XForge.Scripture.Tests/Services/MachineProjectServiceTests.cs
+++ test/SIL.XForge.Scripture.Tests/Services/MachineProjectServiceTests.cs
@@ -2725,8 +2725,8 @@ public class MachineProjectServiceTests
             }
         }
 
-        private static MockText GetMockTrainingData(bool source) =>
-            new MockText
+        private static SFTrainingText GetMockTrainingData(bool source) =>
+            new SFTrainingText
             {
                 Id = Data01,
                 Segments = new List<SFTextSegment>
