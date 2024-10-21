5a6db92a1cb1123b57dab97acbfb7a545ffe78b3
Raymond Luong <raymond_luong@sil.org>
2024-10-21 16:29:28 -0600

    Code review fixes

--- src/SIL.XForge.Scripture/ClientApp/src/app/shared/notice/notice.types.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/notice/notice.types.ts
@@ -1,5 +1,5 @@
 export const noticeTypes = ['primary', 'secondary', 'success', 'warning', 'error', 'info', 'light', 'dark'] as const;
 export type NoticeType = (typeof noticeTypes)[number];
 
-export const noticeModes = ['fill-light', 'fill-dark', 'fill-extra-dark', 'outline'] as const;
+export const noticeModes = ['fill-light', 'fill-dark', 'fill-extra-dark', 'outline', 'basic'] as const;
 export type NoticeMode = (typeof noticeModes)[number];
