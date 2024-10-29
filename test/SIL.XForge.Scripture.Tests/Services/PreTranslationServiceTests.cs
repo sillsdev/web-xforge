2d3eefbf949e2082cc6a06e81a302e26c1c4b64d
Peter Chapman <peter@conglomo.co.nz>
2024-10-30 10:30:18 +1300

    Add Parallel Corpora support to Pre-Translation Service

--- test/SIL.XForge.Scripture.Tests/Services/PreTranslationServiceTests.cs
+++ test/SIL.XForge.Scripture.Tests/Services/PreTranslationServiceTests.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using NSubstitute;
+using NSubstitute.Extensions;
 using NUnit.Framework;
 using Serval.Client;
 using SIL.XForge.DataAccess;
@@ -17,16 +18,92 @@ namespace SIL.XForge.Scripture.Services;
 public class PreTranslationServiceTests
 {
     private const string Project01 = "project01";
-    private const string Project02 = "project02";
-    private const string Project03 = "project03";
     private const string Corpus01 = "corpus01";
+    private const string ParallelCorpus01 = "parallelCorpus01";
     private const string TranslationEngine01 = "translationEngine01";
+
+    [TestCase(true)]
+    [TestCase(false)]
+    public async Task GetPreTranslationParametersAsync_CompatibleWithLegacyCorpora(bool uploadParatextZipFile)
+    {
+        // Set up test environment
+        var env = new TestEnvironment();
+        await env.SetupProjectSecretAsync(
+            new ServalData
+            {
+                PreTranslationEngineId = TranslationEngine01,
+                Corpora = new Dictionary<string, ServalCorpus>
+                {
+                    {
+                        "another_corpus",
+                        new ServalCorpus { PreTranslate = false }
+                    },
+                    {
+                        Corpus01,
+                        new ServalCorpus { PreTranslate = true, UploadParatextZipFile = uploadParatextZipFile, }
+                    },
+                },
+            }
+        );
+
+        // SUT
+        (string translationEngineId, string corpusId, bool useParatextVerseRef) =
+            await env.Service.GetPreTranslationParametersAsync(Project01);
+        Assert.AreEqual(TranslationEngine01, translationEngineId);
+        Assert.AreEqual(Corpus01, corpusId);
+        Assert.AreEqual(uploadParatextZipFile, useParatextVerseRef);
+    }
+
+    [Test]
+    public async Task GetPreTranslationParametersAsync_CompatibleWithParallelCorpora()
+    {
+        // Set up test environment
+        var env = new TestEnvironment();
+        await env.SetupProjectSecretAsync(
+            new ServalData
+            {
+                ParallelCorpusIdForPreTranslate = ParallelCorpus01,
+                PreTranslationEngineId = TranslationEngine01,
+                CorpusFiles = [new ServalCorpusFile { CorpusId = Corpus01, ProjectId = Project01 }],
+            }
+        );
+
+        // SUT
+        (string translationEngineId, string corpusId, bool useParatextVerseRef) =
+            await env.Service.GetPreTranslationParametersAsync(Project01);
+        Assert.AreEqual(TranslationEngine01, translationEngineId);
+        Assert.AreEqual(Corpus01, corpusId);
+        Assert.IsTrue(useParatextVerseRef);
+    }
+
+    [Test]
+    public async Task GetPreTranslationParametersAsync_ThrowsExceptionWhenNoPreTranslationConfigured()
+    {
+        // Set up test environment
+        var env = new TestEnvironment();
+        await env.SetupProjectSecretAsync(new ServalData());
+
+        // SUT
+        Assert.ThrowsAsync<DataNotFoundException>(() => env.Service.GetPreTranslationParametersAsync(Project01));
+    }
+
+    [Test]
+    public void GetPreTranslationParametersAsync_ThrowsExceptionWhenProjectSecretMissing()
+    {
+        // Set up test environment
+        var env = new TestEnvironment();
+
+        // SUT
+        Assert.ThrowsAsync<DataNotFoundException>(
+            () => env.Service.GetPreTranslationParametersAsync("invalid_project_id")
+        );
+    }
 
     [Test]
     public async Task GetPreTranslationsAsync_CombinesSegmentedVerses()
     {
         // Set up test environment
-        var env = new TestEnvironment();
+        var env = new TestEnvironment(new TestEnvironmentOptions { MockPreTranslationParameters = true });
         const int bookNum = 64;
         const int chapterNum = 1;
         string textId = PreTranslationService.GetTextId(bookNum, chapterNum);
@@ -83,34 +160,12 @@ public class PreTranslationServiceTests
     }
 
     [Test]
-    public void GetPreTranslationsAsync_ThrowsExceptionWhenProjectSecretMissing()
-    {
-        // Set up test environment
-        var env = new TestEnvironment();
-
-        // SUT
-        Assert.ThrowsAsync<DataNotFoundException>(
-            () => env.Service.GetPreTranslationsAsync("invalid_project_id", 40, 1, CancellationToken.None)
-        );
-    }
-
-    [Test]
-    public void GetPreTranslationsAsync_ThrowsExceptionWhenNoPreTranslationConfigured()
-    {
-        // Set up test environment
-        var env = new TestEnvironment();
-
-        // SUT
-        Assert.ThrowsAsync<DataNotFoundException>(
-            () => env.Service.GetPreTranslationsAsync(Project02, 40, 1, CancellationToken.None)
-        );
-    }
-
-    [Test]
     public async Task GetPreTranslationsAsync_ReturnsEmptyArrayIfNoPreTranslations_Paratext()
     {
         // Set up test environment
-        var env = new TestEnvironment(new TestEnvironmentOptions { UseParatextZipFile = true });
+        var env = new TestEnvironment(
+            new TestEnvironmentOptions { MockPreTranslationParameters = true, UseParatextZipFile = true }
+        );
         const int bookNum = 40;
         const int chapterNum = 1;
         string textId = PreTranslationService.GetTextId(bookNum);
@@ -139,7 +194,7 @@ public class PreTranslationServiceTests
     public async Task GetPreTranslationsAsync_ReturnsEmptyArrayIfNoPreTranslations_Text()
     {
         // Set up test environment
-        var env = new TestEnvironment();
+        var env = new TestEnvironment(new TestEnvironmentOptions { MockPreTranslationParameters = true });
         const int bookNum = 40;
         const int chapterNum = 1;
         string textId = PreTranslationService.GetTextId(bookNum, chapterNum);
@@ -168,7 +223,9 @@ public class PreTranslationServiceTests
     public async Task GetPreTranslationsAsync_ReturnsUsablePreTranslations_Paratext()
     {
         // Set up test environment
-        var env = new TestEnvironment(new TestEnvironmentOptions { UseParatextZipFile = true });
+        var env = new TestEnvironment(
+            new TestEnvironmentOptions { MockPreTranslationParameters = true, UseParatextZipFile = true }
+        );
         const int bookNum = 40;
         const int chapterNum = 1;
         string textId = PreTranslationService.GetTextId(bookNum);
@@ -248,7 +305,7 @@ public class PreTranslationServiceTests
     public async Task GetPreTranslationsAsync_ReturnsUsablePreTranslations_Text()
     {
         // Set up test environment
-        var env = new TestEnvironment();
+        var env = new TestEnvironment(new TestEnvironmentOptions { MockPreTranslationParameters = true });
         const int bookNum = 40;
         const int chapterNum = 1;
         string textId = PreTranslationService.GetTextId(bookNum, chapterNum);
@@ -325,34 +382,12 @@ public class PreTranslationServiceTests
     }
 
     [Test]
-    public void GetPreTranslationUsfmAsync_ThrowsExceptionWhenProjectSecretMissing()
-    {
-        // Set up test environment
-        var env = new TestEnvironment();
-
-        // SUT
-        Assert.ThrowsAsync<DataNotFoundException>(
-            () => env.Service.GetPreTranslationUsfmAsync("invalid_project_id", 40, 1, CancellationToken.None)
-        );
-    }
-
-    [Test]
-    public void GetPreTranslationUsfmAsync_ThrowsExceptionWhenNoPreTranslationConfigured()
-    {
-        // Set up test environment
-        var env = new TestEnvironment();
-
-        // SUT
-        Assert.ThrowsAsync<DataNotFoundException>(
-            () => env.Service.GetPreTranslationUsfmAsync(Project02, 40, 1, CancellationToken.None)
-        );
-    }
-
-    [Test]
     public async Task GetPreTranslationUsfmAsync_ReturnsEntireBook()
     {
         // Set up test environment
-        var env = new TestEnvironment(new TestEnvironmentOptions { UseParatextZipFile = true });
+        var env = new TestEnvironment(
+            new TestEnvironmentOptions { MockPreTranslationParameters = true, UseParatextZipFile = true }
+        );
 
         // SUT
         string usfm = await env.Service.GetPreTranslationUsfmAsync(Project01, 40, 0, CancellationToken.None);
@@ -363,7 +398,9 @@ public class PreTranslationServiceTests
     public async Task GetPreTranslationUsfmAsync_ReturnsChapterOneWithIntroductoryMaterial()
     {
         // Set up test environment
-        var env = new TestEnvironment(new TestEnvironmentOptions { UseParatextZipFile = true });
+        var env = new TestEnvironment(
+            new TestEnvironmentOptions { MockPreTranslationParameters = true, UseParatextZipFile = true }
+        );
 
         // SUT
         string usfm = await env.Service.GetPreTranslationUsfmAsync(Project01, 40, 1, CancellationToken.None);
@@ -374,7 +411,9 @@ public class PreTranslationServiceTests
     public async Task GetPreTranslationUsfmAsync_ReturnsSpecificChapter()
     {
         // Set up test environment
-        var env = new TestEnvironment(new TestEnvironmentOptions { UseParatextZipFile = true });
+        var env = new TestEnvironment(
+            new TestEnvironmentOptions { MockPreTranslationParameters = true, UseParatextZipFile = true }
+        );
 
         // SUT
         string usfm = await env.Service.GetPreTranslationUsfmAsync(Project01, 40, 2, CancellationToken.None);
@@ -385,7 +424,9 @@ public class PreTranslationServiceTests
     public async Task GetPreTranslationUsfmAsync_ReturnsEmptyStringForMissingChapter()
     {
         // Set up test environment
-        var env = new TestEnvironment(new TestEnvironmentOptions { UseParatextZipFile = true });
+        var env = new TestEnvironment(
+            new TestEnvironmentOptions { MockPreTranslationParameters = true, UseParatextZipFile = true }
+        );
 
         // SUT
         string usfm = await env.Service.GetPreTranslationUsfmAsync(Project01, 40, 3, CancellationToken.None);
@@ -393,31 +434,7 @@ public class PreTranslationServiceTests
     }
 
     [Test]
-    public void UpdatePreTranslationStatusAsync_ThrowsExceptionWhenNoPreTranslationConfigured()
-    {
-        // Set up test environment
-        var env = new TestEnvironment();
-
-        // SUT
-        Assert.ThrowsAsync<DataNotFoundException>(
-            () => env.Service.UpdatePreTranslationStatusAsync(Project02, CancellationToken.None)
-        );
-    }
-
-    [Test]
     public void UpdatePreTranslationStatusAsync_ThrowsExceptionWhenProjectMissing()
-    {
-        // Set up test environment
-        var env = new TestEnvironment();
-
-        // SUT
-        Assert.ThrowsAsync<DataNotFoundException>(
-            () => env.Service.UpdatePreTranslationStatusAsync(Project03, CancellationToken.None)
-        );
-    }
-
-    [Test]
-    public void UpdatePreTranslationStatusAsync_ThrowsExceptionWhenProjectSecretMissing()
     {
         // Set up test environment
         var env = new TestEnvironment();
@@ -432,7 +449,7 @@ public class PreTranslationServiceTests
     public async Task UpdatePreTranslationStatusAsync_NoDrafts()
     {
         // Set up test environment
-        var env = new TestEnvironment();
+        var env = new TestEnvironment(new TestEnvironmentOptions { MockPreTranslationParameters = true });
 
         env.TranslationEnginesClient.GetAllPretranslationsAsync(
                 TranslationEngine01,
@@ -469,7 +486,9 @@ public class PreTranslationServiceTests
     public async Task UpdatePreTranslationStatusAsync_Paratext()
     {
         // Set up test environment
-        var env = new TestEnvironment(new TestEnvironmentOptions { UseParatextZipFile = true });
+        var env = new TestEnvironment(
+            new TestEnvironmentOptions { MockPreTranslationParameters = true, UseParatextZipFile = true }
+        );
 
         env.TranslationEnginesClient.GetAllPretranslationsAsync(
                 TranslationEngine01,
@@ -515,7 +534,7 @@ public class PreTranslationServiceTests
     public async Task UpdatePreTranslationStatusAsync_Text()
     {
         // Set up test environment
-        var env = new TestEnvironment();
+        var env = new TestEnvironment(new TestEnvironmentOptions { MockPreTranslationParameters = true });
 
         env.TranslationEnginesClient.GetAllPretranslationsAsync(
                 TranslationEngine01,
@@ -559,6 +578,7 @@ public class PreTranslationServiceTests
 
     private class TestEnvironmentOptions
     {
+        public bool MockPreTranslationParameters { get; init; }
         public bool UseParatextZipFile { get; init; }
     }
 
@@ -572,35 +592,7 @@ public class PreTranslationServiceTests
         public TestEnvironment(TestEnvironmentOptions? options = null)
         {
             options ??= new TestEnvironmentOptions();
-            var projectSecrets = new MemoryRepository<SFProjectSecret>(
-                [
-                    new SFProjectSecret
-                    {
-                        Id = Project01,
-                        ServalData = new ServalData
-                        {
-                            PreTranslationEngineId = TranslationEngine01,
-                            Corpora = new Dictionary<string, ServalCorpus>
-                            {
-                                {
-                                    "another_corpus",
-                                    new ServalCorpus { PreTranslate = false }
-                                },
-                                {
-                                    Corpus01,
-                                    new ServalCorpus
-                                    {
-                                        PreTranslate = true,
-                                        UploadParatextZipFile = options.UseParatextZipFile,
-                                    }
-                                },
-                            },
-                        },
-                    },
-                    new SFProjectSecret { Id = Project02 },
-                    new SFProjectSecret { Id = Project03 },
-                ]
-            );
+            ProjectSecrets = new MemoryRepository<SFProjectSecret>([new SFProjectSecret { Id = Project01 },]);
 
             RealtimeService = new SFMemoryRealtimeService();
             SFProject[] sfProjects =
@@ -632,7 +624,6 @@ public class PreTranslationServiceTests
                         },
                     ],
                 },
-                new SFProject { Id = Project02 },
             ];
             RealtimeService.AddRepository("sf_projects", OTType.Json0, new MemoryRepository<SFProject>(sfProjects));
             TranslationEnginesClient = Substitute.For<ITranslationEnginesClient>();
@@ -646,11 +637,35 @@ public class PreTranslationServiceTests
                     CancellationToken.None
                 )
                 .Returns(MatthewBookUsfm);
-            Service = new PreTranslationService(projectSecrets, RealtimeService, TranslationEnginesClient);
+            Service = Substitute.ForPartsOf<PreTranslationService>(
+                ProjectSecrets,
+                RealtimeService,
+                TranslationEnginesClient
+            );
+            if (options.MockPreTranslationParameters)
+            {
+                Service
+                    .Configure()
+                    .GetPreTranslationParametersAsync(Project01)
+                    .Returns(
+                        Task.FromResult<(string, string, bool)>(
+                            (TranslationEngine01, Corpus01, options.UseParatextZipFile)
+                        )
+                    );
+            }
         }
 
+        private MemoryRepository<SFProjectSecret> ProjectSecrets { get; }
         public SFMemoryRealtimeService RealtimeService { get; }
         public PreTranslationService Service { get; }
         public ITranslationEnginesClient TranslationEnginesClient { get; }
+
+        /// <summary>
+        /// Sets up the Project Secret.
+        /// </summary>
+        /// <param name="servalData">The Serval configuration data.</param>
+        /// <returns>The asynchronous task.</returns>
+        public async Task SetupProjectSecretAsync(ServalData? servalData) =>
+            await ProjectSecrets.UpdateAsync(Project01, u => u.Set(p => p.ServalData, servalData));
     }
 }
