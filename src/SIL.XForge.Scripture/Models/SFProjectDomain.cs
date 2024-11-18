7adb748422e72620ce0b3d0b0475888f308c6092
Peter Chapman <peter@conglomo.co.nz>
2024-11-18 17:01:41 -0600

    Add SFProjectRights service

---
+++ src/SIL.XForge.Scripture/Models/SFProjectDomain.cs
@@ -0,0 +1,21 @@
+namespace SIL.XForge.Scripture.Models;
+
+public static class SFProjectDomain
+{
+    public const string Texts = "texts";
+    public const string Project = "project";
+    public const string ProjectUserConfigs = "project_user_configs";
+    public const string Questions = "questions";
+    public const string Answers = "answers";
+    public const string AnswerComments = "answer_comments";
+    public const string AnswerStatus = "answer_status";
+    public const string Likes = "likes";
+    public const string BiblicalTerms = "biblical_terms";
+    public const string PTNoteThreads = "pt_note_threads";
+    public const string SFNoteThreads = "sf_note_threads";
+    public const string Notes = "notes";
+    public const string TextAudio = "text_audio";
+    public const string TrainingData = "training_data";
+    public const string Drafts = "drafts";
+    public const string UserInvites = "user_invites";
+}
