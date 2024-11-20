e97d229586e11509ad36e240555aeb0469fced53
Joseph Myers <wjosephmyers@gmail.com>
2024-11-20 16:07:50 -0600

    Localization

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/draft-generation-steps/draft-generation-steps.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/draft-generation-steps/draft-generation-steps.component.ts
@@ -307,7 +307,7 @@ export class DraftGenerationStepsComponent extends SubscriptionDisposable implem
   onTrainingBookSelect(selectedBooks: number[]): void {
     this.userSelectedTrainingBooks = selectedBooks.map((bookNum: number) => ({
       number: bookNum,
-      name: Canon.bookNumberToEnglishName(bookNum),
+      name: this.i18n.localizeBook(bookNum),
       source: this.trainingSources[0].shortName,
       target: this.trainingTargets[0].shortName
     }));
@@ -322,7 +322,7 @@ export class DraftGenerationStepsComponent extends SubscriptionDisposable implem
   onTranslateBookSelect(selectedBooks: number[]): void {
     this.userSelectedTranslateBooks = selectedBooks.map((bookNum: number) => ({
       number: bookNum,
-      name: Canon.bookNumberToEnglishName(bookNum)
+      name: this.i18n.localizeBook(bookNum)
     }));
     this.clearErrorMessage();
   }
@@ -351,7 +351,7 @@ export class DraftGenerationStepsComponent extends SubscriptionDisposable implem
   }
 
   selectedTranslateBooks(): string {
-    return this.userSelectedTranslateBooks.map(b => b.name).join(', ');
+    return this.i18n.enumerateList(this.userSelectedTranslateBooks.map(b => this.i18n.localizeBook(b.number)));
   }
 
   /**
@@ -374,8 +374,12 @@ export class DraftGenerationStepsComponent extends SubscriptionDisposable implem
     this.userSelectedTrainingBooks = newSelectedTrainingBooks;
   }
 
+  get firstTrainingSource(): string {
+    return this.trainingSources[0].shortName;
+  }
+
   bookNames(books: number[]): string {
-    return books.map(bookNum => this.i18n.localizeBook(bookNum)).join(', ');
+    return this.i18n.enumerateList(books.map(bookNum => this.i18n.localizeBook(bookNum)));
   }
 
   private validateCurrentStep(): boolean {
@@ -401,7 +405,7 @@ export class DraftGenerationStepsComponent extends SubscriptionDisposable implem
     this.initialSelectedTranslateBooks = intersection.length > 0 ? intersection : [];
     this.userSelectedTranslateBooks = this.initialSelectedTranslateBooks.map((bookNum: number) => ({
       number: bookNum,
-      name: Canon.bookNumberToEnglishName(bookNum)
+      name: this.i18n.localizeBook(bookNum)
     }));
   }
 
@@ -419,7 +423,7 @@ export class DraftGenerationStepsComponent extends SubscriptionDisposable implem
 
     this.userSelectedTrainingBooks = this.initialSelectedTrainingBooks.map((bookNum: number) => ({
       number: bookNum,
-      name: Canon.bookNumberToEnglishName(bookNum),
+      name: this.i18n.localizeBook(bookNum),
       source: this.trainingSources[0].shortName,
       target: this.trainingTargets[0].shortName
     }));
