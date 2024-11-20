36d4418dcf88b8d93f480e0ea65d4048b96a2119
Joseph Myers <wjosephmyers@gmail.com>
2024-11-20 17:15:11 -0600

    Fixed draft generation steps tests

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/draft-generation-steps/draft-generation-steps.component.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/draft-generation-steps/draft-generation-steps.component.spec.ts
@@ -21,7 +21,12 @@ import { SFProjectService } from '../../../core/sf-project.service';
 import { ProgressService, TextProgress } from '../../../shared/progress-service/progress.service';
 import { NllbLanguageService } from '../../nllb-language.service';
 import { TrainingDataService } from '../training-data/training-data.service';
-import { DraftGenerationStepsComponent, DraftGenerationStepsResult } from './draft-generation-steps.component';
+import {
+  Book,
+  DraftGenerationStepsComponent,
+  DraftGenerationStepsResult,
+  TrainingBook
+} from './draft-generation-steps.component';
 
 describe('DraftGenerationStepsComponent', () => {
   let component: DraftGenerationStepsComponent;
@@ -223,9 +228,9 @@ describe('DraftGenerationStepsComponent', () => {
     });
 
     it('should emit the correct selected books when done', () => {
-      const trainingBooks = [2, 3];
+      const trainingBooks: TrainingBook[] = [{ number: 2 } as any, { number: 3 } as any];
       const trainingDataFiles: string[] = [];
-      const translationBooks = [1, 2];
+      const translationBooks: Book[] = [{ number: 1 } as any];
 
       component.userSelectedTrainingBooks = trainingBooks;
       component.userSelectedTranslateBooks = translationBooks;
@@ -237,7 +242,7 @@ describe('DraftGenerationStepsComponent', () => {
 
       clickConfirmLanguages(fixture);
       const test = fixture.debugElement.queryAll(By.css('mat-step-header'));
-      test[2].nativeElement.click(); //click the last step
+      test[3].nativeElement.click(); //click the last step
       fixture.detectChanges();
 
       const generateDraftButton: HTMLElement = fixture.nativeElement.querySelector('.advance-button');
@@ -246,9 +251,9 @@ describe('DraftGenerationStepsComponent', () => {
       fixture.detectChanges();
 
       expect(component.done.emit).toHaveBeenCalledWith({
-        translationBooks,
+        translationBooks: translationBooks.map(b => b.number),
         trainingDataFiles,
-        trainingBooks: trainingBooks.filter(book => !translationBooks.includes(book)),
+        trainingBooks: trainingBooks.filter(book => !translationBooks.includes(book)).map(b => b.number),
         fastTraining: false
       } as DraftGenerationStepsResult);
       expect(component.isStepsCompleted).toBe(true);
@@ -288,13 +293,13 @@ describe('DraftGenerationStepsComponent', () => {
     }));
 
     it('should update training books when a step changes', fakeAsync(() => {
-      component.userSelectedTranslateBooks = [3, 4]; //complete the first step
+      component.userSelectedTranslateBooks = [{} as any]; //complete the first step
       fixture.detectChanges();
       spyOn(component, 'updateTrainingBooks');
 
       clickConfirmLanguages(fixture);
-      const test = fixture.debugElement.queryAll(By.css('mat-step-header'));
-      test[2].nativeElement.click(); //click the next step
+      const steps = fixture.debugElement.queryAll(By.css('mat-step-header'));
+      steps[2].nativeElement.click(); //click the next step
       fixture.detectChanges();
 
       expect(component.updateTrainingBooks).toHaveBeenCalledTimes(1);
@@ -331,9 +336,9 @@ describe('DraftGenerationStepsComponent', () => {
     }));
 
     it('should emit the fast training value if checked', () => {
-      const trainingBooks = [1, 2];
+      const trainingBooks: TrainingBook[] = [{ number: 2 } as any, { number: 3 } as any];
       const trainingDataFiles: string[] = [];
-      const translationBooks = [3, 4];
+      const translationBooks: Book[] = [{ number: 1 } as any];
 
       component.userSelectedTrainingBooks = trainingBooks;
       component.userSelectedTranslateBooks = translationBooks;
@@ -346,6 +351,7 @@ describe('DraftGenerationStepsComponent', () => {
       const test = fixture.debugElement.queryAll(By.css('mat-step-header'));
       test[3].nativeElement.click(); //click the next step
       fixture.detectChanges();
+      component.tryAdvanceStep();
 
       // Tick the checkbox
       const fastTrainingCheckbox = fixture.nativeElement.querySelector('mat-checkbox.fast-training input');
@@ -359,9 +365,9 @@ describe('DraftGenerationStepsComponent', () => {
       fixture.detectChanges();
 
       expect(component.done.emit).toHaveBeenCalledWith({
-        trainingBooks,
+        trainingBooks: trainingBooks.map(b => b.number),
         trainingDataFiles,
-        translationBooks,
+        translationBooks: translationBooks.map(b => b.number),
         fastTraining: true
       } as DraftGenerationStepsResult);
       expect(generateDraftButton['disabled']).toBe(true);
