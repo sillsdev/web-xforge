4c717cffa0f02404f7ee977f1ce4308f0b9f75d1
Nathaniel Paulus <nathaniel_paulus@sil.org>
2024-11-26 13:14:24 -0500

    Use writing system to determine fallback font and add Noto fonts

--- src/SIL.XForge.Scripture/ClientApp/src/xforge-common/font.service.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/font.service.spec.ts
@@ -1,5 +1,5 @@
 import FONT_FACE_DEFINITIONS from '../../../fonts.json';
-import { FONT_FACE_FALLBACKS, FontService } from './font.service';
+import { FONT_FACE_FALLBACKS, FontResolver, FontService } from './font.service';
 import { MockConsole } from './mock-console';
 
 // Mocking the document with ts-mockito does not work because the type definitions for document.fonts does not include
@@ -14,6 +14,60 @@ class FakeDocument {
 }
 
 const mockedConsole: MockConsole = MockConsole.install();
+describe('FontResolver', () => {
+  const fontResolver = new FontResolver();
+
+  it('can resolve specified_font', () => {
+    const resolution = fontResolver.resolveFont({ defaultFont: 'Andika', writingSystem: { tag: 'en' } });
+    expect(resolution).toEqual({
+      requestedFont: 'Andika',
+      resolution: 'specified_font',
+      family: 'Andika',
+      url: FONT_FACE_DEFINITIONS['Andika']
+    });
+  });
+
+  it('can resolve near_match_specified_font', () => {
+    const resolution = fontResolver.resolveFont({ defaultFont: 'Andika Eng-Lit', writingSystem: { tag: 'en' } });
+    expect(resolution).toEqual({
+      requestedFont: 'Andika Eng-Lit',
+      resolution: 'near_match_specified_font',
+      family: 'Andika',
+      url: FONT_FACE_DEFINITIONS['Andika']
+    });
+  });
+
+  it('can resolve writing_system_default and take region into account', () => {
+    let resolution = fontResolver.resolveFont({ defaultFont: 'Unknown font', writingSystem: { tag: 'unk-Arab' } });
+    expect(resolution).toEqual({
+      requestedFont: 'Unknown font',
+      resolution: 'writing_system_default',
+      family: 'Scheherazade New',
+      url: FONT_FACE_DEFINITIONS['Scheherazade New']
+    });
+    resolution = fontResolver.resolveFont({ defaultFont: 'Unknown font', writingSystem: { tag: 'unk-Arab-NE' } });
+    expect(resolution).toEqual({
+      requestedFont: 'Unknown font',
+      resolution: 'writing_system_default',
+      family: 'Harmattan',
+      url: FONT_FACE_DEFINITIONS['Harmattan']
+    });
+  });
+
+  it('can fall back to the default font', () => {
+    const resolution = fontResolver.resolveFont({ defaultFont: 'Unknown font', writingSystem: { tag: 'unk' } });
+    expect(resolution).toEqual({
+      requestedFont: 'Unknown font',
+      resolution: 'default_font',
+      family: 'Charis SIL',
+      url: FONT_FACE_DEFINITIONS['Charis SIL']
+    });
+  });
+});
+
+function spec(font?: string): { defaultFont?: string; writingSystem: { tag: string } } {
+  return { defaultFont: font, writingSystem: { tag: 'en' } };
+}
 
 describe('FontService', () => {
   let fontService: FontService;
@@ -23,31 +77,42 @@ describe('FontService', () => {
   });
 
   it('should default to Charis SIL when font is not specified', () => {
-    expect(fontService.getCSSFontName(undefined)).toEqual('Charis SIL');
-    expect(fontService.getCSSFontName('')).toEqual('Charis SIL');
+    mockedConsole.expectAndHide(/No font definition for /);
+    expect(fontService.getFontFamilyFromProject(spec(undefined))).toEqual('Charis SIL');
+    expect(fontService.getFontFamilyFromProject(spec(''))).toEqual('Charis SIL');
+    mockedConsole.verify();
+    mockedConsole.reset();
   });
 
   it('should default to Charis SIL when font is not recognized', () => {
-    mockedConsole.expectAndHide(/No font definition/);
-    expect(fontService.getCSSFontName('zyz123')).toEqual('Charis SIL');
+    mockedConsole.expectAndHide(/No font definition for zyz123/);
+    expect(fontService.getFontFamilyFromProject(spec('zyz123'))).toEqual('Charis SIL');
     mockedConsole.verify();
     mockedConsole.reset();
   });
 
   it('should default to Charis SIL for proprietary serif fonts', () => {
-    expect(fontService.getCSSFontName('Times New Roman')).toEqual('Charis SIL');
-    expect(fontService.getCSSFontName('Cambria')).toEqual('Charis SIL');
-    expect(fontService.getCSSFontName('Sylfaen')).toEqual('Charis SIL');
+    mockedConsole.expectAndHide(/No font definition for /);
+    expect(fontService.getFontFamilyFromProject(spec('Times New Roman'))).toEqual('Charis SIL');
+    expect(fontService.getFontFamilyFromProject(spec('Cambria'))).toEqual('Charis SIL');
+    expect(fontService.getFontFamilyFromProject(spec('Sylfaen'))).toEqual('Charis SIL');
+    mockedConsole.verify();
+    mockedConsole.reset();
   });
 
   it('should default to Andika for proprietary sans-serif fonts', () => {
-    expect(fontService.getCSSFontName('Arial')).toEqual('Andika');
-    expect(fontService.getCSSFontName('Verdana')).toEqual('Andika');
-    expect(fontService.getCSSFontName('Tahoma')).toEqual('Andika');
+    mockedConsole.expectAndHide(/No font definition for /);
+    expect(fontService.getFontFamilyFromProject(spec('Arial'))).toEqual('Andika');
+    expect(fontService.getFontFamilyFromProject(spec('Verdana'))).toEqual('Andika');
+    expect(fontService.getFontFamilyFromProject(spec('Tahoma'))).toEqual('Andika');
+    mockedConsole.verify();
+    mockedConsole.reset();
   });
 
   it('should fall back from Annapurna SIL Thami to Annapurna SIL', () => {
-    expect(fontService.getCSSFontName('Annapurna SIL Thami')).toEqual('Annapurna SIL');
+    mockedConsole.expectAndHide(/No font definition for /);
+    expect(fontService.getFontFamilyFromProject(spec('Annapurna SIL Thami'))).toEqual('Annapurna SIL');
+    mockedConsole.verify();
   });
 
   it('should not have any broken font fallbacks', () => {
@@ -56,10 +121,29 @@ describe('FontService', () => {
   });
 
   it('should only load each font once', () => {
-    fontService.getCSSFontName('Charis SIL');
-    fontService.getCSSFontName('Charis SIL');
-    fontService.getCSSFontName('Charis SIL');
+    fontService.getFontFamilyFromProject(spec('Charis SIL'));
+    fontService.getFontFamilyFromProject(spec('Charis SIL'));
+    fontService.getFontFamilyFromProject(spec('Charis SIL'));
 
     expect(((fontService as any).document as FakeDocument).addCount).toEqual(1);
+  });
+
+  it("should default to the specified writing system's default font", () => {
+    mockedConsole.expectAndHide(/No font definition for /);
+    const tagToExpectedFont = {
+      'aaa-Arab-NE': 'Harmattan',
+      // This should Awami Nastaliq, but non-Graphite browsers have to fall back to Scheherazade New
+      'aaa-Arab-PK': 'Scheherazade New',
+      'aaa-Arab': 'Scheherazade New',
+      'aaa-Deva-IN': 'Annapurna SIL'
+    };
+
+    for (const [tag, font] of Object.entries(tagToExpectedFont)) {
+      expect(fontService.getFontFamilyFromProject({ defaultFont: 'Unknown font', writingSystem: { tag } })).toEqual(
+        font
+      );
+    }
+    mockedConsole.verify();
+    mockedConsole.reset();
   });
 });
