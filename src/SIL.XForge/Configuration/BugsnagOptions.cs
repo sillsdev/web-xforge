0aa4a53ff67ac7d71d020c9320ff66e585ce3b50
Peter Chapman <peter@conglomo.co.nz>
2024-12-02 10:24:23 +1300

    Add test cases

--- src/SIL.XForge/Configuration/BugsnagOptions.cs
+++ src/SIL.XForge/Configuration/BugsnagOptions.cs
@@ -2,7 +2,7 @@ namespace SIL.XForge.Configuration;
 
 public class BugsnagOptions
 {
-    public string ApiKey { get; set; }
-    public string[] NotifyReleaseStages { get; set; }
-    public string ReleaseStage { get; set; }
+    public string ApiKey { get; init; } = string.Empty;
+    public string[] NotifyReleaseStages { get; init; } = [];
+    public string ReleaseStage { get; init; } = string.Empty;
 }
