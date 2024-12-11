bd5e4f6ec1640a2ab4a3983fccacd896e8b4a3cc
Peter Chapman <peter@conglomo.co.nz>
2024-12-12 09:59:39 +1300

    Refactor event metric saving

--- src/SIL.XForge/Services/EventMetricService.cs
+++ src/SIL.XForge/Services/EventMetricService.cs
@@ -1,5 +1,9 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using Newtonsoft.Json;
 using SIL.XForge.DataAccess;
 using SIL.XForge.EventMetrics;
 
@@ -7,6 +11,60 @@ namespace SIL.XForge.Services;
 
 public class EventMetricService(IRepository<EventMetric> eventMetrics) : IEventMetricService
 {
-    public IEnumerable<EventMetric> GetEventMetrics(string projectId, int pageIndex, int pageSize) =>
-        eventMetrics.Query().Skip(pageIndex * pageSize).Where(m => m.ProjectId == projectId).Take(pageSize);
+    public IEnumerable<EventMetric> GetEventMetrics(string? projectId, int pageIndex, int pageSize = 10)
+    {
+        if (projectId is null)
+        {
+            return [];
+        }
+
+        return eventMetrics.Query().Where(m => m.ProjectId == projectId).Skip(pageIndex * pageSize).Take(pageSize);
+    }
+
+    public async Task SaveEventMetricAsync(
+        string? projectId,
+        string? userId,
+        string eventType,
+        EventScope eventScope,
+        Dictionary<string, object> argumentsWithNames
+    )
+    {
+        // Process the arguments into a MongoDB format for the payload
+        var payload = new Dictionary<string, BsonValue>();
+        foreach (var kvp in argumentsWithNames)
+        {
+            payload[kvp.Key] = kvp.Value switch
+            {
+                int value => new BsonInt32(value),
+                long value => new BsonInt64(value),
+                bool value => new BsonBoolean(value),
+                Array array => new BsonArray(array),
+                double value => new BsonDouble(value),
+                float value => new BsonDouble(value),
+                string value => new BsonString(value),
+                decimal value => new BsonDecimal128(value),
+                DateTime value => new BsonDateTime(value),
+                null => BsonNull.Value,
+                _
+                    => BsonValue.Create(
+                        JsonConvert.DeserializeObject<Dictionary<string, object>>(
+                            JsonConvert.SerializeObject(kvp.Value)
+                        )
+                    ),
+            };
+        }
+
+        // Write the event metric
+        await eventMetrics.InsertAsync(
+            new EventMetric
+            {
+                Id = ObjectId.GenerateNewId().ToString(),
+                EventType = eventType,
+                Payload = payload,
+                ProjectId = projectId,
+                Scope = eventScope,
+                UserId = userId,
+            }
+        );
+    }
 }
