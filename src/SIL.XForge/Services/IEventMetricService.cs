8c20c543846a6a6a80a1cd9cac35993cb246aea3
Peter Chapman <peter@conglomo.co.nz>
2024-12-12 09:59:39 +1300

    Refactor event metric saving

--- src/SIL.XForge/Services/IEventMetricService.cs
+++ src/SIL.XForge/Services/IEventMetricService.cs
@@ -1,9 +1,17 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using SIL.XForge.EventMetrics;
 
 namespace SIL.XForge.Services;
 
 public interface IEventMetricService
 {
-    IEnumerable<EventMetric> GetEventMetrics(string projectId, int pageIndex, int pageSize);
+    IEnumerable<EventMetric> GetEventMetrics(string? projectId, int pageIndex, int pageSize = 10);
+    Task SaveEventMetricAsync(
+        string? projectId,
+        string? userId,
+        string eventType,
+        EventScope eventScope,
+        Dictionary<string, object> argumentsWithNames
+    );
 }
