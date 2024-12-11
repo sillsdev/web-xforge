4b808b8c66424dff14b345360bb78d1d34986581
Peter Chapman <peter@conglomo.co.nz>
2024-12-11 13:56:27 +1300

    Add unit tests

--- test/SIL.XForge.Scripture.Tests/StartupTests.cs
+++ test/SIL.XForge.Scripture.Tests/StartupTests.cs
@@ -1,6 +1,9 @@
+using System;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
@@ -11,93 +14,124 @@ namespace SIL.XForge.Scripture;
 [TestFixture]
 public class StartupTests
 {
-    private IConfiguration _configuration;
-    private IWebHostEnvironment _env;
-    private ILoggerFactory _loggerFactory;
-    private Startup _startup;
-    private HttpContext _context;
-
-    [SetUp]
-    public void Init()
+    [Test]
+    public void ConfigureServices_Success()
     {
-        _configuration = Substitute.For<IConfiguration>();
-        _env = Substitute.For<IWebHostEnvironment>();
-        _loggerFactory = Substitute.For<ILoggerFactory>();
-        _startup = new Startup(_configuration, _env, _loggerFactory);
-        Assert.NotNull(_startup, "Setup");
-        _context = Substitute.For<HttpContext>();
-        _context.Request.Method = HttpMethods.Get;
+        var env = new TestEnvironment();
+
+        // SUT
+        IServiceProvider actual = env.Startup.ConfigureServices(env.Services);
+        Assert.IsInstanceOf<AutofacServiceProvider>(actual);
     }
 
     [Test]
     public void IsSpaRoute_MethodNotGet_NotRoute()
     {
-        _context.Request.Method = HttpMethods.Post;
-        _context.Request.Path = new PathString("/system-administration");
+        var env = new TestEnvironment();
+        env.Context.Request.Method = HttpMethods.Post;
+        env.Context.Request.Path = new PathString("/system-administration");
 
-        Assert.IsFalse(_startup.IsSpaRoute(_context));
+        // SUT
+        bool actual = env.Startup.IsSpaRoute(env.Context);
+        Assert.IsFalse(actual);
     }
 
     [Test]
     public void IsSpaRoute_RootPath_NotRoute()
     {
-        _context.Request.Path = new PathString("/");
+        var env = new TestEnvironment();
+        env.Context.Request.Path = new PathString("/");
 
-        Assert.IsFalse(_startup.IsSpaRoute(_context));
+        // SUT
+        bool actual = env.Startup.IsSpaRoute(env.Context);
+        Assert.IsFalse(actual);
     }
 
     [Test]
     public void IsSpaRoute_InvalidPath_NotRoute()
     {
-        _context.Request.Path = new PathString("/invalid-path");
+        var env = new TestEnvironment();
+        env.Context.Request.Path = new PathString("/invalid-path");
 
-        Assert.IsFalse(_startup.IsSpaRoute(_context));
+        // SUT
+        bool actual = env.Startup.IsSpaRoute(env.Context);
+        Assert.IsFalse(actual);
     }
 
     [Test]
     public void IsSpaRoute_ValidPath_IsRoute()
     {
-        _context.Request.Path = new PathString("/system-administration");
+        var env = new TestEnvironment();
+        env.Context.Request.Path = new PathString("/system-administration");
 
-        Assert.IsTrue(_startup.IsSpaRoute(_context));
+        // SUT
+        bool actual = env.Startup.IsSpaRoute(env.Context);
+        Assert.IsTrue(actual);
     }
 
     [Test]
     public void IsSpaRoute_ValidMultiPath_IsRoute()
     {
-        _context.Request.Path = new PathString("/system-administration/more");
+        var env = new TestEnvironment();
+        env.Context.Request.Path = new PathString("/system-administration/more");
 
-        Assert.IsTrue(_startup.IsSpaRoute(_context));
+        // SUT
+        bool actual = env.Startup.IsSpaRoute(env.Context);
+        Assert.IsTrue(actual);
     }
 
     [Test]
     public void IsSpaRoute_DevelopmentPath_IsRoute()
     {
-        _env.EnvironmentName = Environments.Development;
-        _startup = new Startup(_configuration, _env, _loggerFactory);
-        _context.Request.Path = new PathString("/sockjs-node");
+        var env = new TestEnvironment(Environments.Development);
+        env.Context.Request.Path = new PathString("/sockjs-node");
 
-        Assert.IsTrue(_startup.IsSpaRoute(_context));
+        // SUT
+        bool actual = env.Startup.IsSpaRoute(env.Context);
+        Assert.IsTrue(actual);
     }
 
     [Test]
     public void IsSpaRoute_DevelopmentPathValidPost_IsRoute()
     {
-        _env.EnvironmentName = Environments.Development;
-        _startup = new Startup(_configuration, _env, _loggerFactory);
-        _context.Request.Method = HttpMethods.Post;
-        _context.Request.Path = new PathString("/sockjs-node");
+        var env = new TestEnvironment(Environments.Development);
+        env.Context.Request.Method = HttpMethods.Post;
+        env.Context.Request.Path = new PathString("/sockjs-node");
 
-        Assert.IsTrue(_startup.IsSpaRoute(_context));
+        // SUT
+        bool actual = env.Startup.IsSpaRoute(env.Context);
+        Assert.IsTrue(actual);
     }
 
     [Test]
     public void IsSpaRoute_ProductionPath_IsRoute()
     {
-        _env.EnvironmentName = Environments.Production;
-        _startup = new Startup(_configuration, _env, _loggerFactory);
-        _context.Request.Path = new PathString("/styles.a2f070be0b37085d72ba.css");
+        var env = new TestEnvironment(Environments.Production);
+        env.Context.Request.Path = new PathString("/styles.a2f070be0b37085d72ba.css");
 
-        Assert.IsTrue(_startup.IsSpaRoute(_context));
+        // SUT
+        bool actual = env.Startup.IsSpaRoute(env.Context);
+        Assert.IsTrue(actual);
+    }
+
+    private class TestEnvironment
+    {
+        public TestEnvironment(string? environmentName = null)
+        {
+            Context.Request.Method = HttpMethods.Get;
+            var configuration = Substitute.For<IConfiguration>();
+            var loggerFactory = Substitute.For<ILoggerFactory>();
+            var environment = Substitute.For<IWebHostEnvironment>();
+            if (!string.IsNullOrWhiteSpace(environmentName))
+            {
+                environment.EnvironmentName = environmentName;
+            }
+
+            Startup = new Startup(configuration, environment, loggerFactory);
+        }
+
+        public HttpContext Context { get; } = Substitute.For<HttpContext>();
+        public IServiceCollection Services { get; } = new ServiceCollection();
+        public Startup Startup { get; }
     }
 }
