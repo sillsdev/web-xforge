15588d7b4bbab426ff3fbcc91d18701db8080254
Peter Chapman <peter@conglomo.co.nz>
2024-12-17 08:16:15 +1300

    WIP: Event Metrics Log Frontend

--- src/SIL.XForge.Scripture/ClientApp/src/app/event-metrics/event-metrics.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/event-metrics/event-metrics.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { EventMetricsLogComponent } from './event-metrics-log.component';
 
 @Component({
   selector: 'app-event-metrics',
   templateUrl: './event-metrics.component.html',
   styleUrls: ['./event-metrics.component.scss'],
   standalone: true,
-  imports: []
+  imports: [EventMetricsLogComponent]
 })
 export class EventMetricsComponent {}
