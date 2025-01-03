95668daff2f934e66c8f1eb1ec3cb0aae63e6ebd
Raymond Luong <raymond_luong@sil.org>
2025-01-03 12:07:07 -0700

    Add python 3 versions of PT python scripts

--- src/SIL.XForge.Scripture/ParatextMerge.py
+++ src/SIL.XForge.Scripture/ParatextMerge.py
@@ -1,6 +1,5 @@
-import os, uuid
+import sys, os, uuid
 from shutil import copyfile
-
 
 def merge(ui, repo, args, **kwargs):
     global fullNameMine
@@ -12,10 +11,10 @@ def merge(ui, repo, args, **kwargs):
     global mergeToken
     if len(kwargs) != 1:
         return True
-    hgRelativeDirName = ".hg"
-    mergeRelativeDir = "merge"
-    mergeListFile = "Merges.txt"
-    mergeToken = "#Merge"
+    hgRelativeDirName = b'.hg'
+    mergeRelativeDir = b'merge'
+    mergeListFile = b'Merges.txt'
+    mergeToken = b'#Merge'
     fullNameMine = os.path.abspath(args[0])
     fullNameParent = os.path.abspath(args[1])
     fullNameTheirs = os.path.abspath(args[2])
@@ -29,55 +28,37 @@ def FindRepositoryRoot(ui):
     global mergeDir
     global repositoryDir
     d = os.path.dirname(fullNameMine)
-    while d and d != "/":
+    root = os.path.abspath(os.sep.encode())
+    while d and d != root:
         if os.path.isdir(os.path.join(d, hgRelativeDirName)):
             repositoryDir = d
             mergeDir = os.path.join(repositoryDir, mergeRelativeDir)
             return True
-        d = os.path.abspath(os.path.join(d, os.pardir))
+        d = os.path.abspath(os.path.join(d, os.pardir.encode()))
 
-    ui.warn("File " + fullNameMine + " is not in a repository. No .hg found.")
+    ui.warn(b'File ' + fullNameMine + b' is not in a repository. No .hg found.')
     return False
 
 
 def PrintInfo(ui):
-    ui.write(
-        "Mine : "
-        + fullNameMine
-        + "\nParent: "
-        + fullNameParent
-        + "\nTheirs:"
-        + fullNameTheirs
-        + "\nRoot : "
-        + repositoryDir
-        + "\nMerge: "
-        + mergeDir
-    )
+    ui.write(b'Mine : ' + fullNameMine + b'\nParent: ' + fullNameParent + b'\nTheirs:' + fullNameTheirs + b'\nRoot : ' + repositoryDir + b'\nMerge: ' + mergeDir)
 
 
 def Run(ui):
     if not os.path.isdir(mergeDir):
         os.makedirs(mergeDir)
-    relativePathMine = fullNameMine[len(repositoryDir) + 1 :]
-    mergePathParent = os.path.join(mergeRelativeDir, str(uuid.uuid4()))
-    mergePathTheirs = os.path.join(mergeRelativeDir, str(uuid.uuid4()))
-    ui.debug(
-        "Relative File Names\nMine :"
-        + relativePathMine
-        + "\nParent: "
-        + mergePathParent
-        + "\nTheirs: "
-        + mergePathTheirs
-        + "\n"
-    )
-    ui.debug("Moving parent.\n")
+    relativePathMine = fullNameMine[len(repositoryDir) + 1:]
+    mergePathParent = os.path.join(mergeRelativeDir, str(uuid.uuid4()).encode())
+    mergePathTheirs = os.path.join(mergeRelativeDir, str(uuid.uuid4()).encode())
+    ui.debug(b'Relative File Names\nMine :' + relativePathMine + b'\nParent: ' + mergePathParent + b'\nTheirs: ' + mergePathTheirs + b'\n')
+    ui.debug(b'Moving parent.\n')
     copyfile(fullNameParent, os.path.join(repositoryDir, mergePathParent))
-    ui.debug("Moving Theirs.\n")
+    ui.debug(b'Moving Theirs.\n')
     copyfile(fullNameTheirs, os.path.join(repositoryDir, mergePathTheirs))
-    ui.debug("Done moving.\n")
-    with open(os.path.join(mergeDir, mergeListFile), "a", 0) as (text_file):
-        text_file.write(mergeToken + "\n")
-        text_file.write(relativePathMine + "\n")
-        text_file.write(mergePathParent + "\n")
-        text_file.write(mergePathTheirs + "\n")
+    ui.debug(b'Done moving.\n')
+    with open(os.path.join(mergeDir, mergeListFile), 'ab', 0) as (text_file):
+        text_file.write(mergeToken + b'\n')
+        text_file.write(relativePathMine + b'\n')
+        text_file.write(mergePathParent + b'\n')
+        text_file.write(mergePathTheirs + b'\n')
         os.fsync(text_file)
