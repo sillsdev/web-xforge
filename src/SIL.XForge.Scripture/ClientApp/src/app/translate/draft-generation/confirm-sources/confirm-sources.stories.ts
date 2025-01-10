9c1965b257c4c36bf3be7c033dd2118cbc85d557
Joseph Myers <wjosephmyers@gmail.com>
2025-01-10 13:21:48 +0700

    Fixed storybook file

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/confirm-sources/confirm-sources.stories.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/confirm-sources/confirm-sources.stories.ts
@@ -1,54 +1,51 @@
 import { CommonModule } from '@angular/common';
 import { TranslocoModule } from '@ngneat/transloco';
 import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
-import { createTestProjectProfile } from 'realtime-server/lib/esm/scriptureforge/models/sf-project-test-data';
+import { of } from 'rxjs';
 import { instance, mock, when } from 'ts-mockito';
-import { ActivatedProjectService } from 'xforge-common/activated-project.service';
-import { SFProjectProfileDoc } from '../../../core/models/sf-project-profile-doc';
+import { DraftSource, DraftSourcesAsArrays, DraftSourcesService } from '../draft-sources.service';
 import { ConfirmSourcesComponent } from './confirm-sources.component';
 
-const mockedActivatedProjectService = mock(ActivatedProjectService);
+const mockDraftService = mock(DraftSourcesService);
 
-when(mockedActivatedProjectService.projectDoc).thenReturn({
-  data: createTestProjectProfile({
-    name: 'Test Project',
-    translateConfig: {
-      source: {
-        projectRef: 'source-project',
-        shortName: 'SP',
-        name: 'THIS PROJECT SHOULD NOT BE SHOWN!!!!!****',
-        paratextId: 'source-project',
+when(mockDraftService.getDraftProjectSources()).thenReturn(
+  of({
+    draftingSources: [
+      {
+        projectRef: 'alternate-drafting-source',
+        shortName: 'ADS',
+        name: 'Alternate Drafting Source',
+        paratextId: 'alternate-drafting-source',
         writingSystem: { tag: 'es' }
-      },
-      draftConfig: {
-        alternateTrainingSourceEnabled: true,
-        alternateTrainingSource: {
-          projectRef: 'alternate-training-source',
-          shortName: 'ATS',
-          name: 'Alternate Training Source',
-          paratextId: 'alternate-training-source',
-          writingSystem: { tag: 'es' }
-        },
-        additionalTrainingSourceEnabled: true,
-        additionalTrainingSource: {
-          projectRef: 'additional-training-source',
-          shortName: 'ATS',
-          name: 'Additional Training Source',
-          paratextId: 'additional-training-source',
-          writingSystem: { tag: 'es' }
-        },
-        alternateSourceEnabled: true,
-        alternateSource: {
-          projectRef: 'alternate-drafting-source',
-          shortName: 'ADS',
-          name: 'Alternate Drafting Source',
-          paratextId: 'alternate-drafting-source',
-          writingSystem: { tag: 'es' }
-        }
-      }
-    }
-  })
-} as SFProjectProfileDoc);
+      } as DraftSource
+    ],
+    trainingSources: [
+      {
+        projectRef: 'alternate-training-source',
+        shortName: 'ATS',
+        name: 'Alternate Training Source',
+        paratextId: 'alternate-training-source',
+        writingSystem: { tag: 'es' }
+      } as DraftSource,
+      {
+        projectRef: 'additional-training-source',
+        shortName: 'ATS',
+        name: 'Additional Training Source',
+        paratextId: 'additional-training-source',
+        writingSystem: { tag: 'es' }
+      } as DraftSource
+    ],
+    trainingTargets: [
+      {
+        projectRef: 'test-proj',
+        shortName: 'TP',
+        name: 'Test Project',
+        paratextId: 'test-proj-id',
+        writingSystem: { tag: 'eng' }
+      } as DraftSource
+    ]
+  } as DraftSourcesAsArrays)
+);
 
 const meta: Meta = {
   title: 'Translate/ConfirmSources',
@@ -56,7 +53,7 @@ const meta: Meta = {
   decorators: [
     moduleMetadata({
       imports: [CommonModule, TranslocoModule],
-      providers: [{ provide: ActivatedProjectService, useValue: instance(mockedActivatedProjectService) }]
+      providers: [{ provide: DraftSourcesService, useValue: instance(mockDraftService) }]
     })
   ]
 };
