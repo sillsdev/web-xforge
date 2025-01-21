ab90e057cbf7b055ef054b7f74a4962495cd6967
Peter Chapman <peter@conglomo.co.nz>
2025-01-21 14:01:23 +1300

    SF-3133 Add finishing build state

--- src/SIL.XForge.Scripture/ClientApp/src/app/machine-api/build-states.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/machine-api/build-states.ts
@@ -2,6 +2,7 @@ export enum BuildStates {
   Queued = 'QUEUED',
   Pending = 'PENDING',
   Active = 'ACTIVE',
+  Finishing = 'FINISHING',
   Completed = 'COMPLETED',
   Faulted = 'FAULTED',
   Canceled = 'CANCELED'
