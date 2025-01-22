d2a04ef4a3cd6eb1c74b87185c86c474859f5097
Nathaniel Paulus <nathaniel_paulus@sil.org>
2025-01-22 16:46:14 -0500

    Add script to check if all external links are valid

--- src/SIL.XForge.Scripture/ClientApp/src/xforge-common/external-url.service.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/external-url.service.ts
@@ -1,52 +1,14 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
+import { ExternalUrls } from './external-url-class';
 import { I18nService } from './i18n.service';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class ExternalUrlService {
-  paratext = 'https://paratext.org/';
-  transcelerator = 'https://software.sil.org/transcelerator/';
-  communitySupport = 'https://community.scripture.software.sil.org/c/scripture-forge/19';
-  announcementPage = 'https://software.sil.org/scriptureforge/news/';
-
-  constructor(private readonly i18n: I18nService) {}
-
-  get helps(): string {
-    const localeUrlPortion = this.i18n.locale.helps || I18nService.defaultLocale.helps!;
-    return localeUrlPortion === '' ? environment.helps : `${environment.helps}/${localeUrlPortion}`;
-  }
-
-  get manual(): string {
-    return this.helps + '/manual';
-  }
-
-  get autoDrafts(): string {
-    return this.helps + '/understanding-drafts';
-  }
-
-  get rolesHelpPage(): string {
-    return this.manual + '/#t=concepts%2Froles.htm';
-  }
-
-  get transceleratorImportHelpPage(): string {
-    return this.helps + '/adding-questions#1850d745ac9e8003815fc894b8baaeb7';
-  }
-
-  get csvImportHelpPage(): string {
-    return this.helps + '/adding-questions#1850d745ac9e8085960dd88b648f0c7a';
-  }
-
-  get chapterAudioHelpPage(): string {
-    return this.helps + '/adding-questions#1850d745ac9e80e795f3d611356e74d5';
-  }
-
-  get sharingSettingsHelpPage(): string {
-    return this.helps + '/managing-checkers#1850d745ac9e8097ad4efcb063fc2603';
-  }
-
-  get graphite(): string {
-    return 'https://graphite.sil.org/';
+/**
+ * This service class extends the ExternalUrls class to make it injectable.
+ */
+@Injectable({ providedIn: 'root' })
+export class ExternalUrlService extends ExternalUrls {
+  constructor(i18n: I18nService) {
+    super(i18n, { helpUrl: environment.helps, defaultLocaleHelpString: I18nService.defaultLocale.helps! });
   }
 }
