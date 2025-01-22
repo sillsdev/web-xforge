d5660448f4237faa11008f8c5085c59c156707ef
siltomato <colin_moore@sil.org>
2025-01-22 14:46:29 -0500

    SF-3172 Improve editor xml tag stripping

--- src/SIL.XForge.Scripture/ClientApp/src/xforge-common/util/string-util.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/util/string-util.ts
@@ -14,3 +14,26 @@ export function areStringArraysEqual(a: string[], b: string[]): boolean {
 
   return true;
 }
+
+/**
+ * Removes html tags from a string while preserving the text content.  Stray angle brackets are also removed.
+ * @param content The string with possible HTML tags to process.
+ * @returns The string with all tags and angle brackets removed.
+ */
+export function stripHtml(content: string): string {
+  if (content == null) {
+    return '';
+  }
+
+  // Skip processing if no angle brackets
+  if (!/[<>]/.test(content)) {
+    return content;
+  }
+
+  // Use 'text/html' to avoid parsing errors with 'text/xml', as it is more lenient
+  const doc: Document = new DOMParser().parseFromString(content, 'text/html');
+  let result: string = doc.documentElement.textContent || '';
+
+  // Remove any remaining stray angle brackets
+  return result.replace(/[<>]/g, '');
+}
