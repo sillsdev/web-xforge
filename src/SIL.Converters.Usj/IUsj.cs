832b6a08775ff6bcd5b82d860a0b7169bb4fc1a8
Peter Chapman <peter@conglomo.co.nz>
2025-01-28 15:56:19 +1300

    SF-3184 Add USJ conversion support to and from USX

---
+++ src/SIL.Converters.Usj/IUsj.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+
+namespace SIL.Converters.Usj
+{
+    /// <summary>
+    /// An interface for Unified Scripture JSON (USJ) types.
+    /// </summary>
+    public interface IUsj
+    {
+        /// <summary>
+        /// The JSON representation of scripture contents from USFM/USX.
+        /// </summary>
+        /// <value>This will either be a <see cref="UsjMarker"/> or <see cref="string"/>.</value>
+        /// <remarks>Nullable. The contents will be laid out in order.</remarks>
+        ArrayList Content { get; set; }
+
+        /// <summary>
+        /// The USJ spec type.
+        /// </summary>
+        string Type { get; set; }
+
+        /// <summary>
+        /// The USJ spec version.
+        /// </summary>
+        string Version { get; set; }
+    }
+}
