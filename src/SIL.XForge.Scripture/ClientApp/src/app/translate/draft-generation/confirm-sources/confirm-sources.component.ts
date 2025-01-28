3ae70186329c6a761f0e6cb0f62e39dbad1d92ba
Raymond Luong <raymond_luong@sil.org>
2025-01-28 16:01:22 -0700

    SF-3162 Warn user when training and drafting sources are different

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/confirm-sources/confirm-sources.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/confirm-sources/confirm-sources.component.ts
@@ -1,18 +1,18 @@
 import { Component, DestroyRef, EventEmitter, OnInit, Output } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { MatCheckboxChange, MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatIconModule } from '@angular/material/icon';
 import { TranslocoModule } from '@ngneat/transloco';
 import { SFProjectProfile } from 'realtime-server/lib/esm/scriptureforge/models/sf-project';
 import { TranslateSource } from 'realtime-server/lib/esm/scriptureforge/models/translate-config';
 import { I18nService } from 'xforge-common/i18n.service';
-import { NoticeComponent } from '../../../shared/notice/notice.component';
 import { DraftSourcesService } from '../draft-sources.service';
+import { LanguageCodesConfirmationComponent } from '../language-codes-confirmation/language-codes-confirmation.component';
 
 @Component({
   selector: 'app-confirm-sources',
   standalone: true,
-  imports: [TranslocoModule, NoticeComponent, MatCheckboxModule, MatIconModule],
+  imports: [TranslocoModule, MatCheckboxModule, MatIconModule, LanguageCodesConfirmationComponent],
   templateUrl: './confirm-sources.component.html',
   styleUrl: './confirm-sources.component.scss'
 })
@@ -29,27 +29,6 @@ export class ConfirmSourcesComponent implements OnInit {
     private readonly draftSourcesService: DraftSourcesService
   ) {}
 
-  ngOnInit(): void {
-    this.draftSourcesService
-      .getDraftProjectSources()
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe(async ({ trainingTargets, trainingSources, draftingSources }) => {
-        this.trainingSources = trainingSources.filter(s => s !== undefined);
-        this.trainingTargets = trainingTargets.filter(t => t !== undefined);
-        this.draftingSources = draftingSources.filter(s => s !== undefined);
-      });
-  }
-
-  confirmationChanged(change: MatCheckboxChange): void {
-    this.languageCodesVerified.emit(change.checked);
-  }
-
-  displayNameForProjectsLanguages(projects: (TranslateSource | SFProjectProfile | undefined)[]): string {
-    const uniqueTags = Array.from(new Set(projects.filter(p => p != null).map(p => p.writingSystem.tag)));
-    const displayNames = uniqueTags.map(tag => this.i18nService.getLanguageDisplayName(tag) ?? tag);
-    return this.i18nService.enumerateList(displayNames);
-  }
-
   get referenceLanguage(): string {
     return this.displayNameForProjectsLanguages(this.trainingSources);
   }
@@ -64,5 +43,26 @@ export class ConfirmSourcesComponent implements OnInit {
 
   get draftingSourceShortNames(): string {
     return this.i18nService.enumerateList(this.draftingSources.filter(p => p != null).map(p => p.shortName));
+  }
+
+  ngOnInit(): void {
+    this.draftSourcesService
+      .getDraftProjectSources()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(async ({ trainingTargets, trainingSources, draftingSources }) => {
+        this.trainingSources = trainingSources.filter(s => s !== undefined);
+        this.trainingTargets = trainingTargets.filter(t => t !== undefined);
+        this.draftingSources = draftingSources.filter(s => s !== undefined);
+      });
+  }
+
+  confirmationChanged(checked: boolean): void {
+    this.languageCodesVerified.emit(checked);
+  }
+
+  displayNameForProjectsLanguages(projects: (TranslateSource | SFProjectProfile | undefined)[]): string {
+    const uniqueTags = Array.from(new Set(projects.filter(p => p != null).map(p => p.writingSystem.tag)));
+    const displayNames = uniqueTags.map(tag => this.i18nService.getLanguageDisplayName(tag) ?? tag);
+    return this.i18nService.enumerateList(displayNames);
   }
 }
