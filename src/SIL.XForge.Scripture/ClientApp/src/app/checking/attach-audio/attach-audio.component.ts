eec2e0baa4af1a21b64168457d31e6ba2121d156
Joseph Myers <wjosephmyers@gmail.com>
2025-01-29 13:40:10 +0700

    Added Upload Audio to Questions
    A good portion of this was restored from its last working state (last year), before we removed it, and updated. The file processing comes solely from the existence of the uploadAudioFile property. I also restored the old Firefox .ogg file workaround, though I didn't test that it's still required (yet).
    
    The upload button must be enabled to be visible. This is controlled by a boolean Input on the control, and it's false for its usage within the answers.
    
    Also accounting for the 'uploaded' state in text-and-audio, since beforehand it was only looking for the recorded 'processed' state.

--- src/SIL.XForge.Scripture/ClientApp/src/app/checking/attach-audio/attach-audio.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/checking/attach-audio/attach-audio.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, ViewChild } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { InvalidFileItem } from 'angular-file/file-upload/fileTools';
 import { firstValueFrom } from 'rxjs';
 import { DialogService } from 'xforge-common/dialog.service';
 import {
@@ -7,6 +8,7 @@ import {
   AudioRecorderDialogData,
   AudioRecorderDialogResult
 } from '../../shared/audio-recorder-dialog/audio-recorder-dialog.component';
+import { AudioAttachment } from '../checking/checking-audio-player/checking-audio-player.component';
 import { SingleButtonAudioPlayerComponent } from '../checking/single-button-audio-player/single-button-audio-player.component';
 import { TextAndAudioComponent } from '../text-and-audio/text-and-audio.component';
 
@@ -18,6 +20,9 @@ import { TextAndAudioComponent } from '../text-and-audio/text-and-audio.componen
 export class AttachAudioComponent {
   @ViewChild(SingleButtonAudioPlayerComponent) audioPlayer?: SingleButtonAudioPlayerComponent;
   @Input() textAndAudio?: TextAndAudioComponent;
+  @Input() isUploadEnabled: boolean = false;
+
+  protected uploadAudioFile: File = {} as File;
 
   constructor(private readonly dialogService: DialogService) {}
 
@@ -46,5 +51,30 @@ export class AttachAudioComponent {
 
   toggleAudio(): void {
     this.audioPlayer?.playing ? this.audioPlayer?.stop() : this.audioPlayer?.play();
+  }
+
+  protected set lastInvalids(value: InvalidFileItem[]) {
+    if (value == null) {
+      return;
+    }
+    // Firefox does not recognize the valid .ogg file type because it reads it as a video, so handle it here
+    if (value.length > 0 && value[0].file.type === 'video/ogg') {
+      this.uploadAudioFile = value[0].file;
+      this.processAudioFileUpload();
+    }
+  }
+
+  protected processAudioFileUpload(): void {
+    if (this.uploadAudioFile.name != null) {
+      const audio: AudioAttachment = {};
+      audio.url = URL.createObjectURL(this.uploadAudioFile);
+      audio.blob = this.uploadAudioFile;
+      audio.fileName = this.uploadAudioFile.name;
+      audio.status = 'uploaded';
+
+      if (this.textAndAudio != null) {
+        this.textAndAudio.setAudioAttachment(audio);
+      }
+    }
   }
 }
