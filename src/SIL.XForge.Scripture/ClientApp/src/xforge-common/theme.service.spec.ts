20fddc9df767a6a598f9833df2a63e14e4a65d11
Nigel Wells <nigel-wells@hotmail.com>
2025-02-03 17:09:17 +1300

    Implement tests

--- src/SIL.XForge.Scripture/ClientApp/src/xforge-common/theme.service.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/theme.service.spec.ts
@@ -1,16 +1,77 @@
 import { TestBed } from '@angular/core/testing';
+import { anyString, mock, when } from 'ts-mockito';
+import { LocalSettingsService } from './local-settings.service';
+import { configureTestingModule } from './test-utils';
+import { DOCUMENT } from './browser-globals';
+import { Theme, ThemeService } from './theme.service';
 
-import { ThemeService } from './theme.service';
+const mockedLocalSettingsService = mock(LocalSettingsService);
+
+class MockHTMLBodyElement {
+  private classSet = new Set<string>();
+
+  get className(): string {
+    return Array.from(this.classSet).join(' ');
+  }
+
+  classList = {
+    add: (className: string) => {
+      this.classSet.add(className);
+    },
+    remove: (className: string) => {
+      this.classSet.delete(className);
+    },
+    reset: () => {
+      this.classSet.clear();
+    }
+  };
+}
 
 describe('ThemeService', () => {
-  let service: ThemeService;
+  configureTestingModule(() => ({
+    providers: [
+      { provide: LocalSettingsService, useMock: mockedLocalSettingsService },
+      { provide: DOCUMENT, useValue: { body: new MockHTMLBodyElement() } }
+    ]
+  }));
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ThemeService);
+    (TestBed.inject(DOCUMENT) as any).body.classList.reset();
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('default theme should be light', () => {
+    const env = new TestEnvironment();
+    expect(env.theme).toEqual(Theme.Light);
+    expect(env.bodyClass).toEqual(Theme.Light);
+  });
+
+  it('can change theme to dark', () => {
+    const env = new TestEnvironment();
+    expect(env.theme).toEqual(Theme.Light);
+    env.service.setDarkMode(true);
+    expect(env.theme).toEqual(Theme.Dark);
+    expect(env.bodyClass).toEqual(Theme.Dark);
+  });
+
+  it('sets correct theme based on local storage', () => {
+    when(mockedLocalSettingsService.get(anyString())).thenReturn(Theme.Dark);
+    const env = new TestEnvironment();
+    expect(env.theme).toEqual(Theme.Dark);
+    expect(env.bodyClass).toEqual(Theme.Dark);
   });
 });
+
+class TestEnvironment {
+  readonly service: ThemeService;
+
+  constructor() {
+    this.service = TestBed.inject(ThemeService);
+  }
+  get theme(): string {
+    return this.service.theme;
+  }
+
+  get bodyClass(): string {
+    return (TestBed.inject(DOCUMENT) as any).body.className;
+  }
+}
