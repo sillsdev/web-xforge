c33c308b79dd3bc851f9a8c0e9560464f6cd0dde
Raymond Luong <raymond_luong@sil.org>
2025-02-10 15:11:45 -0700

    Fix storybook

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/confirm-sources/confirm-sources.stories.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/confirm-sources/confirm-sources.stories.ts
@@ -1,13 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { TranslocoModule } from '@ngneat/transloco';
 import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+import { SFProjectRole } from 'realtime-server/lib/esm/scriptureforge/models/sf-project-role';
+import { createTestProjectProfile } from 'realtime-server/lib/esm/scriptureforge/models/sf-project-test-data';
 import { of } from 'rxjs';
 import { instance, mock, when } from 'ts-mockito';
+import { ActivatedProjectService } from 'xforge-common/activated-project.service';
+import { AuthService } from 'xforge-common/auth.service';
+import { SFProjectProfileDoc } from '../../../core/models/sf-project-profile-doc';
 import { DraftSource, DraftSourcesAsArrays, DraftSourcesService } from '../draft-sources.service';
 import { LanguageCodesConfirmationComponent } from '../language-codes-confirmation/language-codes-confirmation.component';
 import { ConfirmSourcesComponent } from './confirm-sources.component';
 
 const mockDraftService = mock(DraftSourcesService);
+const mockActivatedProject = mock(ActivatedProjectService);
+const mockAuthService = mock(AuthService);
 
 when(mockDraftService.getDraftProjectSources()).thenReturn(
   of({
@@ -48,13 +55,24 @@ when(mockDraftService.getDraftProjectSources()).thenReturn(
   } as DraftSourcesAsArrays)
 );
 
+when(mockActivatedProject.projectId).thenReturn('test-proj');
+when(mockActivatedProject.projectDoc).thenReturn({
+  id: 'test-proj',
+  data: createTestProjectProfile({ userRoles: { user1: SFProjectRole.ParatextAdministrator } })
+} as SFProjectProfileDoc);
+when(mockAuthService.currentUserId).thenReturn('user1');
+
 const meta: Meta = {
   title: 'Translate/ConfirmSources',
   component: ConfirmSourcesComponent,
   decorators: [
     moduleMetadata({
       imports: [CommonModule, TranslocoModule, LanguageCodesConfirmationComponent],
-      providers: [{ provide: DraftSourcesService, useValue: instance(mockDraftService) }]
+      providers: [
+        { provide: DraftSourcesService, useValue: instance(mockDraftService) },
+        { provide: ActivatedProjectService, useValue: instance(mockActivatedProject) },
+        { provide: AuthService, useValue: instance(mockAuthService) }
+      ]
     })
   ]
 };
