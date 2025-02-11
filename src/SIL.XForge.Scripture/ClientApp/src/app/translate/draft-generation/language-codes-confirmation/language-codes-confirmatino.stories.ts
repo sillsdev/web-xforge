886d06383d9675e557f0bae483354e02dfb756bb
Raymond Luong <raymond_luong@sil.org>
2025-02-11 15:11:51 -0700

    Add storybook for language code component

---
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/language-codes-confirmation/language-codes-confirmatino.stories.ts
@@ -0,0 +1,108 @@
+import { CommonModule } from '@angular/common';
+import { TranslocoModule } from '@ngneat/transloco';
+import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+import { TranslocoMarkupComponent } from 'ngx-transloco-markup';
+import { SFProjectRole } from 'realtime-server/lib/esm/scriptureforge/models/sf-project-role';
+import { createTestProjectProfile } from 'realtime-server/lib/esm/scriptureforge/models/sf-project-test-data';
+import { of } from 'rxjs';
+import { instance, mock, when } from 'ts-mockito';
+import { ActivatedProjectService } from 'xforge-common/activated-project.service';
+import { AuthService } from 'xforge-common/auth.service';
+import { SFProjectProfileDoc } from '../../../core/models/sf-project-profile-doc';
+import { DraftSource, DraftSourcesAsArrays, DraftSourcesService } from '../draft-sources.service';
+import { LanguageCodesConfirmationComponent } from './language-codes-confirmation.component';
+
+const mockDraftService = mock(DraftSourcesService);
+const mockActivatedProject = mock(ActivatedProjectService);
+const mockAuthService = mock(AuthService);
+
+when(mockDraftService.getDraftProjectSources()).thenReturn(
+  of({
+    draftingSources: [
+      {
+        projectRef: 'alternate-drafting-source',
+        shortName: 'ADS',
+        name: 'Alternate Drafting Source',
+        paratextId: 'alternate-drafting-source',
+        writingSystem: { tag: 'es' }
+      } as DraftSource
+    ],
+    trainingSources: [
+      {
+        projectRef: 'alternate-training-source',
+        shortName: 'ATS',
+        name: 'Alternate Training Source',
+        paratextId: 'alternate-training-source',
+        writingSystem: { tag: 'es' }
+      } as DraftSource,
+      {
+        projectRef: 'additional-training-source',
+        shortName: 'ATS',
+        name: 'Additional Training Source',
+        paratextId: 'additional-training-source',
+        writingSystem: { tag: 'es' }
+      } as DraftSource
+    ],
+    trainingTargets: [
+      {
+        projectRef: 'test-proj',
+        shortName: 'TP',
+        name: 'Test Project',
+        paratextId: 'test-proj-id',
+        writingSystem: { tag: 'eng' }
+      } as DraftSource
+    ]
+  } as DraftSourcesAsArrays)
+);
+
+when(mockActivatedProject.projectId).thenReturn('test-proj');
+when(mockActivatedProject.projectDoc).thenReturn({
+  id: 'test-proj',
+  data: createTestProjectProfile({ userRoles: { user1: SFProjectRole.ParatextAdministrator } })
+} as SFProjectProfileDoc);
+when(mockAuthService.currentUserId).thenReturn('user1');
+
+const meta: Meta = {
+  title: 'Translate/LanguageCodesConfirmation',
+  component: LanguageCodesConfirmationComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [CommonModule, TranslocoModule, TranslocoMarkupComponent, LanguageCodesConfirmationComponent],
+      providers: [
+        { provide: DraftSourcesService, useValue: instance(mockDraftService) },
+        { provide: ActivatedProjectService, useValue: instance(mockActivatedProject) },
+        { provide: AuthService, useValue: instance(mockAuthService) }
+      ]
+    })
+  ]
+};
+
+export default meta;
+
+interface StoryState {}
+
+type Story = StoryObj<StoryState>;
+
+export const Default: Story = {
+  args: {}
+};
+
+export const SameSourceAndTargetCodes: Story = {
+  args: {
+    targetLanguageTag: 'es'
+  }
+};
+
+export const DifferentSourceCodes: Story = {
+  args: {
+    draftingSources: [
+      {
+        projectRef: 'alternate-drafting-source',
+        shortName: 'ADS',
+        name: 'Alternate Drafting Source',
+        paratextId: 'alternate-drafting-source',
+        writingSystem: { tag: 'cat' }
+      } as DraftSource
+    ]
+  }
+};
--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/language-codes-confirmation/language-codes-confirmation.component.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/language-codes-confirmation/language-codes-confirmation.component.spec.ts
@@ -2,31 +2,31 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TranslocoMarkupComponent } from 'ngx-transloco-markup';
 import { SFProjectRole } from 'realtime-server/lib/esm/scriptureforge/models/sf-project-role';
 import { createTestProjectProfile } from 'realtime-server/lib/esm/scriptureforge/models/sf-project-test-data';
+import { of } from 'rxjs';
 import { anything, mock, when } from 'ts-mockito';
+import { ActivatedProjectService } from 'xforge-common/activated-project.service';
 import { AuthService } from 'xforge-common/auth.service';
+import { I18nService } from 'xforge-common/i18n.service';
 import { configureTestingModule, TestTranslocoModule } from 'xforge-common/test-utils';
 import { UICommonModule } from 'xforge-common/ui-common.module';
-import { ActivatedProjectService } from '../../../../xforge-common/activated-project.service';
-import { I18nService } from '../../../../xforge-common/i18n.service';
 import { SFProjectProfileDoc } from '../../../core/models/sf-project-profile-doc';
 import { NoticeComponent } from '../../../shared/notice/notice.component';
-import { DraftSourcesAsArrays } from '../draft-sources.service';
+import { DraftSourcesAsArrays, DraftSourcesService } from '../draft-sources.service';
 import { LanguageCodesConfirmationComponent } from './language-codes-confirmation.component';
 
+const mockI18nService = mock(I18nService);
+const mockActivatedProject: ActivatedProjectService = mock(ActivatedProjectService);
+const mockAuthService = mock(AuthService);
+const mockDraftSourcesService = mock(DraftSourcesService);
+
 describe('LanguageCodesConfirmationComponent', () => {
-  let component: LanguageCodesConfirmationComponent;
-  let fixture: ComponentFixture<LanguageCodesConfirmationComponent>;
-
-  const mockI18nService = mock(I18nService);
-  const mockActivatedProject: ActivatedProjectService = mock(ActivatedProjectService);
-  const mockAuthService = mock(AuthService);
-
   configureTestingModule(() => ({
     imports: [TestTranslocoModule, UICommonModule, NoticeComponent, TranslocoMarkupComponent],
     providers: [
       { provide: I18nService, useMock: mockI18nService },
       { provide: ActivatedProjectService, useMock: mockActivatedProject },
-      { provide: AuthService, useMock: mockAuthService }
+      { provide: AuthService, useMock: mockAuthService },
+      { provide: DraftSourcesService, useMock: mockDraftSourcesService }
     ]
   }));
 
@@ -39,17 +39,12 @@ describe('LanguageCodesConfirmationComponent', () => {
       id: 'target',
       data: createTestProjectProfile({ userRoles: { user1: SFProjectRole.ParatextAdministrator } })
     } as SFProjectProfileDoc);
-
-    fixture = TestBed.createComponent(LanguageCodesConfirmationComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should show standard message', () => {
-    component.draftSources = getStandardDraftSources();
-    fixture.detectChanges();
-    expect(component.sourceSideLanguageCodes.length).toEqual(1);
-    expect(component.showSourceAndTargetLanguagesIdenticalWarning).toBe(false);
+    const env = new TestEnvironment({ draftSources: getStandardDraftSources() });
+    expect(env.component.sourceSideLanguageCodes.length).toEqual(1);
+    expect(env.component.showSourceAndTargetLanguagesIdenticalWarning).toBe(false);
   });
 
   it('shows standard message when language codes are equivalent language', () => {
@@ -57,18 +52,16 @@ describe('LanguageCodesConfirmationComponent', () => {
     // Both map to the Chinese language
     draftSources.draftingSources[0]!.writingSystem.tag = 'zh-CN';
     draftSources.trainingSources[0]!.writingSystem.tag = 'cmn-Hans';
-    component.draftSources = draftSources;
-    fixture.detectChanges();
-    expect(component.sourceSideLanguageCodes.length).toEqual(1);
+    const env = new TestEnvironment({ draftSources });
+    expect(env.component.sourceSideLanguageCodes.length).toEqual(1);
   });
 
   it('should show target and source language codes identical message', () => {
     const draftSources = getStandardDraftSources();
     draftSources.trainingTargets[0]!.writingSystem.tag = draftSources.trainingSources[0]!.writingSystem.tag;
-    component.draftSources = draftSources;
-    fixture.detectChanges();
-    expect(component.sourceSideLanguageCodes.length).toEqual(1);
-    expect(component.showSourceAndTargetLanguagesIdenticalWarning).toBe(true);
+    const env = new TestEnvironment({ draftSources });
+    expect(env.component.sourceSideLanguageCodes.length).toEqual(1);
+    expect(env.component.showSourceAndTargetLanguagesIdenticalWarning).toBe(true);
   });
 
   it('should show training source language codes different message', () => {
@@ -81,20 +74,31 @@ describe('LanguageCodesConfirmationComponent', () => {
       writingSystem: { tag: 'zh' },
       texts: []
     });
-    component.draftSources = draftSources;
-    fixture.detectChanges();
-    expect(component.sourceSideLanguageCodes.length).toEqual(3);
-    expect(component.showSourceAndTargetLanguagesIdenticalWarning).toBe(false);
+    const env = new TestEnvironment({ draftSources });
+    // Chinese as the additional training source, and Spanish for the training and drafting source
+    expect(env.component.sourceSideLanguageCodes.length).toEqual(2);
+    expect(env.component.showSourceAndTargetLanguagesIdenticalWarning).toBe(false);
   });
 
   it('can emit languages confirmed when checkbox is checked', () => {
-    component.draftSources = getStandardDraftSources();
-    fixture.detectChanges();
-    const emitSpy = spyOn(component.languageCodesVerified, 'emit');
-    component.confirmationChanged({ checked: true } as any);
+    const env = new TestEnvironment({ draftSources: getStandardDraftSources() });
+    const emitSpy = spyOn(env.component.languageCodesVerified, 'emit');
+    env.component.confirmationChanged({ checked: true } as any);
     expect(emitSpy).toHaveBeenCalledWith(true);
   });
 });
+
+class TestEnvironment {
+  component: LanguageCodesConfirmationComponent;
+  fixture: ComponentFixture<LanguageCodesConfirmationComponent>;
+
+  constructor(args: { draftSources: DraftSourcesAsArrays }) {
+    when(mockDraftSourcesService.getDraftProjectSources()).thenReturn(of(args.draftSources));
+    this.fixture = TestBed.createComponent(LanguageCodesConfirmationComponent);
+    this.component = this.fixture.componentInstance;
+    this.fixture.detectChanges();
+  }
+}
 
 function getStandardDraftSources(): DraftSourcesAsArrays {
   return {
