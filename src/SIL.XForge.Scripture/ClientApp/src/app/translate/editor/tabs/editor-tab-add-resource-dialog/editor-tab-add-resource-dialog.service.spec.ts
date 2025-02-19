1fdfeab456015e214b97f24ae1f1afd8740305b3
josephmyers <43733878+josephmyers@users.noreply.github.com>
2025-02-20 00:28:58 +0700

    SF-3138 Update projects list dynamically in Add Resource dialog (#2998)

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/tabs/editor-tab-add-resource-dialog/editor-tab-add-resource-dialog.service.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/tabs/editor-tab-add-resource-dialog/editor-tab-add-resource-dialog.service.spec.ts
@@ -1,13 +1,17 @@
 import { TestBed } from '@angular/core/testing';
-import { mock, verify, when } from 'ts-mockito';
+import { BehaviorSubject } from 'rxjs';
+import { mock, resetCalls, verify, when } from 'ts-mockito';
 import { configureTestingModule } from 'xforge-common/test-utils';
+import { SFUserProjectsService } from 'xforge-common/user-projects.service';
 import { ParatextProject } from '../../../../core/models/paratext-project';
+import { SFProjectProfileDoc } from '../../../../core/models/sf-project-profile-doc';
 import { ParatextService, SelectableProject } from '../../../../core/paratext.service';
 import { EditorTabAddResourceDialogService } from './editor-tab-add-resource-dialog.service';
 
 describe('EditorTabAddResourceDialogService', () => {
   let service: EditorTabAddResourceDialogService;
   const mockParatextService = mock(ParatextService);
+  const mockProjectsService = mock(SFUserProjectsService);
   const mockProjects: ParatextProject[] = [
     { id: '1', name: 'Project 1' },
     { id: '2', name: 'Project 2' }
@@ -18,34 +22,59 @@ describe('EditorTabAddResourceDialogService', () => {
   ] as any;
 
   configureTestingModule(() => ({
-    providers: [{ provide: ParatextService, useMock: mockParatextService }]
+    providers: [
+      { provide: ParatextService, useMock: mockParatextService },
+      { provide: SFUserProjectsService, useMock: mockProjectsService }
+    ]
   }));
 
+  const projectDocs = new BehaviorSubject<SFProjectProfileDoc[]>([]);
+
   beforeEach(() => {
+    when(mockProjectsService.projectDocs$).thenReturn(projectDocs);
     service = TestBed.inject(EditorTabAddResourceDialogService);
   });
 
   it('should return cached projects if available', async () => {
-    service.projects = mockProjects;
+    when(mockParatextService.getProjects()).thenResolve(mockProjects);
     expect(await service.getProjects()).toEqual(mockProjects);
   });
 
-  it('should call getProjects if projects are not cached', async () => {
+  it('should return updated projects if they change', async () => {
     when(mockParatextService.getProjects()).thenResolve(mockProjects);
     expect(await service.getProjects()).toEqual(mockProjects);
-    expect(service.projects).toEqual(mockProjects);
+
+    const newProjects = [mockProjects[0]];
+    when(mockParatextService.getProjects()).thenResolve(newProjects);
+    expect(await service.getProjects()).not.toEqual(newProjects);
+
+    projectDocs.next([{} as SFProjectProfileDoc]);
+    await new Promise(resolve => setTimeout(resolve, 0)); // Wait for async subscription to execute
+    expect(await service.getProjects()).toEqual(newProjects);
+  });
+
+  it('should call getProjects if projects are not cached', async () => {
+    resetCalls(mockParatextService);
+
+    when(mockParatextService.getProjects()).thenResolve(mockProjects);
+    expect(await service.getProjects()).toEqual(mockProjects);
+    expect(await service.getProjects()).toEqual(mockProjects);
+
     verify(mockParatextService.getProjects()).once();
   });
 
   it('should return cached resources if available', async () => {
-    service.resources = mockResources;
+    when(mockParatextService.getResources()).thenResolve(mockResources);
     expect(await service.getResources()).toEqual(mockResources);
   });
 
   it('should call getResources if resources are not cached', async () => {
+    resetCalls(mockParatextService);
+
     when(mockParatextService.getResources()).thenResolve(mockResources);
     expect(await service.getResources()).toEqual(mockResources);
-    expect(service.resources).toEqual(mockResources);
+    expect(await service.getResources()).toEqual(mockResources);
+
     verify(mockParatextService.getResources()).once();
   });
 });
