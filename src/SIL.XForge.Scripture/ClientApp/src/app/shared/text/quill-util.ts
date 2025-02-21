01a2b672efbd3eef5c321981649366ba2e6b67a8
siltomato <colin_moore@sil.org>
2025-02-21 12:20:36 -0500

    Refactor quill-scripture.ts to allow testing

---
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/text/quill-util.ts
@@ -0,0 +1,45 @@
+import Quill from 'quill';
+import { DeltaOperation, StringMap } from 'rich-text';
+
+/**
+ * Gets the attributes at the given position in the editor.
+ * @param editor The Quill editor.
+ * @param editorPosition The position in the editor.
+ * @returns A map of attributes at the given position.
+ */
+export function getAttributesAtPosition(editor: Quill, editorPosition: number): StringMap {
+  // The format of the insertion point may only contain the block level formatting,
+  // the format classes and other information we get from the character following the insertion point
+  const insertionFormat: StringMap = editor.getFormat(editorPosition);
+  const characterFormat: StringMap = editor.getFormat(editorPosition, 1);
+
+  if (characterFormat['segment'] != null) {
+    for (const key of Object.keys(characterFormat)) {
+      // we ignore text anchor formatting because we cannot depend on the character format to tell us if it is needed
+      if (key !== 'text-anchor') {
+        insertionFormat[key] = characterFormat[key];
+      }
+    }
+  }
+
+  return insertionFormat;
+}
+
+/**
+ * Gets the retain count from the given Delta operation.
+ * @param op The delta op.
+ * @returns The retain count if the op is a 'retain' operation, otherwise undefined.
+ * @throws Error if the 'retain' operation is invalid.
+ */
+export function getRetainCount(op: DeltaOperation): number | undefined {
+  if (op?.retain != null) {
+    if (typeof op.retain === 'number') {
+      return op.retain;
+    }
+
+    // The type definition allows it, but we shouldn't encounter an object 'retain'
+    throw new Error(`Invalid 'retain' operation`);
+  }
+
+  return undefined;
+}
