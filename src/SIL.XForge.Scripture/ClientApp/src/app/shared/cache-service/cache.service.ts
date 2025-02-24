8d16679ce56007c2605c7aa672f381a28eef13fa
Joseph Myers <wjosephmyers@gmail.com>
2025-02-24 12:23:15 +0700

    Reimplemented cache service to use a managed query
    This is cleaner than managing the doc's manually,
    as it allows the realtime service to dispose of
    the query when appropriate.

--- src/SIL.XForge.Scripture/ClientApp/src/app/shared/cache-service/cache.service.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/cache-service/cache.service.ts
@@ -1,63 +1,45 @@
 import { DestroyRef, Injectable } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { TEXTS_COLLECTION } from 'realtime-server/lib/esm/scriptureforge/models/text-data';
 import { ActivatedProjectService } from '../../../xforge-common/activated-project.service';
-import { SFProjectProfileDoc } from '../../core/models/sf-project-profile-doc';
-import { TextDoc, TextDocId } from '../../core/models/text-doc';
-import { PermissionsService } from '../../core/permissions.service';
+import { RealtimeQuery } from '../../../xforge-common/models/realtime-query';
+import { RealtimeService } from '../../../xforge-common/realtime.service';
+import { filterNullish } from '../../../xforge-common/util/rxjs-util';
+import { TextDoc } from '../../core/models/text-doc';
 import { SFProjectService } from '../../core/sf-project.service';
 
 @Injectable({ providedIn: 'root' })
 export class CacheService {
-  private readonly subscribedTexts: TextDoc[] = [];
+  private query?: RealtimeQuery<TextDoc>;
+  private sourceQuery?: RealtimeQuery<TextDoc>;
 
   constructor(
     private readonly projectService: SFProjectService,
-    private readonly permissionsService: PermissionsService,
-    private readonly currentProject: ActivatedProjectService,
-    destroyRef: DestroyRef
+    private readonly realtimeService: RealtimeService,
+    private readonly destroyRef: DestroyRef,
+    currentProject: ActivatedProjectService
   ) {
-    currentProject.projectId$.pipe(takeUntilDestroyed(destroyRef)).subscribe(async projectId => {
-      if (projectId == null) return;
+    currentProject.projectId$.pipe(takeUntilDestroyed(destroyRef), filterNullish()).subscribe(async projectId => {
+      this.query?.dispose();
+      this.sourceQuery?.dispose();
 
-      this.uncache();
+      this.query = await this.cache(projectId);
+
       const project = await this.projectService.getProfile(projectId);
-      await this.cache(project);
+      const sourceId = project.data!.translateConfig.source?.projectRef;
+      if (sourceId != null) {
+        this.sourceQuery = await this.cache(sourceId);
+      }
     });
   }
 
-  private uncache(): void {
-    for (const t of this.subscribedTexts) {
-      t.adapter.destroy();
-    }
+  private async cache(projectId: string): Promise<RealtimeQuery<TextDoc>> {
+    const params = {
+      ['_id']: { $regex: `^${projectId}:.*` }
+    };
+    const query = await this.realtimeService.subscribeQuery<TextDoc>(TEXTS_COLLECTION, params, this.destroyRef);
+    query.subscribe();
 
-    this.subscribedTexts.length = 0;
-  }
-
-  private async cache(project: SFProjectProfileDoc): Promise<void> {
-    await this.loadAllChapters(project);
-  }
-
-  private async loadAllChapters(project: SFProjectProfileDoc): Promise<void> {
-    if (project?.data != null) {
-      const sourceId = project.data.translateConfig.source?.projectRef;
-
-      for (const text of project.data.texts) {
-        for (const chapter of text.chapters) {
-          if (this.currentProject.projectId != null && this.currentProject.projectId !== project.id) return;
-
-          const textDocId = new TextDocId(project.id, text.bookNum, chapter.number, 'target');
-          if (await this.permissionsService.canAccessText(textDocId)) {
-            this.subscribedTexts.push(await this.projectService.getText(textDocId));
-          }
-
-          if (text.hasSource && sourceId != null) {
-            const sourceTextDocId = new TextDocId(sourceId, text.bookNum, chapter.number, 'target');
-            if (await this.permissionsService.canAccessText(sourceTextDocId)) {
-              this.subscribedTexts.push(await this.projectService.getText(sourceTextDocId));
-            }
-          }
-        }
-      }
-    }
+    return query;
   }
 }
