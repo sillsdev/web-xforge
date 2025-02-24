51c78c8c818af6d72a820888f15563da17fa728e
siltomato <colin_moore@sil.org>
2025-02-24 17:22:05 -0500

    Replace multiple newlines with space on editor paste

--- src/SIL.XForge.Scripture/ClientApp/src/app/shared/text/quill-editor-registration/quill-clipboard.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/text/quill-editor-registration/quill-clipboard.ts
@@ -34,8 +34,8 @@ export class DisableHtmlClipboard extends QuillClipboard {
 
     const text = e.clipboardData.getData('text/plain');
     const cleanedText = text
-      .replace(/(?:\r?\n)+/, ' ') // Replace new lines with spaces
-      .replace(/\\/g, ''); // Remove backslashes
+      .replace(/\\/g, '') // Remove all backslashes first
+      .replace(/(?:\r?\n)+/g, ' '); // Replace all new lines with spaces
 
     const pasteDelta = this.convert({ text: cleanedText });
 
