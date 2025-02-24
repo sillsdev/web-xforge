2b11e668407c05713f9ddfa129e52936e4d43463
Peter Chapman <peter@conglomo.co.nz>
2025-02-25 08:49:34 +1300

    Fix dependabot alerts (#3036)
    rade Storybook to work with updated dependencies

--- src/SIL.XForge.Scripture/Services/MachineServiceCollectionExtensions.cs
+++ src/SIL.XForge.Scripture/Services/MachineServiceCollectionExtensions.cs
@@ -2,7 +2,7 @@ using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Net;
 using System.Net.Http;
-using IdentityModel.Client;
+using Duende.IdentityModel.Client;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
