6f28cdcecc8b694304e440875eb27547537fba77
Joseph Myers <wjosephmyers@gmail.com>
2025-02-24 12:23:15 +0700

    WIP

--- src/SIL.XForge.Scripture/ClientApp/src/app/shared/cache-service/cache.service.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/cache-service/cache.service.ts
@@ -1,6 +1,10 @@
 import { DestroyRef, Injectable } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { TEXTS_COLLECTION } from 'realtime-server/lib/esm/scriptureforge/models/text-data';
 import { ActivatedProjectService } from '../../../xforge-common/activated-project.service';
+import { RealtimeQuery } from '../../../xforge-common/models/realtime-query';
+import { RealtimeService } from '../../../xforge-common/realtime.service';
+import { filterNullish } from '../../../xforge-common/util/rxjs-util';
 import { SFProjectProfileDoc } from '../../core/models/sf-project-profile-doc';
 import { TextDoc, TextDocId } from '../../core/models/text-doc';
 import { PermissionsService } from '../../core/permissions.service';
@@ -9,17 +13,19 @@ import { SFProjectService } from '../../core/sf-project.service';
 @Injectable({ providedIn: 'root' })
 export class CacheService {
   private readonly subscribedTexts: TextDoc[] = [];
+  private query?: RealtimeQuery<TextDoc>;
 
   constructor(
     private readonly projectService: SFProjectService,
     private readonly permissionsService: PermissionsService,
     private readonly currentProject: ActivatedProjectService,
-    destroyRef: DestroyRef
+    private readonly realtimeService: RealtimeService,
+    private readonly destroyRef: DestroyRef
   ) {
-    currentProject.projectId$.pipe(takeUntilDestroyed(destroyRef)).subscribe(async projectId => {
-      if (projectId == null) return;
+    currentProject.projectId$.pipe(takeUntilDestroyed(destroyRef), filterNullish()).subscribe(async projectId => {
+      // if (projectId == null) return;
 
-      this.uncache();
+      // this.uncache();
       const project = await this.projectService.getProfile(projectId);
       await this.cache(project);
     });
@@ -28,13 +34,21 @@ export class CacheService {
   private uncache(): void {
     for (const t of this.subscribedTexts) {
       t.adapter.destroy();
+      t.dispose();
     }
 
     this.subscribedTexts.length = 0;
   }
 
   private async cache(project: SFProjectProfileDoc): Promise<void> {
-    await this.loadAllChapters(project);
+    // await this.loadAllChapters(project);
+    const params = {
+      // [obj<TextDoc>().pathStr(u => u.id)]: { $regex: `.*`, $options: 'i' }
+      // [obj<User>().pathStr(u => u.displayName)]: { $regex: `.*Joseph Myers.*`, $options: 'i' }
+    };
+    this.query = await this.realtimeService.subscribeQuery<TextDoc>(TEXTS_COLLECTION, params, this.destroyRef);
+    // this.query = await this.realtimeService.subscribeQuery<TextDoc>('users', params, this.destroyRef);
+    const projectDocs = this.query.docs.filter(td => td.id.includes(project.id));
   }
 
   private async loadAllChapters(project: SFProjectProfileDoc): Promise<void> {
