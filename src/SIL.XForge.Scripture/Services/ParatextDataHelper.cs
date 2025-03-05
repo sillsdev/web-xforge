4ab0e3ecff844d77b1192724bfb8c3f08c5161c0
Peter Chapman <peter@conglomo.co.nz>
2025-03-05 13:21:24 +1300

    Check for permission downgrade to observer before syncing

--- src/SIL.XForge.Scripture/Services/ParatextDataHelper.cs
+++ src/SIL.XForge.Scripture/Services/ParatextDataHelper.cs
@@ -1,3 +1,4 @@
+using System;
 using Paratext.Data;
 using Paratext.Data.Repository;
 
@@ -8,6 +9,14 @@ public class ParatextDataHelper : IParatextDataHelper
 {
     public void CommitVersionedText(ScrText scrText, string comment)
     {
+        // Commit() will fail silently if the user is an Observer,
+        // so throw an error if the user is an Observer.
+        if (!scrText.Permissions.HaveRoleNotObserver)
+        {
+            throw new InvalidOperationException("User does not have permission to commit.");
+        }
+
+        // Write the commit to the repository
         VersionedText vText = VersioningManager.Get(scrText);
         vText.Commit(comment, null, false);
     }
