539fda014430e5343e777adc24763a28e9ba5b29
Nathaniel Paulus <nathaniel_paulus@sil.org>
2025-03-12 11:33:12 -0400

    Add e2e tests

---
+++ src/SIL.XForge.Scripture/ClientApp/e2e/e2e-test-run-logger.ts
@@ -0,0 +1,29 @@
+import { preset, ScreenshotContext } from './e2e-globals.ts';
+
+interface ScreenshotEvent {
+  fileName: string;
+  context: ScreenshotContext;
+}
+
+export class E2ETestRunLogger {
+  private timeStarted = new Date();
+  private screenshotEvents: ScreenshotEvent[] = [];
+
+  async saveToFile(): Promise<void> {
+    const data = {
+      timeStarted: this.timeStarted,
+      timeEnded: new Date(),
+      preset,
+      screenshotEvents: this.screenshotEvents
+    };
+
+    const filePath = `${preset.outputDir}/run_log.json`;
+    console.log(`Saving run log to ${filePath}...`);
+    await Deno.mkdir(preset.outputDir, { recursive: true });
+    await Deno.writeTextFile(filePath, JSON.stringify(data, null, 2));
+  }
+
+  logScreenshot(fileName: string, context: ScreenshotContext): void {
+    this.screenshotEvents.push({ fileName, context });
+  }
+}
