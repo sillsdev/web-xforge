4f3399c68e9364d9db02b0dbcf15f391b57cc4f0
Peter Chapman <peter@conglomo.co.nz>
2025-03-12 13:34:28 +1300

    Add support for loading resources from USJ

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/platform-editor/render-react-directive.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/platform-editor/render-react-directive.component.ts
@@ -1,6 +1,16 @@
-import { Directive, ElementRef, inject, Input, OnChanges, OnDestroy } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  EventEmitter,
+  inject,
+  Input,
+  OnChanges,
+  OnDestroy,
+  Output,
+  SimpleChanges
+} from '@angular/core';
 import { ComponentProps, createElement, ElementType } from 'react';
-import { createRoot } from 'react-dom/client';
+import { createRoot, Root } from 'react-dom/client';
 
 @Directive({
   selector: '[appRenderReact]',
@@ -9,14 +19,43 @@ import { createRoot } from 'react-dom/client';
 export class RenderReactDirective<Comp extends ElementType> implements OnChanges, OnDestroy {
   @Input({ required: true }) appRenderReact!: Comp;
   @Input() props?: ComponentProps<Comp>;
+  @Output() rendered = new EventEmitter<void>();
 
-  private root = createRoot(inject(ElementRef).nativeElement);
+  private root?: Root;
+  private hostElement: HTMLElement;
+  private mutationObserver?: MutationObserver;
 
-  ngOnChanges(): void {
-    this.root.render(createElement(this.appRenderReact, this.props));
+  constructor() {
+    this.hostElement = inject(ElementRef).nativeElement;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.root == null) {
+      this.root = createRoot(this.hostElement);
+    }
+
+    if (changes['appRenderReact'] || changes['props']) {
+      if (this.appRenderReact) {
+        this.root.render(createElement(this.appRenderReact, this.props ?? {}));
+
+        // Start observing DOM mutations to detect React render completion
+        this.mutationObserver = new MutationObserver(() => {
+          this.rendered.emit();
+          this.mutationObserver?.disconnect();
+        });
+
+        // Observe changes in the child nodes and attributes within the rendered React component
+        this.mutationObserver.observe(this.hostElement, {
+          childList: true,
+          subtree: true,
+          attributes: true
+        });
+      }
+    }
   }
 
   ngOnDestroy(): void {
-    this.root.unmount();
+    this.root?.unmount();
+    this.mutationObserver?.disconnect();
   }
 }
