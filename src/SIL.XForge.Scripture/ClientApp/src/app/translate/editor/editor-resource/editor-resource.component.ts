36ea05009e3e54bff51f08d63aeaed6b8b67cfe3
kylebuss <kyle_buss@sil.org>
2025-03-13 10:43:16 -0500

    SF-3259 Fix resources not showing copyright notices

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/editor-resource/editor-resource.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/editor-resource/editor-resource.component.ts
@@ -1,5 +1,7 @@
 import { AfterViewInit, Component, DestroyRef, Input, OnChanges, ViewChild } from '@angular/core';
+import { translate } from '@ngneat/transloco';
 import { combineLatest, EMPTY, startWith, Subject, switchMap } from 'rxjs';
+import { DialogService } from 'xforge-common/dialog.service';
 import { FontService } from 'xforge-common/font.service';
 import { quietTakeUntilDestroyed } from 'xforge-common/util/rxjs-util';
 import { SFProjectProfileDoc } from '../../../core/models/sf-project-profile-doc';
@@ -8,7 +10,8 @@ import { TextComponent } from '../../../shared/text/text.component';
 import { formatFontSizeToRems } from '../../../shared/utils';
 @Component({
   selector: 'app-editor-resource',
-  templateUrl: './editor-resource.component.html'
+  templateUrl: './editor-resource.component.html',
+  styleUrl: '../editor.component.scss'
 })
 export class EditorResourceComponent implements AfterViewInit, OnChanges {
   @Input() projectId?: string;
@@ -22,13 +25,17 @@ export class EditorResourceComponent implements AfterViewInit, OnChanges {
   isRightToLeft = false;
   fontSize?: string;
   font?: string;
+  hasCopyrightBanner: boolean = false;
+  copyrightBanner?: string;
+  copyrightNotice?: string;
 
   inputChanged$ = new Subject<void>();
 
   constructor(
     private readonly destroyRef: DestroyRef,
     private readonly projectService: SFProjectService,
-    private readonly fontService: FontService
+    private readonly fontService: FontService,
+    private readonly dialogService: DialogService
   ) {}
 
   ngOnChanges(): void {
@@ -52,9 +59,20 @@ export class EditorResourceComponent implements AfterViewInit, OnChanges {
         })
       )
       .subscribe((projectDoc: SFProjectProfileDoc) => {
+        this.hasCopyrightBanner = projectDoc.data?.copyrightBanner != null;
+        this.copyrightBanner = projectDoc.data?.copyrightBanner ?? '';
+        this.copyrightNotice = projectDoc.data?.copyrightNotice;
         this.isRightToLeft = projectDoc.data?.isRightToLeft ?? false;
         this.fontSize = formatFontSizeToRems(projectDoc.data?.defaultFontSize);
         this.font = this.fontService.getFontFamilyFromProject(projectDoc);
       });
+  }
+
+  showCopyrightNotice(): void {
+    this.dialogService.openCopyrightNoticeDialog(this.copyrightNotice ?? this.copyrightBanner ?? '');
+  }
+
+  moreInfo(): string {
+    return translate('editor.more_info');
   }
 }
