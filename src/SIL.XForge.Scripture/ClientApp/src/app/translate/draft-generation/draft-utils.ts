23399e522c0a2af9850c261555769981176b581a
Nateowami <nathaniel_paulus@sil.org>
2025-03-21 14:15:48 -0400

    Remove unused locale code logic from draft utils (#3106)

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/draft-utils.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/draft-utils.ts
@@ -2,30 +2,6 @@ import { SFProjectProfile } from 'realtime-server/lib/esm/scriptureforge/models/
 import { TranslateSource } from 'realtime-server/lib/esm/scriptureforge/models/translate-config';
 import { SelectableProjectWithLanguageCode } from '../../core/paratext.service';
 
-export function englishNameFromCode(code: string): string {
-  const locale = new Intl.Locale(code);
-  return new Intl.DisplayNames(['en'], { type: 'language' }).of(locale.language) ?? '';
-}
-
-export function areLanguageCodesEquivalent(code1: string, code2: string): boolean {
-  return code1 === code2 || englishNameFromCode(code1) === englishNameFromCode(code2);
-}
-
-/**
- * Given a list of language codes, counts the number of codes that are "unique" in the sense that they have different
- * English names.
- *
- * This is a *very rough* way of determining whether languages are "equivalent" for the purposes of training a model.
- *
- * As an example, zh and cmn both map to "Chinese" in English, so they would be considered equivalent.
- *
- * TODO Create a more robust way of determining whether languages are equivalent, that isn't browser-dependent.
- */
-export function countNonEquivalentLanguageCodes(languageCodes: string[]): number {
-  const uniqueTags = new Set(languageCodes);
-  return new Set(Array.from(uniqueTags).map(englishNameFromCode)).size;
-}
-
 /** Represents draft sources as a set of two {@link TranslateSource} arrays, and one {@link SFProjectProfile} array. */
 export interface DraftSourcesAsTranslateSourceArrays {
   trainingSources: TranslateSource[];
