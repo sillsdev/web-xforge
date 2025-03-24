4252f5e1621ebfe8f7d6db07d837efd4c5497b41
Raymond Luong <raymond_luong@sil.org>
2025-03-24 16:41:29 -0600

    SF-3286 Fix sync failed when chapters missing

--- test/SIL.XForge.Scripture.Tests/Services/DeltaUsxMapperTests.cs
+++ test/SIL.XForge.Scripture.Tests/Services/DeltaUsxMapperTests.cs
@@ -1316,6 +1316,113 @@ public class DeltaUsxMapperTests
     }
 
     [Test]
+    public void ToUsx_NewChapter_AddedBetweenChapters()
+    {
+        var chapterDeltas = new[]
+        {
+            new ChapterDelta(
+                1,
+                2,
+                true,
+                Delta
+                    .New()
+                    .InsertChapter("1")
+                    .InsertVerse("1")
+                    .InsertText("This is verse 1.", "verse_1_1")
+                    .InsertVerse("2")
+                    .InsertText("This is verse 2.", "verse_1_2")
+                    .InsertPara("p")
+            ),
+            new ChapterDelta(
+                2,
+                2,
+                true,
+                Delta
+                    .New()
+                    .InsertChapter("2")
+                    .InsertVerse("1")
+                    .InsertText("New chapter verse 1.", "verse_2_1")
+                    .InsertVerse("2")
+                    .InsertText("New chapter verse 2.", "verse_2_2")
+                    .InsertPara("p")
+            ),
+            new ChapterDelta(
+                3,
+                2,
+                true,
+                Delta
+                    .New()
+                    .InsertChapter("3")
+                    .InsertVerse("1")
+                    .InsertText("This is verse 1.", "verse_3_1")
+                    .InsertVerse("2")
+                    .InsertText("This is verse 2.", "verse_3_2")
+                    .InsertPara("p")
+            ),
+        };
+
+        var mapper = new DeltaUsxMapper(_mapperGuidService, _logger, _exceptionHandler);
+
+        XDocument original = Usx(
+            "PHM",
+            Chapter("1"),
+            Para("p", Verse("1"), "This is verse 1.", Verse("2"), "This is verse 2."),
+            Chapter("3"),
+            Para("p", Verse("1"), "This is verse 1.", Verse("2"), "This is verse 2.")
+        );
+
+        XDocument expected = Usx(
+            "PHM",
+            Chapter("1"),
+            Para("p", Verse("1"), "This is verse 1.", Verse("2"), "This is verse 2."),
+            Chapter("2"),
+            Para("p", Verse("1"), "New chapter verse 1.", Verse("2"), "New chapter verse 2."),
+            Chapter("3"),
+            Para("p", Verse("1"), "This is verse 1.", Verse("2"), "This is verse 2.")
+        );
+
+        // SUT
+        XDocument newUsxDoc = mapper.ToUsx(original, chapterDeltas);
+        Assert.IsTrue(XNode.DeepEquals(newUsxDoc, expected));
+    }
+
+    [Test]
+    public void ToUsx_FirstChapterMissing()
+    {
+        var chapterDeltas = new[]
+        {
+            new ChapterDelta(
+                2,
+                2,
+                true,
+                Delta
+                    .New()
+                    .InsertText("Introductory material")
+                    .InsertPara("toc1")
+                    .InsertChapter("2")
+                    .InsertVerse("1")
+                    .InsertText("This is verse 1.", "verse_2_1")
+                    .InsertVerse("2")
+                    .InsertText("This is verse 2.", "verse_2_2")
+                    .InsertPara("p")
+            ),
+        };
+
+        var mapper = new DeltaUsxMapper(_mapperGuidService, _logger, _exceptionHandler);
+
+        XDocument expected = Usx(
+            "PHM",
+            Para("toc1", "Introductory material"),
+            Chapter("2"),
+            Para("p", Verse("1"), "This is verse 1.", Verse("2"), "This is verse 2.")
+        );
+
+        // SUT
+        XDocument newUsxDoc = mapper.ToUsx(expected, chapterDeltas);
+        Assert.IsTrue(XNode.DeepEquals(newUsxDoc, expected));
+    }
+
+    [Test]
     public void ToUsx_BlankLine()
     {
         var chapterDelta = new ChapterDelta(
@@ -3701,6 +3808,20 @@ public class DeltaUsxMapperTests
 \p E
 \v 2 F \nd ND\nd*
 """
+        );
+    }
+
+    [Test]
+    public void Roundtrip_MissingChapters()
+    {
+        AssertRoundtrips(
+            """
+            \id PRO - A
+            \c 2
+            \v 1 My son, if thou wilt...
+            \c 3
+            \v 1 My son, forget not...
+            """
         );
     }
 
