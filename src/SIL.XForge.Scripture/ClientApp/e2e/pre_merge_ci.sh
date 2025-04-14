70cf75e0ac0a1b7aeeed201021e766e6da7fccae
MarkS <marksvc@gmail.com>
2025-04-14 14:43:56 -0600

    Max retries

--- src/SIL.XForge.Scripture/ClientApp/e2e/pre_merge_ci.sh
+++ src/SIL.XForge.Scripture/ClientApp/e2e/pre_merge_ci.sh
@@ -8,15 +8,15 @@ function shutDownServer() {
 
   kill $SERVER_PID
   sleep 3s
-  ps $SERVER_PID ||:
+  ps $SERVER_PID || :
 
-  kill -5 $SERVER_PID||:
+  kill -5 $SERVER_PID || :
   sleep 3s
-  ps $SERVER_PID ||:
+  ps $SERVER_PID || :
 
-  kill -9 $SERVER_PID||:
+  kill -9 $SERVER_PID || :
   sleep 3s
-  ps $SERVER_PID ||:
+  ps $SERVER_PID || :
 }
 
 trap 'shutDownServer' EXIT
@@ -28,9 +28,12 @@ nohup dotnet run &
 SERVER_PID=$!
 echo "Server started with PID: $SERVER_PID"
 
-until nc -z localhost 5000; do
-  echo "Waiting for server to start..."
+max_retries=30
+attempt=1
+until nc -z localhost 5000 || ((attempt > max_retries)); do
+  echo "Waiting for server to start... (attempt $attempt)"
   sleep 1
+  attempt=$((attempt + 1))
 done
 echo "Server is up and running."
 
