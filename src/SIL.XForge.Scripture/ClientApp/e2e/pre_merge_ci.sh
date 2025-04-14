f997d01af9c982763ee8de089fd5436c25c14a5c
MarkS <marksvc@gmail.com>
2025-04-14 16:03:38 -0600

    Wait fong ng serve

--- src/SIL.XForge.Scripture/ClientApp/e2e/pre_merge_ci.sh
+++ src/SIL.XForge.Scripture/ClientApp/e2e/pre_merge_ci.sh
@@ -28,18 +28,31 @@ nohup dotnet run &
 SERVER_PID=$!
 echo "Server started with PID: $SERVER_PID"
 
-max_retries=30
+max_retries=3
 attempt=1
 until nc -z localhost 5000 || ((attempt > max_retries)); do
-  echo "Waiting for server to start... (attempt $attempt)"
-  sleep 1
+  echo "Waiting for dotnet server to start... (attempt $attempt)"
+  sleep 10
   attempt=$((attempt + 1))
 done
 if ((attempt > max_retries)); then
-  echo "Server failed to start after $max_retries attempts."
+  echo "dotnet server failed to start after $max_retries attempts."
   exit 1
 fi
-echo "Server is up and running."
+echo "dotnet server is up and running."
 
+attempt=1
+until nc -z localhost 4200 || ((attempt > max_retries)); do
+  echo "Waiting for ng server to start... (attempt $attempt)"
+  sleep 10
+  attempt=$((attempt + 1))
+done
+if ((attempt > max_retries)); then
+  echo "ng server failed to start after $max_retries attempts."
+  exit 1
+fi
+echo "ng server is up and running."
+
+sleep 5s
 cd "${SCRIPT_DIR}"
 ./e2e.mts pre_merge_ci
