af20c4511f3339e4d09652f6385338a09dcc00b1
Peter Chapman <peter@conglomo.co.nz>
2025-04-16 14:00:28 +1200

    Update get builds API

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/draft-history-list/draft-history-entry/draft-history-entry.component.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/draft-history-list/draft-history-entry/draft-history-entry.component.spec.ts
@@ -1,10 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { mock } from 'ts-mockito';
+import { UserProfile } from 'realtime-server/lib/esm/common/models/user';
+import { createTestUserProfile } from 'realtime-server/lib/esm/common/models/user-test-data';
+import { anything, mock, when } from 'ts-mockito';
 import { I18nService } from 'xforge-common/i18n.service';
+import { UserProfileDoc } from 'xforge-common/models/user-profile-doc';
 import { configureTestingModule, TestTranslocoModule } from 'xforge-common/test-utils';
+import { UserService } from 'xforge-common/user.service';
 import { DraftHistoryEntryComponent } from './draft-history-entry.component';
 
 const mockedI18nService = mock(I18nService);
+const mockedUserService = mock(UserService);
 
 describe('DraftHistoryEntryComponent', () => {
   let component: DraftHistoryEntryComponent;
@@ -12,10 +17,17 @@ describe('DraftHistoryEntryComponent', () => {
 
   configureTestingModule(() => ({
     imports: [TestTranslocoModule],
-    providers: [{ provide: I18nService, useMock: mockedI18nService }]
+    providers: [
+      { provide: I18nService, useMock: mockedI18nService },
+      { provide: UserService, useMock: mockedUserService }
+    ]
   }));
 
   beforeEach(() => {
+    const user: UserProfile = createTestUserProfile();
+    const userDoc = { id: 'sf-user-id', data: user } as UserProfileDoc;
+    when(mockedUserService.getProfile(anything())).thenResolve(userDoc);
+
     fixture = TestBed.createComponent(DraftHistoryEntryComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
