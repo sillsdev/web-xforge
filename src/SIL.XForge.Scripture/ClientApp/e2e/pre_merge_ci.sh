35b81f3598220beb689497000e30a4a44e43a5dc
MarkS <marksvc@gmail.com>
2025-04-16 15:12:41 -0600

    improve script

--- src/SIL.XForge.Scripture/ClientApp/e2e/pre_merge_ci.sh
+++ src/SIL.XForge.Scripture/ClientApp/e2e/pre_merge_ci.sh
@@ -1,64 +1,79 @@
 #!/bin/bash
-set -xueo pipefail
+set -ueo pipefail
 
 SCRIPT_DIR="$(dirname "$(readlink --canonicalize "$0")")"
+PROGRAM_NAME="E2ELauncher"
 
-function shutDownServer() {
-  echo "Shutting down server with PID: $SERVER_PID"
-
-  kill $SERVER_PID
-  sleep 3s
-  ps $SERVER_PID || :
-
-  kill -5 $SERVER_PID || :
-  sleep 3s
-  ps $SERVER_PID || :
-
-  kill -9 $SERVER_PID || :
-  sleep 3s
-  ps $SERVER_PID || :
+function output() {
+  echo "${PROGRAM_NAME}:" "$@"
 }
 
-trap 'shutDownServer' EXIT
+function shutDownServer() {
+  local pid="$1"
+  output "Shutting down server with PID ${pid}."
 
-cd "${SCRIPT_DIR}"
-cd "../.."
+  kill -TERM "${pid}" 2>/dev/null || {
+    output "Error sending SIGTERM."
+    exit 1
+  }
 
-DOTNET_LOG=$(mktemp)
-echo "Logging dotnet to ${DOTNET_LOG}"
-nohup dotnet run &>${DOTNET_LOG} &
-SERVER_PID=$!
-echo "Server started with PID: $SERVER_PID"
+  local timeout="35"
+  for ((i = 0; i < timeout; i++)); do
+    if ! kill -0 "${pid}" 2>/dev/null; then
+      output "Server shut down."
+      return
+    fi
+    sleep 1s
+  done
 
-sleep 10s
-max_retries=10
-attempt=1
-until nc -z localhost 5000 || ((attempt > max_retries)); do
-  echo "Waiting for dotnet server to start... (attempt $attempt)"
+  # If still running, send SIGKILL
+  output "Timeout. Sending SIGKILL."
+  kill -KILL "${pid}" 2>/dev/null
+}
+
+function startServer() {
+  cd "${SCRIPT_DIR}/../.."
+  local DOTNET_LOG="$(mktemp)"
+  output "Logging dotnet output to ${DOTNET_LOG}"
+  nohup dotnet run &>"${DOTNET_LOG}" &
+  local SERVER_PID="$!"
+  trap "shutDownServer ${SERVER_PID}" EXIT
+  output "Server started with PID: ${SERVER_PID}"
+
+  sleep 20s
+  local max_retries=10
+  local attempt=1
+  until nc -z localhost 5000 &>/dev/null || ((attempt > max_retries)); do
+    output "Waiting for dotnet server to start... (attempt $attempt)"
+    sleep 10s
+    attempt=$((attempt + 1))
+  done
+  if ((attempt > max_retries)); then
+    output "dotnet server failed to start after $max_retries attempts."
+    output "dotnet output:"
+    cat "${DOTNET_LOG}"
+    exit 1
+  fi
+  output "dotnet server is up and running."
+
   sleep 10s
-  attempt=$((attempt + 1))
-done
-if ((attempt > max_retries)); then
-  echo "dotnet server failed to start after $max_retries attempts."
-  echo "Log output:"
-  cat "${DOTNET_LOG}"
-  exit 1
-fi
-echo "dotnet server is up and running."
+  attempt=1
+  until nc -z localhost 4200 &>/dev/null || ((attempt > max_retries)); do
+    output "Waiting for ng server to start... (attempt $attempt)"
+    sleep 10s
+    attempt=$((attempt + 1))
+  done
+  if ((attempt > max_retries)); then
+    output "ng server failed to start after $max_retries attempts."
+    output "dotnet output:"
+    cat "${DOTNET_LOG}"
+    exit 1
+  fi
+  output "ng server is up and running."
 
-sleep 10s
-attempt=1
-until nc -z localhost 4200 || ((attempt > max_retries)); do
-  echo "Waiting for ng server to start... (attempt $attempt)"
-  sleep 10s
-  attempt=$((attempt + 1))
-done
-if ((attempt > max_retries)); then
-  echo "ng server failed to start after $max_retries attempts."
-  exit 1
-fi
-echo "ng server is up and running."
+  sleep 5s
+}
 
-sleep 5s
+startServer
 cd "${SCRIPT_DIR}"
 ./e2e.mts pre_merge_ci
