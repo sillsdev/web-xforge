14798e7aaa4a32e5ba4c66ed02a28c5dd9b3b500
josephmyers <43733878+josephmyers@users.noreply.github.com>
2025-05-19 22:49:00 +0700

    SF-3278 Preserve draft source fields even if they're undefined (#3197)
    Preserve source fields even if they're undefined
    
    This fixes a bug where editing the second field after clearing the first caused the second field to be removed. The new behavior will preserve both fields, until the user saves or advances to the next step.

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/draft-sources/draft-sources.component.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/draft-sources/draft-sources.component.spec.ts
@@ -236,6 +236,26 @@ describe('DraftSourcesComponent', () => {
       tick();
       verify(mockedSFProjectService.onlineUpdateSettings(env.activatedProjectDoc.id, anything())).once();
     }));
+
+    it('can edit second source after first is cleared', fakeAsync(() => {
+      const env = new TestEnvironment();
+      tick();
+      env.fixture.detectChanges();
+      env.clickLanguageCodesConfirmationCheckbox();
+
+      // Remove the first training source.
+      env.component.sourceSelected(env.component.trainingSources, 0, undefined);
+      // Confirm that we have 1 other training source.
+      expect(env.component.trainingSources[1]).not.toBeNull();
+      env.fixture.detectChanges();
+      tick();
+
+      // SUT
+      env.component.sourceSelected(env.component.trainingSources, 1, undefined);
+      env.fixture.detectChanges();
+      tick();
+      expect(env.component.trainingSources.length).toEqual(2);
+    }));
   });
 
   describe('sourceArraysToSettingsChange', () => {
