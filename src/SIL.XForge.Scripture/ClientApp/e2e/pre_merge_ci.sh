775d353f0675ef09563ca16691e21ace6f7569e1
Nathaniel Paulus <nathaniel_paulus@sil.org>
2025-05-24 10:37:42 -0400

    Update tests to await startup of application, and run all e2e tests

--- src/SIL.XForge.Scripture/ClientApp/e2e/pre_merge_ci.sh
+++ src/SIL.XForge.Scripture/ClientApp/e2e/pre_merge_ci.sh
@@ -50,42 +50,12 @@ function startServer() {
   local SERVER_PID="$!"
   trap "shutDownServer ${SERVER_PID}" EXIT
   output "Server started with PID ${SERVER_PID}"
-
-  sleep 20s
-  local max_retries=10
-  local attempt=1
-  until nc -z localhost 5000 &>/dev/null || ((attempt > max_retries)); do
-    output "Waiting for dotnet server to be ready. (Attempt ${attempt}/${max_retries})"
-    sleep 10s
-    attempt=$((attempt + 1))
-  done
-  if ((attempt > max_retries)); then
-    output "dotnet server failed to be ready after ${max_retries} attempts."
-    output "dotnet output:"
-    cat "${DOTNET_LOG}"
-    exit 1
-  fi
-  output "dotnet server is up and running."
-
-  sleep 10s
-  attempt=1
-  until nc -z localhost 4200 &>/dev/null || ((attempt > max_retries)); do
-    output "Waiting for ng server to be ready. (Attempt ${attempt}/${max_retries})"
-    sleep 10s
-    attempt=$((attempt + 1))
-  done
-  if ((attempt > max_retries)); then
-    output "ng server failed to be ready after ${max_retries} attempts."
-    output "dotnet output:"
-    cat "${DOTNET_LOG}"
-    exit 1
-  fi
-  output "ng server is up and running."
-
-  sleep 5s
+  output "Awaiting application startup before running tests"
+  cd "${SCRIPT_DIR}"
+  ./await-application-startup.mts
 }
 
 output "$(date -Is) Starting."
 startServer
 cd "${SCRIPT_DIR}"
-./e2e.mts pre_merge_ci generate_draft
+./e2e.mts pre_merge_ci
