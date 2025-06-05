bdd53fb28a6c1714492c6934c57bff32776f544d
Peter Chapman <peter@conglomo.co.nz>
2025-06-05 12:57:49 +1200

    SF-3415 Add sync endpoint to the paratext-api

--- src/SIL.XForge.Scripture/Services/SFProjectService.cs
+++ src/SIL.XForge.Scripture/Services/SFProjectService.cs
@@ -91,8 +91,50 @@ public class SFProjectService : ProjectService<SFProject, SFProjectSecret>, ISFP
     protected override string ProjectAdminRole => SFProjectRole.Administrator;
 
     /// <summary>
-    /// Returns SF project id of created project.
+    /// Connects to a Paratext project or syncs an existing project.
     /// </summary>
+    /// <param name="curUserId">The current user identifier.</param>
+    /// <param name="projectId">The Paratext project identifier or the Scripture Forge project identifier.</param>
+    /// <returns>The Scripture Forge project identifier.</returns>
+    /// <exception cref="DataNotFoundException">The project or user does not exist.</exception>
+    /// <exception cref="ForbiddenException">The user is not an administrator or translator.</exception>
+    /// <exception cref="InvalidOperationException">The project exists on disk but not in the database.</exception>
+    /// <exception cref="UnauthorizedAccessException">
+    /// The user cannot access the Paratext Registry or Archives.
+    /// </exception>
+    public async Task<string> ConnectOrSyncAsync(string curUserId, string projectId)
+    {
+        // Ensure we have a valid user
+        Attempt<UserSecret> userSecretAttempt = await _userSecrets.TryGetAsync(curUserId);
+        if (!userSecretAttempt.TryResult(out UserSecret userSecret))
+        {
+            throw new DataNotFoundException("The user does not exist.");
+        }
+
+        // Connect or sync, depending if the project exists
+        SFProject? project = await RealtimeService
+            .QuerySnapshots<SFProject>()
+            .SingleOrDefaultAsync(p => p.Id == projectId || p.ParatextId == projectId);
+        if (project is null)
+        {
+            return await CreateProjectAsync(curUserId, new SFProjectCreateSettings { ParatextId = projectId });
+        }
+        else
+        {
+            await SyncAsync(curUserId, project.Id);
+            return project.Id;
+        }
+    }
+
+    /// <summary>
+    /// Connects to a Paratext project,
+    /// </summary>
+    /// <param name="curUserId">The current user identifier.</param>
+    /// <param name="settings">The create project settings.</param>
+    /// <returns>The Scripture Forge identifier of the created project.</returns>
+    /// <exception cref="DataNotFoundException">The user or project does not exist.</exception>
+    /// <exception cref="InvalidOperationException">The project already exists.</exception>
+    /// <exception cref="ForbiddenException">The user does not have permission to connect to the project.</exception>
     public async Task<string> CreateProjectAsync(string curUserId, SFProjectCreateSettings settings)
     {
         Attempt<UserSecret> userSecretAttempt = await _userSecrets.TryGetAsync(curUserId);
