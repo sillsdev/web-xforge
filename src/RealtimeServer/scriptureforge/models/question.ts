6b19d1b90215e99d03f94681de5b548dc6f596c3
Peter Chapman <peter@conglomo.co.nz>
2025-06-19 17:06:50 +1200

    Add index to improve question querying performance (#3275)

--- src/RealtimeServer/scriptureforge/models/question.ts
+++ src/RealtimeServer/scriptureforge/models/question.ts
@@ -1,9 +1,19 @@
 import { PROJECT_DATA_INDEX_PATHS, ProjectData } from '../../common/models/project-data';
+import { obj } from '../../common/utils/obj-path';
 import { Answer } from './answer';
 import { VerseRefData } from './verse-ref-data';
 
 export const QUESTIONS_COLLECTION = 'questions';
-export const QUESTION_INDEX_PATHS: string[] = PROJECT_DATA_INDEX_PATHS;
+export const QUESTION_INDEX_PATHS = [
+  ...PROJECT_DATA_INDEX_PATHS,
+  // Index for CheckingQuestionsService.queryQuestions() and CheckingQuestionsService.queryAdjacentQuestions()
+  {
+    [obj<Question>().pathStr(n => n.projectRef)]: 1,
+    [obj<Question>().pathStr(n => n.isArchived)]: 1,
+    [obj<Question>().pathStr(n => n.verseRef.bookNum)]: 1,
+    [obj<Question>().pathStr(n => n.verseRef.chapterNum)]: 1
+  }
+];
 
 export function getQuestionDocId(projectId: string, questionId: string): string {
   return `${projectId}:${questionId}`;
