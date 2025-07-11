ba181a8f4662522ca9f760ff391e9815fa80ce37
Nathaniel Paulus <nathaniel_paulus@sil.org>
2025-07-10 21:03:52 -0400

    Add notice for upcoming DBL maintenance

--- src/SIL.XForge.Scripture/ClientApp/src/app/shared/global-notices/global-notices.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/global-notices/global-notices.component.ts
@@ -4,7 +4,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { TranslocoModule } from '@ngneat/transloco';
-import { I18nService } from 'xforge-common/i18n.service';
+import { AuthService } from 'xforge-common/auth.service';
+import { I18nKeyForComponent, I18nService } from 'xforge-common/i18n.service';
 import { NoticeComponent } from '../notice/notice.component';
 
 @Component({
@@ -15,18 +16,32 @@ import { NoticeComponent } from '../notice/notice.component';
   styleUrl: './global-notices.component.scss'
 })
 export class GlobalNoticesComponent {
-  // This is only an input so that the Storybook can turn this on even when it's off in the app
-  @Input() showDowntimeNotice = false;
+  // This is only an input so that the Storybook show the notice even when it's hidden in the app
+  @Input() hideNotice = false;
+
+  get showNotice(): boolean {
+    // Hide the notice for users that are not logged in since it mentions DBL and shouldn't be shown on whitelabled site
+    return !this.hideNotice && this.userLoggedIn === true;
+  }
+
+  messageKey: I18nKeyForComponent<'global_notices'> = 'dbl_maintenance';
 
   upcomingDowntime = {
-    start: new Date('2024-12-04 16:30 UTC'),
-    durationMin: 2,
-    durationMax: 6,
+    start: new Date('2025-07-13 12:00:00 UTC'),
+    durationMin: 24,
+    durationMax: 48,
     durationUnit: 'hour',
-    detailsUrl: 'https://software.sil.org/scriptureforge/scripture-forge-will-be-unavailable-for-scheduled-maintenance/'
+    detailsUrl: 'https://paratext.org/2025/07/09/dbl-down-two-days-in-july/'
   } as const;
 
-  constructor(readonly i18n: I18nService) {}
+  userLoggedIn?: boolean;
+
+  constructor(
+    readonly i18n: I18nService,
+    private readonly authService: AuthService
+  ) {
+    this.authService.isLoggedIn.then(isLoggedIn => (this.userLoggedIn = isLoggedIn));
+  }
 
   get duration(): string {
     return this.i18n.translateStatic(`global_notices.${this.upcomingDowntime.durationUnit}_range`, {
--- src/SIL.XForge.Scripture/ClientApp/src/app/shared/global-notices/global-notices.stories.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/global-notices/global-notices.stories.ts
@@ -1,14 +1,25 @@
-import { Meta, StoryObj } from '@storybook/angular';
+import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+import { instance, mock, when } from 'ts-mockito';
+import { AuthService } from 'xforge-common/auth.service';
 import { GlobalNoticesComponent } from './global-notices.component';
+
+const mockedAuthService = mock(AuthService);
+when(mockedAuthService.isLoggedIn).thenResolve(true);
 
 const meta: Meta<GlobalNoticesComponent> = {
   title: 'Shared/Global Notices',
-  component: GlobalNoticesComponent
+  component: GlobalNoticesComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [GlobalNoticesComponent],
+      providers: [{ provide: AuthService, useValue: instance(mockedAuthService) }]
+    })
+  ]
 };
 
 export default meta;
 type Story = StoryObj<GlobalNoticesComponent>;
 
 export const Default: Story = {
-  args: { showDowntimeNotice: true }
+  args: { hideNotice: false }
 };
