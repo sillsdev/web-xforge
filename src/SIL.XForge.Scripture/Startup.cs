f5c24b4a79e5d3e0c27aeab9e679d5c72425617d
MarkS <marksvc@gmail.com>
2025-07-24 17:42:32 -0600

    Fix dotnet accessing angular
    Allow access thru to @vite and @fs.
    
    Show string "open your browser.." to help `UseAngularCliServer` detect
    that `ng serve` is ready.
    
    ng serve does not have a --progress argument.

--- src/SIL.XForge.Scripture/Startup.cs
+++ src/SIL.XForge.Scripture/Startup.cs
@@ -48,6 +48,8 @@ public class Startup
         "vendor.js.map",
         "main.js",
         "main.js.map",
+        "@vite",
+        "@fs",
         "manifest.json",
         "sockjs-node",
         "3rdpartylicenses.txt",
@@ -308,6 +310,9 @@ public class Startup
                             string npmScript = "start";
                             Console.WriteLine($"Info: SF is serving angular using script {npmScript}.");
                             spa.UseAngularCliServer(npmScript);
+                            // Note that dotnet will need to see and parse a line like
+                            // "open your browser on http://localhost:4200/ "
+                            // (https://stackoverflow.com/q/60189930).
                             break;
 
                         case SpaDevServerStartup.Listen:
