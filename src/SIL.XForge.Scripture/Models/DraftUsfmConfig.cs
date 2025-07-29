a31b7fdbfab7c437977250432513187b37739605
Raymond Luong <raymond_luong@sil.org>
2025-07-29 11:20:11 -0600

    Connect quotation denormalization to frontend UI

--- src/SIL.XForge.Scripture/Models/DraftUsfmConfig.cs
+++ src/SIL.XForge.Scripture/Models/DraftUsfmConfig.cs
@@ -7,7 +7,14 @@ public static class ParagraphBreakFormat
     public const string MoveToEnd = "move_to_end";
 }
 
+public static class QuoteStyle
+{
+    public const string Automatic = "automatic";
+    public const string Straight = "straight";
+}
+
 public class DraftUsfmConfig
 {
     public string ParagraphFormat { get; set; } = ParagraphBreakFormat.BestGuess;
+    public string QuoteFormat { get; set; } = QuoteStyle.Automatic;
 }
