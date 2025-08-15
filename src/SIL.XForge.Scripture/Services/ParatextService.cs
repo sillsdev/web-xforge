c05cafbf5abfa7f0e59da6ac6441146957f762d7
Nathaniel Paulus <nathaniel_paulus@sil.org>
2025-08-15 09:48:16 -0400

    Revert "SF-3482 Switch to new DBL staging server in development (#3354)"
    This reverts commit af723c51c4a35ee28f7dca762d3eaae1a1548cf4.

--- src/SIL.XForge.Scripture/Services/ParatextService.cs
+++ src/SIL.XForge.Scripture/Services/ParatextService.cs
@@ -126,7 +126,9 @@ public class ParatextService : DisposableBase, IParatextService
         {
             _httpClientHandler.ServerCertificateCustomValidationCallback =
                 HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
-            _dblServerUri = "https://pt-resources-adapter.staging.library.bible/";
+            // This should be paratext-qa.thedigitalbiblelibrary.org, but it's broken as of 2021-04 and
+            // qa.thedigitalbiblelibrary.org should be just as good, at least for the time being.
+            _dblServerUri = "https://qa.thedigitalbiblelibrary.org/";
             _registryServerUri = "https://registry-dev.paratext.org";
             _registryClient.BaseAddress = new Uri(_registryServerUri);
             _sendReceiveServerUri = InternetAccess.uriDevelopment;
