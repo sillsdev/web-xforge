5c1bad666986f254436f1d4417913d3ce17ad71c
Raymond Luong <raymond_luong@sil.org>
2025-09-09 12:55:32 -0600

    Use enum converter attribute

--- src/SIL.Converters.Usj/LowerCaseNamingStrategy.cs
+++ src/SIL.Converters.Usj/LowerCaseNamingStrategy.cs
@@ -5,7 +5,7 @@ namespace SIL.Converters.Usj
     /// <summary>
     /// Ensures that the JSON properties for the USJ data model are in lower case.
     /// </summary>
-    internal class LowerCaseNamingStrategy : NamingStrategy
+    public class LowerCaseNamingStrategy : NamingStrategy
     {
         /// <inheritdoc />
         protected override string ResolvePropertyName(string name) => name.ToLowerInvariant();
