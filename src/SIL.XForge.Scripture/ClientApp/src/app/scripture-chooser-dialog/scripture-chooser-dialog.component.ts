5a138cb685c7a354da98c1ece5ec71a61e5596ea
Nathaniel Paulus <nathaniel_paulus@sil.org>
2025-09-16 12:51:33 -0400

    SF-3562 Fix scripture chooser layout and book highlight

--- src/SIL.XForge.Scripture/ClientApp/src/app/scripture-chooser-dialog/scripture-chooser-dialog.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/scripture-chooser-dialog/scripture-chooser-dialog.component.ts
@@ -34,6 +34,7 @@ export class ScriptureChooserDialogComponent implements OnInit {
   showing: 'books' | 'chapters' | 'verses' | 'rangeEnd' = 'books';
   otBooks: string[] = [];
   ntBooks: string[] = [];
+  dcBooks: string[] = [];
   chapters: number[] = [];
   verses: number[] = [];
   closeFocuses: number = 0;
@@ -59,10 +60,13 @@ export class ScriptureChooserDialogComponent implements OnInit {
     const books = Object.keys(this.data.booksAndChaptersToShow);
     this.otBooks = books
       .filter(book => Canon.isBookOT(book))
-      .sort((a, b) => this.data.booksAndChaptersToShow[a].bookNum - this.data.booksAndChaptersToShow[b].bookNum);
+      .sort((a, b) => Canon.bookIdToNumber(a) - Canon.bookIdToNumber(b));
     this.ntBooks = books
-      .filter(book => !Canon.isBookOT(book))
-      .sort((a, b) => this.data.booksAndChaptersToShow[a].bookNum - this.data.booksAndChaptersToShow[b].bookNum);
+      .filter(book => Canon.isBookNT(book))
+      .sort((a, b) => Canon.bookIdToNumber(a) - Canon.bookIdToNumber(b));
+    this.dcBooks = books
+      .filter(book => Canon.isBookDC(book))
+      .sort((a, b) => Canon.bookIdToNumber(a) - Canon.bookIdToNumber(b));
 
     if (this.data.rangeStart != null) {
       const rangeStart = this.data.rangeStart;
