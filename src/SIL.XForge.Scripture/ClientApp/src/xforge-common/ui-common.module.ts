9d20251386b862b32232f531b2b6ae7fbe5a75ab
siltomato <colin_moore@sil.org>
2025-10-12 09:29:56 -0400

    migrate UICommonModule providers

--- src/SIL.XForge.Scripture/ClientApp/src/xforge-common/ui-common.module.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/ui-common.module.ts
@@ -1,5 +1,5 @@
 import { BidiModule } from '@angular/cdk/bidi';
-import { ModuleWithProviders, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatBadgeModule } from '@angular/material/badge';
@@ -13,16 +13,16 @@ import { MatOptionModule } from '@angular/material/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule } from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatPaginatorIntl, MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatRadioModule } from '@angular/material/radio';
-import { MAT_SELECT_CONFIG, MatSelectModule } from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSliderModule } from '@angular/material/slider';
@@ -32,13 +32,11 @@ import { MatStepperModule } from '@angular/material/stepper';
 import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MAT_TOOLTIP_DEFAULT_OPTIONS, MatTooltipModule } from '@angular/material/tooltip';
-import { TranslocoService } from '@ngneat/transloco';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { AutofocusDirective } from './autofocus.directive';
 import { BlurOnClickDirective } from './blur-on-click.directive';
 import { L10nNumberPipe } from './l10n-number.pipe';
-import { Paginator } from './paginator/paginator.component';
 import { RouterLinkDirective } from './router-link.directive';
 import { ScrollIntoViewDirective } from './scroll-into-view';
 
@@ -97,48 +95,6 @@ const modules = [
     ScrollIntoViewDirective,
     RouterLinkDirective,
     L10nNumberPipe
-  ],
-  providers: [
-    {
-      provide: MatPaginatorIntl,
-      useClass: Paginator,
-      deps: [TranslocoService]
-    },
-    {
-      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
-      useValue: { appearance: 'outline', hideRequiredMarker: true }
-    },
-    {
-      provide: MAT_TOOLTIP_DEFAULT_OPTIONS,
-      useValue: { disableTooltipInteractivity: true }
-    },
-    {
-      provide: MAT_SELECT_CONFIG,
-      useValue: { panelWidth: null }
-    }
   ]
 })
-export class UICommonModule {
-  static forRoot(): ModuleWithProviders<UICommonModule> {
-    return {
-      ngModule: UICommonModule,
-      providers: [
-        ...(NgCircleProgressModule.forRoot({
-          // Defaults
-          radius: 100,
-          outerStrokeWidth: 8,
-          innerStrokeWidth: 4,
-          outerStrokeColor: '#298ed1',
-          innerStrokeColor: '#95c4e6',
-          animationDuration: 1000,
-          startFromZero: false,
-          titleFontSize: '48',
-          unitsFontSize: '20',
-          showSubtitle: false,
-          responsive: true,
-          renderOnClick: false
-        }).providers ?? [])
-      ]
-    };
-  }
-}
+export class UICommonModule {}
