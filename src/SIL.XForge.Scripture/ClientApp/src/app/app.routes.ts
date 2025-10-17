2a3bd5d40e28f4e3dfb9f917f6e07174a71feafa
siltomato <colin_moore@sil.org>
2025-10-16 22:11:13 -0400

    migrate CheckingModule

--- src/SIL.XForge.Scripture/ClientApp/src/app/app.routes.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { AuthGuard } from 'xforge-common/auth.guard';
 import { SystemAdminAuthGuard } from 'xforge-common/system-admin-auth.guard';
 import { SystemAdministrationComponent } from 'xforge-common/system-administration/system-administration.component';
+import { CheckingOverviewComponent } from './checking/checking-overview/checking-overview.component';
+import { CheckingComponent } from './checking/checking/checking.component';
 import { ConnectProjectComponent } from './connect-project/connect-project.component';
 import { EventMetricsAuthGuard } from './event-metrics/event-metrics-auth.guard';
 import { EventMetricsComponent } from './event-metrics/event-metrics.component';
@@ -14,7 +16,7 @@ import { ServalAdministrationComponent } from './serval-administration/serval-ad
 import { ServalProjectComponent } from './serval-administration/serval-project.component';
 import { SettingsComponent } from './settings/settings.component';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
-import { SettingsAuthGuard, SyncAuthGuard, UsersAuthGuard } from './shared/project-router.guard';
+import { CheckingAuthGuard, SettingsAuthGuard, SyncAuthGuard, UsersAuthGuard } from './shared/project-router.guard';
 import { SyncComponent } from './sync/sync.component';
 import { UsersComponent } from './users/users.component';
 
@@ -24,6 +26,13 @@ export const APP_ROUTES: Routes = [
   { path: 'login', redirectTo: 'projects', pathMatch: 'full' },
   { path: 'join/:shareKey', component: JoinComponent },
   { path: 'join/:shareKey/:locale', component: JoinComponent },
+  {
+    path: 'projects/:projectId/checking/:bookId/:chapter',
+    component: CheckingComponent,
+    canActivate: [CheckingAuthGuard]
+  },
+  { path: 'projects/:projectId/checking/:bookId', component: CheckingComponent, canActivate: [CheckingAuthGuard] },
+  { path: 'projects/:projectId/checking', component: CheckingOverviewComponent, canActivate: [CheckingAuthGuard] },
   { path: 'projects/:projectId/event-log', component: EventMetricsComponent, canActivate: [EventMetricsAuthGuard] },
   { path: 'projects/:projectId/settings', component: SettingsComponent, canActivate: [SettingsAuthGuard] },
   { path: 'projects/:projectId/sync', component: SyncComponent, canActivate: [SyncAuthGuard] },
