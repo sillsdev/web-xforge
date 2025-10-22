6911b6e6f4fd4db7aeab8bb4be0aedaf4cef8cc3
siltomato <colin_moore@sil.org>
2025-10-22 16:42:47 -0400

    use makeEnvironmentProviders where appropriate

--- src/SIL.XForge.Scripture/ClientApp/src/xforge-common/test-online-status-providers.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/xforge-common/test-online-status-providers.ts
@@ -1,17 +1,17 @@
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { EnvironmentProviders, Provider } from '@angular/core';
+import { EnvironmentProviders, makeEnvironmentProviders } from '@angular/core';
 import { mock, when } from 'ts-mockito';
 
 /**
  * Provides a mocked Navigator (defaults to online).
  */
-export function provideTestOnlineStatus(): (Provider | EnvironmentProviders)[] {
+export function provideTestOnlineStatus(): EnvironmentProviders {
   const mockedNavigator = mock(Navigator);
   when(mockedNavigator.onLine).thenReturn(true);
-  return [
+  return makeEnvironmentProviders([
     provideHttpClient(withInterceptorsFromDi()),
     provideHttpClientTesting(),
     { provide: Navigator, useValue: mockedNavigator }
-  ];
+  ]);
 }
