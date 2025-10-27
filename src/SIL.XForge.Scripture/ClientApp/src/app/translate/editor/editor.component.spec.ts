44200ed3d6400833e2edbbea4def0ec2fe4a5a87
Joseph Myers <wjosephmyers@gmail.com>
2025-10-27 15:26:26 -0500

    Added requirement that the latest build be completed
    This adds a new backend method to return the latest build. The front end verifies that the latest build is completed and not any other state. This was changed to account for canceled builds. In order to communicate with Serval, the latest build has to be complete (and contain the draft being used).

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/editor.component.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/editor.component.spec.ts
@@ -4892,6 +4892,7 @@ class TestEnvironment {
     when(mockedDraftGenerationService.pollBuildProgress(anything())).thenReturn(
       of({ state: BuildStates.Completed } as BuildDto)
     );
+    when(mockedDraftGenerationService.getLastPreTranslationBuild(anything())).thenReturn(of(undefined));
     when(
       mockedDraftGenerationService.getGeneratedDraftDeltaOperations(
         anything(),
