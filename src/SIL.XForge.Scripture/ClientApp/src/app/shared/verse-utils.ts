264935839d65e3b66541c7e5282cae8caf1d490e
siltomato <colin_moore@sil.org>
2025-11-18 13:09:50 -0500

    move verse-related functions to verse-utils

--- src/SIL.XForge.Scripture/ClientApp/src/app/shared/verse-utils.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/verse-utils.ts
@@ -1,4 +1,5 @@
 import { Canon, VerseRef } from '@sillsdev/scripture';
+import { attributeFromMouseEvent } from './utils';
 
 // Regular expression for getting the verse from a segment ref
 // Some projects will have the right to left marker in the segment attribute which we need to account for
@@ -40,6 +41,77 @@ export function verseSlug(verse: VerseRef): string {
   return 'verse_' + verse.chapterNum + '_' + (verse.verse == null ? verse.verseNum : verse.verse);
 }
 
+/**
+ * Combines start and end verse reference strings into a single VerseRef.
+ * @param startStr The starting verse reference string.
+ * @param endStr The optional ending verse reference string.
+ * @returns A VerseRef representing the range, single verse, or undefined if invalid.
+ */
+export function combineVerseRefStrs(startStr: string, endStr?: string): VerseRef | undefined {
+  if (startStr === '') {
+    // no start ref
+    return undefined;
+  }
+
+  const start = VerseRef.tryParse(startStr);
+  if (!start.success) {
+    // invalid start ref
+    return undefined;
+  }
+
+  if (endStr == null || endStr === '') {
+    // no end ref
+    return start.verseRef;
+  }
+
+  const end = VerseRef.tryParse(endStr);
+  if (!end.success || start.verseRef.BBBCCC !== end.verseRef.BBBCCC) {
+    // invalid end ref
+    return undefined;
+  }
+
+  if (start.verseRef.equals(end.verseRef)) {
+    // start and end refs are the same
+    return start.verseRef;
+  }
+
+  // range
+  const rangeStr = `${startStr}-${end.verseRef.verse}`;
+  const range = VerseRef.tryParse(rangeStr);
+  if (!range.success) {
+    return undefined;
+  }
+  return range.verseRef;
+}
+
+/**
+ * Get the verses numbers from a verse reference.
+ * @returns The verse numbers in the VerseRef as integers.
+ * */
+export function getVerseNumbers(verseRef: VerseRef): number[] {
+  const verseList: number[] = [];
+  if (verseRef.verse == null) {
+    verseList.push(verseRef.verseNum); // no bridge or segment info included in verse
+    return verseList;
+  }
+
+  let verseStr = '';
+  for (let i = 0; i < verseRef.verse.length; i++) {
+    if (verseRef.verse[i].match(/[0-9]/i)) {
+      verseStr += verseRef.verse[i];
+    } else if (verseStr.length > 0) {
+      verseList.push(parseInt(verseStr));
+      verseStr = '';
+    }
+  }
+
+  if (verseStr.length > 0) {
+    verseList.push(parseInt(verseStr)); // add any accumulated digits
+  }
+
+  return verseList;
+}
+
 export function verseRefFromMouseEvent(event: MouseEvent, bookNum: number): VerseRef | undefined {
   const clickSegment = attributeFromMouseEvent(event, 'USX-SEGMENT', 'data-segment');
   if (clickSegment == null) {
@@ -48,13 +120,4 @@ export function verseRefFromMouseEvent(event: MouseEvent, bookNum: number): Vers
   const segmentParts = clickSegment.split('_', 3);
   const versePart = segmentParts[2].split('/')[0];
   return new VerseRef(Canon.bookNumberToId(bookNum), segmentParts[1], versePart);
-}
-
-function attributeFromMouseEvent(event: MouseEvent, tagName: string, attributeName: string): string | undefined {
-  for (let element = event.target as Element | null; element != null; element = element.parentElement) {
-    if (element.tagName === tagName) {
-      return element.getAttribute(attributeName) ?? undefined;
-    }
-  }
-  return undefined;
 }
