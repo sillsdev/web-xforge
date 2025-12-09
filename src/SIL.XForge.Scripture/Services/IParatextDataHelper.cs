1af236564519ed22dfa34404a0ddf79f8b9d0872
Joseph Myers <wjosephmyers@gmail.com>
2025-12-09 15:11:33 -0600

    PR comments

--- src/SIL.XForge.Scripture/Services/IParatextDataHelper.cs
+++ src/SIL.XForge.Scripture/Services/IParatextDataHelper.cs
@@ -1,8 +1,19 @@
+using System;
+using System.Collections.Generic;
 using Paratext.Data;
+using Paratext.Data.ProjectComments;
+using SIL.XForge.Scripture.Models;
 
 namespace SIL.XForge.Scripture.Services;
 
 public interface IParatextDataHelper
 {
     void CommitVersionedText(ScrText scrText, string comment);
+
+    IReadOnlyList<ParatextNote> GetNotes(
+        CommentManager? commentManager,
+        CommentTags? commentTags,
+        Func<CommentThread, bool>? predicate = null,
+        bool includeInactiveThreads = true
+    );
 }
