644e5f972820634d6cabb11b28564157a81c76a9
Nathaniel Paulus <nathaniel_paulus@sil.org>
2025-12-11 16:27:00 -0500

    Integrate onboarding form

--- src/SIL.XForge.Scripture/ClientApp/src/app/project-select/project-select.component.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/project-select/project-select.component.spec.ts
@@ -5,7 +5,6 @@ import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { getTestTranslocoModule } from 'xforge-common/test-utils';
 import { isSafari } from 'xforge-common/utils';
 import { SelectableProject } from '../core/models/selectable-project';
-import { CustomValidatorState, SFValidators } from '../shared/sfvalidators';
 import { ProjectSelectComponent } from './project-select.component';
 
 describe('ProjectSelectComponent', () => {
@@ -117,45 +116,13 @@ describe('ProjectSelectComponent', () => {
     const env = new TestEnvironment();
     env.clickInput();
     env.inputText('does not exist');
+    env.textInputElement.dispatchEvent(new Event('blur'));
+    env.fixture.detectChanges();
     expect(env.selectionInvalidMessage).not.toBeNull();
     env.inputText('p');
     env.clickInput();
     env.clickOption(0, 0);
     expect(env.selectionInvalidMessage).toBeNull();
-  }));
-
-  it('allows marking the selection invalid', fakeAsync(() => {
-    const env = new TestEnvironment();
-    expect(env.selectionInvalidMessage).toBeNull();
-    env.component.projectSelect.customValidate(SFValidators.customValidator(CustomValidatorState.InvalidProject));
-    tick();
-    env.fixture.detectChanges();
-    expect(env.selectionInvalidMessage).not.toBeNull();
-  }));
-
-  it('allows using a custom error state matcher', fakeAsync(() => {
-    const env = new TestEnvironment();
-    const invalidMessageMapper = {
-      invalidProject: 'Please select a valid project',
-      bookNotFound: 'Genesis on the selected project',
-      noWritePermissions: 'You do not have permission'
-    };
-    env.component.projectSelect.invalidMessageMapper = invalidMessageMapper;
-    expect(env.selectionInvalidMessage).toBeNull();
-    env.component.projectSelect.customValidate(SFValidators.customValidator(CustomValidatorState.InvalidProject));
-    tick();
-    env.fixture.detectChanges();
-    expect(env.selectionInvalidMessage!.textContent).toContain('Please select a valid project');
-
-    env.component.projectSelect.customValidate(SFValidators.customValidator(CustomValidatorState.BookNotFound));
-    tick();
-    env.fixture.detectChanges();
-    expect(env.selectionInvalidMessage!.textContent).toContain('Genesis on the selected project');
-
-    env.component.projectSelect.customValidate(SFValidators.customValidator(CustomValidatorState.NoWritePermissions));
-    tick();
-    env.fixture.detectChanges();
-    expect(env.selectionInvalidMessage!.textContent).toContain('You do not have permission');
   }));
 
   it('updates the filtered list when the resources update', fakeAsync(() => {
@@ -234,6 +201,8 @@ class TestEnvironment {
     nonSelectableProjects?: SelectableProject[]
   ) {
     TestBed.configureTestingModule({
+      // declarations: [HostComponent],
+      // imports: [UICommonModule, TestTranslocoModule, NoopAnimationsModule, ProjectSelectComponent]
       imports: [ReactiveFormsModule, getTestTranslocoModule(), HostComponent],
       providers: [provideNoopAnimations()]
     });
@@ -251,7 +220,7 @@ class TestEnvironment {
   }
 
   get selectionInvalidMessage(): HTMLElement | null {
-    return this.fixture.nativeElement.querySelector('#invalidSelection');
+    return this.fixture.nativeElement.querySelector('mat-error');
   }
 
   get textInputElement(): HTMLInputElement {
