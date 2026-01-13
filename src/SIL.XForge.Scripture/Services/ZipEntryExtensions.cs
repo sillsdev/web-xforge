9a67163734793e8fead869b07e96550e57e383a1
Peter Chapman <peter@conglomo.co.nz>
2026-01-13 14:13:31 +1300

    Add checking for symlinks, allow updating resources, improved unit tests

---
+++ src/SIL.XForge.Scripture/Services/ZipEntryExtensions.cs
@@ -0,0 +1,17 @@
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace SIL.XForge.Scripture.Services;
+
+/// <summary>
+/// Extension methods for zip files.
+/// </summary>
+public static class ZipEntryExtensions
+{
+    /// <summary>
+    /// Determines if the zip entry is a symbolic link.
+    /// </summary>
+    /// <param name="entry">The zip entry</param>
+    /// <returns><c>true</c> if the zip entry is a symbolic link; otherwise, <c>false</c>.</returns>
+    public static bool IsSymLink(this ZipEntry entry) =>
+        ((entry.ExternalFileAttributes >> 16) & 0xFFFF & 0xA000) == 0xA000;
+}
