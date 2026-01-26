e1dde9d0953c4bb319659c686eec9829b2a68754
Peter Chapman <peter@conglomo.co.nz>
2026-01-27 11:55:26 +1300

    SF-3674 Don't show the apply draft button if the selected draft is empty

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/editor-draft/editor-draft.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/editor-draft/editor-draft.component.ts
@@ -97,7 +97,7 @@ export class EditorDraftComponent implements AfterViewInit, OnChanges {
   generateDraftUrl?: string;
   targetProject?: SFProjectProfile;
   textDocId?: TextDocId;
-  isDraftReady = false;
+  canSelectDraft = false;
   isDraftApplied = false;
   userAppliedDraft = false;
 
@@ -159,6 +159,10 @@ export class EditorDraftComponent implements AfterViewInit, OnChanges {
 
   get doesLatestCompletedHaveDraft(): boolean {
     return this.projectService.hasDraft(this.targetProject, this.bookNum, true);
+  }
+
+  get hasDraftToApply(): boolean {
+    return this.targetProject != null && this.bookNum != null && this.chapter != null && this.draftDelta?.ops != null;
   }
 
   get isLatestBuildCompleted(): boolean {
@@ -271,6 +275,13 @@ export class EditorDraftComponent implements AfterViewInit, OnChanges {
             return of({ targetOps, draftOps });
           }
 
+          // If there are previous draft revisions, we should still show the selector to choose them
+          this.canSelectDraft = this._draftRevisions.length > 1;
+
+          // Clear the ops used to calculate whether the draft can be applied
+          this.draftDelta = undefined;
+          this.targetDelta = undefined;
+
           // No generated verse segments were found, return an empty draft
           this.draftCheckState = 'draft-empty';
           return EMPTY;
@@ -292,7 +303,7 @@ export class EditorDraftComponent implements AfterViewInit, OnChanges {
           this.draftCheckState = 'draft-present';
         }
 
-        this.isDraftReady = this.draftCheckState === 'draft-present' || this.draftCheckState === 'draft-legacy';
+        this.canSelectDraft = this.draftCheckState === 'draft-present' || this.draftCheckState === 'draft-legacy';
       });
 
     combineLatest([
@@ -361,7 +372,7 @@ export class EditorDraftComponent implements AfterViewInit, OnChanges {
 
   private setInitialState(): void {
     this.draftCheckState = 'draft-unknown';
-    this.isDraftReady = false;
+    this.canSelectDraft = false;
     this.isDraftApplied = false;
     this.userAppliedDraft = false;
   }
