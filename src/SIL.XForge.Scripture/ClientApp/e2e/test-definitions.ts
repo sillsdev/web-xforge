9e34290bc0df678638dd4b98d5805ae33231304a
Nathaniel Paulus <nathaniel_paulus@sil.org>
2026-02-13 18:38:06 -0500

    Remove submit-draft-signup e2e test in favor of onboarding-flow test

--- src/SIL.XForge.Scripture/ClientApp/e2e/test-definitions.ts
+++ src/SIL.XForge.Scripture/ClientApp/e2e/test-definitions.ts
@@ -7,7 +7,6 @@ import { generateDraft } from './workflows/generate-draft.ts';
 import { localizedScreenshots } from './workflows/localized-screenshots.ts';
 import { onboardingFlow } from './workflows/onboarding-flow.ts';
 import { runSmokeTests, traverseHomePageAndLoginPage } from './workflows/smoke-tests.mts';
-import { submitDraftSignupForm } from './workflows/submit-draft-signup.ts';
 
 export const tests = {
   home_and_login: async (_engine: BrowserType, page: Page, screenshotContext: ScreenshotContext) => {
@@ -24,9 +23,6 @@ export const tests = {
   },
   generate_draft: async (_engine: BrowserType, page: Page, screenshotContext: ScreenshotContext) => {
     await generateDraft(page, screenshotContext, secrets.users[0]);
-  },
-  submit_draft_signup: async (_engine: BrowserType, page: Page, screenshotContext: ScreenshotContext) => {
-    await submitDraftSignupForm(page, screenshotContext, secrets.users[0]);
   },
   onboarding_flow: async (engine: BrowserType, page: Page, screenshotContext: ScreenshotContext) => {
     await onboardingFlow(engine, page, screenshotContext, secrets.users[0]);
