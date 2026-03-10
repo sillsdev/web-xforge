f1b2f2c5de8cd372e19d110558bfa78875cba394
Peter Chapman <peter@conglomo.co.nz>
2026-03-10 15:03:22 +1300

    SF-3730 Require authentication for SignalR notifications

--- src/SIL.XForge.Scripture/Services/DraftNotificationHub.cs
+++ src/SIL.XForge.Scripture/Services/DraftNotificationHub.cs
@@ -1,12 +1,12 @@
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.SignalR;
+using SIL.XForge.Realtime;
 using SIL.XForge.Scripture.Models;
 
 namespace SIL.XForge.Scripture.Services;
 
-[Authorize]
-public class DraftNotificationHub : Hub<IDraftNotifier>, IDraftNotifier
+public class DraftNotificationHub(IRealtimeService realtimeService)
+    : NotificationHubBase<IDraftNotifier>(realtimeService),
+        IDraftNotifier
 {
     /// <summary>
     /// Notifies subscribers to a project of draft application progress.
@@ -23,16 +23,9 @@ public class DraftNotificationHub : Hub<IDraftNotifier>, IDraftNotifier
     /// is sufficient for all other users to subscribe to, although they will not receive all draft
     /// progress notifications, only the final success message.
     /// </remarks>
-    public async Task NotifyDraftApplyProgress(string projectId, DraftApplyState draftApplyState) =>
+    public async Task NotifyDraftApplyProgress(string projectId, DraftApplyState draftApplyState)
+    {
+        await EnsurePermissionAsync(projectId);
         await Clients.Group(projectId).NotifyDraftApplyProgress(projectId, draftApplyState);
-
-    /// <summary>
-    /// Subscribe to notifications for a project.
-    ///
-    /// This is called from the frontend via <c>project-notification.service.ts</c>.
-    /// </summary>
-    /// <param name="projectId">The Scripture Forge project identifier.</param>
-    /// <returns>The asynchronous task.</returns>
-    public async Task SubscribeToProject(string projectId) =>
-        await Groups.AddToGroupAsync(Context.ConnectionId, projectId);
+    }
 }
