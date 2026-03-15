4e819c7b6a55027593eb0009506c617c655b7b06
Peter Chapman <peter@conglomo.co.nz>
2026-03-16 12:57:35 +1300

    SF-3745 Frontend changes

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/editor-draft/editor-draft.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/editor/editor-draft/editor-draft.component.ts
@@ -163,7 +163,7 @@ export class EditorDraftComponent implements AfterViewInit, OnChanges {
   }
 
   get doesLatestCompletedHaveDraft(): boolean {
-    return this.projectService.hasDraft(this.targetProject, this.bookNum, true);
+    return this.projectService.hasDraft(this.targetProject, this.bookNum, this.chapter, true);
   }
 
   get hasDraftToApply(): boolean {
