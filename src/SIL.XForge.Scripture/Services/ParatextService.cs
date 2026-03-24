1464e9220344c6928e481c61dea1011789cb02d6
Peter Chapman <peter@conglomo.co.nz>
2026-03-25 11:41:55 +1300

    Update to ParatextData 9.6 Beta

--- src/SIL.XForge.Scripture/Services/ParatextService.cs
+++ src/SIL.XForge.Scripture/Services/ParatextService.cs
@@ -2524,7 +2524,8 @@ public class ParatextService : DisposableBase, IParatextService
             usx.CreateNavigator(),
             XPathExpression.Compile("*[false()]"),
             out string usfm,
-            scrText.Settings.AllowInvisibleChars
+            scrText.Settings.AllowInvisibleChars,
+            scrText.Settings.UsfmRequiresPlusOnNestedStyles
         );
         log?.AppendLine($"Created usfm of {usfm}");
 
