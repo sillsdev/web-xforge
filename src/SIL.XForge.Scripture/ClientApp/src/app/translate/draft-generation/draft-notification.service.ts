343edf7d28842ebfad3c07ac3823971fe65dd7e3
Peter Chapman <peter@conglomo.co.nz>
2026-04-01 10:15:11 +1300

    SF-3449 SF-3751 Fix SignalR HubConnection errors

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/draft-notification.service.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/draft-notification.service.ts
@@ -1,37 +1,23 @@
 import { Injectable } from '@angular/core';
-import {
-  AbortError,
-  HubConnection,
-  HubConnectionBuilder,
-  HubConnectionState,
-  IHttpConnectionOptions
-} from '@microsoft/signalr';
+import { HubConnectionBuilder } from '@microsoft/signalr';
 import { AuthService } from 'xforge-common/auth.service';
 import { OnlineStatusService } from 'xforge-common/online-status.service';
+import { NotificationServiceBase } from '../../core/notification-service-base';
 
+/**
+ * Provides notification for draft-level events via SignalR.
+ */
 @Injectable({
   providedIn: 'root'
 })
-export class DraftNotificationService {
-  private connection: HubConnection;
-  private options: IHttpConnectionOptions = {
-    accessTokenFactory: async () => (await this.authService.getAccessToken()) ?? ''
-  };
-  private openConnections: number = 0;
-
-  constructor(
-    private authService: AuthService,
-    private readonly onlineService: OnlineStatusService
-  ) {
+export class DraftNotificationService extends NotificationServiceBase {
+  constructor(authService: AuthService, onlineService: OnlineStatusService) {
+    super(authService, onlineService);
     this.connection = new HubConnectionBuilder()
       .withUrl('/draft-notifications', this.options)
       .withAutomaticReconnect()
       .withStatefulReconnect()
       .build();
-  }
-
-  get appOnline(): boolean {
-    return this.onlineService.isOnline && this.onlineService.isBrowserOnline;
   }
 
   removeNotifyDraftApplyProgressHandler(handler: any): void {
@@ -40,43 +26,5 @@ export class DraftNotificationService {
 
   setNotifyDraftApplyProgressHandler(handler: any): void {
     this.connection.on('notifyDraftApplyProgress', handler);
-  }
-
-  async start(): Promise<void> {
-    this.openConnections++;
-    if (
-      this.connection.state !== HubConnectionState.Connected &&
-      this.connection.state !== HubConnectionState.Connecting &&
-      this.connection.state !== HubConnectionState.Reconnecting
-    ) {
-      await this.connection.start().catch(err => {
-        // Suppress AbortErrors, as they are not caused by server error, but the SignalR connection state
-        // These will be thrown if a user navigates away quickly after
-        // starting the sync or the app loses internet connection
-        if (err instanceof AbortError || !this.appOnline) {
-          return;
-        } else {
-          throw err;
-        }
-      });
-    }
-  }
-
-  async stop(): Promise<void> {
-    // Only stop the connection if this is the last open connection
-    if (this.openConnections > 0 && --this.openConnections === 0) {
-      await this.connection.stop();
-    }
-  }
-
-  async subscribeToProject(projectId: string): Promise<void> {
-    await this.connection.invoke('subscribeToProject', projectId).catch(err => {
-      // This error is thrown when a user navigates away quickly after starting the sync
-      if (err.message === "Cannot send data if the connection is not in the 'Connected' State.") {
-        return;
-      } else {
-        throw err;
-      }
-    });
   }
 }
