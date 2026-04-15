14d8f02f16601eb92f4ac182c67fe1c9e03a23d0
Peter Chapman <peter@conglomo.co.nz>
2026-04-15 15:07:46 +1200

    Allow Serval admins to sync resources

--- src/SIL.XForge.Scripture/ClientApp/src/app/shared/project-router.guard.spec.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/shared/project-router.guard.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-import { mock } from 'ts-mockito';
+import { SystemRole } from 'realtime-server/lib/esm/common/models/system-role';
+import { SFProjectRole } from 'realtime-server/lib/esm/scriptureforge/models/sf-project-role';
+import { createTestProjectProfile } from 'realtime-server/lib/esm/scriptureforge/models/sf-project-test-data';
+import { mock, when } from 'ts-mockito';
 import { AuthGuard } from 'xforge-common/auth.guard';
+import { AuthService } from 'xforge-common/auth.service';
 import { configureTestingModule } from 'xforge-common/test-utils';
+import { UserService } from 'xforge-common/user.service';
+import { SFProjectProfileDoc } from '../core/models/sf-project-profile-doc';
 import { SFProjectService } from '../core/sf-project.service';
-import { DraftNavigationAuthGuard } from './project-router.guard';
+import { DraftNavigationAuthGuard, SyncAuthGuard } from './project-router.guard';
 
 const mockedAuthGuard = mock(AuthGuard);
+const mockedAuthService = mock(AuthService);
 const mockedProjectService = mock(SFProjectService);
+const mockedUserService = mock(UserService);
 
 describe('DraftNavigationAuthGuard', () => {
   configureTestingModule(() => ({
@@ -29,9 +37,88 @@ describe('DraftNavigationAuthGuard', () => {
   });
 });
 
+describe('SyncAuthGuard', () => {
+  configureTestingModule(() => ({
+    providers: [
+      { provide: AuthGuard, useMock: mockedAuthGuard },
+      { provide: AuthService, useMock: mockedAuthService },
+      { provide: SFProjectService, useMock: mockedProjectService },
+      { provide: UserService, useMock: mockedUserService }
+    ]
+  }));
+
+  it('administrators can access sync', async () => {
+    // navigate away
+    const env = new SyncAuthGuardTestEnvironment(false);
+    expect(
+      env.service.check({
+        data: createTestProjectProfile({ userRoles: { user01: SFProjectRole.ParatextAdministrator } })
+      } as SFProjectProfileDoc)
+    ).toBe(true);
+  });
+
+  it('translators can access sync', async () => {
+    // navigate away
+    const env = new SyncAuthGuardTestEnvironment(false);
+    expect(
+      env.service.check({
+        data: createTestProjectProfile({ userRoles: { user01: SFProjectRole.ParatextTranslator } })
+      } as SFProjectProfileDoc)
+    ).toBe(true);
+  });
+
+  it('consultants cannot access sync', async () => {
+    // navigate away
+    const env = new SyncAuthGuardTestEnvironment(false);
+    expect(
+      env.service.check({
+        data: createTestProjectProfile({ userRoles: { user01: SFProjectRole.ParatextConsultant } })
+      } as SFProjectProfileDoc)
+    ).toBe(false);
+  });
+
+  it('serval administrators can sync resources they have read access to', async () => {
+    // navigate away
+    const env = new SyncAuthGuardTestEnvironment(true);
+    expect(
+      env.service.check({
+        data: createTestProjectProfile({
+          userRoles: { user01: SFProjectRole.ParatextObserver },
+          paratextId: 'ResourceResource'
+        })
+      } as SFProjectProfileDoc)
+    ).toBe(true);
+  });
+
+  it('serval administrators cannot sync projects they have read access to', async () => {
+    // navigate away
+    const env = new SyncAuthGuardTestEnvironment(true);
+    expect(
+      env.service.check({
+        data: createTestProjectProfile({
+          userRoles: { user01: SFProjectRole.ParatextObserver }
+        })
+      } as SFProjectProfileDoc)
+    ).toBe(false);
+  });
+});
+
 class DraftNavigationTestEnvironment {
   service: DraftNavigationAuthGuard;
   constructor() {
     this.service = TestBed.inject(DraftNavigationAuthGuard);
+  }
+}
+
+class SyncAuthGuardTestEnvironment {
+  service: SyncAuthGuard;
+  constructor(servalAdmin: boolean) {
+    this.service = TestBed.inject(SyncAuthGuard);
+    when(mockedUserService.currentUserId).thenReturn('user01');
+    if (servalAdmin) {
+      when(mockedAuthService.currentUserRoles).thenReturn([SystemRole.ServalAdmin]);
+    } else {
+      when(mockedAuthService.currentUserRoles).thenReturn([SystemRole.User]);
+    }
   }
 }
