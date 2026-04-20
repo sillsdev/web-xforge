8f20bc7ebb98a80bfd14378ab8c3254a3d69761e
Peter Chapman <peter@conglomo.co.nz>
2026-04-21 10:17:16 +1200

    Return Confidence Interval in Pre-translations

--- src/SIL.XForge.Scripture/Models/PreTranslation.cs
+++ src/SIL.XForge.Scripture/Models/PreTranslation.cs
@@ -1,7 +1,29 @@
 namespace SIL.XForge.Scripture.Models;
 
+/// <summary>
+/// A pre-translation of a verse or segment.
+/// </summary>
 public class PreTranslation
 {
+    /// <summary>
+    /// Gets or sets the reference identifier.
+    /// </summary>
+    /// <remarks>
+    /// The segment will be in the format verse_1_2 or verse_1_3b.
+    /// </remarks>
     public string Reference { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the translated verse or segment.
+    /// </summary>
     public string Translation { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the confidence score for the translation
+    /// </summary>
+    /// <remarks>
+    /// This will be from 0.0 to 1.0 for a valid confidence score,
+    /// or -1 if no confidence score is present for the translation.
+    /// </remarks>
+    public double Confidence { get; set; }
 }
