f2423b59206cdca802b894a3787a43f076a89990
Peter Chapman <peter@conglomo.co.nz>
2026-04-29 12:50:04 +1200

    Add model for draft metrics

---
+++ src/SIL.XForge.Scripture/Models/DraftMetrics.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using SIL.XForge.Models;
+
+namespace SIL.XForge.Scripture.Models;
+
+/// <summary>
+/// The metrics for a specific draft.
+/// </summary>
+public class DraftMetrics : IIdentifiable
+{
+    /// <summary>
+    /// Gets or sets the identifier.
+    /// </summary>
+    /// <remarks>This is in the format projectId:buildId.</remarks>
+    public required string Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the confidence scores for every book in the draft.
+    /// </summary>
+    public List<BookConfidence> BookConfidences { get; set; } = [];
+
+    /// <summary>
+    /// Gets or sets the confidence scores for every chapter in the draft.
+    /// </summary>
+    public List<ChapterConfidence> ChapterConfidences { get; set; } = [];
+
+    /// <summary>
+    /// Gets or sets the quality estimation configuration for this draft.
+    /// </summary>
+    public required QualityEstimationConfig QualityEstimationConfig { get; set; }
+
+    /// <summary>
+    /// The confidence scores for every verse in the draft.
+    /// </summary>
+    public List<VerseConfidence> VerseConfidences { get; set; } = [];
+}
+
+public class VerseConfidence : VerseRefData
+{
+    public VerseConfidence() { }
+
+    public VerseConfidence(int bookNum, int chapterNum, int verseNum, double confidence)
+        : base(bookNum, chapterNum, verseNum) => Confidence = confidence;
+
+    public VerseConfidence(int bookNum, int chapterNum, string verse, double confidence)
+        : base(bookNum, chapterNum, verse) => Confidence = confidence;
+
+    public double Confidence { get; set; }
+}
+
+public class BookConfidence
+{
+    public required int BookNum { get; set; }
+    public required double Confidence { get; set; }
+    public required string Label { get; set; }
+    public required double ProjectedChrF3 { get; set; }
+    public required double Usability { get; set; }
+}
+
+public class ChapterConfidence : BookConfidence
+{
+    public required int ChapterNum { get; set; }
+}
--- src/SIL.XForge.Scripture/Models/VerseRefData.cs
+++ src/SIL.XForge.Scripture/Models/VerseRefData.cs
@@ -1,47 +1,62 @@
-#nullable disable warnings
 using SIL.Scripture;
 
 namespace SIL.XForge.Scripture.Models;
 
-public class VerseRefData
+/// <summary>
+/// A verse reference that can be stored in Mongo or the RealtimeServer.
+/// </summary>
+public class VerseRefData()
 {
-    private static void ParseVerseNumber(string vNum, out int number, out string segment)
-    {
-        int j;
-        for (j = 0; j < vNum.Length && char.IsDigit(vNum[j]); ++j) { }
-
-        number = 0;
-        if (j > 0)
-        {
-            string num = vNum[..j];
-            int.TryParse(num, out number); // Can't fail, we have already validated digits
-        }
-
-        segment = vNum[j..];
-    }
-
-    public VerseRefData() { }
-
+    /// <summary>
+    /// Initializes a new instance of the <see cref="VerseRefData"/> class with a verse as a number.
+    /// </summary>
+    /// <param name="bookNum">The book number.</param>
+    /// <param name="chapterNum">The chapter number.</param>
+    /// <param name="verseNum">The verse number.</param>
+    /// <remarks>This should only be used when you want <see cref="Verse"/> to by <c>null</c>.</remarks>
     public VerseRefData(int bookNum, int chapterNum, int verseNum)
+        : this()
     {
         BookNum = bookNum;
         ChapterNum = chapterNum;
         VerseNum = verseNum;
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="VerseRefData"/> class.
+    /// </summary>
+    /// <param name="bookNum">The book number.</param>
+    /// <param name="chapterNum">The chapter number.</param>
+    /// <param name="verse">The verse.</param>
     public VerseRefData(int bookNum, int chapterNum, string verse)
+        : this()
     {
         BookNum = bookNum;
         ChapterNum = chapterNum;
         Verse = verse;
-        ParseVerseNumber(verse, out int verseNum, out _);
-        VerseNum = verseNum;
+        VerseNum = ParseVerseNumber(verse);
     }
 
+    /// <summary>
+    /// Gets or sets the book number.
+    /// </summary>
     public int BookNum { get; set; }
+
+    /// <summary>
+    /// Gets or sets the chapter number.
+    /// </summary>
     public int ChapterNum { get; set; }
+
+    /// <summary>
+    /// Gets or sets the verse number.
+    /// </summary>
     public int VerseNum { get; set; }
-    public string Verse { get; set; }
+
+    /// <summary>
+    /// Gets or sets the verse.
+    /// </summary>
+    /// <remarks>If <c>null</c>, the <see cref="VerseNum"/> should be used instead.</remarks>
+    public string? Verse { get; set; }
 
     public VerseRef ToVerseRef()
     {
@@ -52,4 +67,13 @@ public class VerseRefData
     }
 
     public override string ToString() => ToVerseRef().ToString();
+
+    private static int ParseVerseNumber(string verse)
+    {
+        int i;
+        for (i = 0; i < verse.Length && char.IsDigit(verse[i]); ++i) { }
+        if (i > 0 && int.TryParse(verse[..i], out int number))
+            return number;
+        return 0;
+    }
 }
