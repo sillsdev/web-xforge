d35360f716248b91e6eb5077eecfbc8c34540df4
Peter Chapman <peter@conglomo.co.nz>
2026-05-12 14:41:31 +1200

    Display confidence data and allow export on the Serval Admin page

---
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/build-confidences/build-confidences.ts
@@ -0,0 +1,30 @@
+/** The build confidences DTO */
+export interface BuildConfidences {
+  projectId: string;
+  buildId: string;
+  bookConfidences: BookConfidence[];
+  chapterConfidences: ChapterConfidence[];
+}
+
+/** The confidence values */
+export interface Confidence {
+  confidence: number;
+  label: UsabilityLabel;
+  projectedChrF3: number;
+  usability: number;
+}
+
+export interface BookConfidence extends Confidence {
+  bookNum: number;
+}
+
+export interface ChapterConfidence extends BookConfidence {
+  chapterNum: number;
+}
+
+/** The usability label */
+export enum UsabilityLabel {
+  Red = 'Red',
+  Yellow = 'Yellow',
+  Green = 'Green'
+}
