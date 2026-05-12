0e55ec9b5e23dd4bee98c57eba1abb09c2f4e479
Peter Chapman <peter@conglomo.co.nz>
2026-05-12 14:41:31 +1200

    Display confidence data and allow export on the Serval Admin page

---
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/draft-generation/build-confidences/display-confidence.component.spec.ts
@@ -0,0 +1,57 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { configureTestingModule } from 'xforge-common/test-utils';
+import { Confidence, UsabilityLabel } from './build-confidences';
+import { DisplayConfidenceComponent } from './display-confidence.component';
+
+describe('DisplayConfidenceComponent', () => {
+  configureTestingModule(() => ({
+    imports: [DisplayConfidenceComponent]
+  }));
+
+  it('good quality', () => {
+    const env = new TestEnvironment({
+      label: UsabilityLabel.Green
+    });
+    expect(env.confidenceValueElement(UsabilityLabel.Green)).not.toBeNull();
+    expect(env.confidenceValueElement(UsabilityLabel.Yellow)).toBeNull();
+    expect(env.confidenceValueElement(UsabilityLabel.Red)).toBeNull();
+  });
+
+  it('moderate quality', () => {
+    const env = new TestEnvironment({
+      label: UsabilityLabel.Yellow
+    });
+    expect(env.confidenceValueElement(UsabilityLabel.Green)).toBeNull();
+    expect(env.confidenceValueElement(UsabilityLabel.Yellow)).not.toBeNull();
+    expect(env.confidenceValueElement(UsabilityLabel.Red)).toBeNull();
+  });
+
+  it('poor quality', () => {
+    const env = new TestEnvironment({
+      label: UsabilityLabel.Red
+    });
+    expect(env.confidenceValueElement(UsabilityLabel.Green)).toBeNull();
+    expect(env.confidenceValueElement(UsabilityLabel.Yellow)).toBeNull();
+    expect(env.confidenceValueElement(UsabilityLabel.Red)).not.toBeNull();
+  });
+
+  /** Provides helpers for constructing test data for DisplayConfidenceComponent tests. */
+  class TestEnvironment {
+    readonly component: DisplayConfidenceComponent;
+    readonly fixture: ComponentFixture<DisplayConfidenceComponent>;
+
+    constructor(confidence: Partial<Confidence>) {
+      this.fixture = TestBed.createComponent(DisplayConfidenceComponent);
+      this.component = this.fixture.componentInstance;
+      this.component.confidence = confidence as Confidence;
+      this.fixture.detectChanges();
+    }
+
+    confidenceValueElement(label: UsabilityLabel): DebugElement {
+      return this.fixture.debugElement.query(By.css(`.${label.toLowerCase()}`));
+    }
+  }
+});
