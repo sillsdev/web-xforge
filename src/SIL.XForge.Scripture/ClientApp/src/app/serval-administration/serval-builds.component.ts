3f0759d27f7dcdc1a5227301017d23f0defe4706
MarkS <marksvc@gmail.com>
2026-05-15 16:25:23 -0600

    Name the types

--- src/SIL.XForge.Scripture/ClientApp/src/app/serval-administration/serval-builds.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/serval-administration/serval-builds.component.ts
@@ -59,6 +59,8 @@ import {
   BookAndChapters,
   buildProjectDisplayName,
   BuildReportProblem,
+  BuildReportProblemSeverity,
+  BuildReportProblemSource,
   DraftGenerationBuildStatus,
   Phase,
   ProjectBooks,
@@ -932,8 +934,8 @@ export class ServalBuildsComponent extends DataLoadingComponent implements OnIni
 
   protected problems(
     row: ServalBuildRow,
-    source: BuildReportProblem['source'],
-    severity: BuildReportProblem['severity']
+    source: BuildReportProblemSource,
+    severity: BuildReportProblemSeverity
   ): BuildReportProblem[] {
     return row.report.problems.filter((p: BuildReportProblem) => p.source === source && p.severity === severity);
   }
