12f2482f9079715a3bf3409eda99e3c35eb930bd
Peter Chapman <peter@conglomo.co.nz>
2026-06-04 03:36:54 +1200

    SF-3691 Load base translation versification where specified (#3904)

--- test/SIL.XForge.Scripture.Tests/Services/MockScrStylesheet.cs
+++ test/SIL.XForge.Scripture.Tests/Services/MockScrStylesheet.cs
@@ -1,9 +1,6 @@
-#nullable disable warnings
-namespace Paratext.Data;
+using Paratext.Data;
+
+namespace SIL.XForge.Scripture.Services;
 
 /// <summary>Mock for tests.</summary>
-public class MockScrStylesheet : ScrStylesheet
-{
-    public MockScrStylesheet(string path, string alternatePath = null)
-        : base(path, alternatePath) { }
-}
+public class MockScrStylesheet(string path, string? alternatePath = null) : ScrStylesheet(path, alternatePath);
