d2b59bd94d13ffaa97beb0c5383ec084f900e5cb
Peter Chapman <peter@conglomo.co.nz>
2026-07-07 13:01:53 +1200

    SF-3841 Fix book order for quality estimation results

--- src/SIL.XForge.Scripture/Services/MachineApiService.cs
+++ src/SIL.XForge.Scripture/Services/MachineApiService.cs
@@ -1199,8 +1199,8 @@ public class MachineApiService(
         {
             ProjectId = id[0],
             BuildId = id[1],
-            BookConfidences = draftMetrics.BookConfidences,
-            ChapterConfidences = draftMetrics.ChapterConfidences,
+            BookConfidences = [.. draftMetrics.BookConfidences.OrderBy(c => c.BookNum)],
+            ChapterConfidences = [.. draftMetrics.ChapterConfidences.OrderBy(c => c.BookNum)],
             LowestConfidence = draftMetrics.BookConfidences.OrderBy(b => b.Usability).FirstOrDefault(),
         };
     }
