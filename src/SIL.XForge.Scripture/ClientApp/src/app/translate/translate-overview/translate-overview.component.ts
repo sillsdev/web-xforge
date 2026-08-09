db9f69507d73b92b4945cfac8e7d680fa6ed1eb4
Peter Chapman <peter@conglomo.co.nz>
2026-08-10 11:13:14 +1200

    SF-3882 Remove SMT suggestions from the frontend

--- src/SIL.XForge.Scripture/ClientApp/src/app/translate/translate-overview/translate-overview.component.ts
+++ src/SIL.XForge.Scripture/ClientApp/src/app/translate/translate-overview/translate-overview.component.ts
@@ -1,7 +1,5 @@
-import { HttpErrorResponse } from '@angular/common/http';
-import { Component, DestroyRef, OnDestroy, OnInit } from '@angular/core';
-import { MatButton } from '@angular/material/button';
-import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
+import { Component, DestroyRef, OnInit } from '@angular/core';
+import { MatCard, MatCardHeader, MatCardTitle } from '@angular/material/card';
 import { MatDivider } from '@angular/material/divider';
 import { MatIcon } from '@angular/material/icon';
 import {
@@ -12,7 +10,6 @@ import {
   MatListItemMeta,
   MatListItemTitle
 } from '@angular/material/list';
-import { MatProgressBar } from '@angular/material/progress-bar';
 import { MatTooltip } from '@angular/material/tooltip';
 import { ActivatedRoute } from '@angular/router';
 import { TranslocoModule } from '@ngneat/transloco';
@@ -22,10 +19,8 @@ import { ANY_INDEX, obj } from 'realtime-server/lib/esm/common/utils/obj-path';
 import { SFProject } from 'realtime-server/lib/esm/scriptureforge/models/sf-project';
 import { SF_PROJECT_RIGHTS, SFProjectDomain } from 'realtime-server/lib/esm/scriptureforge/models/sf-project-rights';
 import { isParatextRole } from 'realtime-server/lib/esm/scriptureforge/models/sf-project-role';
-import { TextInfo } from 'realtime-server/lib/esm/scriptureforge/models/text-info';
-import { asyncScheduler, Subscription, timer } from 'rxjs';
-import { filter, map, repeat, retry, tap, throttleTime } from 'rxjs/operators';
-import { AuthService } from 'xforge-common/auth.service';
+import { asyncScheduler, Subscription } from 'rxjs';
+import { filter, map, throttleTime } from 'rxjs/operators';
 import { DataLoadingComponent } from 'xforge-common/data-loading-component';
 import { DonutChartComponent } from 'xforge-common/donut-chart/donut-chart.component';
 import { I18nService } from 'xforge-common/i18n.service';
@@ -38,14 +33,9 @@ import { UserService } from 'xforge-common/user.service';
 import { quietTakeUntilDestroyed } from 'xforge-common/util/rxjs-util';
 import { SFProjectProfileDoc } from '../../core/models/sf-project-profile-doc';
 import { SFProjectService } from '../../core/sf-project.service';
-import { TranslationEngineService } from '../../core/translation-engine.service';
-import { RemoteTranslationEngine } from '../../machine-api/remote-translation-engine';
 import { NoticeComponent } from '../../shared/notice/notice.component';
 import { BookProgress, ProgressService, ProjectProgress } from '../../shared/progress-service/progress.service';
 import { FontUnsupportedMessageComponent } from '../font-unsupported-message/font-unsupported-message.component';
-import { SmtRetirementNoticeComponent } from '../smt-retirement-notice/smt-retirement-notice.component';
-import { TrainingProgressComponent } from '../training-progress/training-progress.component';
-const ENGINE_QUALITY_STAR_COUNT = 3;
 const TEXT_PATH_TEMPLATE = obj<SFProject>().pathTemplate(p => p.texts[ANY_INDEX]);
 
 @Component({
@@ -69,51 +59,27 @@ const TEXT_PATH_TEMPLATE = obj<SFProject>().pathTemplate(p => p.texts[ANY_INDEX]
     MatListItemTitle,
     MatListItemLine,
     MatListItemMeta,
-    MatProgressBar,
-    MatCardContent,
-    MatCardActions,
-    MatButton,
-    TrainingProgressComponent,
-    SmtRetirementNoticeComponent,
     NoticeComponent,
     L10nNumberPipe,
     L10nPercentPipe
   ]
 })
-export class TranslateOverviewComponent extends DataLoadingComponent implements OnInit, OnDestroy {
-  trainingPercentage: number = 0;
-  isTraining: boolean = false;
-  readonly engineQualityStars: number[];
-  engineQuality: number = 0;
-  engineConfidence: number = 0;
-  trainedSegmentCount: number = 0;
+export class TranslateOverviewComponent extends DataLoadingComponent implements OnInit {
   projectProgress?: ProjectProgress;
-  private trainingSub?: Subscription;
-  private translationEngine?: RemoteTranslationEngine;
   private projectDoc?: SFProjectProfileDoc;
   private projectDataChangesSub?: Subscription;
 
   constructor(
     private readonly activatedRoute: ActivatedRoute,
-    private readonly authService: AuthService,
     private readonly onlineStatusService: OnlineStatusService,
     noticeService: NoticeService,
     private readonly projectService: SFProjectService,
-    private readonly translationEngineService: TranslationEngineService,
     private readonly userService: UserService,
     public readonly progressService: ProgressService,
     readonly i18n: I18nService,
     private destroyRef: DestroyRef
   ) {
     super(noticeService, 'TranslateOverviewComponent');
-    this.engineQualityStars = [];
-    for (let i = 0; i < ENGINE_QUALITY_STAR_COUNT; i++) {
-      this.engineQualityStars.push(i);
-    }
-  }
-
-  get translationSuggestionsEnabled(): boolean {
-    return this.projectDoc?.data?.translateConfig.translationSuggestionsEnabled ?? false;
   }
 
   get canEditTexts(): boolean {
@@ -128,24 +94,12 @@ export class TranslateOverviewComponent extends DataLoadingComponent implements 
     return this.onlineStatusService.isOnline;
   }
 
-  get showCannotTrainEngineMessage(): boolean {
-    if (this.projectDoc?.data == null || !this.isOnline) {
-      return false;
-    }
-    const hasSourceBooks: boolean = this.translationEngineService.checkHasSourceBooks(this.projectDoc.data);
-    return this.translationSuggestionsEnabled && !hasSourceBooks;
-  }
-
   get projectId(): string | undefined {
     return this.projectDoc?.id;
   }
 
   get isPTUser(): boolean {
     return isParatextRole(this.projectDoc?.data?.userRoles[this.userService.currentUserId]);
-  }
-
-  get hasMinimumSegmentsToTrainStatisticalEngine(): boolean {
-    return (this.projectProgress?.translatedVerseSegments ?? 0) >= 10;
   }
 
   ngOnInit(): void {
@@ -161,10 +115,6 @@ export class TranslateOverviewComponent extends DataLoadingComponent implements 
         void this.onlineStatusService.online.then(async () => {
           this.loadingStarted();
           try {
-            if (this.translationEngine == null) {
-              this.setupTranslationEngine();
-            }
-            await this.updateEngineStats();
             this.projectProgress = await this.progressService.getProgress(this.projectId!, { maxStalenessMs: 30_000 });
           } finally {
             this.loadingFinished();
@@ -176,11 +126,6 @@ export class TranslateOverviewComponent extends DataLoadingComponent implements 
         }
         this.projectDataChangesSub = this.projectDoc.remoteChanges$
           .pipe(
-            tap(() => {
-              if (this.translationEngine == null || !this.translationSuggestionsEnabled) {
-                this.setupTranslationEngine();
-              }
-            }),
             filter(ops => ops.some(op => TEXT_PATH_TEMPLATE.matches(op.p))),
             // TODO Find a better solution than merely throttling remote changes
             throttleTime(1000, asyncScheduler, { leading: true, trailing: true })
@@ -188,7 +133,6 @@ export class TranslateOverviewComponent extends DataLoadingComponent implements 
           .subscribe(async () => {
             this.loadingStarted();
             try {
-              await this.updateEngineStats();
               this.projectProgress = await this.progressService.getProgress(this.projectId!, {
                 maxStalenessMs: 30_000
               });
@@ -197,31 +141,6 @@ export class TranslateOverviewComponent extends DataLoadingComponent implements 
             }
           });
       });
-  }
-
-  ngOnDestroy(): void {
-    if (this.trainingSub != null) {
-      this.trainingSub.unsubscribe();
-    }
-  }
-
-  startTraining(): void {
-    if (this.translationEngine == null) {
-      return;
-    }
-    this.trainingPercentage = 0;
-    this.isTraining = true;
-    void this.translationEngine
-      .startTraining()
-      .catch((error: any) => {
-        this.isTraining = false;
-        if (error instanceof HttpErrorResponse && error.status === 401) {
-          void this.authService.requestParatextCredentialUpdate();
-        } else {
-          this.noticeService.showError(this.i18n.translateStatic('translate_overview.training_unavailable'));
-        }
-      })
-      .then(() => void this.listenForStatus());
   }
 
   getBookNameFromId(bookId: string): string {
@@ -238,74 +157,5 @@ export class TranslateOverviewComponent extends DataLoadingComponent implements 
       return 0;
     }
     return this.bookTranslatedSegments(bookProgress) / bookProgress.verseSegments;
-  }
-
-  getBookName(text: TextInfo): string {
-    return this.i18n.localizeBook(text.bookNum);
-  }
-
-  getBookId(text: TextInfo): string {
-    return Canon.bookNumberToId(text.bookNum);
-  }
-
-  private setupTranslationEngine(): void {
-    if (this.trainingSub != null) {
-      this.trainingSub.unsubscribe();
-      this.trainingSub = undefined;
-    }
-    this.translationEngine = undefined;
-    if (this.projectDoc?.data == null) {
-      return;
-    }
-    const hasSourceBooks: boolean = this.translationEngineService.checkHasSourceBooks(this.projectDoc.data);
-    if (!this.translationSuggestionsEnabled || !this.canEditTexts || !hasSourceBooks) {
-      return;
-    }
-
-    this.translationEngine = this.translationEngineService.createTranslationEngine(this.projectDoc.id);
-    this.listenForStatus();
-  }
-
-  private listenForStatus(): void {
-    if (this.translationEngine == null) {
-      return;
-    }
-    this.trainingSub?.unsubscribe();
-    this.trainingSub = this.translationEngine
-      .listenForTrainingStatus()
-      .pipe(
-        tap({
-          error: () => {
-            // error while listening
-            this.isTraining = false;
-          },
-          complete: () => {
-            // training completed successfully
-            this.isTraining = false;
-            void this.updateEngineStats();
-          }
-        }),
-        repeat(),
-        retry({ delay: () => timer(30000) })
-      )
-      .subscribe(progress => {
-        this.trainingPercentage = Math.round(progress.percentCompleted * 100);
-        this.isTraining = true;
-      });
-  }
-
-  private async updateEngineStats(): Promise<void> {
-    if (this.translationEngine == null) {
-      return;
-    }
-    const stats = await this.translationEngine.getStats();
-
-    this.engineConfidence = Math.round(stats.confidence * 100) / 100;
-
-    const rescaledConfidence = Math.min(1.0, stats.confidence / 0.6);
-    const quality = rescaledConfidence * ENGINE_QUALITY_STAR_COUNT;
-    this.engineQuality = Math.round(quality * 2) / 2;
-
-    this.trainedSegmentCount = stats.trainedSegmentCount;
   }
 }
